eb601567b67f416df204a6df9fc84dd33da9a424
Lê Nguyên Hoang <len.hoang.lnh@gmail.com>
2021-04-25 07:12:44 +0200

    Update non-certified email domain messages
    I added in particular a link to this wiki page: https://wiki.tournesol.app/index.php/51%25_attack
    Also, I've used space indentation and made each line shorter to pass CI tests.

--- frontend/src/components/EmailAddVerifyAlert.js
+++ frontend/src/components/EmailAddVerifyAlert.js
@@ -33,19 +33,35 @@ export default () => {
     <>
       {!isCertified && domainRejected && (
         <Alert severity="error" className={`${classes.alertTop} class_rejected`}>
-          Note that the email domain of your verified email address is currently not trusted by Tournesol. 
-          We fear that this email domain could be used by malicious entities to create fake accounts, 
-          <a href="https://www.youtube.com/watch?v=w5JRKUndWNk&list=PL8ovs-QtxcNxcwlsTF5O9NXtr3NAj_SVc" target="_blank">which have become predominant on Facebook</a>. 
-          As a result, your ratings will not impact the certified Tournesol scores, and your comments will not be visible. 
-          Note that you can gain capabilities by registering another email address from a trusted email domain
+          The email domain of your verified email address 
+          is currently not trusted by Tournesol. 
+          We fear that this email domain could be used by malicious 
+          entities to create numerous fake accounts
+          and gain control of Tournesol through a 
+          <a href="https://wiki.tournesol.app/index.php/51%25_attack" target="_blank">
+            51% attack</a>. 
+          As a result, your ratings will not impact the certified Tournesol scores, 
+          and your comments will not be visible. 
+          You can gain the capabilities of certified Tournesol accounts 
+          by registering another email address from a 
+          <a href="https://wiki.tournesol.app/index.php/Trusted_email_domains" target="_blank">
+            trusted email domain</a>
 
           <EmailAddVerify />
         </Alert>)}
       {!isCertified && !domainRejected && (
         <Alert severity="info" className={`${classes.alertTop} class_pending`}>
           We are currently assessing the trustworthiness of the email domain associated to your
-          email address. Note that, as a result, your ratings may not impact the Tournesol
-          scores, and your comments will not be visible.
+          email address.
+          This manual operation may take a few days.
+          This certification is critical for the safety of Tournesol, 
+          as it protects the collaborative platform against 
+          <a href="https://wiki.tournesol.app/index.php/51%25_attack" target="_blank">
+            51% attacks</a>.  
+          You can gain the capabilities of certified Tournesol accounts 
+          by registering another email address from a 
+          <a href="https://wiki.tournesol.app/index.php/Trusted_email_domains" target="_blank">
+            trusted email domain</a>
 
           <EmailAddVerify />
         </Alert>)}
