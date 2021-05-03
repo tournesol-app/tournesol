e83631b5534cf6a91ce77cabd8790d260ce1f5ee
Sergei Volodin <sergei.volodin.ch@gmail.com>
2021-05-03 16:20:49 +0000

    Add path to firefox

--- tests.sh
+++ tests.sh
@@ -76,7 +76,7 @@ then
 	if [ "X$(which x11vnc)" != "X" ]
 	then
 		echo "Launching x11vnc"
-		screen -Sdm "xvfb_vnc" x11vnc -display $DISPLAY -loop
+		screen -Sdm "xvfb_vnc" x11vnc -display $DISPLAY -loop -shared -forever
 	fi
 fi
 
