cad5e55db3ef06146ab75c4cab68207aae6764c2
Louis Faucon <lpfaucon@gmail.com>
2021-05-24 19:50:56 +0000

    Add pagination in list of email domains

--- frontend/src/components/EmailDomains.js
+++ frontend/src/components/EmailDomains.js
@@ -1,32 +1,52 @@
 import React from 'react';
 
+import Typography from '@material-ui/core/Typography';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
 import { TournesolAPI } from '../api';
 
 export default () => {
+  const [page, setPage] = React.useState(0);
+  const [totalPages, setTotalPages] = React.useState(0);
   const [requested, setRequested] = React.useState(false);
   const [acceptedDomains, setAcceptedDomains] = React.useState([]);
 
   if (!requested) {
     setRequested(true);
     const api = new TournesolAPI.EmailDomainApi();
-    api.emailDomainList({ limit: 100, status: 'ACK' }, (err, data) => {
+    const limit = 50;
+    api.emailDomainList({ limit, status: 'ACK', offset: page * limit }, (err, data) => {
+      if (!err) setTotalPages(Math.ceil(data.count / limit));
       if (!err) setAcceptedDomains(data.results);
     });
   }
 
+  const changePage = (e) => {
+    setAcceptedDomains([]);
+    setPage(e.target.value);
+    setRequested(false);
+  };
+
   return (
     <div id="domains">
-      <h1>Tournesol domains certification</h1>
-
-      <p>Accepted domains</p>
+      <Typography variant="h3">Tournesol domains certification</Typography>
+      <Typography paragraph>Accepted domains</Typography>
       <ul className="accepted_domains_class">
         {acceptedDomains && acceptedDomains.map((domain) => (
           <li>
-            <a className="domain_link_class" href={`http://${domain.domain.substring(1)}`}>
-              {domain.domain}
-            </a>
+            <Typography paragraph>
+              <a className="domain_link_class" href={`http://${domain.domain.substring(1)}`}>
+                {domain.domain}
+              </a>: {domain.n_verified_emails} contributor{domain.n_verified_emails > 1 ? 's' : ''}
+            </Typography>
           </li>))}
       </ul>
+      <Select value={page} onChange={changePage}>
+        {Array.from({ length: totalPages }, (_, i) => (
+          <MenuItem key={i} value={i}>Page {i + 1}/{totalPages}</MenuItem>
+        ))}
+      </Select>
     </div>
   );
 };
