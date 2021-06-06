42755a007d368fa52a1a8ab37ab925b6b0768846
jst <jst@kleis.ch>
2021-06-06 02:17:05 +0200

    quick refactor

--- offline-webpage/email-receiver.go
+++ offline-webpage/email-receiver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -11,119 +12,176 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type config struct {
+	dbFile                   string
+	address                  string
+	port                     int
+	pageFile                 string
+	emailFormKey             string
+	throttlingDelay          int64
+	throttlingMaxSubmissions int
+}
+
+type server struct {
+	*config
+	*sql.DB
+}
+
+func (s *server) openDB() error {
+	db, err := sql.Open("sqlite3", s.config.dbFile)
+	if err != nil {
+		return err
+	}
+	s.DB = db
+	return nil
+}
+
 func main() {
+	config := &config{
+		dbFile:                   "./emails-to-notify.sqlite",
+		address:                  "127.0.0.1",
+		port:                     8080,
+		pageFile:                 "./index.html",
+		emailFormKey:             "email-to-notify",
+		throttlingDelay:          30,
+		throttlingMaxSubmissions: 2,
+	}
 
-	db, err := sql.Open("sqlite3", "./emails-to-notify.sqlite")
-	if err != nil {
+	s := &server{
+		config: config,
+	}
+	if err := s.openDB(); err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	defer s.DB.Close()
 
-	// sqlStmt := `
-	// create table emails (id integer primary key, email text not null unique, ip text not null, date integer not null);
-	// `
-	// _, err = db.Exec(sqlStmt)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	http.HandleFunc("/", s.handle)
 
-	http.HandleFunc("/", page(db))
-
-	err = http.ListenAndServe(":8080", nil)
-	if err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf("%v:%v", config.address, config.port), nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func errorCodeHandler(code int) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, http.StatusText(code), code)
-	}
-}
+func (s *server) handle(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
 
-func page(db *sql.DB) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
+	case http.MethodGet:
+		http.ServeFile(w, r, s.pageFile)
 
-		case http.MethodGet:
-			http.ServeFile(w, r, "./index.html")
+	case http.MethodPost:
 
-		case http.MethodPost:
-
-			email := r.FormValue("email-to-notify")
-			if email == "" {
-				log.Printf("error: email not found in request\n")
-				errorCodeHandler(http.StatusBadRequest)(w, r)
-				return
-			}
-
-			socket := strings.Split(r.RemoteAddr, ":")
-			if len(socket) != 2 {
-				log.Printf("error while parsing client IP %v\n", socket)
-				errorCodeHandler(http.StatusInternalServerError)(w, r)
-				return
-			}
-			ip := socket[0]
-			now := time.Now().Unix()
-			delay := int64(30)
-			max := 2
-
-			stmt, err := db.Prepare("select count(*) from emails where ip = ? and date >= ?")
-			if err != nil {
-				log.Printf("error: %v while preparing query %v\n", err, stmt)
-				errorCodeHandler(http.StatusInternalServerError)(w, r)
-				return
-			}
-			defer stmt.Close()
-			rows, err := stmt.Query(ip, now-delay)
-			if err != nil {
-				log.Printf("error: %v while executing query %v\n", err, stmt)
-				errorCodeHandler(http.StatusInternalServerError)(w, r)
-				return
-			}
-			if !rows.Next() {
-				log.Printf("error while retrieving the count of email addresses inserted from IP %v\n", ip)
-				errorCodeHandler(http.StatusInternalServerError)(w, r)
-				return
-			}
-			n := 0
-			err = rows.Scan(&n)
-			if err != nil {
-				log.Printf("error: %v parsing the count of email addresses inserted from IP %v\n", err, ip)
-				errorCodeHandler(http.StatusInternalServerError)(w, r)
-				return
-			}
-			if rows.Close() != nil {
-				log.Printf("error closing rows after parsing the count of email addresses inserted from IP %v\n", ip)
-				errorCodeHandler(http.StatusInternalServerError)(w, r)
-				return
-			}
-			log.Printf("%v emails already stored for IP %v during the last %v seconds\n", n, ip, delay)
-
-			if n >= max {
-				log.Printf("too many email addresses submitted by IP %v during the last %v seconds\n", ip, delay)
-				errorCodeHandler(http.StatusForbidden)(w, r)
-				return
-			}
-
-			stmt, err = db.Prepare("insert into emails(email, ip, date) values(?, ?, ?)")
-			if err != nil {
-				log.Printf("error: %v while preparing query %v\n", err, stmt)
-				errorCodeHandler(http.StatusInternalServerError)(w, r)
-				return
-			}
-			defer stmt.Close()
-			_, err = stmt.Exec(email, ip, now)
-			if err != nil {
-				log.Printf("error: %v while executing query %v\n", err, stmt)
-				errorCodeHandler(http.StatusInternalServerError)(w, r)
-				return
-			}
-
-			log.Printf("record inserted: (email: %v, ip: %v, date: %v)\n", email, ip, now)
-			http.ServeFile(w, r, "./index.html")
-
-		default:
+		email := r.FormValue(s.emailFormKey)
+		if email == "" {
+			log.Printf("error: email not found in request\n")
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
 		}
+
+		socket := strings.Split(r.RemoteAddr, ":")
+		if len(socket) != 2 {
+			log.Printf("error while parsing client IP %v\n", socket)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		ip := socket[0]
+
+		now := time.Now().Unix()
+
+		query := "select count(*) from emails where ip = ? and date >= ?"
+		stmt, err := s.Prepare(query)
+		if err != nil {
+			log.Printf("error: %v while preparing query %v\n", err, query)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		defer stmt.Close()
+		rows, err := stmt.Query(ip, now-s.throttlingDelay)
+		if err != nil {
+			log.Printf("error: %v while executing query %v\n", err, stmt)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		if !rows.Next() {
+			log.Printf("error while retrieving the count of email addresses inserted from IP %v during the last %v seconds\n", ip, s.throttlingDelay)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		n := 0
+		if err := rows.Scan(&n); err != nil {
+			log.Printf("error: %v parsing the count of email addresses inserted from IP %v during the last %v seconds\n", err, ip, s.throttlingDelay)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		if rows.Close() != nil {
+			log.Printf("error closing rows after parsing the count of email addresses inserted from IP %v during the last %v seconds\n", ip, s.throttlingDelay)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		log.Printf("%v emails already stored for IP %v during the last %v seconds\n", n, ip, s.throttlingDelay)
+
+		if n >= s.throttlingMaxSubmissions {
+			log.Printf("too many email addresses submitted by IP %v during the last %v seconds\n", ip, s.throttlingDelay)
+			http.Error(w, "too many email addresses submitted, try again later", http.StatusForbidden)
+			return
+		}
+
+		query = "select count(*) from emails where email = ?"
+		stmt, err = s.Prepare(query)
+		if err != nil {
+			log.Printf("error: %v while preparing query %v\n", err, query)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		defer stmt.Close()
+		rows, err = stmt.Query(email)
+		if err != nil {
+			log.Printf("error: %v while executing query %v\n", err, stmt)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		if !rows.Next() {
+			log.Printf("error while checking if email address %v is already stored\n", email)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		n = 0
+		if err := rows.Scan(&n); err != nil {
+			log.Printf("error: %v parsing the count of email address %v appearances\n", err, email)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		if rows.Close() != nil {
+			log.Printf("error closing rows after parsing the count of email address %v appearances\n", email)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		if n > 0 {
+			log.Printf("email address %v already stored\n", email)
+			http.Error(w, "email address already registered", http.StatusBadRequest)
+			return
+		}
+
+		query = "insert into emails(email, ip, date) values(?, ?, ?)"
+		stmt, err = s.Prepare(query)
+		if err != nil {
+			log.Printf("error: %v while preparing query %v\n", err, query)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		defer stmt.Close()
+		_, err = stmt.Exec(email, ip, now)
+		if err != nil {
+			log.Printf("error: %v while executing query %v\n", err, stmt)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		log.Printf("record inserted: (email: %v, ip: %v, date: %v)\n", email, ip, now)
+		http.ServeFile(w, r, s.pageFile)
+
+	default:
+		log.Printf("error: unhandled HTTP method")
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
 	}
 }
--- offline-webpage/go.mod
+++ offline-webpage/go.mod
@@ -2,4 +2,4 @@ module github.com/tournesol-app/tournesol/offline-webpage
 
 go 1.16
 
-require github.com/mattn/go-sqlite3 v1.14.7 // indirect
+require github.com/mattn/go-sqlite3 v1.14.7
