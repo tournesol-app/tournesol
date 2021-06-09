5946f50854ab57a82bf0c06b548ba776a9657fe7
jst <jst@kleis.ch>
2021-06-09 03:46:06 +0200

    tournesol staging server added to inventory

---
+++ ansible/scripts/provisioning-staging.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -Eeuxo pipefail
+
+CURRENT_DIR="$(dirname "$0")"
+
+cd "$CURRENT_DIR/.."
+
+if [[ "${1:-""}" == "apply" ]]
+then
+  CHECK=""
+else
+  CHECK="--check"
+fi
+
+ansible-playbook -i inventory.yml -l tournesol-staging setup.yml \
+  $CHECK \
+  -e "django_database_password=$DJANGO_DATABASE_PASSWORD" \
+  -e "django_secret_key=$DJANGO_SECRET_KEY" \
+
