c81f4b6d6103d781e6880749e55ef45c48e13442
jst <jst@kleis.ch>
2021-06-21 03:43:23 +0200

    save grafana admin password in /root to retrieve it for later runs

--- ansible/scripts/get-staging-secrets.sh
+++ ansible/scripts/get-staging-secrets.sh
@@ -5,3 +5,6 @@ export DJANGO_SECRET_KEY
 
 DJANGO_DATABASE_PASSWORD="$(ssh tournesol.technocracy.dev -- sudo cat /etc/tournesol/settings.yaml | sed -n 's/^DATABASE_PASSWORD: \(.*\)$/\1/p')"
 export DJANGO_DATABASE_PASSWORD
+
+GRAFANA_ADMIN_PASSWORD="$(ssh tournesol.technocracy.dev -- sudo cat /root/grafana_admin_password)"
+export GRAFANA_ADMIN_PASSWORD
