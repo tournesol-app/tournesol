e8b50d6a01b6647284b74dd04eb6e98cb8e8587e
jst <jst@kleis.ch>
2021-07-24 20:23:59 +0200

    fix persistence waring

--- src/app/store.ts
+++ src/app/store.ts
@@ -20,6 +20,11 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: ["persist/PERSIST"],
+    },
+  }),
 });
 
 export const persistor = persistStore(store);
