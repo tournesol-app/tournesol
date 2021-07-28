2837bf64f5d620d2c5e6f9d469048380fd30eb4a
jst <jst@kleis.ch>
2021-07-28 20:55:20 +0200

    cleanup in Login component tests

--- src/features/login/Login.spec.tsx
+++ src/features/login/Login.spec.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import { MemoryRouter } from 'react-router-dom';
 import { Switch, Route } from 'react-router-dom';
 import configureStore, {
@@ -11,8 +12,6 @@ import thunk, { ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from '@reduxjs/toolkit';
 import fetchMock from 'fetch-mock-jest';
 import Cookies from 'js-cookie';
-import { v4 as uuid } from 'uuid';
-import { act } from 'react-dom/test-utils';
 import Login from './Login';
 import { LoginState } from './LoginState.model';
 import {
@@ -20,7 +19,6 @@ import {
   getTokenAsync,
   getLoginAsync,
   getUserInfoAsync,
-  selectLogin,
   getTokenFromRefreshAsync,
 } from './loginSlice';
 import { fetchAuthorization } from './loginAPI';
@@ -393,57 +391,4 @@ describe('login feature', () => {
       store.getActions()[1].meta.requestId
     );
   });
-  // it('should set CSRF token on login', async () => {
-  //   // we should use fetch-mocks and mock the whole server behavior during the auth flow
-  //   // test wouldn't need the API server to be up and would run faster
-  //   // however there are a few calls to mock properly, including CORS preflight and CSRF cookies
-
-  //   // const api_url = process.env.REACT_APP_API_URL;
-  //   // const fetchMocks = [
-  //   //   {
-  //   //     matcher: api_url + '/admin/login/',
-  //   //     method: 'GET',
-  //   //     response: () => {
-  //   //       const cookieString =
-  //   //         'csrftoken=dummy_csrf; Max-Age=3600; Path=/admin/login/;';
-  //   //       document.cookie = cookieString;
-  //   //       return {
-  //   //         status: 200,
-  //   //         config: {
-  //   //           headers: {
-  //   //             'Set-Cookie': cookieString,
-  //   //           },
-  //   //         },
-  //   //       };
-  //   //     },
-  //   //   },
-  //   //   {
-  //   //     matcher: api_url + '/admin/login/',
-  //   //     method: 'POST',
-  //   //     response: () => {
-  //   //       return {
-  //   //         status: 200,
-  //   //         config: {
-  //   //           redirectUrl: '/admin/',
-  //   //         },
-  //   //       };
-  //   //     },
-  //   //   },
-  //   // ];
-  //   // let rendered = component({ login: initialState, fetchMocks: fetchMocks });
-
-  //   let rendered = component({ login: initialState });
-
-  //   // hack to work around jest warnings about actions occurring outside of 'act'
-  //   // https://reactjs.org/link/wrap-tests-with-act
-  //   // assertions should be after the act block
-  //   await act(async () => {
-  //     fireEvent.click(rendered.getByRole('login-button'));
-
-  //     // should go out of the act block
-  //     await waitFor(() => rendered.getAllByText(/User/i));
-  //   });
-
-  //   //await waitFor(() => rendered.getAllByText(/User/i));
-  // });
 });
