9f98d89dedde71537965c7ef3484ed616d83dc71
jst <jst@kleis.ch>
2021-07-28 20:42:06 +0200

    change PrivateRoute tests to match jest canonical style

--- src/features/login/PrivateRoute.spec.tsx
+++ src/features/login/PrivateRoute.spec.tsx
@@ -35,29 +35,31 @@ const renderComponent = ({
     </Provider>
   );
 
-test('renders protected page when logged', async () => {
-  const anHourInMS = 1000 * 60 * 60;
-  const now = new Date();
-  const anHourLater = new Date(now.getTime() + anHourInMS);
-  let login = { ...initialState };
-  login.access_token = 'dummy_token';
-  login.access_token_expiration_date = anHourLater.toString();
+describe('private route wrapper', () => {
+  it('should render protected page when logged', async () => {
+    const anHourInMS = 1000 * 60 * 60;
+    const now = new Date();
+    const anHourLater = new Date(now.getTime() + anHourInMS);
+    let login = { ...initialState };
+    login.access_token = 'dummy_token';
+    login.access_token_expiration_date = anHourLater.toString();
 
-  const { getByText } = renderComponent({
-    login: login,
-    targetPath: '/protected',
-    loginPage: 'login_marker',
-    protectedPage: 'protected_marker',
+    const { getByText } = renderComponent({
+      login: login,
+      targetPath: '/protected',
+      loginPage: 'login_marker',
+      protectedPage: 'protected_marker',
+    });
+    await waitFor(() => getByText('protected_marker'));
   });
-  await waitFor(() => getByText('protected_marker'));
-});
 
-test('renders login page when not logged', async () => {
-  const { getByText } = renderComponent({
-    login: initialState,
-    targetPath: '/protected',
-    loginPage: 'login_marker',
-    protectedPage: 'protected_marker',
+  it('should render login page when not logged', async () => {
+    const { getByText } = renderComponent({
+      login: initialState,
+      targetPath: '/protected',
+      loginPage: 'login_marker',
+      protectedPage: 'protected_marker',
+    });
+    await waitFor(() => getByText('login_marker'));
   });
-  await waitFor(() => getByText('login_marker'));
 });
