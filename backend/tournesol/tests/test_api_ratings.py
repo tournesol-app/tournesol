99cd49957a18b221167da6b6d41b4a1efcd0ce92
Alexandre Muller <alexandre.muller@polyconseil.fr>
2021-09-16 09:46:09 +0200

    Add few tests

---
+++ backend/tournesol/tests/test_api_ratings.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import User
+
+class RatingApi(TestCase):
+    """
+    TestCase of the Rating API.
+
+    """
+    _user = "user_with_one_video"
+
+    def setUp(self):
+        user1 = User.objects.create(username=self._user)
+
+
+    def test_try_to_create(self):
+        factory = APIClient()
+        user = User.objects.get(username=self._user)
+        factory.force_authenticate(user=user)
+        response = factory.post(
+            "/users/me/contributor_ratings/NeADlWSDFAQ",
+            args=[user.username],
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    def test_try_to_update(self):
+        factory = APIClient()
+        user = User.objects.get(username=self._user)
+        factory.force_authenticate(user=user)
+        response = factory.put(
+            "/users/me/contributor_ratings/NeADlWSDFAQ",
+            {'video_id':'NeADlWSDFAQ'},
+            args=[user.username],
+            format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
