1c7ab605f92c5286d443b37a34ee14edb83e31a5
Alexandre Muller <alexandre.muller@polyconseil.fr>
2021-09-24 16:52:45 +0200

    Add button to rate a video in recommendation page

--- frontend/src/features/videos/VideoList.tsx
+++ frontend/src/features/videos/VideoList.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
 
-import { Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { Typography, Button, makeStyles } from '@material-ui/core';
 import type { PaginatedVideoList, Video } from 'src/services/openapi';
 import VideoCard from '../videos/VideoCard';
 
+const useStyles = makeStyles(() => ({
+  card: {
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+}));
+
 function VideoList({ videos }: { videos: PaginatedVideoList }) {
+  const classes = useStyles();
+
   return (
     <div>
       {videos.results?.length ? (
         videos.results.map((video: Video) => (
-          <VideoCard video={video} key={video.video_id} />
+          <div className={classes.card} key={video.video_id}>
+            <VideoCard video={video} />
+            <Button size="small" variant="contained" color="primary">
+              <Link to={`/comparison/?videoA=${video.video_id}`}>
+                Compare this video
+              </Link>
+            </Button>
+          </div>
         ))
       ) : (
         <Typography variant="h5" component="h2">
