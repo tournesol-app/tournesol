2a0c6346a6b368333b3ca63449d46e9777116ee5
jst <jst@kleis.ch>
2021-10-10 14:33:13 +0200

    adapt docker-compose and related scripts to use specified container names

--- backend/dev-env/run-db-and-local-django.sh
+++ backend/dev-env/run-db-and-local-django.sh
@@ -36,7 +36,7 @@ echo 'Importing public dataset'
 tar xvf "$CURRENT_DIR"/../scripts/dataset-import/dump-for-migrations-core-0004-tournesol-0007.sql.tgz
 sudo mv dump.sql "$CURRENT_DIR"/data/db/
 sudo chown 999:999 "$CURRENT_DIR"/data/db/dump.sql
-docker exec --env PGPASSWORD=password dev-env_db_1 bash -c "psql -1 -q -d tournesol -U user < /var/lib/postgresql/data/dump.sql"
+docker exec --env PGPASSWORD=password tournesol-dev-db bash -c "psql -1 -q -d tournesol -U user < /var/lib/postgresql/data/dump.sql"
 sudo rm "$CURRENT_DIR"/data/db/dump.sql
 
 echo 'Creating Superuser:'
@@ -52,7 +52,7 @@ echo 'Creating OAuth Application:'
 OAUTH_CLIENT_ID="YlfkLzvVjmGw3gjJzdlFuMFWcR64fAk4WNg5ucGg"
 OAUTH_CLIENT_SECRET="iB9j9hM5ekFpKlZQ6uNGloFJIWLVnq8LoG7SNdCtHY5oM7w9KY0XjpaDuwwJ40BshH7jKYZmXniaybhrQf5p4irAOMWv82RdYRMD6TTSJciZEAxn9onpKQoUgUeDqsRj"
 now="$(date +%Y-%m-%d)"
-docker exec --env PGPASSWORD=password dev-env_db_1 bash -c "psql -d tournesol -U user <<< \"insert into oauth2_provider_application (client_id, redirect_uris, client_type, authorization_grant_type, client_secret, name, skip_authorization, algorithm, created, updated) values ('$OAUTH_CLIENT_ID', 'http://localhost:8000/admin/', 'confidential', 'password', '$OAUTH_CLIENT_SECRET','Frontend', true, 'RS256', '$now', '$now');\""
+docker exec --env PGPASSWORD=password tournesol-dev-db bash -c "psql -d tournesol -U user <<< \"insert into oauth2_provider_application (client_id, redirect_uris, client_type, authorization_grant_type, client_secret, name, skip_authorization, algorithm, created, updated) values ('$OAUTH_CLIENT_ID', 'http://localhost:8000/admin/', 'confidential', 'password', '$OAUTH_CLIENT_SECRET','Frontend', true, 'RS256', '$now', '$now');\""
 
 echo 'Creating Swagger UI OAuth Application:'
 # OAUTH_CLIENT_ID="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 40 | head -n 1)" || true
@@ -60,7 +60,7 @@ echo 'Creating Swagger UI OAuth Application:'
 OAUTH_CLIENT_ID="vY17xBi0MZKZCotrfma5ympAd0hq30OudU78HZAY"
 OAUTH_CLIENT_SECRET="ZJ5FZeHomIgq6uNpVgNKwJiXDfFZz1HijDhsQJlXXnFKF6R7bUqc49Dv5MNL3cYTUrE1axrTtJTSr6IkHCc417ye8bLR8facpmhD4TwQqg7ktIQ047Y2Xp0rRcKLlIvq"
 now="$(date +%Y-%m-%d)"
-docker exec --env PGPASSWORD=password dev-env_db_1 bash -c "psql -d tournesol -U user <<< \"insert into oauth2_provider_application (client_id, redirect_uris, client_type, authorization_grant_type, client_secret, name, skip_authorization, algorithm, created, updated) values ('$OAUTH_CLIENT_ID', 'http://localhost:8000/docs/', 'confidential', 'password', '$OAUTH_CLIENT_SECRET','Swagger UI', true, 'RS256', '$now', '$now');\""
+docker exec --env PGPASSWORD=password tournesol-dev-db bash -c "psql -d tournesol -U user <<< \"insert into oauth2_provider_application (client_id, redirect_uris, client_type, authorization_grant_type, client_secret, name, skip_authorization, algorithm, created, updated) values ('$OAUTH_CLIENT_ID', 'http://localhost:8000/docs/', 'confidential', 'password', '$OAUTH_CLIENT_SECRET','Swagger UI', true, 'RS256', '$now', '$now');\""
 
 echo 'Launching Django:'
 python manage.py runserver 127.0.0.1:8000
