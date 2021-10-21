71b2f2b469d8d53e8be5b0f4ad40ad6ec3349a29
Adrien M <a@matissart.net>
2021-10-21 17:57:36 +0200

    Adapt run-docker-compose script for dev env and e2e tests

--- dev-env/run-docker-compose.sh
+++ dev-env/run-docker-compose.sh
@@ -2,6 +2,21 @@
 
 set -Eeuxo pipefail
 
+function wait_for_backend() {
+  set +ex
+  for i in `seq 1 50`; do
+    curl localhost:8000
+    if [[ "$?" == "0" ]]; then
+      echo ""
+      return 0
+    fi
+    echo "Waiting for backend to be ready..."
+    sleep 1
+  done
+  echo "Backend is unreachable"
+  exit 1
+}
+
 CURRENT_DIR="$(realpath -e "$(dirname "$0")")"
 cd "$CURRENT_DIR"
 
@@ -16,20 +31,18 @@ DB_UID=$(id -u) \
 DB_GID=$(id -g) \
 docker-compose up --build --force-recreate -d
 
-echo 'Waiting a Bit for Backend to be Ready...'
-sleep 30
+wait_for_backend
 
 echo 'Importing public dataset'
 tar xvf "$CURRENT_DIR"/../backend/scripts/dataset-import/dump-for-migrations-core-0004-tournesol-0007.sql.tgz
 mv dump.sql "$CURRENT_DIR"/db-data/
-docker exec --env PGPASSWORD=password tournesol-dev-db bash -c "psql -1 -q -d tournesol -U user < /var/lib/postgresql/data/dump.sql"
+docker exec --env PGPASSWORD=password tournesol-dev-db bash -c "psql -1 -q -d tournesol -U tournesol < /var/lib/postgresql/data/dump.sql"
 rm "$CURRENT_DIR"/db-data/dump.sql
 
 echo 'Creating Superuser:'
-# docker exec -ti tournesol-dev-api python manage.py createsuperuser
 USERNAME="${1:-"$USER"}"
-PASSWORD="${2:-"yop"}"
-EMAIL="${3:-"$USER@kleis.ch"}"
+PASSWORD="${2:-"tournesol"}"
+EMAIL="${3:-""}"
 "$CURRENT_DIR/../backend/dev-env/create-superuser.exp" "$USERNAME" "$PASSWORD" "$EMAIL"
 
 echo 'Creating OAuth Application:'
@@ -38,7 +51,7 @@ echo 'Creating OAuth Application:'
 OAUTH_CLIENT_ID="YlfkLzvVjmGw3gjJzdlFuMFWcR64fAk4WNg5ucGg"
 OAUTH_CLIENT_SECRET="iB9j9hM5ekFpKlZQ6uNGloFJIWLVnq8LoG7SNdCtHY5oM7w9KY0XjpaDuwwJ40BshH7jKYZmXniaybhrQf5p4irAOMWv82RdYRMD6TTSJciZEAxn9onpKQoUgUeDqsRj"
 now="$(date +%Y-%m-%d)"
-docker exec --env PGPASSWORD=password tournesol-dev-db bash -c "psql -d tournesol -U user <<< \"insert into oauth2_provider_application (client_id, redirect_uris, client_type, authorization_grant_type, client_secret, name, skip_authorization, algorithm, created, updated) values ('$OAUTH_CLIENT_ID', 'http://localhost:8000/admin/', 'confidential', 'password', '$OAUTH_CLIENT_SECRET','Frontend', true, 'RS256', '$now', '$now');\""
+docker exec --env PGPASSWORD=password tournesol-dev-db bash -c "psql -d tournesol -U tournesol <<< \"insert into oauth2_provider_application (client_id, redirect_uris, client_type, authorization_grant_type, client_secret, name, skip_authorization, algorithm, created, updated) values ('$OAUTH_CLIENT_ID', 'http://localhost:8000/admin/', 'confidential', 'password', '$OAUTH_CLIENT_SECRET','Frontend', true, 'RS256', '$now', '$now');\""
 
 echo 'Creating Swagger UI OAuth Application:'
 # OAUTH_CLIENT_ID="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 40 | head -n 1)" || true
@@ -46,4 +59,4 @@ echo 'Creating Swagger UI OAuth Application:'
 OAUTH_CLIENT_ID="vY17xBi0MZKZCotrfma5ympAd0hq30OudU78HZAY"
 OAUTH_CLIENT_SECRET="ZJ5FZeHomIgq6uNpVgNKwJiXDfFZz1HijDhsQJlXXnFKF6R7bUqc49Dv5MNL3cYTUrE1axrTtJTSr6IkHCc417ye8bLR8facpmhD4TwQqg7ktIQ047Y2Xp0rRcKLlIvq"
 now="$(date +%Y-%m-%d)"
-docker exec --env PGPASSWORD=password tournesol-dev-db bash -c "psql -d tournesol -U user <<< \"insert into oauth2_provider_application (client_id, redirect_uris, client_type, authorization_grant_type, client_secret, name, skip_authorization, algorithm, created, updated) values ('$OAUTH_CLIENT_ID', 'http://localhost:8000/docs/', 'confidential', 'password', '$OAUTH_CLIENT_SECRET','Swagger UI', true, 'RS256', '$now', '$now');\""
+docker exec --env PGPASSWORD=password tournesol-dev-db bash -c "psql -d tournesol -U tournesol <<< \"insert into oauth2_provider_application (client_id, redirect_uris, client_type, authorization_grant_type, client_secret, name, skip_authorization, algorithm, created, updated) values ('$OAUTH_CLIENT_ID', 'http://localhost:8000/docs/', 'confidential', 'password', '$OAUTH_CLIENT_SECRET','Swagger UI', true, 'RS256', '$now', '$now');\""
