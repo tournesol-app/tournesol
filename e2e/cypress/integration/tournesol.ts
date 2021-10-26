1b2e5683c22ab591c0250acb07d9a71c89d1510d
Adrien Matissart <a@matissart.net>
2021-10-26 10:15:39 +0200

    e2e: Add test on rate later list

--- e2e/cypress/integration/tournesol.ts
+++ e2e/cypress/integration/tournesol.ts
@@ -1,6 +1,6 @@
 const baseUrl = Cypress.config('baseUrl');
 
-describe('Tournesol home page', () => {
+describe('Tournesol basic tests', () => {
   it('loads correctly', () => {
     cy.visit('/');
     cy.contains('Tournesol').should('be.visible');
@@ -13,7 +13,18 @@ describe('Tournesol home page', () => {
     cy.focused().type('user');
     cy.get('input[name="password"]').click().type('tournesol').type('{enter}');
     cy.url().should('equal', `${baseUrl}/`);
+    cy.contains('Log in').should('not.exist');
     cy.contains('Logout').click();
-    cy.contains('Log in').should('be.visible')
+    cy.contains('Log in').should('be.visible');
+  })
+
+  it('can add video to rate later list', () => {
+    cy.visit('/rate_later');
+    cy.url().should('include', '/login');
+    cy.focused().type('user');
+    cy.get('input[name="password"]').click().type('tournesol').type('{enter}');
+    cy.contains('0 video').should('be.visible');
+    cy.get('input[placeholder="Video id or URL"]').type('dQw4w9WgXcQ').type('{enter}');
+    cy.contains('1 video').should('be.visible');
   })
 })
