92bd711f02ac91f977901a9cfefa5bf393c02ae7
Adrien Matissart <a@matissart.net>
2021-10-28 15:40:08 +0200

    Update UserAdmin config to help maintenance and support on user accounts

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -2,7 +2,10 @@
 Defines Admin interface for Tournesol's core app
 """
 
+from typing import List, Tuple
 from django.contrib import admin
+from django.contrib.admin.utils import flatten_fieldsets
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from .models import (
     User,
     UserPreference,
@@ -25,8 +28,27 @@ class DegreeAdmin(admin.ModelAdmin):
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(DjangoUserAdmin):
+    ordering = ['-date_joined']
+
+    def get_fieldsets(self, request, obj) -> List[Tuple]:
+        fieldsets = super().get_fieldsets(request, obj=obj)
+        if not obj:
+            return fieldsets
+
+        # For existing user, show all fields from our custom User model that
+        # are not already present on the page, in a separate fieldsets.
+        existing_fields = flatten_fieldsets(fieldsets)
+        return fieldsets + (
+            ('Other fields', {
+                'classes': ('collapse',),
+                'fields': tuple(
+                    f.name
+                    for f in User._meta.local_fields
+                    if f.name != 'id' and f.name not in existing_fields
+                ),
+            }),
+        )
 
 
 @admin.register(Expertise)
