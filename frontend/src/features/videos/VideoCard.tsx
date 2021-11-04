aa6e94582d935e9489375f3741f86ce475654b3c
Adrien Matissart <a@matissart.net>
2021-11-04 16:45:26 +0100

    VideoCard: improve layout on mobile

--- frontend/src/features/videos/VideoCard.tsx
+++ frontend/src/features/videos/VideoCard.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactPlayer from 'react-player/youtube';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid, Box } from '@material-ui/core';
 
 import { mainCriteriaNamesObj } from 'src/utils/constants';
 import type { VideoSerializerWithCriteria } from 'src/services/openapi';
@@ -49,25 +49,12 @@ const useStyles = makeStyles(() => ({
     color: '#4A473E',
     overflow: 'auto',
   },
-  application_details: {
-    width: '100%',
-    display: 'flex',
-    flexWrap: 'wrap',
-    alignItems: 'center',
-    padding: 4,
-  },
   nb_tournesol: {
     fontFamily: 'Poppins',
     fontStyle: 'normal',
     fontWeight: 'bold',
     fontSize: '32px',
     lineHeight: '32px',
-    // display: 'flex',
-    // alignItems: 'center',
-    // color: '#6A6658',
-    // marginLeft: '8.92px',
-    // marginTop: '5px',
-    marginRight: '8px',
   },
   ratings: {
     marginRight: '4px',
@@ -75,48 +62,28 @@ const useStyles = makeStyles(() => ({
     fontStyle: 'italic',
     fontWeight: 'normal',
     fontSize: '15px',
-    lineHeight: '22px',
     color: '#A09B87',
-    marginTop: '12px',
   },
   contributors: {
     fontFamily: 'Poppins',
     fontStyle: 'italic',
     fontWeight: 500,
     fontSize: '15px',
-    lineHeight: '22px',
     textDecorationLine: 'underline',
     color: '#B38B00',
-    marginTop: '12px',
   },
   rated: {
     fontFamily: 'Poppins',
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: '15px',
-    lineHeight: '22px',
     color: '#847F6E',
-    marginLeft: 16,
-    display: 'flex',
-    alignItems: 'center',
-  },
-  logo: {
-    marginLeft: 8,
+    gap: '8px',
   },
   top: {
     display: 'flex',
     flexWrap: 'wrap',
     alignContent: 'space-between',
-  },
-  later: {
-    marginLeft: '159.75px',
-  },
-  moreInfo: {
-    marginLeft: '21.75px',
-  },
-  detailsImage: {
-    marginLeft: '116px',
-    paddingBottom: 0,
   },
 }));
 
@@ -155,9 +122,6 @@ function VideoCard({
       min_criteria = criteria.criteria;
     }
   });
-  // TODO: this is commented out because it will included in a future release
-  // const nb_ratings = 51;
-  // const nb_contributors = 18;
 
   return (
     <Grid container spacing={1} className={classes.main}>
@@ -174,12 +138,6 @@ function VideoCard({
           <Typography className={classes.title} variant="h5">
             {video.name}
           </Typography>
-          {/* <img className={classes.later} src={'/svg/later.svg'} alt="logo" />
-            <img
-              className={classes.moreInfo}
-              src={'/svg/more_info.svg'}
-              alt="logo"
-            /> */}
         </div>
         <div className={classes.youtube_complements}>
           {video.views && (
@@ -196,51 +154,53 @@ function VideoCard({
             <span className={classes.channel}>{video.uploader}</span>
           )}
         </div>
-        <div className={classes.application_details}>
+        <Box
+          display="flex"
+          flexWrap="wrap"
+          alignItems="center"
+          style={{ gap: '12px' }}
+        >
           {max_criteria.length > 0 && (
             <>
-              <div className={classes.logo}>
+              <Box display="flex" alignItems="center">
                 <img
                   className="tournesol"
                   src={'/svg/tournesol.svg'}
                   alt="logo"
                   title="Overall score"
                 />
-              </div>
-              <span className={classes.nb_tournesol}>
-                {total_score.toFixed(0)}
-              </span>
+                <span className={classes.nb_tournesol}>
+                  {total_score.toFixed(0)}
+                </span>
+              </Box>
+
               {!!video.rating_n_ratings && video.rating_n_ratings > 0 && (
-                <>
-                  <p className={classes.ratings}>
+                <Box>
+                  <span className={classes.ratings}>
                     {video.rating_n_ratings} Ratings by
-                  </p>
-                  <p className={classes.contributors}>
+                  </span>
+                  <span className={classes.contributors}>
                     {video.rating_n_contributors} contributors
-                  </p>
-                </>
+                  </span>
+                </Box>
               )}
-              <div className={classes.rated}>
+              <Box display="flex" alignItems="center" className={classes.rated}>
                 <span>Rated high:</span>
                 <img
-                  className={classes.logo}
                   src={`/svg/${max_criteria}.svg`}
                   alt={max_criteria}
                   title={mainCriteriaNamesObj[max_criteria]}
                 />
-              </div>
-              <div className={classes.rated}>
                 <span>Rated low:</span>
                 <img
-                  className={classes.logo}
                   src={`/svg/${min_criteria}.svg`}
                   alt={min_criteria}
                   title={mainCriteriaNamesObj[min_criteria]}
                 />
-              </div>
+              </Box>
             </>
           )}
-        </div>
+        </Box>
       </Grid>
       <Grid item xs={12} sm={1}>
         {actions.map((Action, index) => (
