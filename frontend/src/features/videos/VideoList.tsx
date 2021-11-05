88db333b6c2df7b986e1cafe303fdb631f068905
Louis Faucon <lpfaucon@gmail.com>
2021-11-06 00:30:53 +0100

    Refactor actions and add style to VideoCards

--- frontend/src/features/videos/VideoList.tsx
+++ frontend/src/features/videos/VideoList.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
-import { Typography, makeStyles, Grid, IconButton } from '@material-ui/core';
-import { Add as AddIcon } from '@material-ui/icons';
+import { Typography, makeStyles, Grid } from '@material-ui/core';
 import type {
   PaginatedVideoSerializerWithCriteriaList,
   VideoSerializerWithCriteria,
-  Video,
 } from 'src/services/openapi';
 import VideoCard from '../videos/VideoCard';
-import { CompareNowAction } from 'src/utils/action';
-import { UsersService } from 'src/services/openapi';
+import { CompareNowAction, AddToRateLaterList } from 'src/utils/action';
 import { useLoginState } from 'src/hooks';
 
 const useStyles = makeStyles(() => ({
@@ -26,26 +23,9 @@ function VideoList({
   const classes = useStyles();
   const { isLoggedIn } = useLoginState();
 
-  const AddToRateLaterList = ({ videoId }: { videoId: string }) => {
-    const video_id = videoId;
-    return (
-      <div>
-        {isLoggedIn && (
-          <IconButton
-            size="medium"
-            color="secondary"
-            onClick={async () => {
-              await UsersService.usersMeVideoRateLaterCreate({
-                video: { video_id } as Video,
-              });
-            }}
-          >
-            <AddIcon />
-          </IconButton>
-        )}
-      </div>
-    );
-  };
+  const actions = isLoggedIn
+    ? [CompareNowAction, AddToRateLaterList]
+    : [CompareNowAction];
 
   return (
     <div>
@@ -53,10 +33,7 @@ function VideoList({
         videos.results.map((video: VideoSerializerWithCriteria) => (
           <Grid container className={classes.card} key={video.video_id}>
             <Grid item xs={12}>
-              <VideoCard
-                video={video}
-                actions={[CompareNowAction, AddToRateLaterList]}
-              />
+              <VideoCard video={video} actions={actions} />
             </Grid>
           </Grid>
         ))
--- frontend/src/utils/action.tsx
+++ frontend/src/utils/action.tsx
@@ -1,16 +1,63 @@
 import React from 'react';
 
-import { IconButton } from '@material-ui/core';
-import { Compare as CompareIcon } from '@material-ui/icons';
+import { IconButton, Tooltip } from '@material-ui/core';
+import CompareIcon from '@material-ui/icons/Compare';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import { Video, UsersService } from 'src/services/openapi';
 
 export const CompareNowAction = ({ videoId }: { videoId: string }) => {
   return (
-    <IconButton
-      size="medium"
-      color="secondary"
-      href={`/comparison/?videoA=${videoId}`}
-    >
-      <CompareIcon />
-    </IconButton>
+    <Tooltip title="Compare now" placement="left">
+      <IconButton
+        size="medium"
+        href={`/comparison/?videoA=${videoId}`}
+        style={{ color: '#CDCABC' }}
+      >
+        <CompareIcon />
+      </IconButton>
+    </Tooltip>
   );
+};
+
+export const AddToRateLaterList = ({ videoId }: { videoId: string }) => {
+  const video_id = videoId;
+  return (
+    <Tooltip title="Rate later" placement="left">
+      <IconButton
+        size="medium"
+        color="secondary"
+        onClick={() =>
+          UsersService.usersMeVideoRateLaterCreate({
+            video: { video_id } as Video,
+          })
+        }
+        style={{ color: '#CDCABC' }}
+      >
+        <AddIcon />
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+export const RemoveFromRateLater = (asyncCallback?: () => void) => {
+  const RemoveFromRateLaterComponnent = ({ videoId }: { videoId: string }) => {
+    const video_id = videoId;
+    return (
+      <Tooltip title="Remove" placement="left">
+        <IconButton
+          size="medium"
+          onClick={async () => {
+            await UsersService.usersMeVideoRateLaterDestroy(video_id);
+            if (asyncCallback) await asyncCallback();
+          }}
+          style={{ color: '#CDCABC' }}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
+    );
+  };
+  return RemoveFromRateLaterComponnent;
 };
