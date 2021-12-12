ad094c6d6016d3e15e9e5a0e2ec385477fd1217f
Louis Faucon <lpfaucon@gmail.com>
2021-12-12 21:23:03 +0100

    Simplify duration display and hide hours when 0

--- frontend/src/utils/video.ts
+++ frontend/src/utils/video.ts
@@ -143,21 +143,11 @@ export async function getVideoForComparison(
 }
 
 export const convertDurationToClockDuration = (duration: number) => {
-  let stringDuration = '';
   const roundToTwoDigits = (number: number) => {
-    if (number < 10) {
-      return `0${number}`;
-    } else {
-      return `${number}`;
-    }
+    return number < 10 ? `0${number}` : `${number}`;
   };
-  const list_duration = [];
-  list_duration.push(Math.floor(duration / 3600));
-  duration = duration % 3600;
-  list_duration.push(Math.floor(duration / 60));
-  list_duration.push(duration % 60);
-  list_duration.forEach((number) => {
-    stringDuration = stringDuration.concat(`${roundToTwoDigits(number)}:`);
-  });
-  return stringDuration.slice(0, -1);
+  const hours = Math.floor(duration / 3600);
+  const minutes = roundToTwoDigits(Math.floor((duration % 3600) / 60));
+  const seconds = roundToTwoDigits(duration % 60);
+  return hours > 0 ? `${hours}:${minutes}:${seconds}` : `${minutes}:${seconds}`;
 };
