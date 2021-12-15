813e129828ccd379d994856443932b3df99bb42d
Adrien Matissart <amatissart@users.noreply.github.com>
2021-12-15 16:02:05 +0100

    dev-env: Initialize database with test data including sample accounts, videos and scores (#365)
    * dev-env: Import dataset with videos, comparisons, and computed scores. Remove obsolete dumps.
    
    * dev-env: Add test data with videos, comparisons, scores, and sample accounts
    
    * dev-env: wait longer for services
    
    * dev-env: Disable eslint plugin in CI for faster startup in e2e workflow

--- dev-env/run-docker-compose.sh
+++ dev-env/run-docker-compose.sh
@@ -7,29 +7,45 @@ DB_DIR="db-data"
 CURRENT_DIR="$(realpath -e "$(dirname "$0")")"
 cd "$CURRENT_DIR"
 
+function is_db_ready() {
+  [ "$(docker inspect tournesol-dev-db --format '{{.State.Health.Status}}')" == "healthy" ]
+}
+
+function is_api_ready() {
+  curl -s localhost:8000 --max-time 1 -o /dev/null
+}
+
+function is_front_ready() {
+  curl -s localhost:3000 --max-time 1 -o /dev/null
+}
+
 function compose_up() {
   DB_UID=$(id -u) \
   DB_GID=$(id -g) \
-  docker-compose up --build --force-recreate -d
+  docker-compose up --build --force-recreate -d "$@"
 }
 
-function wait_for_backend() {
+function wait_for() {
+  local command=$1;
+  local service_name=${2:-"service"}
   set +e
-  for _ in $(seq 1 50); do
-    if curl -s localhost:8000 -o /dev/null; then
+  for _ in $(seq 1 60); do
+    if "$command"; then
       echo ""
       return 0
     fi
-    echo "Waiting for backend to be ready..."
-    sleep 1
+    echo "Waiting for $service_name to be ready..."
+    sleep 2
   done
-  echo "Backend is unreachable."
+  echo "$service_name is unreachable."
   exit 1
 }
 
 if [[ "${1:-""}" == 'restart' ]]; then
   echo "Recreating dev containers..."
   compose_up
+  wait_for is_front_ready "front"
+  echo "You can now access Tournesol on http://localhost:3000"
   exit
 fi
 
@@ -47,15 +63,17 @@ fi
 rm -rf $DB_DIR
 mkdir -p $DB_DIR
 
-compose_up
+compose_up db
+wait_for is_db_ready "db"
 
-wait_for_backend
-
-echo 'Importing public dataset'
-tar xvf "$CURRENT_DIR"/../backend/scripts/dataset-import/dump-for-migrations-core-0004-tournesol-0007.sql.tgz
+echo 'Importing dev-env dump'
+tar xvf "$CURRENT_DIR"/dump-for-dev-env.sql.tgz
 mv dump.sql "$CURRENT_DIR"/$DB_DIR/
 docker exec --env PGPASSWORD=password tournesol-dev-db bash -c "psql -1 -q -d tournesol -U tournesol < /var/lib/postgresql/data/dump.sql"
 rm "$CURRENT_DIR"/$DB_DIR/dump.sql
+
+compose_up
+wait_for is_api_ready "api"
 
 echo 'Creating Superuser:'
 USERNAME="${1:-"user"}"
@@ -78,3 +96,8 @@ OAUTH_CLIENT_ID="vY17xBi0MZKZCotrfma5ympAd0hq30OudU78HZAY"
 OAUTH_CLIENT_SECRET="ZJ5FZeHomIgq6uNpVgNKwJiXDfFZz1HijDhsQJlXXnFKF6R7bUqc49Dv5MNL3cYTUrE1axrTtJTSr6IkHCc417ye8bLR8facpmhD4TwQqg7ktIQ047Y2Xp0rRcKLlIvq"
 now="$(date +%Y-%m-%d)"
 docker exec --env PGPASSWORD=password tournesol-dev-db bash -c "psql -d tournesol -U tournesol <<< \"insert into oauth2_provider_application (client_id, redirect_uris, client_type, authorization_grant_type, client_secret, name, skip_authorization, algorithm, created, updated) values ('$OAUTH_CLIENT_ID', 'http://localhost:8000/docs/', 'confidential', 'password', '$OAUTH_CLIENT_SECRET','Swagger UI', true, 'RS256', '$now', '$now');\""
+
+wait_for is_front_ready "front"
+
+echo "The dev env has been created successfully!"
+echo "You can now access Tournesol on http://localhost:3000"
