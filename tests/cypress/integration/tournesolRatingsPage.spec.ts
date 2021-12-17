40788c62f65aae238dcfbf3bde85eb04a9d95604
Adrien Matissart <a@matissart.net>
2021-12-17 15:47:14 +0100

    Add missing ContributorRatings in dev-env database, adapt e2e tests

--- tests/cypress/integration/tournesolRatingsPage.spec.ts
+++ tests/cypress/integration/tournesolRatingsPage.spec.ts
@@ -1,11 +1,19 @@
 describe('"My rated videos" page', () => {
-    it('can change username', () => {
+    it('can list and update ratings status', () => {
       cy.visit('/ratings');
       cy.focused().type("user1");
       cy.get('input[name="password"]').click().type("tournesol").type('{enter}');
 
       // All rated videos are listed.
-      cy.contains('Showing videos 1 to 20 of 62').should('be.visible');
+      cy.contains('Showing videos 1 to 20 of 263').should('be.visible');
+
+      // Mark all videos as public.
+      cy.contains('button', 'Options').click();
+      cy.contains('button', 'Mark all as public').click();
+
+      // Close options
+      cy.contains('button', 'Options').click();
+      cy.contains('Filter by visibility').should('not.exist');
 
       // Toggle public status of first video
       cy.contains('label', 'Public').click();
@@ -17,13 +25,16 @@ describe('"My rated videos" page', () => {
 
       // Mark all videos as public, the filter is reset and all videos are listed
       cy.contains('button', 'Mark all as public').click();
-      cy.contains('Showing videos 1 to 20 of 62').should('be.visible');
+      cy.contains('Showing videos 1 to 20 of 263').should('be.visible');
     })
 
     it('visit ratings page with filter in URL', () => {
       cy.visit('/ratings?isPublic=false');
       cy.focused().type("user1");
       cy.get('input[name="password"]').click().type("tournesol").type('{enter}');
-      cy.contains('No video in this list').should('be.visible');
+      cy.contains('button', 'Options').click();
+      cy.contains('label', 'Private').within(
+        () => cy.get('input[type=checkbox]').should('be.checked')
+      );
     })
   });
