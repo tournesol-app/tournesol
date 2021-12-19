5249b4c11b9cbe23b2f249f17de3c18691f88989
JST <jstainer@kleis.ch>
2021-12-19 15:25:25 +0100

    Kleis partnership description changed

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -316,10 +316,11 @@ const AboutPage = () => {
               </Typography>
               <Typography paragraph>
                 We received support from the technology and consulting company
-                Kleis who helped us shape our organizational processes, develop
-                the vision of our product, make foundational technical choices,
-                and adopt efficient developments practices. Our partnership with
-                Kleis has been extremely impactful.
+                Kleis to help us shape our organizational processes and adopt
+                efficient developments practices. Our partnership with Kleis is
+                extremely structuring for the development of our vision for the
+                product and to bring perspective for foundational technical
+                choices.
               </Typography>
             </ContentBox>
           </Link>
