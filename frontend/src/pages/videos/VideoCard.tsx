dad89f503b26b70af368b2dee7c070323d0811c2
sintaz1992 <sina.shah71@gmail.com>
2022-01-06 15:12:03 +0100

    added a radar chart with npm, import it in pages/videocard_feed the api data

--- frontend/src/pages/videos/VideoCard.tsx
+++ frontend/src/pages/videos/VideoCard.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import RadarChart from 'react-svg-radar-chart';
+import 'react-svg-radar-chart/build/css/index.css';
 import makeStyles from '@mui/styles/makeStyles';
 import { useParams } from 'react-router-dom';
 import { VideoCardFromId } from 'src/features/videos/VideoCard';
+import { useVideoMetadata } from 'src/features/videos/VideoApi';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -18,11 +20,37 @@ const useStyles = makeStyles(() => ({
 function VideoCardPage() {
   const classes = useStyles();
   const { video_id } = useParams<{ video_id: string }>();
+  const video = useVideoMetadata(video_id);
 
+  const captions: any = video.criteria_scores
+    ? Object.fromEntries(
+        video.criteria_scores.map((criteria) => [
+          criteria.criteria,
+          criteria.criteria,
+        ])
+      )
+    : null;
+
+  const data: any = video.criteria_scores
+    ? {
+        data: Object.fromEntries(
+          video.criteria_scores.map((criteria) => [
+            criteria.criteria,
+            criteria.score,
+          ])
+        ),
+        meta: { color: 'blue' },
+      }
+    : null;
   return (
     <div>
       <div className={classes.root}>
         <VideoCardFromId videoId={video_id} />
+      </div>
+      <div>
+        {video.criteria_scores && (
+          <RadarChart captions={captions} data={[data]} size={550} />
+        )}
       </div>
     </div>
   );
