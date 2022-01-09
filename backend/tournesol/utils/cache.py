6786d59f2cc1b2f545db7c4462b03a2381acd4da
Adrien Matissart <amatissart@users.noreply.github.com>
2022-01-09 19:54:23 +0100

    [back] feat: enable localized error messages in API (#425)
    * Enable localized error messages in API
    
    * Update import order in backend/tournesol/utils/cache.py
    
    Co-authored-by: Gresille & Siffle <39056254+GresilleSiffle@users.noreply.github.com>
    
    Co-authored-by: Gresille & Siffle <39056254+GresilleSiffle@users.noreply.github.com>

---
+++ backend/tournesol/utils/cache.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.utils import translation
+from django.views.decorators.cache import cache_page
+
+
+def cache_page_no_i18n(timeout: float):
+    """
+    This decorator activates the default language before calling `cache_page`.
+
+    By default, `cache_page` includes the request language code in its cache key.
+    Some API responses (e.g public export files) don't depend on the language
+    and can be cached under a single key using this decorator.
+    """
+
+    def decorator(view):
+        cached_view = cache_page(timeout=timeout)(view)
+
+        def wrapper(request, *args, **kwargs):
+            translation.activate(settings.LANGUAGE_CODE)
+            request.LANGUAGE_CODE = translation.get_language()
+            return cached_view(request, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
