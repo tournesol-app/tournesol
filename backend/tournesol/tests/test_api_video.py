320144c25107e1fed016b46ad85b3b2c0fea75e5
Adrien Matissart <a@matissart.net>
2022-01-11 15:53:11 +0100

    Use BigIntegerField to store video view count

--- backend/tournesol/tests/test_api_video.py
+++ backend/tournesol/tests/test_api_video.py
@@ -593,3 +593,8 @@ class VideoApi(TestCase):
         resp = client.get("/video/?uploader=uploader2")
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp.data["count"], 2)
+
+    def test_video_views_stored_on_64bits(self):
+        Video.objects.filter(video_id=self._video_id_01).update(views=9_000_000_000)
+        video = Video.objects.get(video_id=self._video_id_01)
+        self.assertEqual(video.views, 9_000_000_000)
