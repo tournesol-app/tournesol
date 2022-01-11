73ac72d8b69c13ffded9bc315b23977451f7c10d
aidanjungo <aidan@cfse.ch>
2022-01-11 19:49:12 +0100

    Use fail-fast for no data message

--- analytics/pages/page_public_dataset.py
+++ analytics/pages/page_public_dataset.py
@@ -30,206 +30,193 @@ def add_expander_select_user():
             "You can select one or several users. If you select none the entire public data set will be use."
         )
 
-        if isinstance(st.session_state.df, pd.DataFrame):
-            df = st.session_state.df
-            all_users = df["public_username"].unique()
-            selected_users = st.multiselect("", all_users)
-            if len(selected_users):
-                df = df[df["public_username"].isin(selected_users)]
-
-            st.session_state.df = df
-            st.session_state.all_users = all_users
-            st.session_state.selected_users = selected_users
-
-        else:
+        if not isinstance(st.session_state.df, pd.DataFrame):
             st.warning(MSG_NO_DATA)
+            return
+
+        df = st.session_state.df
+        all_users = df["public_username"].unique()
+        selected_users = st.multiselect("", all_users)
+        if len(selected_users):
+            df = df[df["public_username"].isin(selected_users)]
+        st.session_state.df = df
+        st.session_state.all_users = all_users
+        st.session_state.selected_users = selected_users
 
 
 def add_expander_raw_data():
 
     with st.expander("Raw data"):
 
-        if isinstance(st.session_state.df, pd.DataFrame):
-            df = st.session_state.df
-            st.write(f"There are {df['video_a'].size} comparisons in this dataset")
-            st.write(df)
-        else:
+        if not isinstance(st.session_state.df, pd.DataFrame):
             st.warning(MSG_NO_DATA)
+            return
+
+        df = st.session_state.df
+        st.write(f"There are {df['video_a'].size} comparisons in this dataset")
+        st.write(df)
 
 
 def add_expander_statistics():
 
     with st.expander("Statistics"):
 
-        if isinstance(st.session_state.df, pd.DataFrame):
-            df = st.session_state.df
-
-            df_stats = (
-                df["public_username"]
-                .value_counts()
-                .reset_index()
-                .rename(
-                    columns={
-                        "index": "public_username",
-                        "public_username": "Nb of public comparison",
-                    }
-                )
-            )
-
-            df_stats["Nb of video"] = df_stats["public_username"].apply(
-                lambda x: len(get_unique_video_list(df[df["public_username"] == x]))
-            )
-            st.write("Number of public comparisons by user:")
-            st.write(df_stats)
-        else:
+        if not isinstance(st.session_state.df, pd.DataFrame):
             st.warning(MSG_NO_DATA)
+            return
+
+        df = st.session_state.df
+        df_stats = (
+            df["public_username"]
+            .value_counts()
+            .reset_index()
+            .rename(
+                columns={
+                    "index": "public_username",
+                    "public_username": "Nb of public comparison",
+                }
+            )
+        )
+
+        df_stats["Nb of video"] = df_stats["public_username"].apply(
+            lambda x: len(get_unique_video_list(df[df["public_username"] == x]))
+        )
+        st.write("Number of public comparisons by user:")
+        st.write(df_stats)
 
 
 def add_expander_correlation_coefficients():
 
     with st.expander("Correlation coefficients"):
 
-        if isinstance(st.session_state.df, pd.DataFrame):
-            df = st.session_state.df
-
-            st.markdown(
-                "The table bellow shows the pearson correlation coefficient between the criteria."
-            )
-
-            df_correl = df[CRITERIA].corr()
-
-            fig = plt.figure()
-
-            sns.heatmap(
-                df_correl,
-                cmap="Blues",
-                linewidths=0.5,
-                annot=True,
-                fmt=".2g",
-                annot_kws={"size": 7},
-            )
-
-            st.pyplot(fig)
-
-        else:
+        if not isinstance(st.session_state.df, pd.DataFrame):
             st.warning(MSG_NO_DATA)
+            return
+
+        df = st.session_state.df
+        st.markdown(
+            "The table bellow shows the pearson correlation coefficient between the criteria."
+        )
+        df_correl = df[CRITERIA].corr()
+        fig = plt.figure()
+        sns.heatmap(
+            df_correl,
+            cmap="Blues",
+            linewidths=0.5,
+            annot=True,
+            fmt=".2g",
+            annot_kws={"size": 7},
+        )
+        st.pyplot(fig)
 
 
 def add_expander_detailed_correlation():
 
     with st.expander("Detailed correlation"):
 
-        if isinstance(st.session_state.df, pd.DataFrame):
+        if not isinstance(st.session_state.df, pd.DataFrame):
+            st.warning(MSG_NO_DATA)
+            return
 
-            df = st.session_state.df
-            selected_users = st.session_state.selected_users
+        df = st.session_state.df
+        selected_users = st.session_state.selected_users
 
-            # Select data
-            col1, col2, col3 = st.columns(3)
-            with col1:
-                x = st.selectbox("x", CRITERIA, 1)
-            with col2:
-                y = st.selectbox("y", CRITERIA, 4)
-            with col3:
-                st.write(" ")
+        # Select data
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            x = st.selectbox("x", CRITERIA, 1)
+        with col2:
+            y = st.selectbox("y", CRITERIA, 4)
+        with col3:
+            st.write(" ")
 
-            # Filter
-            col1_exp, col2_exp = st.columns(2)
-            with col1_exp:
-                filt = st.selectbox("Filter:", CRITERIA)
-            with col2_exp:
-                score_filt = st.slider("Filter between:", -10, 10, (-10, 10), 1)
+        # Filter
+        col1_exp, col2_exp = st.columns(2)
+        with col1_exp:
+            filt = st.selectbox("Filter:", CRITERIA)
+        with col2_exp:
+            score_filt = st.slider("Filter between:", -10, 10, (-10, 10), 1)
 
-            df_filt = df.loc[(df[filt] >= score_filt[0]) & (df[filt] <= score_filt[1])]
+        df_filt = df.loc[(df[filt] >= score_filt[0]) & (df[filt] <= score_filt[1])]
+        add_regressions = False
 
+        if len(selected_users):
+            add_regressions = st.checkbox("Add linear regressions")
+            users = selected_users
+        else:
+            users = st.session_state.all_users
             add_regressions = False
-            if len(selected_users):
-                add_regressions = st.checkbox("Add linear regressions")
-                users = selected_users
-            else:
-                users = st.session_state.all_users
-                add_regressions = False
+        fig = go.Figure()
 
-            fig = go.Figure()
-            for u, user in enumerate(users):
-                df_user = df_filt[df_filt["public_username"] == user]
+        for u, user in enumerate(users):
+            df_user = df_filt[df_filt["public_username"] == user]
+            fig.add_trace(
+                go.Scatter(
+                    x=df_user[x],
+                    y=df_user[y],
+                    name=user,
+                    mode="markers",
+                    marker_color=TCOLOR[u % len(TCOLOR)],
+                )
+            )
+
+            # TODO: Cloud be improved/simplified
+            if add_regressions:
+                df_user.dropna(inplace=True)
+                x_fit = df_user[x].to_numpy().reshape((-1, 1))
+                y_fit = df_user[y].to_numpy().reshape((-1, 1))
+                model = LinearRegression()
+                model.fit(x_fit, y_fit)
+                x_range = np.linspace(-10, 10, 100).reshape((-1, 1)).tolist()
+                y_range = model.predict(x_range).tolist()
+                x_range = [val[0] for val in x_range]
+                y_range = [val[0] for val in y_range]
+
                 fig.add_trace(
                     go.Scatter(
-                        x=df_user[x],
-                        y=df_user[y],
-                        name=user,
-                        mode="markers",
+                        x=x_range,
+                        y=y_range,
+                        name=f"linear reg",
+                        mode="lines",
                         marker_color=TCOLOR[u % len(TCOLOR)],
                     )
                 )
 
-                # TODO: Cloud be improved/simplified
-                if add_regressions:
-
-                    df_user.dropna(inplace=True)
-
-                    x_fit = df_user[x].to_numpy().reshape((-1, 1))
-                    y_fit = df_user[y].to_numpy().reshape((-1, 1))
-
-                    model = LinearRegression()
-                    model.fit(x_fit, y_fit)
-
-                    x_range = np.linspace(-10, 10, 100).reshape((-1, 1)).tolist()
-                    y_range = model.predict(x_range).tolist()
-
-                    x_range = [val[0] for val in x_range]
-                    y_range = [val[0] for val in y_range]
-
-                    fig.add_trace(
-                        go.Scatter(
-                            x=x_range,
-                            y=y_range,
-                            name=f"linear reg",
-                            mode="lines",
-                            marker_color=TCOLOR[u % len(TCOLOR)],
-                        )
-                    )
-
-            fig.update_layout(xaxis=dict(title=x), yaxis=dict(title=y))
-            fig.update_xaxes(showline=True, linewidth=2, linecolor="black")
-            fig.update_yaxes(showline=True, linewidth=2, linecolor="black")
-
-            st.plotly_chart(fig)
-
-        else:
-            st.warning(MSG_NO_DATA)
+        fig.update_layout(xaxis=dict(title=x), yaxis=dict(title=y))
+        fig.update_xaxes(showline=True, linewidth=2, linecolor="black")
+        fig.update_yaxes(showline=True, linewidth=2, linecolor="black")
+        st.plotly_chart(fig)
 
 
 def add_expander_cursor_position():
 
     with st.expander("Cursor position histogram"):
 
-        if isinstance(st.session_state.df, pd.DataFrame):
+        if not isinstance(st.session_state.df, pd.DataFrame):
+            st.warning(MSG_NO_DATA)
+            return
 
-            df = st.session_state.df
-            selected_users = st.session_state.selected_users
+        df = st.session_state.df
+        selected_users = st.session_state.selected_users
 
-            st.markdown("For any criteria, you can see how user(s) place the cursor.")
+        st.markdown("For any criteria, you can see how user(s) place the cursor.")
 
-            selected_crit = st.selectbox("Select a criteria:", CRITERIA)
+        selected_crit = st.selectbox("Select a criteria:", CRITERIA)
 
-            fig = go.Figure()
+        fig = go.Figure()
 
-            if len(selected_users):
+        if len(selected_users):
 
-                for user in selected_users:
-                    df_user = df[df["public_username"] == user]
-                    fig.add_trace(go.Histogram(x=df_user[selected_crit], name=user, nbinsx=21))
-
-            else:
-                fig.add_trace(go.Histogram(x=df[selected_crit], name="all users", nbinsx=21))
-
-            fig.update_layout(barmode="overlay")
-            fig.update_traces(opacity=0.7)
-            st.plotly_chart(fig)
+            for user in selected_users:
+                df_user = df[df["public_username"] == user]
+                fig.add_trace(go.Histogram(x=df_user[selected_crit], name=user, nbinsx=21))
 
         else:
-            st.warning(MSG_NO_DATA)
+            fig.add_trace(go.Histogram(x=df[selected_crit], name="all users", nbinsx=21))
+
+        fig.update_layout(barmode="overlay")
+        fig.update_traces(opacity=0.7)
+        st.plotly_chart(fig)
 
 
 def app():
