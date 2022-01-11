b793ae711856e0414e32a809df3d81be863e67ce
sintaz1992 <sina.shah71@gmail.com>
2022-01-11 17:28:19 +0100

    container added to center the radarchart,bug fixed if API does not give information

--- frontend/src/pages/videos/VideoCard.tsx
+++ frontend/src/pages/videos/VideoCard.tsx
@@ -5,6 +5,7 @@ import makeStyles from '@mui/styles/makeStyles';
 import { useParams } from 'react-router-dom';
 import { VideoCardFromId } from 'src/features/videos/VideoCard';
 import { useVideoMetadata } from 'src/features/videos/VideoApi';
+import Container from '@mui/material/Container';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -20,38 +21,52 @@ const useStyles = makeStyles(() => ({
 function VideoCardPage() {
   const classes = useStyles();
   const { video_id } = useParams<{ video_id: string }>();
+  console.log(video_id);
   const video = useVideoMetadata(video_id);
-
+  console.log(video);
   const captions: any = video.criteria_scores
-    ? Object.fromEntries(
-        video.criteria_scores.map((criteria) => [
-          criteria.criteria,
-          criteria.criteria,
-        ])
-      )
+    ? video.criteria_scores.length > 0
+      ? Object.fromEntries(
+          video.criteria_scores.map((criteria) => [
+            criteria.criteria,
+            criteria.criteria,
+          ])
+        )
+      : {
+          d: 'd',
+        }
     : null;
 
   const data: any = video.criteria_scores
-    ? {
-        data: Object.fromEntries(
-          video.criteria_scores.map((criteria) => [
-            criteria.criteria,
-            criteria.score,
-          ])
-        ),
-        meta: { color: 'blue' },
-      }
+    ? video.criteria_scores.length > 0
+      ? {
+          data: Object.fromEntries(
+            video.criteria_scores.map((criteria) => [
+              criteria.criteria,
+              criteria.score,
+            ])
+          ),
+          meta: { color: 'purple' },
+        }
+      : {
+          data: {
+            d: 0,
+          },
+          meta: { color: 'blue' },
+        }
     : null;
+  console.log(captions);
+  console.log(video.criteria_scores);
   return (
     <div>
       <div className={classes.root}>
         <VideoCardFromId videoId={video_id} />
       </div>
-      <div>
+      <Container maxWidth="sm">
         {video.criteria_scores && (
           <RadarChart captions={captions} data={[data]} size={550} />
         )}
-      </div>
+      </Container>
     </div>
   );
 }
