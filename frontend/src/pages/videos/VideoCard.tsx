4d8ef7319d4c3c5eab5929f939e62b46dcf293b4
Louis Faucon <lpfaucon@gmail.com>
2022-01-12 19:18:48 +0100

    switch to Recharts to have icons as labels

--- frontend/src/pages/videos/VideoCard.tsx
+++ frontend/src/pages/videos/VideoCard.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import RadarChart from 'react-svg-radar-chart';
-import 'react-svg-radar-chart/build/css/index.css';
 import makeStyles from '@mui/styles/makeStyles';
 import { useParams } from 'react-router-dom';
 import { VideoCardFromId } from 'src/features/videos/VideoCard';
@@ -8,6 +6,43 @@ import { useVideoMetadata } from 'src/features/videos/VideoApi';
 import Container from '@mui/material/Container';
 
 import { mainCriteriaNamesObj } from 'src/utils/constants';
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  ResponsiveContainer,
+} from 'recharts';
+
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from 'recharts';
+
+const renderCustomAxisTick = ({ x, y, payload }: any) => {
+  const path =
+    'M899.072 99.328q9.216 13.312 17.92 48.128t16.384 81.92 13.824 100.352 11.264 102.912 9.216 90.112 6.144 60.928q4.096 30.72 7.168 70.656t5.632 79.872 4.096 75.264 2.56 56.832q-13.312 16.384-30.208 25.6t-34.304 11.264-34.304-2.56-30.208-16.896q-1.024-10.24-3.584-33.28t-6.144-53.76-8.192-66.56-8.704-71.68q-11.264-83.968-23.552-184.32-7.168 37.888-11.264 74.752-4.096 31.744-6.656 66.56t-0.512 62.464q1.024 18.432 3.072 29.184t4.608 19.968 5.12 21.504 5.12 34.304 5.12 56.832 4.608 90.112q-11.264 24.576-50.688 42.496t-88.576 29.696-97.28 16.896-74.752 5.12q-18.432 0-46.08-2.56t-60.416-7.168-66.048-12.288-61.952-17.92-49.664-24.064-28.16-30.208q2.048-55.296 5.12-90.112t5.632-56.832 5.12-34.304 5.12-21.504 4.096-19.968 3.584-29.184q2.048-27.648-0.512-62.464t-6.656-66.56q-4.096-36.864-11.264-74.752-13.312 100.352-24.576 184.32-5.12 35.84-9.216 71.68t-8.192 66.56-6.656 53.76-2.56 33.28q-13.312 12.288-30.208 16.896t-34.304 2.56-33.792-11.264-29.696-25.6q0-21.504 2.048-56.832t4.096-75.264 5.632-79.872 6.656-70.656q2.048-20.48 6.144-60.928t9.728-90.112 11.776-102.912 13.824-100.352 16.384-81.92 17.92-48.128q20.48-12.288 56.32-25.6t73.216-26.624 71.168-25.088 50.176-22.016q10.24 13.312 16.896 61.44t13.312 115.712 15.36 146.432 23.04 153.6l38.912-334.848-29.696-25.6 43.008-54.272 15.36 2.048 15.36-2.048 43.008 54.272-29.696 25.6 38.912 334.848q14.336-74.752 23.04-153.6t15.36-146.432 13.312-115.712 16.896-61.44q16.384 10.24 50.176 22.016t71.168 25.088 73.216 26.624 56.32 25.6';
+
+  return (
+    <svg
+      x={x - 12}
+      y={y - 12}
+      width={24}
+      height={24}
+      viewBox="0 0 100 100"
+      fill="#666"
+    >
+      <text x={0} y={50}>
+        {payload.value}
+      </text>
+    </svg>
+  );
+};
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -28,31 +63,26 @@ function VideoCardPage() {
   const shouldDisplayChart =
     video.criteria_scores && video.criteria_scores.length > 0;
 
-  console.log(mainCriteriaNamesObj);
-  const captions: any = mainCriteriaNamesObj;
-
-  const data: any = shouldDisplayChart
-    ? {
-        data: Object.fromEntries(
-          video.criteria_scores.map((criteria) => [
-            criteria.criteria,
-            Math.max(0, Math.min(criteria.score as number, 1)),
-          ])
-        ),
-        meta: { color: 'purple' },
-      }
-    : null;
+  const data: any = shouldDisplayChart ? video.criteria_scores : null;
 
   return (
     <div>
       <div className={classes.root}>
         <VideoCardFromId videoId={video_id} />
       </div>
-      <Container maxWidth="sm">
-        {shouldDisplayChart && (
-          <RadarChart captions={captions} data={[data]} size={550} />
-        )}
-      </Container>
+
+      {shouldDisplayChart && (
+        <RadarChart width={600} height={300} outerRadius="80%" data={data}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="criteria" tick={renderCustomAxisTick} />
+          <Radar
+            dataKey="score"
+            stroke="#8884d8"
+            fill="#8884d8"
+            fillOpacity={0.6}
+          />
+        </RadarChart>
+      )}
     </div>
   );
 }
