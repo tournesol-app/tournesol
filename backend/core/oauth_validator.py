4c61e2e352b3bc3835856de3dafd68e698ca7090
Adrien Matissart <a@matissart.net>
2022-01-24 19:59:29 +0100

    Accept email on login and to reset password

--- backend/core/oauth_validator.py
+++ backend/core/oauth_validator.py
@@ -2,6 +2,8 @@
 Defines additional validators for Tournesol's authentication
 """
 
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
 from oauth2_provider.oauth2_validators import OAuth2Validator
 
 
@@ -20,3 +22,20 @@ class CustomOAuth2Validator(OAuth2Validator):
     def get_userinfo_claims(self, request):
         claims = super().get_userinfo_claims(request)
         return claims
+
+    def validate_user(self, username, password, client, request, *args, **kwargs):
+        user_found = super().validate_user(username, password, client, request, *args, **kwargs)
+        if user_found:
+            return user_found
+        # support authentication with email as username
+        if not user_found and username and "@" in username:
+            try:
+                user = get_user_model().objects.get(email=username)
+            except ObjectDoesNotExist:
+                return False
+            return super().validate_user(user.username, password, client, request, *args, **kwargs)
+
+    def save_bearer_token(self, token, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            token["username"] = request.user.username
+        return super().save_bearer_token(token, request, *args, **kwargs)
