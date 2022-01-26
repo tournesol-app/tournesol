368422d18cd95d4b8bc5cf66878babc33f3cb10a
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-01-26 11:23:09 +0100

    change opacity when a button is disabled

--- browser-extension/html/tournesol-iframe.js
+++ browser-extension/html/tournesol-iframe.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const IFRAME_ID = 'x-tournesol-iframe-inner';
   const REFRESH_BUTTON_ID = 'action-refresh-iframe';
 
+  const BUTTONS_INITIAL_OPACITY = '1';
+  const BUTTONS_DISABLED_OPACITY = '0.4';
 
   const enableRefreshButton = function enableRefreshButton() {
     const refresh = document.getElementById(REFRESH_BUTTON_ID);
     refresh.removeAttribute('disabled');
+    refresh.style.opacity = BUTTONS_INITIAL_OPACITY;
   }
 
   const refreshIframe = function refreshIframe() {
@@ -13,6 +16,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const refresh = document.getElementById(REFRESH_BUTTON_ID);
 
     refresh.disabled = true;
+    refresh.style.opacity = BUTTONS_DISABLED_OPACITY;
     iframe.addEventListener('load', enableRefreshButton);
     iframe.src = iframe.src;
   }
