7fd7f434a34c6d26268c574c0133a5420450698a
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-02-03 15:07:02 +0100

    fix rebase on main + add new fields to entity

--- backend/tournesol/migrations/0019_rename_video_to_entity.py
+++ backend/tournesol/migrations/0019_rename_video_to_entity.py
@@ -5,6 +5,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('tournesol', '0018_fill_missing_video_duration'),
+        ('twitterbot', '0001_initial')
     ]
 
     operations = [
