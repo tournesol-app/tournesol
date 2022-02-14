86030ba1ef832c5e80503fe74fe1a642ff2cc678
Adrien Matissart <a@matissart.net>
2022-02-14 10:38:57 +0100

    Add docstring on PollsView

--- backend/tournesol/views/polls.py
+++ backend/tournesol/views/polls.py
@@ -9,6 +9,9 @@ logger = logging.getLogger(__name__)
 
 
 class PollsView(RetrieveAPIView):
+    """
+    Retrieve a poll and its related criteria.
+    """
     permission_classes = []
     queryset = Poll.objects.prefetch_related("criteriarank_set__criteria")
     lookup_field = "name"
