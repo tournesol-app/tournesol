4038e00c027f5ee9cd9dc6cd7c1186effbc2ffc4
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-02-16 17:27:01 +0100

    css and translation tweaks

--- frontend/src/features/recommendation/SearchFilter.tsx
+++ frontend/src/features/recommendation/SearchFilter.tsx
@@ -9,7 +9,7 @@ import LanguageFilter from './LanguageFilter';
 import DateFilter from './DateFilter';
 import CriteriaFilter from './CriteriaFilter';
 import UploaderFilter from './UploaderFilter';
-import SafeFilter from './SafeFilter';
+import AdvancedFilter from './AdvancedFilter';
 import {
   recommendationFilters,
   defaultRecommendationFilters,
@@ -65,10 +65,12 @@ function SearchFilter() {
               value={filterParams.get(recommendationFilters.date) ?? ''}
               onChange={(value) => setFilter(recommendationFilters.date, value)}
             />
-            <SafeFilter
-              value={filterParams.get('unsafe') ?? ''}
-              onChange={(value) => setFilter('unsafe', value)}
-            />
+            <Box mt={2}>
+              <AdvancedFilter
+                value={filterParams.get('unsafe') ?? ''}
+                onChange={(value) => setFilter('unsafe', value)}
+              />
+            </Box>
           </Grid>
           <Grid item xs={6} md={3} lg={2} data-testid="search-language-filter">
             <LanguageFilter
