efbb249295fa87ff06bf72482e8da1bfcd253c2c
ramprakashram <connect2ramprakash@gmail.com>
2022-02-20 20:46:31 +0530

    [fix] : fixes test failure by adding the correct data-testid to SearchFilter

--- frontend/src/features/recommendation/SearchFilter.tsx
+++ frontend/src/features/recommendation/SearchFilter.tsx
@@ -51,7 +51,7 @@ function SearchFilter() {
           </Box>
         )}
         <Grid container spacing={4} sx={{ marginBottom: '8px' }}>
-          <Grid item xs={6} md={3} lg={2} data-testid="search-date-filter">
+          <Grid item xs={6} md={3} lg={2} data-testid="search-date-safe-filter">
             <DateFilter
               value={filterParams.get(recommendationFilters.date) ?? ''}
               onChange={(value) => setFilter(recommendationFilters.date, value)}
