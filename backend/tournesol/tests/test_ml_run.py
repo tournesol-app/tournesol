38765d3764cbab871b24e2b7c53c4fd70e07afc7
Lucas Tarasconi <lucas@tarasconi.fr>
2022-02-23 16:36:49 +0100

    Update test for divergent opinion

--- backend/tournesol/tests/test_ml_run.py
+++ backend/tournesol/tests/test_ml_run.py
@@ -5,7 +5,7 @@ from core.models import EmailDomain
 from core.tests.factories.user import UserFactory
 from tournesol.models import ContributorRatingCriteriaScore, EntityCriteriaScore
 
-from .factories.comparison import ComparisonCriteriaScoreFactory
+from .factories.comparison import ComparisonCriteriaScoreFactory, VideoFactory
 
 
 class TestMlTrain(TestCase):
@@ -21,8 +21,11 @@ class TestMlTrain(TestCase):
         user1 = UserFactory(email="user1@verified.test")
         user2 = UserFactory(email="user2@verified.test")
 
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=user1)
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=user2)
+        self.video1 = VideoFactory()
+        self.video2 = VideoFactory()
+
+        ComparisonCriteriaScoreFactory(comparison__user=user1, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=10)
+        ComparisonCriteriaScoreFactory(comparison__user=user2, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=10)
 
         not_trusted_user1 = UserFactory(email="not_trusted_user1@not_verified.test")
         not_trusted_user2 = UserFactory(email="not_trusted_user2@not_verified.test")
@@ -35,21 +38,22 @@ class TestMlTrain(TestCase):
         not_trusted_user9 = UserFactory(email="not_trusted_user9@not_verified.test")
         not_trusted_user10 = UserFactory(email="not_trusted_user10@not_verified.test")
 
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=not_trusted_user1)
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=not_trusted_user2)
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=not_trusted_user3)
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=not_trusted_user4)
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=not_trusted_user5)
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=not_trusted_user6)
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=not_trusted_user7)
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=not_trusted_user8)
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=not_trusted_user9)
-        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=not_trusted_user10)
+        ComparisonCriteriaScoreFactory(comparison__user=not_trusted_user1, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=-10)
+        ComparisonCriteriaScoreFactory(comparison__user=not_trusted_user2, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=-10)
+        ComparisonCriteriaScoreFactory(comparison__user=not_trusted_user3, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=-10)
+        ComparisonCriteriaScoreFactory(comparison__user=not_trusted_user4, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=-10)
+        ComparisonCriteriaScoreFactory(comparison__user=not_trusted_user5, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=-10)
+        ComparisonCriteriaScoreFactory(comparison__user=not_trusted_user6, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=-10)
+        ComparisonCriteriaScoreFactory(comparison__user=not_trusted_user7, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=-10)
+        ComparisonCriteriaScoreFactory(comparison__user=not_trusted_user8, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=-10)
+        ComparisonCriteriaScoreFactory(comparison__user=not_trusted_user9, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=-10)
+        ComparisonCriteriaScoreFactory(comparison__user=not_trusted_user10, comparison__entity_1=self.video1, comparison__entity_2=self.video2, score=-10)
 
-    
     def test_ml_train(self):
         self.assertEqual(EntityCriteriaScore.objects.count(), 0)
         self.assertEqual(ContributorRatingCriteriaScore.objects.count(), 0)
         call_command("ml_train")
-        self.assertEqual(EntityCriteriaScore.objects.count(), 40)
-        self.assertEqual(ContributorRatingCriteriaScore.objects.count(), 240)
+        self.assertEqual(EntityCriteriaScore.objects.count(), 2)
+        self.assertEqual(ContributorRatingCriteriaScore.objects.count(), 24)
+        self.assertLess(EntityCriteriaScore.objects.get(entity_id=self.video1.id).score, 0)
+        self.assertGreater(EntityCriteriaScore.objects.get(entity_id=self.video2.id).score, 0)
