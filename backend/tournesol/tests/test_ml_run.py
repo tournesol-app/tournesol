07147f7a843e582770f17f94565077c3b8dc0c2b
Lucas Tarasconi <lucas@tarasconi.fr>
2022-02-25 16:58:21 +0100

    Update test using factory

--- backend/tournesol/tests/test_ml_run.py
+++ backend/tournesol/tests/test_ml_run.py
@@ -3,9 +3,10 @@ from django.test import TestCase
 
 from core.models import EmailDomain
 from core.tests.factories.user import UserFactory
-from tournesol.models import EntityCriteriaScore
+from tournesol.models import ComparisonCriteriaScore, EntityCriteriaScore, Poll
 
 from .factories.comparison import ComparisonCriteriaScoreFactory
+from .factories.poll import PollWithCriterasFactory
 
 
 class TestMlTrain(TestCase):
@@ -14,10 +15,26 @@ class TestMlTrain(TestCase):
             domain="@verified.test",
             status=EmailDomain.STATUS_ACCEPTED
         )
+
+        self.poll = PollWithCriterasFactory.create()
+
         user = UserFactory(email="user@verified.test")
+        # Comparison on default poll
         ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=user)
-    
-    def test_ml_train(self):
+        # Comparison on custom poll
+        ComparisonCriteriaScoreFactory.create_batch(10, comparison__user=user, comparison__poll=self.poll)
+
+        
+            
+    def test_ml_on_multiple_poll(self):
+
         self.assertEqual(EntityCriteriaScore.objects.count(), 0)
+        self.assertEqual(ComparisonCriteriaScore.objects.exclude(comparison__poll=self.poll).count(),10)
+        self.assertEqual(ComparisonCriteriaScore.objects.filter(comparison__poll=self.poll).count(),10)
+        self.assertEqual(len(self.poll.criterias_list),1)
+
         call_command("ml_train")
-        self.assertEqual(EntityCriteriaScore.objects.count(), 20)
+
+        self.assertEqual(EntityCriteriaScore.objects.count(), 40)
+        self.assertEqual(EntityCriteriaScore.objects.filter(poll=self.poll).count(),20)
+        self.assertEqual(EntityCriteriaScore.objects.filter(poll=Poll.default_poll()).count(),20)
