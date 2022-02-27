eb55e9c4a00bda7117f19a2fe3e88ed6eaf830c1
aidanjungo <aidan@cfse.ch>
2022-02-27 14:30:53 +0100

    Improve uploader_twitter_account and add tests

--- backend/twitterbot/tests/test_get_twitter_account.py
+++ backend/twitterbot/tests/test_get_twitter_account.py
@@ -1,13 +1,26 @@
-from twitterbot.uploader_twitter_account import get_twitter_account
+from twitterbot.uploader_twitter_account import get_twitter_account_from_uploader_id
+from unittest.mock import MagicMock, patch
 
-# Those tests should probably be replaced by mock functions
 
+@patch("twitterbot.uploader_twitter_account.requests.get")
+def test_get_twitter_account_from_uploader_id(mock_requests):
+    """Test function for get_twitter_account_from_uploader_id"""
 
-def test_get_twitter_account():
-    """Test get_twitter_account function"""
+    # Correct response from youtube
+    with open("./twitterbot/tests/mock_resp.txt") as f:
+        mock_resp = f.read()
 
-    assert get_twitter_account("dblEwDMeJTo") == "@le_science4all"
+    mock_requests.return_value = MagicMock(status_code=200, text=mock_resp)
 
-    assert get_twitter_account("VKsekCHBuHI") == "@HygieneMentale"
+    assert (
+        get_twitter_account_from_uploader_id("UC0NCbj8CxzeCGIF6sODJ-7A")
+        == "@le_science4all"
+    )
 
-    assert get_twitter_account("u-rk8nRs4LA") is None
+    # Bad response ("This channel does not exist."
+    with open("./twitterbot/tests/mock_bad_resp.txt") as f:
+        mock_bad_resp = f.read()
+
+    mock_requests.return_value = MagicMock(status_code=200, text=mock_bad_resp)
+
+    assert get_twitter_account_from_uploader_id("UC0NCbj8CxzeCGIF6sOZZZZZ") is None
