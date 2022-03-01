9b28c8aae75267c5fa9559531a850699111e84e7
aidanjungo <aidan@cfse.ch>
2022-03-01 20:29:57 +0100

    Adapt to backend refactor

--- backend/twitterbot/uploader_twitter_account.py
+++ backend/twitterbot/uploader_twitter_account.py
@@ -8,10 +8,10 @@ import requests
 from tournesol.utils.api_youtube import get_video_metadata
 
 
-def get_twitter_account_from_uploader_id(uploader_id):
+def get_twitter_account_from_channel_id(channel_id):
     """Get Twitter account from uploader id"""
 
-    uploader_url = f"https://www.youtube.com/channel/{uploader_id}"
+    uploader_url = f"https://www.youtube.com/channel/{channel_id}"
 
     resp = requests.get(uploader_url, headers={"user-agent": "curl/7.68.0"})
 
@@ -29,6 +29,6 @@ def get_twitter_account_from_video_id(video_id):
     """Get Twitter account from video id"""
 
     metadata = get_video_metadata(video_id, compute_language=False)
-    uploader_id = metadata["uploader_id"]
+    channel_id = metadata["channel_id"]
 
-    return get_twitter_account_from_uploader_id(uploader_id)
+    return get_twitter_account_from_channel_id(channel_id)
