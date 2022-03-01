d79bac89e7d5003c792fd5136d9881c32bf912b3
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-03-01 10:31:42 +0100

    make `EntityNoExtraFieldSerializer` readonly

--- backend/tournesol/serializers/entity.py
+++ backend/tournesol/serializers/entity.py
@@ -178,6 +178,7 @@ class EntityNoExtraFieldSerializer(EntitySerializer):
             "metadata",
         ]
         read_only_fields = [
+            "uid",
             "type",
             "metadata",
         ]
