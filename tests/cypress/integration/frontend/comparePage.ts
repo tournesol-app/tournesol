bd6c7dd780bbbb4cc2ee3bf4e1abb22738bc2e25
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-03-02 11:59:58 +0100

    simplify the test support pasting YouTube video ID

--- tests/cypress/integration/frontend/comparePage.ts
+++ tests/cypress/integration/frontend/comparePage.ts
@@ -1,75 +1,67 @@
 describe('Comparison page', () => {
-    describe('authorization', () => {
-        it('is not accessible by anonymous users', () => {
-            cy.visit('/comparison');
-            cy.location('pathname').should('equal', '/login');
-            cy.contains('log in to tournesol', {matchCase: false}).should('be.visible');
-        })
+  describe('authorization', () => {
+    it('is not accessible by anonymous users', () => {
+      cy.visit('/comparison');
+      cy.location('pathname').should('equal', '/login');
+      cy.contains('log in to tournesol', {matchCase: false}).should('be.visible');
+    })
 
-        it('is accessible by authenticated users', () => {
-            cy.visit('/comparison');
+    it('is accessible by authenticated users', () => {
+      cy.visit('/comparison');
 
-            cy.focused().type('user1');
-            cy.get('input[name="password"]').click().type('tournesol').type('{enter}');
+      cy.focused().type('user1');
+      cy.get('input[name="password"]').click().type('tournesol').type('{enter}');
 
-            cy.location('pathname').should('equal', '/comparison');
-            cy.contains('submit a comparison', {matchCase: false}).should('be.visible');
-        })
-    });
+      cy.location('pathname').should('equal', '/comparison');
+      cy.contains('submit a comparison', {matchCase: false}).should('be.visible');
+    })
+  });
 
-    describe('video selectors', () => {
-        const videoAUrl = 'https://www.youtube.com/watch?v=u83A7DUNMHs';
-        const videoBUrl = 'https://www.youtube.com/watch?v=m9APFfDGRuk';
+  describe('video selectors', () => {
+    const videoAUrl = 'https://www.youtube.com/watch?v=u83A7DUNMHs';
 
-        it('support pasting YouTube URLs', () => {
-            cy.visit('/comparison');
+    it('support pasting YouTube URLs', () => {
+      cy.visit('/comparison');
 
-            cy.focused().type('user1');
-            cy.get('input[name="password"]').click().type('tournesol').type('{enter}');
+      cy.focused().type('user1');
+      cy.get('input[name="password"]').click()
+        .type('tournesol').type('{enter}');
 
-            cy.contains('video 1', {matchCase: false}).should('be.visible');
-            cy.contains('video 2', {matchCase: false}).should('be.visible');
+      cy.contains('video 1', {matchCase: false}).should('be.visible');
+      cy.contains('video 2', {matchCase: false}).should('be.visible');
 
-            // TODO: write the test
-        })
+      // TODO: a little help is required to write the tests
 
-        it('support pasting YouTube video ID', () => {
-            cy.visit('/comparison');
+      // I didn't find a proper way to paste the URL into the `VideoCard` input field,
+      // and to trigger its `onChange` method to extract the video ID from the URL.
+      // .type() didn't work
+      // .invoke('val', url) didn't work neither
+      // .invoke('val', url).trigger('change') / trigger('input') neither
+    })
 
-            cy.focused().type('user1');
-            cy.get('input[name="password"]').click().type('tournesol').type('{enter}');
+    it('support pasting YouTube video ID', () => {
+      cy.visit('/comparison');
 
-            const inputs = cy.get('input[placeholder="Paste URL or Video ID"]');
-            inputs.should('have.length', 2);
+      cy.focused().type('user1');
+      cy.get('input[name="password"]').click()
+        .type('tournesol').type('{enter}');
 
-            inputs.first().type(videoAUrl.split('?v=')[1], {delay: 0});
-            inputs.first().get('div[data-testid=video-card-info]').within(() => {
-                // the video title is displayed
-                cy.contains(
-                    'Science4VeryAll : Lê fait enfin un effort de vulgarisation sur Étincelles !!',
-                    {matchCase: false}
-                ).should('be.visible');
-                // the upload date is displayed
-                cy.contains('2021-11-22', {matchCase: false}).should('be.visible');
-                // the number of view is displayed
-                cy.contains('views', {matchCase: false}).should('be.visible');
-            });
+      // two cards must be displayed
+      cy.get('input[placeholder="Paste URL or Video ID"]')
+        .should('have.length', 2);
 
-            cy.get('input[placeholder="Paste URL or Video ID"]')
-                .last().type(videoBUrl.split('?v=')[1], {delay: 0});
+      cy.get('input[placeholder="Paste URL or Video ID"]').first()
+        .type(videoAUrl.split('?v=')[1]);
 
-            cy.get('input[placeholder="Paste URL or Video ID"]')
-                .last().get('div[data-testid=video-card-info]').within(() => {
-                // the video title is displayed
-                cy.contains(
-                    'Pourquoi les réseaux sociaux sont si clivants ?',
-                    {matchCase: false}
-                ).should('be.visible');
-                // the upload date is displayed
-                cy.contains('2022-02-03', {matchCase: false}).should('be.visible');
-                // the number of view is displayed
-                cy.contains('views', {matchCase: false}).should('be.visible');
-            });
-        })
-    });
+      // the video title, upload date, and the number of views must be displayed
+      cy.get('div[data-testid=video-card-info]').first().within(() => {
+        cy.contains(
+          'Science4VeryAll : Lê fait enfin un effort de vulgarisation sur Étincelles !!',
+          {matchCase: false}
+        ).should('be.visible');
+        cy.contains('2021-11-22', {matchCase: false}).should('be.visible');
+        cy.contains('views', {matchCase: false}).should('be.visible');
+      });
+    })
+  });
 });
