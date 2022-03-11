3d5c22c31ca8e7c2c2b37393e31d5bc5538d0dca
Lucas Tarasconi <lucas@tarasconi.fr>
2022-03-11 15:29:46 +0100

    Make recursive algorithm

--- backend/tournesol/views/unconnected_entities.py
+++ backend/tournesol/views/unconnected_entities.py
@@ -24,39 +24,64 @@ class UnconnectedEntitiesView(
     serializer_class = EntitySerializer
     permission_classes = [IsAuthenticated]
 
+    __already_visited_node__ = set()
+
+    def get_related_entities(self, entity):
+        local_entities = set()
+
+        if entity.comparisons_video_1.exists():
+            local_entities.update(
+                set(
+                    map(lambda x: x.entity_2, entity.comparisons_video_1.filter(
+                        poll=self.poll_from_url, user=self.request.user
+                    ))
+                )
+            )
+        if entity.comparisons_video_2.exists():
+            local_entities.update(
+                set(
+                    map(lambda x: x.entity_1, entity.comparisons_video_2.filter(
+                        poll=self.poll_from_url, user=self.request.user
+                    ))
+                )
+            )
+
+        self.__already_visited_node__.add(entity)
+
+        new_set_to_push = set()
+        for element in local_entities:
+            if element not in self.__already_visited_node__:
+                new_set_to_push.update(self.get_related_entities(element))
+
+        local_entities.update(new_set_to_push)
+
+        return local_entities
+
     def get_queryset(self):
-        # Get the main entities
+        # Get related entities from source
+        source_node = Entity.objects.none()
+
+        if self.kwargs.get("id"):
+            entity_id = self.kwargs.get("id")
+            source_node = Entity.objects.get(id=entity_id)
+        else:
+            return Entity.objects.none()
+
+        related_entities = self.get_related_entities(source_node)
+        all_entities = set()
+
         user_comparaisons = Comparison.objects.filter(
             poll=self.poll_from_url, user=self.request.user
         )
 
-        connected_entities_uid_list = []
-        unconnected_entities_uid_list = []
-
-        if self.kwargs.get("id"):
-            # Add search value in the list
-            connected_entities_uid_list.append(self.kwargs.get("id"))
-
-        # Diclaimer : This implementation doesn't work.
-        # It doesn't take non direct link into account
         for comparison in user_comparaisons.iterator():
-            if (
-                comparison.entity_1.id in connected_entities_uid_list and
-                comparison.entity_2.id not in connected_entities_uid_list
-            ):
-                connected_entities_uid_list.append(comparison.entity_2.id)
-            if (
-                comparison.entity_2.id in connected_entities_uid_list and
-                comparison.entity_1.id not in connected_entities_uid_list
-            ):
-                connected_entities_uid_list.append(comparison.entity_1.id)
-            if (
-                comparison.entity_1.id not in connected_entities_uid_list and
-                comparison.entity_2.id not in connected_entities_uid_list
-            ):
-                unconnected_entities_uid_list.append(comparison.entity_1.id)
-                unconnected_entities_uid_list.append(comparison.entity_2.id)
+            if comparison.entity_1 not in all_entities:
+                all_entities.add(comparison.entity_1)
+            if comparison.entity_2 not in all_entities:
+                all_entities.add(comparison.entity_2)
 
-        entities_query_set = Entity.objects.filter(id__in=unconnected_entities_uid_list)
-
-        return entities_query_set
+        return Entity.objects.filter(
+                id__in=map(lambda x: x.id, all_entities)
+                ).exclude(
+                id__in=map(lambda x: x.id, related_entities)
+            )
