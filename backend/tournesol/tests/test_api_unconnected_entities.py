dd722885f9ea75fa9a7ef12fe09939c415387a68
Lucas Tarasconi <lucas@tarasconi.fr>
2022-03-11 12:28:23 +0100

    Update test to make sure it's failed

--- backend/tournesol/tests/test_api_unconnected_entities.py
+++ backend/tournesol/tests/test_api_unconnected_entities.py
@@ -8,7 +8,7 @@ from tournesol.tests.factories.comparison import ComparisonFactory
 from tournesol.tests.factories.entity import VideoFactory
 
 
-class UnconnectedEntitiesTest(TestCase):
+class SimpleAllConnectedTest(TestCase):
     """
     TestCase for the unconnected entities API.
     """
@@ -20,19 +20,20 @@ class UnconnectedEntitiesTest(TestCase):
             self.poll_videos.name
         )
 
-        self.video_1 = VideoFactory()
-
+        video_1 = VideoFactory()
         video_2 = VideoFactory()
         video_3 = VideoFactory()
 
+        self.video_source = video_1
+
         ComparisonFactory(
             user=self.user_1,
-            entity_1=self.video_1,
+            entity_1=video_1,
             entity_2=video_2,
         )
         ComparisonFactory(
             user=self.user_1,
-            entity_1=self.video_1,
+            entity_1=video_1,
             entity_2=video_3,
         )
         ComparisonFactory(
@@ -43,7 +44,7 @@ class UnconnectedEntitiesTest(TestCase):
 
     def test_not_authenticated_cannot_show_unconnected_entities(self):
         response = self.client.get(
-            "{}/{}/".format(self.user_base_url, self.video_1.id),
+            "{}/{}/".format(self.user_base_url, self.video_source.id),
             format="json",
         )
 
@@ -53,7 +54,54 @@ class UnconnectedEntitiesTest(TestCase):
         self.client.force_authenticate(self.user_1)
 
         response = self.client.get(
-            "{}/{}/".format(self.user_base_url, self.video_1.id),
+            "{}/{}/".format(self.user_base_url, self.video_source.id),
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["count"], 0)
+
+
+class AdvancedAllConnectedTest(TestCase):
+    """
+    TestCase for the unconnected entities API.
+    """
+    def setUp(self):
+        self.client = APIClient()
+        self.user_1 = UserFactory()
+        self.poll_videos = Poll.default_poll()
+        self.user_base_url = "/users/me/unconnected_entities/{}".format(
+            self.poll_videos.name
+        )
+
+        video_4 = VideoFactory()
+        video_3 = VideoFactory()
+        video_2 = VideoFactory()
+        video_1 = VideoFactory()
+
+        self.video_source = video_1
+
+        ComparisonFactory(
+            user=self.user_1,
+            entity_1=video_3,
+            entity_2=video_4,
+        )
+        ComparisonFactory(
+            user=self.user_1,
+            entity_1=video_2,
+            entity_2=video_3,
+        )
+        ComparisonFactory(
+            user=self.user_1,
+            entity_1=video_1,
+            entity_2=video_3,
+        )
+
+    def test_all_linked_should_return_empty(self):
+        self.client.force_authenticate(self.user_1)
+
+        response = self.client.get(
+            "{}/{}/".format(self.user_base_url, self.video_source.id),
             format="json",
         )
 
