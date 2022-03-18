f708d3fed1295779bf8029fe92df2e2125d44a6f
Louis Faucon <lpfaucon@gmail.com>
2022-03-18 22:33:55 +0100

    feat: list display for EntityCriteriaScore Admin (#693)

--- backend/tournesol/admin.py
+++ backend/tournesol/admin.py
@@ -104,7 +104,14 @@ class EntityAdmin(admin.ModelAdmin):
 
 @admin.register(EntityCriteriaScore)
 class EntityCriteriaScoreAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'entity',
+        'criteria',
+        'score'
+    )
+    search_fields = (
+        'entity__uid',
+    )
 
 
 @admin.register(ContributorRating)
