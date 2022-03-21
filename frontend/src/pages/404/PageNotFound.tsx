80aac98febf8df3a9ee3b2fdfb30ec0d87e91de1
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-03-21 09:21:49 +0100

    add a foreground image and add translations

--- frontend/src/pages/404/PageNotFound.tsx
+++ frontend/src/pages/404/PageNotFound.tsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 const PageNotFound = () => {
+  const { t } = useTranslation();
   return (
     <Box
-      height="100%"
       sx={{
         backgroundImage: "url('/svg/backgrounds/chandelier.svg')",
         backgroundPosition: 'center',
         backgroundRepeatY: 'no-repeat',
+        height: '100%',
       }}
     >
       <Box
         display="flex"
+        alignItems="center"
         flexDirection="column"
         justifyContent="center"
-        textAlign="center"
-        py={26}
+        pt={26}
+        pb={2}
       >
-        <Typography variant="h2">Sorry, page not found.</Typography>
-        <Typography variant="subtitle1">
-          Please check if the requested address is correct.
+        <Typography variant="h2">
+          {t('pageNotFound.sorryPageNotFound')}
         </Typography>
+        <Typography variant="subtitle1">
+          {t('pageNotFound.theRequestedAddressIsErroneous')}
+        </Typography>
+      </Box>
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        alignItems="flex-end"
+      >
+        <img src="/svg/Watering.svg" height="400px" />
       </Box>
     </Box>
   );
