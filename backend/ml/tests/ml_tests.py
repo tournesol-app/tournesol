7af64e81d206aaa69462f4d0ea9450c2e3662301
Dorian Turba <dorian.turba@gmail.com>
2022-03-26 13:50:10 +0100

    change comparison_data type

--- backend/ml/tests/ml_tests.py
+++ backend/ml/tests/ml_tests.py
@@ -316,18 +316,18 @@ def _id_score_assert(id, score, glob):
 def test_simple_train():
     """test coherency of results for few epochs and very light data"""
     comparison_data = [
-                        [1, 101, 102, "test", 10, 0],
-                        [2, 100, 101, "largely_recommended", 10, 0],
-                        [1, 104, 105, "test", 30 /5 -10, 0],
-                        [99, 100, 101, "largely_recommended", 10, 0],
-                        [2, 108, 107, "test", 10 / 5 - 10, 0],
-                        [0, 100, 102, "test", 70 / 5 - 10, 0],
-                        [0, 104, 105, "test", 70 / 5 - 10, 0],
-                        [0, 109, 110, "test", 0, 0],
-                        [2, 107, 108, "test", 10 / 5 - 10, 0],
-                        [1, 100, 101, "test", 10, 0],
-                        [3, 200, 201, "test", 85 / 5 - 10, 0],
-                        ]
+        (1, 101, 102, "test", 10, 0),
+        (2, 100, 101, "largely_recommended", 10, 0),
+        (1, 104, 105, "test", 30 / 5 - 10, 0),
+        (99, 100, 101, "largely_recommended", 10, 0),
+        (2, 108, 107, "test", 10 / 5 - 10, 0),
+        (0, 100, 102, "test", 70 / 5 - 10, 0),
+        (0, 104, 105, "test", 70 / 5 - 10, 0),
+        (0, 109, 110, "test", 0, 0),
+        (2, 107, 108, "test", 10 / 5 - 10, 0),
+        (1, 100, 101, "test", 10, 0),
+        (3, 200, 201, "test", 85 / 5 - 10, 0),
+    ]
     glob_scores, loc_scores = ml_run(
         comparison_data,
         epochs=2,
