3cab8c89076423fc3c66d4de8ce2b6a51857ab82
Dorian Turba <dorian.turba@gmail.com>
2022-03-26 11:05:01 +0100

    no need for list, generator expression is enough to np.asarray

--- backend/ml/handle_data.py
+++ backend/ml/handle_data.py
@@ -50,7 +50,7 @@ def shape_data(l_ratings):
 
     Returns : one array with 4 columns : userID, vID1, vID2, rating ([-1,1])
     """
-    l_clear = [rating[:3] + (rescale_rating(rating[4]),) for rating in l_ratings]
+    l_clear = (rating[:3] + (rescale_rating(rating[4]),) for rating in l_ratings)
     return np.asarray(l_clear)
 
 
