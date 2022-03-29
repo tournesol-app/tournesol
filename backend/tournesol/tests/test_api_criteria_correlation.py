04aba3dad289b3a7eeb49189435690b5b0669627
Louis Faucon <lpfaucon@gmail.com>
2022-03-29 21:29:19 +0200

    feat: api endpoint for correlations between criterias for contributor ratings

---
+++ backend/tournesol/tests/test_api_criteria_correlation.py
@@ -0,0 +1,102 @@
+
+import numpy as np
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+from tournesol.views.criteria_correlations import compute_correlation, compute_slope
+from tournesol.models.ratings import ContributorRatingCriteriaScore
+from tournesol.tests.factories.entity import EntityFactory
+from tournesol.tests.factories.poll import CriteriaFactory, CriteriaRankFactory, PollFactory, PollWithCriteriasFactory
+from tournesol.tests.factories.ratings import ContributorRatingCriteriaScoreFactory, ContributorRatingFactory
+
+from core.tests.factories.user import UserFactory
+
+
+def test_compute_correlation():
+    assert compute_correlation({}, {}) == None
+    assert compute_correlation({"a": 1}, {"b": 1}) == None
+    assert compute_correlation({"a": 1}, {"a": 1}) == None
+    assert np.allclose(compute_correlation({"a": 1, "b": 2}, {"a": 1, "b": 2}), 1)
+    assert np.allclose(compute_correlation({"a": 1, "b": -2}, {"a": -1, "b": 2}) ,-1)
+    assert np.allclose(compute_correlation({"a": 1, "b": -2, "c": -2, "d": 0}, {"a": -1, "b": 2, "c": 2, "d": 0}) ,-1)
+    assert compute_correlation({"a": 1, "b": 2, "c": 0}, {"a": 3, "b": 3, "c": 1}) < 1
+    # correlation is not defined in the below case 
+    # because all scores on one of the criteria are equal
+    assert compute_correlation({"a": 1, "b": 2}, {"a": 3, "b": 3}) == None
+
+
+def test_compute_slope():
+    assert compute_slope({}, {}) == None
+    assert compute_slope({"a": 1}, {"b": 1}) == None
+    assert compute_slope({"a": 1}, {"a": 1}) == None
+    assert compute_slope({"a": 1, "b": -2}, {"a": -1, "b": 2}) == -1
+    assert compute_slope({"a": 1, "b": 2}, {"a": 3, "b": 3}) == 0
+
+    entities="qwertzuiopasdfghjklyxcvbnm"
+    assert np.allclose(compute_slope(
+        {e: 42 - 2 * i for i, e in enumerate(entities)}, 
+        {e: 24 + 3 * i for i, e in enumerate(entities)}, 
+    ), -3/2)
+    assert np.allclose(compute_slope(
+        {e: i for i, e in enumerate(entities)}, 
+        {e: 24 + 3 * i for i, e in enumerate(entities)}, 
+    ), 3)
+
+
+
+class CorrelationAPI(TestCase):
+
+    def setUp(self):
+        self.user = UserFactory(username="user_test_correlation")
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+        self.poll = PollFactory()
+        criterias = [
+            CriteriaRankFactory(criteria__name="hello", poll=self.poll, rank=1),
+            CriteriaRankFactory(criteria__name="world", poll=self.poll, rank=2),
+            CriteriaRankFactory(criteria__name="!", poll=self.poll, rank=3)
+        ]
+        self.entities = EntityFactory.create_batch(10)
+        self.ratings = [ContributorRatingFactory(user=self.user, entity=entity) for entity in self.entities]
+
+    def test_correlations_no_data(self):
+        response = self.client.get(f"/users/me/criteria_correlations/{self.poll.name}/")
+        assert response.status_code == status.HTTP_200_OK
+        response_json = response.json()
+        assert "criterias" in response_json
+        assert " ".join(response_json["criterias"]) == "hello world !"
+        assert "correlations" in response_json
+        assert len(response_json["correlations"]) == 3
+        assert response_json["correlations"][0] == [None, None, None]
+        assert "slopes" in response_json
+        assert len(response_json["slopes"]) == 3
+        assert response_json["slopes"][0] == [None, None, None]
+
+    def test_correlation_perfect_slope(self):
+        for i, rating in enumerate(self.ratings):
+            ContributorRatingCriteriaScoreFactory(
+                contributor_rating=rating,
+                criteria="hello",
+                score=0.1 * i,
+            )
+            ContributorRatingCriteriaScoreFactory(
+                contributor_rating=rating,
+                criteria="world",
+                score=0.2 * i,
+            )
+
+        response = self.client.get(f"/users/me/criteria_correlations/{self.poll.name}/")
+        assert response.status_code == status.HTTP_200_OK
+        response_json = response.json()
+        assert "correlations" in response_json
+        assert response_json["correlations"] == [
+            [1, 1, None],
+            [1, 1, None],
+            [None, None, None]
+        ]
+        assert response_json["slopes"] == [
+            [1, 1/2, None],
+            [2, 1, None],
+            [None, None, None]
+        ]
---
+++ backend/tournesol/views/criteria_correlations.py
@@ -0,0 +1,68 @@
+
+from scipy.stats import linregress
+import numpy as np
+from django.http import JsonResponse
+from rest_framework.generics import GenericAPIView
+from tournesol.models.ratings import ContributorRating
+
+from tournesol.models.poll import Poll
+
+def compute_correlation(scores_1: dict, scores_2: dict) -> int:
+    """
+    inputs are dictionaries with entities as keys and scores as values
+    for a given user and a given rating criteria
+    """
+
+    # At least two datapoints are necessary to compute correlations
+    if sum(entity in scores_1 for entity in scores_2) > 1:
+        h = np.corrcoef(
+            [scores_1[e] for e in scores_1 if e in scores_2],
+            [scores_2[e] for e in scores_1 if e in scores_2],
+        )
+        return None if np.isnan(h[0][1]) else h[0][1] 
+    else:
+        return None
+
+
+def compute_slope(scores_1: dict, scores_2: dict) -> int:
+    """ 
+    inputs are dictionaries with entities as keys and scores as values
+    for a given user and a given rating criteria
+    """
+
+    # At least two datapoints are necessary to compute slopes
+    if sum(entity in scores_1 for entity in scores_2) > 1:
+        return linregress(
+            [scores_1[e] for e in scores_1 if e in scores_2],
+            [scores_2[e] for e in scores_1 if e in scores_2],
+        ).slope
+    else:
+        return None
+
+class ContributorCriteriaCorrelationsView(GenericAPIView):
+    
+    def get(self, request, poll_name):
+        poll = Poll.objects.get(name=poll_name)
+        criterias = poll.criterias_list
+        n = len(criterias)
+
+        ratings = ContributorRating.objects.prefetch_related("criteria_scores").filter(user=request.user)
+
+        scores = {criteria: {} for criteria in criterias}
+        for r in ratings:
+            for s in r.criteria_scores.all():
+                scores[s.criteria][r.entity] = s.score
+
+        
+
+        return JsonResponse({
+            "criterias": criterias,
+            "correlations": [
+                [compute_correlation(scores[c1], scores[c2]) for c1 in criterias]
+                for c2 in criterias
+            ],
+            "slopes": [
+                [compute_slope(scores[c1], scores[c2]) for c1 in criterias]
+                for c2 in criterias
+            ],
+        })
