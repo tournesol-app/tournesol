f5875a9e38b0bec20439259337ca994af24de2b5
Lucas Tarasconi <lucas@tarasconi.fr>
2022-03-30 17:36:04 +0200

    Implement less queries for fetching related entities

--- backend/tournesol/views/unconnected_entities.py
+++ backend/tournesol/views/unconnected_entities.py
@@ -1,6 +1,7 @@
 """
 API endpoints to show unconnected entities
 """
+from collections import defaultdict
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
@@ -25,37 +26,20 @@ class UnconnectedEntitiesView(
     permission_classes = [IsAuthenticated]
 
     __already_visited_node__ = set()
+    __all_entites__ = set()
+    __all_connections__ = defaultdict(set)
 
-    def get_related_entities(self, entity):
-        local_entities = set()
+    def get_related_entities(self, entity_id):
 
-        if entity.comparisons_video_1.exists():
-            local_entities.update(
-                set(
-                    map(lambda x: x.entity_2, entity.comparisons_video_1.filter(
-                        poll=self.poll_from_url, user=self.request.user
-                    ))
-                )
-            )
-        if entity.comparisons_video_2.exists():
-            local_entities.update(
-                set(
-                    map(lambda x: x.entity_1, entity.comparisons_video_2.filter(
-                        poll=self.poll_from_url, user=self.request.user
-                    ))
-                )
-            )
+        self.__already_visited_node__.add(entity_id)
+        self.__all_entites__.remove(entity_id)
 
-        self.__already_visited_node__.add(entity)
+        related_entities = set()
+        for other_node in self.__all_connections__.get(entity_id):
+            if other_node not in self.__already_visited_node__:
+                related_entities.update(self.get_related_entities(other_node))
 
-        new_set_to_push = set()
-        for element in local_entities:
-            if element not in self.__already_visited_node__:
-                new_set_to_push.update(self.get_related_entities(element))
-
-        local_entities.update(new_set_to_push)
-
-        return local_entities
+        return self.__all_connections__.get(entity_id).union(related_entities)
 
     def get_queryset(self):
         # Get related entities from source
@@ -67,7 +51,17 @@ class UnconnectedEntitiesView(
         entity_uid = self.kwargs.get("uid")
         source_node = Entity.objects.get(uid=entity_uid)
 
-        user_related_entities = self.get_related_entities(source_node)
+        comparisons = list(Comparison.objects.filter(
+            poll=self.poll_from_url, user=self.request.user
+        ))
+
+        self.__all_entites__ = set(c.entity_1_id for c in comparisons).union(
+            set(c.entity_2_id for c in comparisons))
+        for c in comparisons:
+            self.__all_connections__[c.entity_1_id].add(c.entity_2_id)
+            self.__all_connections__[c.entity_2_id].add(c.entity_1_id)
+
+        user_related_entities = self.get_related_entities(source_node.id)
         user_all_entities = set()
 
         # Get all comparison for the user
@@ -81,4 +75,4 @@ class UnconnectedEntitiesView(
 
         return Entity.objects \
             .filter(id__in=(entity.id for entity in user_all_entities)) \
-            .exclude(id__in=(entity.id for entity in user_related_entities))
+            .exclude(id__in=(entity_id for entity_id in user_related_entities))
