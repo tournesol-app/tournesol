ae932a61fea96fc636c4eacf8f0de5480b487da6
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-04-01 15:52:28 +0200

    [front] feat: `presidentielle2022` feedback page now invites to add comparisons

--- frontend/src/pages/personal/feedback/FeedbackPagePresidentielle2022.tsx
+++ frontend/src/pages/personal/feedback/FeedbackPagePresidentielle2022.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { Grid, Paper, Typography } from '@mui/material';
+import { Trans, useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { Box, Button, Grid, Paper, Typography } from '@mui/material';
+import { LoaderWrapper } from 'src/components';
 import StackedCandidatesPaper from 'src/features/feedback/StackedCandidatesPaper';
 import { useCurrentPoll } from 'src/hooks/useCurrentPoll';
 import { getUserComparisons } from 'src/utils/api/comparisons';
@@ -15,13 +17,17 @@ const COMPARISONS_NBR_MAX = 100;
 
 const FeedbackPagePresidentielle2022 = () => {
   const { t } = useTranslation();
-  const { name: pollName, criterias } = useCurrentPoll();
+  const { baseUrl, criterias, name: pollName, options } = useCurrentPoll();
 
+  const [isLoading, setIsLoading] = useState(true);
   const [comparisonsNbr, setComparisonsNbr] = useState(0);
   const [ratings, setRatings] = useState<ContributorRating[]>([]);
   const [recommendations, setRecommendations] = useState<
     Array<ContributorRecommendations>
   >([]);
+
+  const minComparisons = options?.tutorialLength || 7;
+  const remainingComparisons = minComparisons - comparisonsNbr;
 
   const weightRecommendationCriteria = (criteria: PollCriteria[]) => {
     const weights = Object.fromEntries(
@@ -67,29 +73,65 @@ const FeedbackPagePresidentielle2022 = () => {
       setComparisonsNbr(comparisons.length);
       setRatings(contributorRatings);
       setRecommendations(contributorRecommendation);
+      setIsLoading(false);
     });
   }, [criterias, pollName]);
 
   return (
     <>
-      <Typography variant="h2" textAlign="center">
-        {t('myFeedbackPage.presidentielle2022.subtitle')}
-      </Typography>
-      <Typography paragraph mt={2} textAlign="justify">
-        {t('myFeedbackPage.presidentielle2022.description')}
-      </Typography>
-      <Grid container spacing={2} textAlign="center">
-        <Grid item xs={12} sm={12} md={6}>
-          <Paper sx={{ height: '300px' }}>WIP</Paper>
-        </Grid>
-        <Grid item xs={12} sm={12} md={6}>
-          <StackedCandidatesPaper
-            comparisonsNbr={comparisonsNbr}
-            ratings={ratings}
-            recommendations={recommendations}
-          />
-        </Grid>
-      </Grid>
+      <LoaderWrapper isLoading={isLoading}>
+        {comparisonsNbr < minComparisons ? (
+          <>
+            <Typography variant="h2" textAlign="center">
+              {t('myFeedbackPage.presidentielle2022.welcomeOnYourResultPage')}
+            </Typography>
+            <Typography paragraph mt={2} textAlign="justify">
+              <Trans
+                t={t}
+                i18nKey="myFeedbackPage.presidentielle2022.notEnoughComparisons"
+                count={remainingComparisons}
+              >
+                We need at least {{ minComparisons }} different comparisons
+                before being able to display your results.{' '}
+                {{ remainingComparisons }} comparisons are still required. Click
+                on the button below, and follow the instructions to continue.
+              </Trans>
+            </Typography>
+            <Box mt={2} display="flex" justifyContent="center">
+              <Button
+                variant="contained"
+                component={Link}
+                to={`${baseUrl}/comparison?series=true`}
+              >
+                {t('myFeedbackPage.presidentielle2022.continueComparisons')}
+              </Button>
+            </Box>
+          </>
+        ) : (
+          <>
+            <Typography variant="h2" textAlign="center">
+              {t(
+                'myFeedbackPage.presidentielle2022.thanksForComparingCandidates'
+              )}
+            </Typography>
+            <Typography paragraph mt={2} textAlign="justify">
+              {t('myFeedbackPage.presidentielle2022.description')}
+            </Typography>
+            <Grid container spacing={2} textAlign="center">
+              <Grid item xs={12} sm={12} md={6}>
+                <Paper sx={{ height: '300px' }}>WIP</Paper>
+              </Grid>
+              <Grid item xs={12} sm={12} md={6}>
+                <StackedCandidatesPaper
+                  comparisonsNbr={comparisonsNbr}
+                  ratings={ratings}
+                  recommendations={recommendations}
+                />
+              </Grid>
+            </Grid>
+          </>
+        )}
+      </LoaderWrapper>
     </>
   );
 };
