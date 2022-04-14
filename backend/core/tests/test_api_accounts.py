aad064b219f638c85d6621da205fb8968ec01521
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-04-14 12:24:49 +0200

    more tests

--- backend/core/tests/test_api_accounts.py
+++ backend/core/tests/test_api_accounts.py
@@ -23,11 +23,11 @@ class AccountsRegisterTestCase(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.existing_user = UserFactory(
+        self.existing_user: User = UserFactory(
             username=self._existing_username, email=self._existing_email
         )
 
-    def test_anon_cant_register_with_existing_email(self):
+    def test_register_with_already_used_email(self) -> None:
         """
         An anonymous user can't register with a variant of an email address
         already in use.
@@ -47,6 +47,7 @@ class AccountsRegisterTestCase(TestCase):
             format="json",
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("email", response.data)
         self.assertEqual(User.objects.all().count(), n_users)
 
         # a user cannot use a lower/upper variant of an email address already
@@ -63,19 +64,59 @@ class AccountsRegisterTestCase(TestCase):
             format="json",
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("email", response.data)
         self.assertEqual(User.objects.all().count(), n_users)
 
         # using a new email address is obviously valid
         valid_payload = {
             "username": self._non_existing_username,
             "email": self._non_existing_email,
-            "password": "password",
-            "password_confirm": "password",
+            "password": "uncommon_password",
+            "password_confirm": "uncommon_password",
         }
         response = self.client.post(
             "/accounts/register/",
             valid_payload,
             format="json",
         )
+
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.all().count(), n_users + 1)
+
+    def test_register_email_with_already_used_email(self) -> None:
+        """
+        An authenticated user can't register with a variant of an email
+        address already in use.
+        """
+        used_email = "also_used@example.org"
+        used_email_alt = "also_USED@example.org"
+
+        UserFactory(email=used_email)
+        self.client.force_authenticate(user=self.existing_user)
+
+        # a user cannot use an email address already in use
+        response = self.client.post(
+            "/accounts/register-email/",
+            {"email": used_email},
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("email", response.data)
+
+        # a user cannot use a lower/upper variant of an email address already
+        # in use
+        response = self.client.post(
+            "/accounts/register-email/",
+            {"email": used_email_alt},
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("email", response.data)
+
+        # using a new email address is obviously valid
+        response = self.client.post(
+            "/accounts/register-email/",
+            {"email": self._non_existing_email},
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
