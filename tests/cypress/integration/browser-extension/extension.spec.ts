007718db26987e18402d70330aef3445a2ee35ef
Adrien Matissart <a@matissart.net>
2022-04-19 16:16:51 +0200

    Fix condition for dismiss button

--- tests/cypress/integration/browser-extension/extension.spec.ts
+++ tests/cypress/integration/browser-extension/extension.spec.ts
@@ -35,7 +35,7 @@ onlyOn('headed', () => {
         cy.visit('https://www.youtube.com/watch?v=6jK9bFWE--g');
         consent();
         cy.get('body').then($body => {
-          if ($body.find("button:contains('Dismiss')") {
+          if ($body.find("button:contains('Dismiss')").length > 0) {
             // Dismiss Youtube Ad if present
             cy.contains('button', 'Dismiss', {matchCase: false}).click();
           }
