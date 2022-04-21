45e8e8ec680d0c221bc639d8705ca79d5623b478
Gresille & Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-04-21 16:55:11 +0200

    [back] feat: allow case insensitive login with email (#834)

--- tests/cypress/integration/frontend/login.ts
+++ tests/cypress/integration/frontend/login.ts
@@ -28,4 +28,17 @@ describe('Login', () => {
     cy.contains('Logout').click();
     cy.contains('Log in').should('be.visible');
   })
+
+  it('can login with lower/upper email variant and logout', () => {
+    cy.visit('/');
+    cy.contains('Log in').click();
+    cy.location('pathname').should('equal', '/login');
+    cy.focused().type('USER1@tournesol.app');
+    cy.get('input[name="password"]').click().type('tournesol').type('{enter}');
+    cy.location('pathname').should('equal', '/');
+    cy.contains('.MuiToolbar-root', 'user1').should('be.visible');
+    cy.contains('Log in').should('not.exist');
+    cy.contains('Logout').click();
+    cy.contains('Log in').should('be.visible');
+  })
 })
