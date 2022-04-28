tournesol-app/tournesol

commit aa33d80cffefc18d089d70a50e7880ee19644b7f

[front] refactor: add an `EntityList` component (#859)

frontend/src/features/recommendation/RecommendationApi.tsx
@@ -3,43 +3,23 @@ import {
   PollCriteria,
   PollsService,
 } from 'src/services/openapi';
+import { YOUTUBE_POLL_NAME } from 'src/utils/constants';
 
-export const getRecommendedVideos = async (
-  limit: number,
-  searchString: string,
-  criterias: PollCriteria[]
-): Promise<PaginatedRecommendationList> => {
-  const dayInMillisecondes = 1000 * 60 * 60 * 24;
-  const conversionTime = new Map();
-  const params = new URLSearchParams(searchString);
-  conversionTime.set('Any', 1);
-  // "Today" is 36 hours to get most of the videos from the previous day
-  conversionTime.set('Today', dayInMillisecondes * 1.5);
-  conversionTime.set('Week', dayInMillisecondes * 7);
-  conversionTime.set('Month', dayInMillisecondes * 31);
-  conversionTime.set('Year', dayInMillisecondes * 365);
-  const dateNow = Date.now();
+const getParamValueAsNumber = (
+  params: URLSearchParams,
+  key: string
+): number | undefined => {
+  const value = params.get(key);
+  return value ? Number(value) : undefined;
+};
 
-  if (params.get('date')) {
-    const date = params.get('date');
-    params.delete('date');
-    if (date != 'Any') {
-      // TODO figure out why the 1 month adding is needed here
-      const limitPublicationDateMilliseconds =
-        dateNow - conversionTime.get(date);
-      const param_date = new Date(limitPublicationDateMilliseconds);
-      const [d, m, y, H, M, S] = [
-        param_date.getDate().toString(),
-        (param_date.getMonth() + 1).toString(),
-        param_date.getFullYear().toString(),
-        param_date.getHours().toString(),
-        param_date.getMinutes().toString(),
-        param_date.getSeconds().toString(),
-      ].map((t) => format(t));
-      params.append('date_gte', `${d}-${m}-${y}-${H}-${M}-${S}`);
-    }
-  }
-
+/**
+ * Replace the key `date` of `params` by a new one compatible with the API.
+ */
+const buildDateURLParameter = (
+  pollName: string,
+  params: URLSearchParams
+): void => {
   function format(str: string) {
     if (str.length == 1) {
       return '0'.concat(str);
@@ -50,34 +30,95 @@ export const getRecommendedVideos = async (
     }
   }
 
-  const getNumberValue = (key: string): number | undefined => {
-    const value = params.get(key);
-    return value ? Number(value) : undefined;
-  };
+  if (pollName === YOUTUBE_POLL_NAME) {
+    const conversionTime = new Map();
+    const dayInMillisecondes = 1000 * 60 * 60 * 24;
 
+    conversionTime.set('Any', 1);
+    // Set today to 36 hours instead of 24; to get most of the videos from the
+    // previous day
+    conversionTime.set('Today', dayInMillisecondes * 1.5);
+    conversionTime.set('Week', dayInMillisecondes * 7);
+    conversionTime.set('Month', dayInMillisecondes * 31);
+    conversionTime.set('Year', dayInMillisecondes * 365);
+    const dateNow = Date.now();
+
+    if (params.get('date')) {
+      const date = params.get('date');
+      params.delete('date');
+      if (date != 'Any') {
+        // TODO: figure out why adding 1 month is needed here
+        const limitPublicationDateMilliseconds =
+          dateNow - conversionTime.get(date);
+        const param_date = new Date(limitPublicationDateMilliseconds);
+        const [d, m, y, H, M, S] = [
+          param_date.getDate().toString(),
+          (param_date.getMonth() + 1).toString(),
+          param_date.getFullYear().toString(),
+          param_date.getHours().toString(),
+          param_date.getMinutes().toString(),
+          param_date.getSeconds().toString(),
+        ].map((t) => format(t));
+        params.append('date_gte', `${d}-${m}-${y}-${H}-${M}-${S}`);
+      }
+    }
+  }
+};
+
+/**
+ * Return a metadata `Record` based on the URL parameters, compliant with the
+ * given poll.
+ *
+ * This `Record` is meant to be used as parameter of the generated API
+ * services.
+ */
+const getMetadataFilter = (
+  pollName: string,
+  params: URLSearchParams
+): Record<string, string | string[]> => {
   const metadata: Record<string, string | string[]> = {};
-  const languageFilter = params.get('language');
-  const uploaderFilter = params.get('uploader');
 
-  if (languageFilter) {
-    metadata['language'] = languageFilter.split(',');
+  // build a filter for the YouTube poll
+  if (pollName === YOUTUBE_POLL_NAME) {
+    const languageFilter = params.get('language');
+    const uploaderFilter = params.get('uploader');
+
+    if (languageFilter) {
+      metadata['language'] = languageFilter.split(',');
+    }
+
+    if (uploaderFilter) {
+      metadata['uploader'] = uploaderFilter;
+    }
   }
 
-  if (uploaderFilter) {
-    metadata['uploader'] = uploaderFilter;
-  }
+  return metadata;
+};
+
+/**
+ * Return the recommendations of a given poll.
+ */
+export const getRecommendations = async (
+  pollName: string,
+  limit: number,
+  searchString: string,
+  criterias: PollCriteria[]
+): Promise<PaginatedRecommendationList> => {
+  const params = new URLSearchParams(searchString);
+
+  buildDateURLParameter(pollName, params);
 
   try {
     return await PollsService.pollsRecommendationsList({
-      name: 'videos',
+      name: pollName,
       limit: limit,
-      offset: getNumberValue('offset'),
+      offset: getParamValueAsNumber(params, 'offset'),
       search: params.get('search') ?? undefined,
       dateGte: params.get('date_gte') ?? undefined,
       unsafe: params.get('unsafe') === 'true' ?? undefined,
-      metadata: metadata,
+      metadata: getMetadataFilter(pollName, params),
       weights: Object.fromEntries(
-        criterias.map((c) => [c.name, getNumberValue(c.name)])
+        criterias.map((c) => [c.name, getParamValueAsNumber(params, c.name)])
       ),
     });
   } catch (err) {

frontend/src/features/entities/EntityList.tsx
@@ -1,0 +1,80 @@
+import React from 'react';
+
+import VideoList from 'src/features/videos/VideoList';
+import { useCurrentPoll } from 'src/hooks';
+import { Recommendation } from 'src/services/openapi/models/Recommendation';
+import { YOUTUBE_POLL_NAME } from 'src/utils/constants';
+import {
+  videoFromRelatedEntity,
+  videoWithScoresFromRecommendation,
+} from 'src/utils/entity';
+import { ActionList, RelatedEntityObject, VideoObject } from 'src/utils/types';
+
+interface Props {
+  entities: RelatedEntityObject[] | Recommendation[] | undefined;
+  actions?: ActionList;
+  settings?: ActionList;
+  emptyMessage?: React.ReactNode;
+  personalScores?: { [uid: string]: number };
+}
+
+/**
+ * Display a list of entities.
+ *
+ * Entities can be pure entities, or entities expanded with data related to
+ * the current poll, like the number of comparisons, the tournesol score,
+ * etc.
+ *
+ * According to the current poll, this component renders a specific child
+ * component able to correctly display the entities.
+ *
+ * Component tree:
+ *
+ *   ParentPage
+ *   |
+ *   +-- EntityList (here)
+ *       |
+ *       +-- CandidateList
+ *       +-- VideoList
+ *       +-- etc.
+ */
+function EntityList({
+  entities,
+  actions,
+  settings = [],
+  personalScores,
+  emptyMessage,
+}: Props) {
+  const { name: pollName } = useCurrentPoll();
+
+  const fromEntitiesToVideos = (
+    entities: RelatedEntityObject[] | Recommendation[] | undefined
+  ): VideoObject[] => {
+    if (!entities) {
+      return [];
+    }
+    return entities.map((entity) => {
+      if ('tournesol_score' in entity) {
+        return videoWithScoresFromRecommendation(entity);
+      }
+
+      return videoFromRelatedEntity(entity);
+    });
+  };
+
+  if (pollName === YOUTUBE_POLL_NAME) {
+    return (
+      <VideoList
+        videos={fromEntitiesToVideos(entities)}
+        actions={actions}
+        settings={settings}
+        personalScores={personalScores}
+        emptyMessage={emptyMessage}
+      />
+    );
+  }
+
+  return <></>;
+}
+
+export default EntityList;

frontend/src/pages/recommendations/RecommendationPage.spec.tsx
@@ -26,7 +26,7 @@ describe('VideoRecommendationPage', () => {
     historySpy = jest.spyOn(history, 'replace');
     navigatorLanguagesGetter = jest.spyOn(window.navigator, 'languages', 'get');
     getRecommendedVideosSpy = jest
-      .spyOn(RecommendationApi, 'getRecommendedVideos')
+      .spyOn(RecommendationApi, 'getRecommendations')
       .mockImplementation(async () => ({ count: 0, results: [] }));
   });
   const component = () => {
@@ -49,6 +49,7 @@ describe('VideoRecommendationPage', () => {
     expect(loadRecommendationsLanguages()).toEqual('fr,en');
     expect(getRecommendedVideosSpy).toHaveBeenCalledTimes(1);
     expect(getRecommendedVideosSpy).toHaveBeenLastCalledWith(
+      'videos',
       20,
       '?language=fr%2Cen',
       expect.anything()
@@ -66,6 +67,7 @@ describe('VideoRecommendationPage', () => {
     expect(loadRecommendationsLanguages()).toEqual('de');
     expect(getRecommendedVideosSpy).toHaveBeenCalledTimes(1);
     expect(getRecommendedVideosSpy).toHaveBeenLastCalledWith(
+      'videos',
       20,
       '?language=de',
       expect.anything()
@@ -82,6 +84,7 @@ describe('VideoRecommendationPage', () => {
     expect(loadRecommendationsLanguages()).toEqual('de');
     expect(getRecommendedVideosSpy).toHaveBeenCalledTimes(1);
     expect(getRecommendedVideosSpy).toHaveBeenLastCalledWith(
+      'videos',
       20,
       '?language=fr',
       expect.anything()

frontend/src/pages/recommendations/RecommendationPage.tsx
@@ -3,30 +3,27 @@ import { useLocation, useHistory } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { Box } from '@mui/material';
 
-import type {
-  PaginatedRecommendationList,
-  Recommendation,
-} from 'src/services/openapi';
-import Pagination from 'src/components/Pagination';
-import VideoList from 'src/features/videos/VideoList';
-import SearchFilter from 'src/features/recommendation/SearchFilter';
-import { getRecommendedVideos } from 'src/features/recommendation/RecommendationApi';
-import { ContentBox, ContentHeader } from 'src/components';
+import type { PaginatedRecommendationList } from 'src/services/openapi';
 import LoaderWrapper from 'src/components/LoaderWrapper';
+import Pagination from 'src/components/Pagination';
+import EntityList from 'src/features/entities/EntityList';
+import SearchFilter from 'src/features/recommendation/SearchFilter';
+import { getRecommendations } from 'src/features/recommendation/RecommendationApi';
+import { ContentBox, ContentHeader } from 'src/components';
+
 import {
   saveRecommendationsLanguages,
   loadRecommendationsLanguages,
   recommendationsLanguagesFromNavigator,
 } from 'src/utils/recommendationsLanguages';
 import { useCurrentPoll } from 'src/hooks/useCurrentPoll';
-import { videoWithCriteriaFromRecommendation } from 'src/utils/entity';
 
 function RecommendationsPage() {
   const { t } = useTranslation();
 
   const history = useHistory();
   const location = useLocation();
-  const { criterias } = useCurrentPoll();
+  const { name: pollName, criterias } = useCurrentPoll();
   const [isLoading, setIsLoading] = useState(true);
 
   const prov: PaginatedRecommendationList = {
@@ -75,19 +72,17 @@ function RecommendationsPage() {
     const fetchVideos = async () => {
       setIsLoading(true);
       setEntities(
-        (await getRecommendedVideos(limit, location.search, criterias)) || []
+        (await getRecommendations(
+          pollName,
+          limit,
+          location.search,
+          criterias
+        )) || []
       );
       setIsLoading(false);
     };
     fetchVideos();
-  }, [location.search, history, searchParams, criterias]);
-
-  const getResultsAsVideos = (results: Recommendation[] | undefined) => {
-    if (!results) {
-      return [];
-    }
-    return results.map((entity) => videoWithCriteriaFromRecommendation(entity));
-  };
+  }, [criterias, history, location.search, pollName, searchParams]);
 
   return (
     <>
@@ -97,8 +92,8 @@ function RecommendationsPage() {
           <SearchFilter />
         </Box>
         <LoaderWrapper isLoading={isLoading}>
-          <VideoList
-            videos={getResultsAsVideos(entities.results)}
+          <EntityList
+            entities={entities.results}
             emptyMessage={
               isLoading ? '' : t('noVideoCorrespondsToSearchCriterias')
             }

frontend/src/pages/videos/VideoRatings.tsx
@@ -10,7 +10,6 @@ import type {
 } from 'src/services/openapi';
 import type { RelatedEntityObject } from 'src/utils/types';
 import Pagination from 'src/components/Pagination';
-import VideoList from 'src/features/videos/VideoList';
 import { UsersService } from 'src/services/openapi';
 import { ContentBox, ContentHeader, LoaderWrapper } from 'src/components';
 import {
@@ -18,8 +17,8 @@ import {
   RatingsContext,
 } from 'src/features/videos/PublicStatusAction';
 import RatingsFilter from 'src/features/ratings/RatingsFilter';
-import { videoFromRelatedEntity } from 'src/utils/entity';
 import { useCurrentPoll } from 'src/hooks/useCurrentPoll';
+import EntityList from 'src/features/entities/EntityList';
 
 const NoRatingMessage = ({ hasFilter }: { hasFilter: boolean }) => {
   const { t } = useTranslation();
@@ -164,8 +163,8 @@ const VideoRatingsPage = () => {
           </Box>
         )}
         <LoaderWrapper isLoading={isLoading}>
-          <VideoList
-            videos={entities.map((ent) => videoFromRelatedEntity(ent))}
+          <EntityList
+            entities={entities}
             settings={[PublicStatusAction]}
             emptyMessage={<NoRatingMessage hasFilter={hasFilter} />}
             personalScores={personalScores}

frontend/src/utils/entity.ts
@@ -22,7 +22,7 @@ export const videoFromRelatedEntity = (entity: RelatedEntityObject): Video => {
   };
 };
 
-export const videoWithCriteriaFromRecommendation = (
+export const videoWithScoresFromRecommendation = (
   entity: Recommendation
 ): VideoSerializerWithCriteria => {
   const video = videoFromRelatedEntity(entity);

commit f004e1850d4bd29e2b123ceca0f105091baf9762

[front] fix: reco. page not displaying anything when... (#862)
...the date filter is active.

The `/polls/` API, used by the recommendations page, expects
the date to be formatted with the ISO 8601 norm. Contrary
to the legacy `/video/` which uses a custom format.

This fix a regression introduced by aa33d80.

frontend/src/features/recommendation/RecommendationApi.tsx
@@ -20,46 +20,25 @@ const buildDateURLParameter = (
   pollName: string,
   params: URLSearchParams
 ): void => {
-  function format(str: string) {
-    if (str.length == 1) {
-      return '0'.concat(str);
-    } else if (str.length == 4) {
-      return str.slice(2);
-    } else {
-      return str;
-    }
-  }
-
   if (pollName === YOUTUBE_POLL_NAME) {
     const conversionTime = new Map();
-    const dayInMillisecondes = 1000 * 60 * 60 * 24;
+    const dayInMillisecs = 1000 * 60 * 60 * 24;
 
     conversionTime.set('Any', 1);
     // Set today to 36 hours instead of 24; to get most of the videos from the
     // previous day
-    conversionTime.set('Today', dayInMillisecondes * 1.5);
-    conversionTime.set('Week', dayInMillisecondes * 7);
-    conversionTime.set('Month', dayInMillisecondes * 31);
-    conversionTime.set('Year', dayInMillisecondes * 365);
-    const dateNow = Date.now();
+    conversionTime.set('Today', dayInMillisecs * 1.5);
+    conversionTime.set('Week', dayInMillisecs * 7);
+    conversionTime.set('Month', dayInMillisecs * 31);
+    conversionTime.set('Year', dayInMillisecs * 365);
 
     if (params.get('date')) {
       const date = params.get('date');
       params.delete('date');
+
       if (date != 'Any') {
-        // TODO: figure out why adding 1 month is needed here
-        const limitPublicationDateMilliseconds =
-          dateNow - conversionTime.get(date);
-        const param_date = new Date(limitPublicationDateMilliseconds);
-        const [d, m, y, H, M, S] = [
-          param_date.getDate().toString(),
-          (param_date.getMonth() + 1).toString(),
-          param_date.getFullYear().toString(),
-          param_date.getHours().toString(),
-          param_date.getMinutes().toString(),
-          param_date.getSeconds().toString(),
-        ].map((t) => format(t));
-        params.append('date_gte', `${d}-${m}-${y}-${H}-${M}-${S}`);
+        const param_date = new Date(Date.now() - conversionTime.get(date));
+        params.append('date_gte', param_date.toISOString());
       }
     }
   }