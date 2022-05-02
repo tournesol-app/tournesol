acc5cda471e3439fc3c316e9b879faa70d783c02
Aidan <53896963+aidanjungo@users.noreply.github.com>
2022-05-02 18:06:14 +0200

    [backend] Integration of TournesolBot into the backend (#567)
    * Connection to the twitter api
    
    * Get uploader twitter account (wip)
    
    * Add requirements
    
    * Use raise error from request
    
    Co-authored-by: Adrien Matissart <amatissart@users.noreply.github.com>
    
    * Use iterator directly in set
    
    Co-authored-by: Adrien Matissart <amatissart@users.noreply.github.com>
    
    * Use youtube api to get uploader_id
    
    * Improve uploader_twitter_account and add tests
    
    * Adapt to backend refactor
    
    * Tournesolbot main functions
    
    * fix import of creds
    
    * Tests for Tournesolbot functions
    
    * Add Twitterbot credentials placeholder to Django settings
    
    * fix lint
    
    * Add Django management command
    
    * Option fix and add listing options
    
    * Improve layout or pythonicness of the code
    
    * Update backend/requirements.txt
    
    Co-authored-by: Adrien Matissart <amatissart@users.noreply.github.com>
    
    * Update backend/twitterbot/management/commands/run_twitterbot.py
    
    Co-authored-by: Adrien Matissart <amatissart@users.noreply.github.com>
    
    * remove twitterbot settings from this PR
    
    * greater or equal
    
    * replaces values_list by values
    
    * move check debug
    
    * Raise error when to few criteria
    
    * fix lint
    
    * fix test
    
    Co-authored-by: Adrien Matissart <amatissart@users.noreply.github.com>

---
+++ backend/twitterbot/management/commands/run_twitterbot.py
@@ -0,0 +1,59 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from twitterbot.tournesolbot import (
+    get_video_recommendations,
+    tweet_video_recommendation,
+)
+
+
+class Command(BaseCommand):
+    help = "Runs Tournesol Twitter Bot"
+
+    def add_arguments(self, parser):
+
+        parser.add_argument(
+            "-n",
+            "--bot-name",
+            type=str,
+            help="Name of the bot '@TournesolBot' or '@TournesolBotFR'",
+        )
+
+        # Optional argument
+        parser.add_argument(
+            "-d",
+            "--debug",
+            action="store_true",
+            default=False,
+            help="Debug mode, will ask for confirmation.",
+        )
+
+        parser.add_argument(
+            "-l",
+            "--list",
+            action="store_true",
+            default=False,
+            help="Print the list of tweetable videos.",
+        )
+
+    def handle(self, *args, **options):
+
+        bot_name = options["bot_name"]
+
+        if options["list"]:
+            language = settings.TWITTERBOT_CREDENTIALS[bot_name]["language"]
+            videos = get_video_recommendations(language)
+
+            for video in videos:
+                print(
+                    video.metadata["video_id"],
+                    video.metadata["name"],
+                    video.metadata["uploader"],
+                    video.tournesol_score,
+                )
+
+        # Temporary, to avoid to tweet anything if not in debug mode
+        if not options["debug"]:
+            return
+
+        tweet_video_recommendation(bot_name, debug=options["debug"])
---
+++ backend/twitterbot/uploader_twitter_account.py
@@ -0,0 +1,36 @@
+"""
+Function to get twitter account of the uploader of a video.
+Used for the Tournesol twitter bot.
+"""
+
+import re
+
+import requests
+
+from tournesol.utils.api_youtube import get_video_metadata
+
+
+def get_twitter_account_from_channel_id(channel_id):
+    """Get Twitter account from uploader id"""
+
+    uploader_url = f"https://www.youtube.com/channel/{channel_id}"
+
+    resp = requests.get(uploader_url, headers={"user-agent": "curl/7.68.0"})
+
+    twitter_names = re.findall(r"twitter.com\%2F(.*?)\"", resp.text)
+    twitter_names = [name for name in twitter_names if name != ""]
+
+    if len({name.lower() for name in twitter_names}) == 1:
+        return "@" + twitter_names[0]
+
+    print("Error getting the uploader id, Twitter account not found")
+    return None
+
+
+def get_twitter_account_from_video_id(video_id):
+    """Get Twitter account from video id"""
+
+    metadata = get_video_metadata(video_id, compute_language=False)
+    channel_id = metadata["channel_id"]
+
+    return get_twitter_account_from_channel_id(channel_id)
