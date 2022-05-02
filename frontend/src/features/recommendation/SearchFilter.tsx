aeb4e6efc66d7799ca7b52975f314f15795a638e
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-05-02 15:58:54 +0200

    do not display video search filters for candidates

--- frontend/src/features/recommendation/SearchFilter.tsx
+++ frontend/src/features/recommendation/SearchFilter.tsx
@@ -2,7 +2,7 @@ import React, { useState, useCallback } from 'react';
 import { Collapse, Grid, Box } from '@mui/material';
 
 import { CollapseButton } from 'src/components';
-import { useListFilter } from 'src/hooks';
+import { useCurrentPoll, useListFilter } from 'src/hooks';
 import LanguageFilter from './LanguageFilter';
 import DateFilter from './DateFilter';
 import CriteriaFilter from './CriteriaFilter';
@@ -11,6 +11,7 @@ import AdvancedFilter from './AdvancedFilter';
 import {
   recommendationFilters,
   defaultRecommendationFilters,
+  YOUTUBE_POLL_NAME,
 } from 'src/utils/constants';
 import { saveRecommendationsLanguages } from 'src/utils/recommendationsLanguages';
 
@@ -24,6 +25,8 @@ import { saveRecommendationsLanguages } from 'src/utils/recommendationsLanguages
 function SearchFilter() {
   const [expanded, setExpanded] = useState(false);
   const [filterParams, setFilter] = useListFilter({ setEmptyValues: true });
+
+  const { name: pollName } = useCurrentPoll();
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
@@ -60,24 +63,42 @@ function SearchFilter() {
           </Box>
         )}
         <Grid container spacing={4} sx={{ marginBottom: '8px' }}>
-          <Grid item xs={6} md={3} lg={2} data-testid="search-date-safe-filter">
-            <DateFilter
-              value={filterParams.get(recommendationFilters.date) ?? ''}
-              onChange={(value) => setFilter(recommendationFilters.date, value)}
-            />
-            <Box mt={2}>
-              <AdvancedFilter
-                value={filterParams.get('unsafe') ?? ''}
-                onChange={(value) => setFilter('unsafe', value)}
-              />
-            </Box>
-          </Grid>
-          <Grid item xs={6} md={3} lg={2} data-testid="search-language-filter">
-            <LanguageFilter
-              value={filterParams.get(recommendationFilters.language) ?? ''}
-              onChange={handleLanguageChange}
-            />
-          </Grid>
+          {pollName === YOUTUBE_POLL_NAME && (
+            <>
+              <Grid
+                item
+                xs={6}
+                md={3}
+                lg={2}
+                data-testid="search-date-safe-filter"
+              >
+                <DateFilter
+                  value={filterParams.get(recommendationFilters.date) ?? ''}
+                  onChange={(value) =>
+                    setFilter(recommendationFilters.date, value)
+                  }
+                />
+                <Box mt={2}>
+                  <AdvancedFilter
+                    value={filterParams.get('unsafe') ?? ''}
+                    onChange={(value) => setFilter('unsafe', value)}
+                  />
+                </Box>
+              </Grid>
+              <Grid
+                item
+                xs={6}
+                md={3}
+                lg={2}
+                data-testid="search-language-filter"
+              >
+                <LanguageFilter
+                  value={filterParams.get(recommendationFilters.language) ?? ''}
+                  onChange={handleLanguageChange}
+                />
+              </Grid>
+            </>
+          )}
           <Grid item xs={12} sm={12} md={6}>
             <CriteriaFilter setFilter={setFilter} />
           </Grid>
