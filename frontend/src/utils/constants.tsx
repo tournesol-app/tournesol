9b83cb745c887a784f478a6c2331b9b83d08b946
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-05-02 15:36:53 +0200

    add missing config

--- frontend/src/utils/constants.tsx
+++ frontend/src/utils/constants.tsx
@@ -5,6 +5,11 @@ import {
   getAllCandidates,
   getTutorialDialogs,
 } from './polls/presidentielle2022';
+import {
+  AddToRateLaterList,
+  AnalysisPageLink,
+  CompareNowAction,
+} from './action';
 
 export const YOUTUBE_POLL_NAME = 'videos';
 export const PRESIDENTIELLE_2022_POLL_NAME = 'presidentielle2022';
@@ -182,13 +187,11 @@ export const polls: Array<SelectablePoll> = [
     ? [
         {
           name: PRESIDENTIELLE_2022_POLL_NAME,
+          defaultAuthEntityActions: [CompareNowAction],
+          defaultAnonEntityActions: [],
           displayOrder: 20,
           path: '/presidentielle2022/',
-          disabledRouteIds: [
-            RouteID.Recommendations,
-            RouteID.MyRateLaterList,
-            RouteID.MyComparedItems,
-          ],
+          disabledRouteIds: [RouteID.MyRateLaterList, RouteID.MyComparedItems],
           iconComponent: HowToVote,
           withSearchBar: false,
           topBarBackground:
@@ -201,6 +204,12 @@ export const polls: Array<SelectablePoll> = [
     : []),
   {
     name: YOUTUBE_POLL_NAME,
+    defaultAuthEntityActions: [
+      CompareNowAction,
+      AddToRateLaterList,
+      AnalysisPageLink,
+    ],
+    defaultAnonEntityActions: [AnalysisPageLink],
     displayOrder: 10,
     path: '/',
     disabledRouteIds: [RouteID.MyFeedback],
