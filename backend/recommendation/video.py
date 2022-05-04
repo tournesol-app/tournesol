dac732722256449db414c61b24368d241d915982
Marc Gay-Balmaz <marc.gay-balmaz@epfl.ch>
2022-05-04 14:08:07 +0200

    [Back] add: Completed methods of class graph, missing db interaction and created global recommender class
    Still to think of : handling of the videos not present in the user specific graphs

--- backend/recommendation/video.py
+++ backend/recommendation/video.py
@@ -1,16 +1,19 @@
+from __future__ import annotations
+
 from functools import total_ordering
 
 
 @total_ordering
 class Video:
     yt_url = ""
-    v1_score = 0
-    v2_score = 0
+    v1_score: float = 0
+    v2_score: dict[Video, float] = {}
+    beta: dict[Video, float] = []
     estimated_information_gains = []
     estimated_related_preferences = []
 
-    def __eq__(self, __o: object) -> bool:
-        return __o.__class__ == self.__class__ and __o.yt_url == self.yt_url
+    def __eq__(self, __o: Video) -> bool:
+        return __o.yt_url == self.yt_url
 
     def __gt__(self, obj):
         return self.v2_score > obj.v2_score
@@ -21,4 +24,4 @@ class Video:
     def __repr__(self) -> str:
         return "Video " + self.yt_url + \
                " with score v1 " + str(self.v1_score) + \
-               " and score v2 " + str(self.v2_score)
+               " and scores v2 " + str(self.v2_score)
