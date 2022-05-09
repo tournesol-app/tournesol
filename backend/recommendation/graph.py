17b196dde5117949013db31a81032bb3718a82ad
Marc Gay-Balmaz <marc.gay-balmaz@epfl.ch>
2022-05-09 20:52:29 +0200

    Added way to sort depending on the first selected video

--- backend/recommendation/graph.py
+++ backend/recommendation/graph.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from core.models import User
+from recommendation.recomended_user import User
 from recommendation.video import Video
 import numpy as np
 
@@ -18,7 +18,7 @@ class Graph:
     LAMBDA_THRESHOLD = 0.5
     MIN_SCALING_ACCURACY = 0.5
 
-    ABSENT_NODE = Video()
+    ABSENT_NODE = Video(None)
 
     adjacency_matrix: np.array
     normalized_adjacency_matrix: np.array
@@ -27,14 +27,16 @@ class Graph:
     similarity_matrix: np.array
 
     @property
-    def nodes(self):
-        return self.nodes
+    def nodes(self) -> list[Video]:
+        return self._nodes
 
-    def __init__(self):
+    def __init__(self, local_user):
+        self.video_comparison_reference: Video
         self.dirty = True
         # init absent node values
         self.ABSENT_NODE.v1_score = -1
         self.ABSENT_NODE.estimated_information_gains = 1
+        self.local_user = local_user
 
     def add_node(self, new_node):
         if new_node not in self.nodes:
@@ -48,7 +50,12 @@ class Graph:
             print("Warning ! unknown node added in edge")
         if node_b not in self.nodes:
             print("Warning ! unknown node added in edge")
-        
+
+        node_b.nb_comparison_with[node_a] += 1
+        node_a.nb_comparison_with[node_b] += 1
+        node_a.comparison_nb += 1
+        node_b.comparison_nb += 1
+
         if node_a <= node_b:
             self.edges.append((node_a, node_b))
             self.graph[node_a].append(node_b)
@@ -68,6 +75,11 @@ class Graph:
             v_index = self.nodes.index(v)
             self.adjacency_matrix[u_index][v_index] = 1
             self.adjacency_matrix[v_index][u_index] = 1
+        for u, v in self.edges:
+            u_index = self.nodes.index(u)
+            v_index = self.nodes.index(v)
+            self.normalized_adjacency_matrix[u_index][v_index] = 1 / sum(self.adjacency_matrix[u_index])
+            self.normalized_adjacency_matrix[v_index][u_index] = 1 / sum(self.adjacency_matrix[u_index])
 
     def build_distance_matrix(self):
         # Compute sigma here
@@ -87,7 +99,7 @@ class Graph:
 
         act_root = starting_node
         depth = 0
-        
+
         while len(unvisited) > 0:
             for act_vid in waiting_for_visit:
                 # act_vid = waiting_for_visit.pop()
@@ -119,7 +131,7 @@ class Graph:
         waiting_for_visit.append(unvisited.pop())
         result: set[Graph] = set()
         act_graph = Graph()
-        
+
         while len(unvisited) > 0:
             for act_vid in waiting_for_visit:
                 # act_vid = waiting_for_visit.pop()
@@ -130,7 +142,7 @@ class Graph:
                     act_graph.add_edge(act_vid, v)
                     if v not in visited and v not in waiting_for_visit:
                         future_visits.add(v)
-            
+
             if len(future_visits) == 0:
                 result.add(act_graph)
                 act_graph = Graph()
@@ -146,8 +158,8 @@ class Graph:
     def build_similarity_matrix(self):
         for i, u in enumerate(self.nodes):
             for j, v in enumerate(self.nodes):
-                self.similarity_matrix[i][j] = np.e ** ((self.distance_matrix[i, j])**2/self.sigma**2)
-    
+                self.similarity_matrix[i][j] = np.e ** ((self.distance_matrix[i, j]) ** 2 / self.sigma ** 2)
+
     def compute_offline_parameters(self, scaling_factor_increasing_videos):
         if self.dirty:
             self.dirty = False
@@ -159,7 +171,9 @@ class Graph:
     def compute_information_gain(self, scaling_factor_increasing_videos):
         # First try to increase the scaling accuracy of the user if necessary
         # TODO Use here data from db
-        if self.local_user.scaling_accuray < self.MIN_SCALING_ACCURACY:
+        # Contributor_scaling + translation -> yes
+        user = self.local_user
+        if user.scaling_accuracy * user.mean_score + user.translation_accuracy < self.MIN_SCALING_ACCURACY:
             for v in self.nodes:
                 for u in self.nodes:
                     if v in scaling_factor_increasing_videos and u in scaling_factor_increasing_videos:
@@ -187,17 +201,23 @@ class Graph:
                         else:
                             v.v2_score[u] = 0
                         # TODO Use here data from db
-                        v.beta[u] += (self.local_user.delta_sigma[u] + self.local_user.delta_sigma[v]/(self.local_user.sigma[u] - self.local_user.sigma[v] + 1))
+                        # from rating uncertainty
+                        # Attention, filtre par poll + critere (1er critere pour le principal)
+                        v.beta[u] += (self.local_user.delta_theta[u] + self.local_user.delta_theta[v] /
+                                      (self.local_user.theta[u] - self.local_user.theta[v] + 1))
                         if max_beta < v.beta[u]:
                             max_beta = v.beta[u]
                 for u in sg.nodes:
                     for v in self.nodes:
-                        v.v2_score[u] += v.beta[u]/max_beta
+                        v.v2_score[u] += v.beta[u] / max_beta
 
     # This doesn't depend on the user -> not done here, well actually yes but not used in most of the graphs
     def update_preferences(self):
         for v in self.nodes:
             for u in set(self.graph[v]):
-                v.estimated_related_preferences[u] = 0
+                v.nb_comparison_with[u] = 0
             for u in self.graph[v]:
-                v.estimated_related_preferences[u] += 1
+                v.nb_comparison_with[u] += 1
+
+    def prepare_for_sorting(self, first_video_id: str = ""):
+        self.video_comparison_reference.uid = first_video_id
