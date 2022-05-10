5d158712fbb7814c60b48fb60d954d76de7cf6a4
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-05-10 10:19:53 +0200

    [front] feat: `CandidateAnalysisPage` now displays real data

--- frontend/src/pages/entities/CandidateAnalysisPage.tsx
+++ frontend/src/pages/entities/CandidateAnalysisPage.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Box, Container, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { Link as RouterLink } from 'react-router-dom';
+
+import { Box, Button, Container, Grid, Paper } from '@mui/material';
+
+import EntityImagery from 'src/components/entity/EntityImagery';
+import EntityCardTitle from 'src/components/entity/EntityCardTitle';
+import EntityCardScores from 'src/components/entity/EntityCardScores';
+import { useCurrentPoll } from 'src/hooks/useCurrentPoll';
 import { Recommendation } from 'src/services/openapi';
 
 interface Props {
@@ -7,10 +15,39 @@ interface Props {
 }
 
 const CandidateAnalysisPage = ({ entity }: Props) => {
+  const { t } = useTranslation();
+  const { baseUrl } = useCurrentPoll();
+
   return (
     <Container>
       <Box py={2}>
-        <Typography variant="h3">{entity.metadata.name}</Typography>
+        <Box display="flex" justifyContent="flex-end">
+          <Button
+            color="secondary"
+            variant="outlined"
+            component={RouterLink}
+            to={`${baseUrl}/recommendations`}
+          >
+            {t('entityAnalysisPage.candidate.goToGlobalRanking')}
+          </Button>
+        </Box>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <EntityImagery entity={entity} compact={true} />
+          </Grid>
+          <Grid item xs={12}>
+            <Paper>
+              <Box p={1}>
+                <EntityCardTitle title={entity.metadata.name} compact={false} />
+                <EntityCardScores
+                  entity={entity}
+                  showTournesolScore={false}
+                  showTotalScore={true}
+                />
+              </Box>
+            </Paper>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
