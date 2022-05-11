94f3f5e88957ae016f605b9e8a2fac2275227d76
Michaël Witrant <mike@lepton.fr>
2022-05-11 15:38:41 +0200

    [front] feat: add criteria scores distribution chart (#881)
    Co-authored-by: Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>

---
+++ frontend/src/features/charts/CriteriaScoresDistribution.tsx
@@ -0,0 +1,143 @@
+import React, { useEffect, useState, useMemo, useCallback } from 'react';
+import { useTranslation, TFunction } from 'react-i18next';
+import {
+  Bar,
+  BarChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  CartesianGrid,
+} from 'recharts';
+
+import { Box } from '@mui/material';
+
+import CriteriaSelector from 'src/features/criteria/CriteriaSelector';
+import { useCurrentPoll } from 'src/hooks';
+import { PollsService, CriteriaDistributionScore } from 'src/services/openapi';
+
+const displayScore = (score: number) => (10 * score).toFixed(0);
+
+const binLabel = (index: number, bins: number[], t: TFunction) => {
+  if (index === 0)
+    return t('criteriaScoresDistribution.lessThan', {
+      score: displayScore(bins[1]),
+    });
+  if (index > 0 && index < bins.length - 2)
+    return t('criteriaScoresDistribution.fromTo', {
+      from: displayScore(bins[index]),
+      to: displayScore(bins[index + 1]),
+    });
+  if (index === bins.length - 2)
+    return t('criteriaScoresDistribution.greaterThan', {
+      score: displayScore(bins[bins.length - 2]),
+    });
+};
+
+const CriteriaScoresDistributionChart = ({
+  criteriaDistributionScore,
+}: {
+  criteriaDistributionScore: CriteriaDistributionScore;
+}) => {
+  const { t } = useTranslation();
+  const { bins, distribution } = criteriaDistributionScore;
+
+  const data = useMemo(
+    () =>
+      distribution.map((value, index) => ({
+        value,
+        label: binLabel(index, bins, t),
+      })),
+    [distribution, bins, t]
+  );
+
+  const tooltipFormatter = useCallback(
+    (value: number) => [value, t('criteriaScoresDistribution.label')],
+    [t]
+  );
+
+  return (
+    <ResponsiveContainer width="100%" height={360}>
+      <BarChart width={430} height={360} data={data}>
+        <CartesianGrid strokeDasharray="4 4" />
+        <XAxis
+          label={{
+            value: t('criteriaScoresDistribution.scores'),
+            position: 'insideBottom',
+            offset: -4,
+            textAnchor: 'middle',
+          }}
+          dataKey="label"
+        />
+        <YAxis
+          label={{
+            value: t('criteriaScoresDistribution.numberOfRatings'),
+            angle: -90,
+            position: 'insideLeft',
+            textAnchor: 'middle',
+          }}
+        />
+        <Tooltip formatter={tooltipFormatter} />
+        <Bar dataKey="value" fill="#1282b2" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+interface CriteriaScoresDistributionProps {
+  uid: string;
+}
+
+const CriteriaScoresDistribution = ({
+  uid,
+}: CriteriaScoresDistributionProps) => {
+  const { name: pollName, options } = useCurrentPoll();
+
+  const mainCriterionName = options?.mainCriterionName || '';
+  const [selectedCriteria, setSelectedCriteria] =
+    useState<string>(mainCriterionName);
+
+  const [criteriaScoresDistribution, setCriteriaScoresDistribution] = useState<
+    CriteriaDistributionScore[]
+  >([]);
+
+  useEffect(() => {
+    const getDistribution = async () => {
+      const result =
+        await PollsService.pollsEntitiesCriteriaScoresDistributionsRetrieve({
+          name: pollName,
+          uid,
+        });
+      setCriteriaScoresDistribution(result.criteria_scores_distributions);
+    };
+    getDistribution();
+  }, [uid, pollName]);
+
+  const criteriaDistributionScore = useMemo(
+    () =>
+      criteriaScoresDistribution.find(
+        ({ criteria }) => criteria === selectedCriteria
+      ),
+    [selectedCriteria, criteriaScoresDistribution]
+  );
+
+  return (
+    <>
+      <Box px={2} pt={1} pb={1}>
+        <CriteriaSelector
+          criteria={selectedCriteria}
+          setCriteria={setSelectedCriteria}
+        />
+      </Box>
+      {criteriaDistributionScore && (
+        <Box py={1}>
+          <CriteriaScoresDistributionChart
+            criteriaDistributionScore={criteriaDistributionScore}
+          />
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default CriteriaScoresDistribution;
--- frontend/src/pages/videos/VideoAnalysisPage.tsx
+++ frontend/src/pages/videos/VideoAnalysisPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import { Box, Grid, Paper, Typography } from '@mui/material';
 
 import Card from 'src/components/Card';
 import VideoCard from 'src/features/videos/VideoCard';
@@ -9,12 +8,16 @@ import { useVideoMetadata } from 'src/features/videos/VideoApi';
 import CriteriaBarChart from 'src/components/CriteriaBarChart';
 import { VideoPlayer } from 'src/components/entity/EntityImagery';
 import { VideoSerializerWithCriteria } from 'src/services/openapi';
+import { useTranslation } from 'react-i18next';
+import CriteriaScoresDistribution from 'src/features/charts/CriteriaScoresDistribution';
 
 export const VideoAnalysis = ({
   video,
 }: {
   video: VideoSerializerWithCriteria;
 }) => {
+  const { t } = useTranslation();
+  const entityId = `yt:${video.video_id}`;
   return (
     <Box display="flex" justifyContent="center">
       <Grid
@@ -38,6 +41,23 @@ export const VideoAnalysis = ({
           <Card>
             <CriteriaBarChart video={video} />
           </Card>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <Paper>
+            <Box
+              p={1}
+              bgcolor="rgb(238, 238, 238)"
+              display="flex"
+              justifyContent="center"
+            >
+              <Typography variant="h5">
+                {t('criteriaScoresDistribution.title')}
+              </Typography>
+            </Box>
+            <Box p={1}>
+              <CriteriaScoresDistribution uid={entityId} />
+            </Box>
+          </Paper>
         </Grid>
       </Grid>
     </Box>
