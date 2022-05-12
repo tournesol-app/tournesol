94d5664ae949bcb866535c0bc5aa27aa0cbf5f32
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-05-12 11:26:53 +0200

    [front] refactor: sync' the style of candidate & video analysis...
    ...also add a call to action button at the top right corner, to invite
    people to compare : )

--- frontend/src/pages/videos/VideoAnalysisPage.tsx
+++ frontend/src/pages/videos/VideoAnalysisPage.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
+import { useParams, Link as RouterLink } from 'react-router-dom';
 
-import { Box, Grid, Paper, Typography } from '@mui/material';
+import { Box, Button, Container, Grid, Paper, Typography } from '@mui/material';
+import CompareIcon from '@mui/icons-material/Compare';
 
 import CriteriaBarChart from 'src/components/CriteriaBarChart';
 import { VideoPlayer } from 'src/components/entity/EntityImagery';
 import CriteriaScoresDistribution from 'src/features/charts/CriteriaScoresDistribution';
 import { useVideoMetadata } from 'src/features/videos/VideoApi';
 import VideoCard from 'src/features/videos/VideoCard';
-import { useLoginState } from 'src/hooks';
+import { useCurrentPoll, useLoginState } from 'src/hooks';
 import { VideoSerializerWithCriteria } from 'src/services/openapi';
 import { PersonalCriteriaScoresContextProvider } from 'src/hooks/usePersonalCriteriaScores';
 import PersonalScoreCheckbox from 'src/components/PersonalScoreCheckbox';
@@ -21,31 +22,40 @@ export const VideoAnalysis = ({
   video: VideoSerializerWithCriteria;
 }) => {
   const { t } = useTranslation();
-  const { uid } = video;
+  const { baseUrl } = useCurrentPoll();
+
+  const entityId = `yt:${video.video_id}`;
   const actions = useLoginState() ? [CompareNowAction, AddToRateLaterList] : [];
 
   const { criteria_scores: criteriaScores } = video;
   const shouldDisplayCharts = criteriaScores && criteriaScores.length > 0;
 
   return (
-    <Box display="flex" justifyContent="center">
-      <Grid
-        container
-        spacing={2}
-        justifyContent="center"
-        maxWidth="md"
-        sx={{ marginTop: 3, marginBottom: 3 }}
-      >
-        <Grid item xs={12} sx={{ aspectRatio: '16 / 9' }}>
-          <VideoPlayer
-            videoId={video.video_id}
-            duration={video.duration}
-            controls
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <VideoCard video={video} actions={actions} showPlayer={false} />
-        </Grid>
+    <Container>
+      <Box py={2}>
+        {/* Top level section, containing links and maybe more in the future. */}
+        <Box mb={2} display="flex" justifyContent="flex-end">
+          <Button
+            color="secondary"
+            variant="contained"
+            endIcon={<CompareIcon />}
+            component={RouterLink}
+            to={`${baseUrl}/comparison?uidA=${entityId}`}
+          >
+            {t('entityAnalysisPage.generic.compare')}
+          </Button>
+        </Box>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={12} sm={12} md={10} sx={{ aspectRatio: '16 / 9' }}>
+            <VideoPlayer
+              videoId={video.video_id}
+              duration={video.duration}
+              controls
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <VideoCard video={video} actions={actions} showPlayer={false} />
+          </Grid>
 
         {/* data visualization */}
         {shouldDisplayCharts && (
