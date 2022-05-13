96cb41a9963a3d69da808af255bcac2ef2c737aa
Michaël Witrant <mike@lepton.fr>
2022-05-13 18:06:05 +0200

    Add checkbox to display personal criteria scores (#828)
    * Add checkbox to display personal scores in charts
    
    * Improve radar chart colors
    
    * align the style of the bar chart with the distribution chart
    
    + more inclusive translation
    
    * Fix typo
    
    * Describe useCriteriaChartData return values
    
    * Change useCriteriaChartData return type name
    
    * Make personal criteria non video specific
    
    * Remove unused type
    
    Co-authored-by: Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>

--- frontend/src/pages/videos/VideoAnalysisPage.tsx
+++ frontend/src/pages/videos/VideoAnalysisPage.tsx
@@ -11,6 +11,8 @@ import { useVideoMetadata } from 'src/features/videos/VideoApi';
 import VideoCard from 'src/features/videos/VideoCard';
 import { useLoginState } from 'src/hooks';
 import { VideoSerializerWithCriteria } from 'src/services/openapi';
+import { PersonalCriteriaScoresContextProvider } from 'src/hooks/usePersonalCriteriaScores';
+import PersonalScoreCheckbox from 'src/components/PersonalScoreCheckbox';
 import { CompareNowAction, AddToRateLaterList } from 'src/utils/action';
 
 export const VideoAnalysis = ({
@@ -19,8 +21,11 @@ export const VideoAnalysis = ({
   video: VideoSerializerWithCriteria;
 }) => {
   const { t } = useTranslation();
-  const entityId = `yt:${video.video_id}`;
+  const { uid } = video;
   const actions = useLoginState() ? [CompareNowAction, AddToRateLaterList] : [];
+
+  const { criteria_scores: criteriaScores } = video;
+  const shouldDisplayCharts = criteriaScores && criteriaScores.length > 0;
 
   return (
     <Box display="flex" justifyContent="center">
@@ -43,40 +48,49 @@ export const VideoAnalysis = ({
         </Grid>
 
         {/* data visualization */}
-        <Grid item xs={12} sm={12} md={6}>
-          <Paper>
-            <Box
-              p={1}
-              bgcolor="rgb(238, 238, 238)"
-              display="flex"
-              justifyContent="center"
-            >
-              <Typography variant="h5">
-                {t('entityAnalysisPage.chart.criteriaScores.title')}
-              </Typography>
-            </Box>
-            <Box p={1}>
-              <CriteriaBarChart video={video} />
-            </Box>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={12} md={6}>
-          <Paper>
-            <Box
-              p={1}
-              bgcolor="rgb(238, 238, 238)"
-              display="flex"
-              justifyContent="center"
-            >
-              <Typography variant="h5">
-                {t('criteriaScoresDistribution.title')}
-              </Typography>
-            </Box>
-            <Box p={1}>
-              <CriteriaScoresDistribution uid={entityId} />
-            </Box>
-          </Paper>
-        </Grid>
+        {shouldDisplayCharts && (
+          <>
+            <Grid item xs={12} sm={12} md={6}>
+              <Paper>
+                <Box
+                  p={1}
+                  bgcolor="rgb(238, 238, 238)"
+                  display="flex"
+                  justifyContent="center"
+                >
+                  <Typography variant="h5">
+                    {t('entityAnalysisPage.chart.criteriaScores.title')}
+                  </Typography>
+                </Box>
+                <PersonalCriteriaScoresContextProvider uid={uid}>
+                  <Box px={2} pt={1}>
+                    <PersonalScoreCheckbox />
+                  </Box>
+                  <Box p={1}>
+                    <CriteriaBarChart video={video} />
+                  </Box>
+                </PersonalCriteriaScoresContextProvider>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <Paper>
+                <Box
+                  p={1}
+                  bgcolor="rgb(238, 238, 238)"
+                  display="flex"
+                  justifyContent="center"
+                >
+                  <Typography variant="h5">
+                    {t('criteriaScoresDistribution.title')}
+                  </Typography>
+                </Box>
+                <Box p={1}>
+                  <CriteriaScoresDistribution uid={uid} />
+                </Box>
+              </Paper>
+            </Grid>
+          </>
+        )}
       </Grid>
     </Box>
   );
