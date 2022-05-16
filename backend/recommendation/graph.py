5ef43e029ee8548133a25bada2e3d87354f8431e
Marc Gay-Balmaz <marc.gay-balmaz@epfl.ch>
2022-05-16 19:38:21 +0200

    Fixed style to improve lint score

---
+++ backend/recommendation/graph.py
@@ -0,0 +1,259 @@
+from __future__ import annotations
+
+from typing import Optional
+
+import numpy as np
+from django.db.models import QuerySet, Avg
+from recommendation.recomended_user import User
+from recommendation.video import Video
+from tournesol.models import ContributorScaling, EntityCriteriaScore
+from tournesol.models import Poll, ContributorRatingCriteriaScore
+
+
+class Graph:
+    # not necessarily the best data structure,
+    # I still have to find out what is the best one between the two
+    edges: list[tuple[Video, Video]] = []
+    _nodes: list[Video]
+    graph: dict[Video, list[Video]] = {}
+    sigma: float
+    dirty: bool
+    _local_user: User
+    local_user_scaling: ContributorScaling
+    _local_poll: Poll
+
+    LAMBDA_THRESHOLD = 0.5
+    MIN_SCALING_ACCURACY = 0.5
+
+    ABSENT_NODE = Video(None)
+
+    adjacency_matrix: np.array
+    normalized_adjacency_matrix: np.array
+
+    distance_matrix: np.array
+    similarity_matrix: np.array
+
+    @property
+    def nodes(self) -> list[Video]:
+        return self._nodes
+
+    def __init__(self, local_user: Optional[User], local_poll: Poll, local_criteria):
+        self.local_user_mean: float = 0
+        self.video_comparison_reference = Video(None)
+        self.dirty = True
+        # init absent node values
+        self.ABSENT_NODE.v1_score = -1
+        self.ABSENT_NODE.estimated_information_gains = 0.75
+        self.NEW_NODE_CONNECTION_SCORE = 0.5
+        self._local_user = local_user
+        self._nodes = []
+        self._local_poll = local_poll
+        self._local_criteria = local_criteria
+
+    def add_node(self, new_node):
+        if new_node not in self.nodes:
+            self.nodes.append(new_node)
+            self.graph[new_node] = []
+            for n in self._nodes:
+                n.nb_comparison_with[new_node.uid] = 0
+                new_node.nb_comparison_with[n.uid] = 0
+        else:
+            print("Warning, trying to insert already present node")
+
+    def add_edge(self, node_a: Video, node_b: Video):
+        if node_a not in self._nodes:
+            print("Warning ! unknown node added in edge")
+        if node_b not in self._nodes:
+            print("Warning ! unknown node added in edge")
+
+        node_b.nb_comparison_with[node_a.uid] += 1
+        node_a.nb_comparison_with[node_b.uid] += 1
+        node_a.comparison_nb += 1
+        node_b.comparison_nb += 1
+
+        if node_a <= node_b:
+            self.edges.append((node_a, node_b))
+            self.graph[node_a].append(node_b)
+            self.graph[node_b].append(node_a)
+        else:
+            self.edges.append((node_b, node_a))
+            self.graph[node_a].append(node_b)
+            self.graph[node_b].append(node_a)
+
+        self.dirty = True
+
+    def build_adjacency_matrix(self):
+        self.adjacency_matrix = np.zeroes(len(self.nodes))
+
+        for u, v in self.edges:
+            u_index = self.nodes.index(u)
+            v_index = self.nodes.index(v)
+            self.adjacency_matrix[u_index][v_index] = 1
+            self.adjacency_matrix[v_index][u_index] = 1
+        for u, v in self.edges:
+            u_index = self.nodes.index(u)
+            v_index = self.nodes.index(v)
+            self.normalized_adjacency_matrix[u_index][v_index] = \
+                1 / sum(self.adjacency_matrix[u_index])
+            self.normalized_adjacency_matrix[v_index][u_index] = \
+                1 / sum(self.adjacency_matrix[u_index])
+
+    def build_distance_matrix(self):
+        # Compute sigma here
+        self.sigma = 1
+        self.distance_matrix = np.zeroes(len(self.nodes))
+        total_max_dist = 0
+        for i, v in enumerate(self.nodes):
+            self.bfs(v, give_distances=True, on_all_sub_graphs=False)
+            total_max_dist += max(self.distance_matrix[i])
+        self.sigma = total_max_dist / len(self.nodes)
+
+    def bfs(self, starting_node, give_distances=True, on_all_sub_graphs=False):
+        visited: list[Video] = []
+        waiting_for_visit: list[Video] = [starting_node]
+        future_visits: set[Video] = set()
+        unvisited: list[Video] = self.nodes.copy()
+
+        act_root = starting_node
+        depth = 0
+
+        while len(unvisited) > 0:
+            for act_vid in waiting_for_visit:
+                # act_vid = waiting_for_visit.pop()
+                visited.append(act_vid)
+                unvisited.remove(act_vid)
+                if give_distances:
+                    self.distance_matrix[act_root][act_vid] = depth
+                    self.distance_matrix[act_vid][act_root] = depth
+                for v in self.graph[act_vid]:
+                    if v not in visited and v not in waiting_for_visit:
+                        future_visits.add(v)
+            if len(future_visits) == 0:
+                if on_all_sub_graphs:
+                    act_root = unvisited.pop()
+                    future_visits.add(act_root)
+                    depth = 0
+                else:
+                    break
+            else:
+                depth += 1
+            waiting_for_visit = list(future_visits)
+
+    def find_connex_subgraphs(self) -> set[Graph]:
+        visited: list[Video] = []
+        waiting_for_visit: list[Video] = []
+        future_visits: set[Video] = set()
+        unvisited: list[Video] = self.nodes.copy()
+
+        waiting_for_visit.append(unvisited.pop())
+        result: set[Graph] = set()
+        act_graph = Graph(self._local_user, self._local_poll, self._local_criteria)
+
+        while len(unvisited) > 0:
+            for act_vid in waiting_for_visit:
+                # act_vid = waiting_for_visit.pop()
+                visited.append(act_vid)
+                unvisited.remove(act_vid)
+                act_graph.add_node(act_vid)
+                for v in self.graph[act_vid]:
+                    act_graph.add_edge(act_vid, v)
+                    if v not in visited and v not in waiting_for_visit:
+                        future_visits.add(v)
+
+            if len(future_visits) == 0:
+                result.add(act_graph)
+                act_graph = Graph(self._local_user, self._local_poll, self._local_criteria)
+                future_visits.add(unvisited.pop())
+            waiting_for_visit = list(future_visits)
+
+        result.add(act_graph)
+        return result
+
+    def is_connex(self) -> bool:
+        return len(self.find_connex_subgraphs()) == 1
+
+    def build_similarity_matrix(self):
+        for i, u in enumerate(self.nodes):
+            for j, v in enumerate(self.nodes):
+                exponent = (self.distance_matrix[i, j]) ** 2 / self.sigma ** 2
+                self.similarity_matrix[i][j] = np.e ** exponent
+
+    def compute_offline_parameters(self, scaling_factor_increasing_videos: list[Video]):
+        if self.dirty and self._local_user is not None:
+            self.dirty = False
+            self.build_adjacency_matrix()
+            self.build_distance_matrix()
+            self.build_similarity_matrix()
+            self.compute_information_gain(scaling_factor_increasing_videos)
+            self.local_user_scaling = ContributorScaling.objects.filter(user=self._local_user)[0]
+            self.local_user_mean = ContributorRatingCriteriaScore.objects \
+                .filter(contributor_rating__user=self._local_user) \
+                .filter(contributor_rating__poll_name=self._local_poll.name) \
+                .aggregate(mean=Avg('score'))['mean']
+        elif self._local_user is None:
+            entity_criteria_scores: QuerySet = EntityCriteriaScore.objects \
+                .filter(comparison__poll__name=self._local_poll.name) \
+                .filter(criteria=self._local_criteria)
+            for ecs in entity_criteria_scores:
+                act_vid = self._nodes[self._nodes.index(ecs.entity.uid)]
+                act_vid.v1_score = self.NEW_NODE_CONNECTION_SCORE + ecs.uncertainty
+                for n in self._nodes:
+                    act_vid.v2_score[n] = self.NEW_NODE_CONNECTION_SCORE + ecs.uncertainty
+
+    def compute_information_gain(self, scaling_factor_increasing_videos: list[Video]):
+        # First try to increase the scaling accuracy of the user if necessary
+        user = self._local_user
+        scale_uncertainty = self.local_user_scaling.scale_uncertainty
+        translation_uncertainty = self.local_user_scaling.translation_uncertainty
+
+        weighted_scaling_uncertainty = scale_uncertainty * self.local_user_mean
+        actual_scaling_accuracy = weighted_scaling_uncertainty + translation_uncertainty
+
+        if actual_scaling_accuracy < self.MIN_SCALING_ACCURACY:
+            for va in self.nodes:
+                for vb in self.nodes:
+                    if va in scaling_factor_increasing_videos and \
+                            vb in scaling_factor_increasing_videos:
+                        va.v1_score = 1
+                        va.v2_score[vb] = 1
+                    else:
+                        va.v1_score = 0
+                        va.v2_score[vb] = 0
+        # Once the scaling factor is high enough, check what video should gain
+        # information being compared by the user
+        else:
+            for sg in self.find_connex_subgraphs():
+                eigenvalues = np.linalg.eigvalsh(sg.normalized_adjacency_matrix)
+                max_beta = 0
+                for vb in sg.nodes:
+                    for va in self.nodes:
+                        # In the case the eigen value is big enough
+                        # => the graph is poorly connected,
+                        # so we should improve connexity
+                        if eigenvalues[1] > self.LAMBDA_THRESHOLD:
+                            u_index = sg.nodes.index(vb)
+                            v_index = sg.nodes.index(va)
+                            va.v2_score[vb] = sg.similarity_matrix[u_index, v_index]
+                        elif va not in sg.nodes:
+                            va.v2_score[vb] = 1
+                        else:
+                            va.v2_score[vb] = 0
+                        va.beta[vb] += (user.delta_theta[vb] + user.delta_theta[va] /
+                                        (user.theta[vb] - user.theta[va] + 1))
+                        if max_beta < va.beta[vb]:
+                            max_beta = va.beta[vb]
+                for vb in sg.nodes:
+                    for va in self.nodes:
+                        va.v2_score[vb] += va.beta[vb] / max_beta
+
+    # This doesn't depend on the user -> not done here,
+    # well actually yes but not used in most of the graphs
+    def update_preferences(self):
+        for v in self.nodes:
+            for u in set(self.graph[v]):
+                v.nb_comparison_with[u.uid] = 0
+            for u in self.graph[v]:
+                v.nb_comparison_with[u.uid] += 1
+
+    def prepare_for_sorting(self, first_video_id: str = ""):
+        self.video_comparison_reference.uid = first_video_id
---
+++ backend/recommendation/recommender.py
@@ -0,0 +1,167 @@
+from django.db.models import QuerySet
+
+from core.models import User
+from recommendation.recomended_user import User as RecommendationUser
+from recommendation.graph import Graph
+from recommendation.video import Video
+from tournesol.models import ComparisonCriteriaScore, Entity, Poll, Comparison
+
+
+class Recommender:
+    # Entity to specific video structure dictionary
+    _entity_to_video: dict[Entity, Video] = {}
+    _comparison_reference_video: Video = Video(None)
+    # Graph containing all videos and existing comparisons, used to get the video preferences
+    _complete_graph: Graph
+    # Dictionary linking a user to its comparison graph, used to get its information gain
+    _user_specific_graphs: dict[User, Graph] = {}
+
+    def __init__(self):
+
+        self.poll = Poll.default_poll()
+        self.criteria = self.poll.criterias_list[0]
+        # build complete graph
+        query: QuerySet = ComparisonCriteriaScore.objects.filter(
+            comparison__poll__name=self.poll.name
+        ).filter(
+            criteria=self.criteria
+        )
+        self._complete_graph = Graph(None, self.poll, self.criteria)
+
+        comparisons = query
+        for c in comparisons:
+            if c.comparison.entity_1 not in self._entity_to_video.keys():
+                self._entity_to_video[c.comparison.entity_1] = \
+                    Video(self._comparison_reference_video, from_entity=c.comparison.entity_1)
+
+                self._complete_graph.add_node(self._entity_to_video[c.comparison.entity_1])
+
+            if c.comparison.entity_2 not in self._entity_to_video.keys():
+                self._entity_to_video[c.comparison.entity_2] = \
+                    Video(self._comparison_reference_video, from_entity=c.comparison.entity_2)
+
+                self._complete_graph.add_node(self._entity_to_video[c.comparison.entity_2])
+
+            self._complete_graph.add_edge(self._entity_to_video[c.comparison.entity_1],
+                                          self._entity_to_video[c.comparison.entity_2])
+
+        # create required user graphs (none at first in fact)
+
+    def get_user_comparability_augmenting_videos(self) -> list[Video]:
+        # I want all entities from the current poll compared by supertrusted user
+        # todo create alias to properly detect supertrusted ?
+        req_entities = Entity.objects \
+            .filter(comparison__poll__name=self.poll.name) \
+            .filter(comparison__user__is_staff=True)
+        return list(map(lambda entity: self._entity_to_video[entity], req_entities))
+
+    def get_user_rate_later_video_list(self, user: User) -> list[Video]:
+        req_entities = Entity.objects \
+            .filter(type=self.poll.name) \
+            .filter(videoratelater__user__email=user.email)
+        return list(map(lambda entity: self._entity_to_video[entity], req_entities))
+
+    def do_offline_computation(self):
+        # for requests => look at ml->inputs
+        scale_aug_vids = self.get_user_comparability_augmenting_videos()
+        for g in self._user_specific_graphs.values():
+            # Will be cached, do at registration
+            g.compute_offline_parameters(scale_aug_vids)
+
+    def register_new_user(self, new_user):
+        recommendation_user = RecommendationUser(self._entity_to_video, new_user,
+                                                 self.criteria, self.poll)
+        self._user_specific_graphs[new_user] = Graph(local_user=recommendation_user,
+                                                     local_poll=self.poll,
+                                                     local_criteria=self.criteria)
+        # build user graph
+        query: QuerySet = ComparisonCriteriaScore.objects.filter(
+            comparison__poll__name=self.poll.name
+        ).filter(
+            criteria=self.criteria
+        ).filter(
+            comparison__user__email=new_user.email
+        )
+        for c in query:
+            va = c.comparison.entity_1
+            vb = c.comparison.entity_2
+            self._user_specific_graphs[new_user].add_edge(va, vb)
+
+    def register_user_comparison(self, user: User, va: Video, vb: Video):
+        self._complete_graph.add_edge(va, vb)
+        if user in self._user_specific_graphs.keys():
+            self._user_specific_graphs[user].add_edge(va, vb)
+
+    def get_first_video_recommendation(self, user: User, nb_video_required: int) -> list[Video]:
+        # Lazily load the user graph
+        if user not in self._user_specific_graphs.keys():
+            self.register_new_user(user)
+        result = []
+
+        # Give the first video id to the graph so the sorting will take that into account
+        self._user_specific_graphs[user].prepare_for_sorting()
+
+        # Prepare the set of videos to sort, taking the videos present in the graph
+        # and append the ones that are not yet compared by the user
+        considered_vids_list = self._prepare_video_list(user)
+
+        max_vid_pref = 0
+        # Todo : take into account the rate later list / already seen videos ?
+        for v in considered_vids_list:
+            v.user_pref = max(v.nb_comparison_with.values()) / v.comparison_nb
+            if v.user_pref > max_vid_pref:
+                max_vid_pref = v.user_pref
+
+        for i in range(nb_video_required):
+            for v in considered_vids_list:
+                act_user_pref = v.user_pref
+                if act_user_pref >= (nb_video_required - i) / (
+                        nb_video_required + 1) * max_vid_pref and v not in result:
+                    result.append(v)
+                    break
+        return result
+
+    def get_second_video_recommendation(self,
+                                        user,
+                                        first_video_id,
+                                        nb_video_required: int) -> list[Video]:
+        # Lazily load the user graphs
+        if user not in self._user_specific_graphs.keys():
+            self.register_new_user(user)
+        result = []
+
+        # Give the first video id to the graph so the sorting will take that into account
+        self._user_specific_graphs[user].prepare_for_sorting(first_video_id)
+
+        # Prepare the set of videos to sort, taking the videos present in the graph and append
+        # the ones that are not yet compared by the user
+        considered_vids_list = self._prepare_video_list(user)
+
+        max_vid_pref = 0
+        for v in considered_vids_list:
+            v.user_pref = v.nb_comparison_with[first_video_id] / v.comparison_nb
+            if v.user_pref > max_vid_pref:
+                max_vid_pref = v.user_pref
+
+        for i in range(nb_video_required):
+            for v in considered_vids_list:
+                act_user_pref = v.user_pref
+                act_min_ratio = (nb_video_required - i) / (nb_video_required + 1)
+                if act_user_pref >= act_min_ratio * max_vid_pref \
+                        and v.uid != first_video_id \
+                        and v not in result:
+                    result.append(v)
+                    break
+        return result
+
+    def _prepare_video_list(self, user):
+        # Prepare the set of videos to sort, taking the videos present in the graph and append
+        # the ones that are not yet compared by the user
+        considered_vids = set(self._user_specific_graphs[user].nodes)
+        tmp = set(self._complete_graph.nodes.copy())
+        tmp = tmp.difference(considered_vids)
+
+        considered_vids.update(tmp)
+        considered_vids_list = list(considered_vids)
+        considered_vids_list.sort(reverse=True)
+        return considered_vids_list
