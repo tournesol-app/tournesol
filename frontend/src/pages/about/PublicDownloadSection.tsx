065ebbb4d8b5d5d2a1d776a1e29265f0f3cfc824
Michaël Witrant <mike@lepton.fr>
2022-05-18 10:54:57 +0200

    [back][front] feat: display statistics by poll (#927)
    * use the prefix `set_` instead of `add_` to make it clear that
      the method is an attribute setter
    
    * rename few variables to make them match the Statistics
      members' names
    
    * remove a duplicated docstring (not useful at the moment, but
      could be in the future if the StatisticsView evolves)
    
    * add some type hints
    
    * use a QuerySet iterator, it's a good habit
    
    Co-authored-by: Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>

--- frontend/src/pages/about/PublicDownloadSection.tsx
+++ frontend/src/pages/about/PublicDownloadSection.tsx
@@ -2,28 +2,33 @@ import React, { useEffect, useState } from 'react';
 import { useTranslation, Trans } from 'react-i18next';
 import { Typography, Button } from '@mui/material';
 import { Statistics, StatsService } from 'src/services/openapi';
+import { useCurrentPoll } from 'src/hooks';
 
 // PublicDownloadSection is a paragraph displayed on the HomePage
 // that helps users know how to download the public video comparisons available for their use case
 const PublicDownloadSection = () => {
   const { t } = useTranslation();
   const api_url = process.env.REACT_APP_API_URL;
+  const { name: pollName } = useCurrentPoll();
 
   const [userCount, setUserCount] = useState<number>(0);
-  const [videoCount, setVideoCount] = useState<number>(0);
+  const [comparedEntityCount, setComparedEntityCount] = useState<number>(0);
   const [comparisonCount, setComparisonCount] = useState<number>(0);
 
   useEffect(() => {
     StatsService.statsRetrieve()
       .then((value: Statistics) => {
-        setUserCount(value.user_count);
-        setVideoCount(value.video_count);
-        setComparisonCount(value.comparison_count);
+        setUserCount(value.active_users.total);
+        const pollStats = value.polls.find(({ name }) => name === pollName);
+        if (pollStats !== undefined) {
+          setComparedEntityCount(pollStats.compared_entities.total);
+          setComparisonCount(pollStats.comparisons.total);
+        }
       })
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+  }, [pollName]);
 
   return (
     <>
@@ -47,8 +52,8 @@ const PublicDownloadSection = () => {
         <Trans t={t} i18nKey="about.publicDatabaseThanksToContributors">
           Finally, we would like to thank all the contributors who compared
           videos on Tournesol. We count so far about {{ userCount }} users who
-          compared {{ comparisonCount }} times more than {{ videoCount }}{' '}
-          videos.
+          compared {{ comparisonCount }} times more than{' '}
+          {{ comparedEntityCount }} videos.
         </Trans>
       </Typography>
 
