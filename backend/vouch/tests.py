f0c1db2be253f15db04abff928f83ec2c1f33462
root <berangere.colbois@epfl.ch>
2022-05-18 14:06:20 +0200

    algorithm improve to handle better users vouching a little or not a all, and draft of tests

--- backend/vouch/tests.py
+++ backend/vouch/tests.py
@@ -1,108 +1,70 @@
-import math
 import random
-from django.test import TestCase
-import pytest
 
-
-import trustalgo
-from core.tests.factories.user import UserFactory
+from attr import Factory
 import numpy as np
+import pytest
+from vouch.models import Voucher
+
+from trust_algo import normalize_trust_values, get_trust_vector, rescale, trust_algo
+"""
+Test module for vouch and trust algo
+"""
+from django.test import TestCase
 from core.models.user import EmailDomain, User
-#Create your tests here.
-# check trusted user are indeed trusted
-@pytest.mark.django_db
-class TestTrustAndUntrustUsers(TestCase):
-    # prepare fake data : 
+from core.tests.factories.user import UserFactory
 
-    def test_trusted_untrusted_user(self):
-        # === prepare fake dats ===
-        # users pre-trusted
-        user_trusted = User.objects.create_user(username="trusted0", email="test@trusted.test")
-        email_domain = EmailDomain.objects.filter(domain="@trusted.test").first()
-        email_domain.status = EmailDomain.STATUS_ACCEPTED
-        email_domain.save()
-
-        # users not pre-trusted
-        user_untrusted = User.objects.create_user(username="untrusted0", email="test@untrustedtrusted.test")
-
-        self.assertTrue(user_trusted.is_trusted)
-        self.assertFalse(user_untrusted.is_trusted)
-
-#check that factory does work
-@pytest.mark.django_db
-class TestTrustAndUntrustUsers(TestCase):
-    # user_trusted = User.objects.create_user(username="trusted0", email="test@trusted.test")
-    # email_domain = EmailDomain.objects.filter(domain="@trusted.test").first()
-    # email_domain.status = EmailDomain.STATUS_ACCEPTED
-    # email_domain.save()
-    users = []
-    def test_trusted_user(self):
-        usert = UserFactory(email = 'person{}@trusted.test')
-        email_domain = EmailDomain.objects.filter(domain="@trusted.test").first()
-        email_domain.status = EmailDomain.STATUS_ACCEPTED
-        email_domain.save()
-        self.assertTrue(usert.is_trusted)
-    
-    def test_untrusted_user(self):
-        usernt = UserFactory(email = 'person{}@ntrusted.test')
-        self.assertFalse(usernt.is_trusted)
-
-
-class TestGenerationofUsers(TestCase):
-    def test_user_set(self):
-        usert = UserFactory(email = 'person{}@trusted.test')
-        email_domain = EmailDomain.objects.filter(domain="@trusted.test").first()
-        email_domain.status = EmailDomain.STATUS_ACCEPTED
-        email_domain.save()
-        #create multiple users (trusted and untrusted)
-        user_trust = np.random.choice([0, 1], size=6, p=[.9, .1]) # 1/10 per user of chance to be trusted
-        user_trusted = np.array([UserFactory(email = 'person0@trusted.test'),UserFactory(email = 'person1@trusted.test'),UserFactory(email = 'person2@trusted.test'),UserFactory(email = 'person3@trusted.test'),UserFactory(email = 'person4@trusted.test')])
-        user_ntrusted = np.array([UserFactory(email = 'person0@ntrusted.test'),UserFactory(email = 'person1@ntrusted.test'),UserFactory(email = 'person2@ntrusted.test'),UserFactory(email = 'person3@ntrusted.test'),UserFactory(email = 'person4@ntrusted.test')])
-        user_set = []
-        for i in range(1):
-            if user_trust[i] == 1:
-                user_set.append(user_trusted[i])
-            else :
-                user_set.append(user_ntrusted[i])
-        
-        i = 0
-        for u in user_set :
-            assert(u.is_trusted == user_trust[i])
-            i += 1
 
 # ========== unit tests - trustalgo ===============
-class UnitTestTrustAlgo(TestCase):
-       
-    # ensure normalization works
-    def test_normalization(test):
-        C = np.random.rand(15,15)
-        C_normalized = trustalgo.normalize_trust_values(C)
-
+# ensure normalization works
+@pytest.mark.django_db
+class UnitTest(TestCase):
+    def test_normalization(self):
+        C = np.random.rand(10,10)
+        user_trust = np.random.randint(2, size=10)
+        C_normalized = normalize_trust_values(C,user_trust)
         for k in range(len(C_normalized[0])):
-         assert(math.isclose(sum(C_normalized[k]),1,rel_tol=1e-10))
+            assert(np.sum(C_normalized[k])==1)
 
     # ensure C is assymetrical (for convergence)
-    def test_C_assymetrical(test):
-        C = np.random.rand(15,15)
-        C_normalized = trustalgo.normalize_trust_values(C)
-        assert (False == np.allclose(C_normalized, C_normalized.T,rtol=1e-10, atol=1e-08))
+    def test_C_assymetrical(self):
+        C = np.random.rand(10,10)
+        user_trust = np.random.randint(2, size=10)
+        C_normalized = normalize_trust_values(C,user_trust)
+        rtol=1e-05
+        atol=1e-08
+        assert (False == np.allclose(C_normalized, C_normalized.T, rtol=rtol, atol=atol))
 
 
-    # ensure threshold is respected
-    def test_threshold(test):
-        user_trust = np.random.choice([0, 1], size=10, p=[.9, .1]) # 1/10 per user of chance to be trusted
-        user_trust[0] = 1 #to ensure at least one user is trusted
-        threshold = random.randint(0,10)/10.0
-        random_vec = np.random.rand(len(user_trust))
-        result = trustalgo.rescale_trust_with_threshold(random_vec,user_trust,threshold)
-        assert(threshold <= np.sum([result[k] if user_trust[k]==1 else 0 for k in range(len(user_trust))]))
 
-        # # ========== unit tests - trustalgo ===============
+    # ensure trust score are in [0,1]
+    def test_trust_values_range(self):
+        C = np.random.rand(10,10)
+        global_trust = trust_algo()
+        for value in global_trust:
+            assert (0<=value and value <=1)
+
+    # # # ======= unit tests - vouching connections =======
+    # #create 10 users from scratch
+    # users_nb = 12
+    # users = []
+    # for i in range (users_nb):    
+    #     users[i]= User.objects.create_user(username=f"test-user{i}", email=f"test{i}@example{i}.test")
+    #     if i == (users_nb % 4) :
+    #         email_domain = EmailDomain.objects.filter(domain="@example{i}.test").first()
+    #         email_domain.status = EmailDomain.STATUS_ACCEPTED
+    #         email_domain.save()
+            
+    # #create vouching connections between them
+    # vouching = np.array(size=(users_nb,users_nb))
+    # for i in range(users_nb):
+    #     for j in range(users_nb):
+    #         if i != j :
+    #             if (random.randint(0,10) < 10):
+    #                 vouching[i][j] = Voucher.objects.create(vouching = users[i],vouchedfor=users[j],trust_value = random.randint(0,10)/10.0)
 
 
-        # # ensure trust score are in [0,1]
-        # def test_trust_values_range():
-        #     global_trust = trust_algo(C,user_trust)
-        #     for value in global_trust:
-        #         assert (0<=value and value <=1)
+
+
+
+
 
