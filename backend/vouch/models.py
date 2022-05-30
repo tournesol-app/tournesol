0ae85c7fbf6c263b207b81baac76d35d00607bee
BerangereColbois <43465852+BerangereColbois@users.noreply.github.com>
2022-05-30 09:08:05 +0200

    [back] feat: add the `vouch` app  (#871)
    Co-authored-by: Gresille & Siffle <39056254+GresilleSiffle@users.noreply.github.com>

---
+++ backend/vouch/models.py
@@ -0,0 +1,54 @@
+"""
+Models related to the vouching system.
+"""
+
+from django.db import models
+
+from core.models.user import User
+
+
+class Voucher(models.Model):
+    """A `Voucher` given by a user to another one.
+
+    A `Voucher` represents a mark of trust a user is able to grant to another
+    user. Each mark of trust has a trust value.
+
+    These vouchers can be used by an algorithm to compute the global trust
+    score given to every user by the network, in order to weight their voting
+    right accordingly.
+    """
+
+    by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        help_text="The user giving the voucher.",
+        related_name="vouchers_given",
+    )
+    to = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        help_text="The user receiving the voucher.",
+        related_name="vouchers_received",
+    )
+    is_public = models.BooleanField(
+        default=False, help_text="Should the voucher be public?"
+    )
+    trust_value = models.FloatField(
+        default=0,
+        help_text="The trust value given by the vouching user to receiving user.",
+    )
+
+    @staticmethod
+    def get_given_by(user):
+        vouchers = Voucher.objects.filter(by=user)
+        return vouchers, vouchers.exists()
+
+    @staticmethod
+    def get_given_to(user):
+        vouchers = Voucher.objects.filter(to=user)
+        return vouchers, vouchers.exists()
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            raise ValueError("Updating a voucher is not allowed.")
+        super().save(*args, **kwargs)
---
+++ backend/vouch/tests/test_model_voucher.py
@@ -0,0 +1,117 @@
+"""
+All test cases of the `Voucher` model.
+"""
+
+from django.test import TestCase
+
+from core.tests.factories.user import UserFactory
+from vouch.models import Voucher
+
+
+class VoucherTestCase(TestCase):
+    """
+    TestCase of the `Voucher` model.
+    """
+
+    # For convenience this _user_0 should neither give nor receive any
+    # voucher.
+    _user_0 = "username_0"
+    _user_1 = "username_1"
+    _user_2 = "username_2"
+    _user_3 = "username_3"
+    _user_4 = "username_4"
+
+    def setUp(self) -> None:
+        self.user_0 = UserFactory(username=self._user_0)
+        self.user_1 = UserFactory(username=self._user_1)
+        self.user_2 = UserFactory(username=self._user_2)
+        self.user_3 = UserFactory(username=self._user_3)
+        self.user_4 = UserFactory(username=self._user_4)
+
+        Voucher.objects.bulk_create(
+            [
+                # user_1 has given one voucher
+                Voucher(by=self.user_1, to=self.user_2, trust_value=12),
+                # user_2 has given two vouchers
+                Voucher(by=self.user_2, to=self.user_1, trust_value=21),
+                Voucher(by=self.user_2, to=self.user_3, trust_value=23),
+                # user_3 has given three vouchers
+                Voucher(by=self.user_3, to=self.user_1, trust_value=31),
+                Voucher(by=self.user_3, to=self.user_2, trust_value=32),
+                Voucher(by=self.user_3, to=self.user_4, trust_value=34),
+                # user_0 has given / received zero voucher
+            ]
+        )
+
+    def test_get_given_to_with_one(self) -> None:
+        """
+        Test `get_given_to` returns the expected voucher when a user has only
+        one voucher.
+        """
+        vouchers, exists = Voucher.get_given_to(self.user_3)
+        self.assertEqual(vouchers.count(), 1)
+        self.assertEqual(vouchers[0].trust_value, 23)
+        self.assertEqual(vouchers[0].by, self.user_2)
+        self.assertEqual(vouchers[0].to, self.user_3)
+        self.assertEqual(exists, True)
+
+    def test_get_given_to_with_several(self) -> None:
+        """
+        Test `get_given_to` returns only the expected vouchers when a user has
+        several vouchers.
+        """
+        vouchers, exists = Voucher.get_given_to(self.user_2)
+        self.assertEqual(vouchers.count(), 2)
+        self.assertEqual(exists, True)
+
+        # Even if the order of the returned vouches is not known, check if
+        # their values are expected.
+        for voucher in vouchers:
+            self.assertEqual(voucher.to, self.user_2)
+            self.assertIn(voucher.by, [self.user_1, self.user_3])
+            self.assertIn(voucher.trust_value, [12, 32])
+
+    def test_get_given_to_with_zero(self) -> None:
+        """
+        Test `get_given_to` returns no voucher when a user has no voucher.
+        """
+        vouchers, exists = Voucher.get_given_to(self.user_0)
+        self.assertEqual(vouchers.count(), 0)
+        self.assertEqual(exists, False)
+
+    def test_get_given_by_with_one(self) -> None:
+        """
+        Test `get_given_by` returns the expected voucher when a user has given
+        only one voucher.
+        """
+        vouchers, exists = Voucher.get_given_by(self.user_1)
+        self.assertEqual(vouchers.count(), 1)
+        self.assertEqual(vouchers[0].trust_value, 12)
+        self.assertEqual(vouchers[0].by, self.user_1)
+        self.assertEqual(vouchers[0].to, self.user_2)
+        self.assertEqual(exists, True)
+
+    def test_get_given_by_with_several(self) -> None:
+        """
+        Test `get_given_by` returns only the expected vouchers when a user has
+        given several vouchers.
+        """
+        vouchers, exists = Voucher.get_given_by(self.user_3)
+        self.assertEqual(vouchers.count(), 3)
+        self.assertEqual(exists, True)
+
+        # Even if the order of the returned vouches is not known, check if
+        # their values are expected.
+        for voucher in vouchers:
+            self.assertEqual(voucher.by, self.user_3)
+            self.assertIn(voucher.to, [self.user_1, self.user_2, self.user_4])
+            self.assertIn(voucher.trust_value, [31, 32, 34])
+
+    def test_get_given_by_with_zero(self) -> None:
+        """
+        Test `get_given_by` returns no voucher when a user has given no
+        voucher.
+        """
+        vouchers, exists = Voucher.get_given_by(self.user_0)
+        self.assertEqual(vouchers.count(), 0)
+        self.assertEqual(exists, False)
