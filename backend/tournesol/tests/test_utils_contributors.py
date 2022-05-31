ff4f46023bac1c8f03aa3b367412e11637e4e031
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-05-31 10:01:58 +0200

    - utils.top_contributors -> utils.contributors so that we will be able   to add other contributors related shortcuts
    - use self.assertEqual instead of assert to keep unicity with the rest
      of the app
    
    - rename the TestCase to stay coherent with the rest of the app

--- backend/tournesol/tests/test_utils_contributors.py
+++ backend/tournesol/tests/test_utils_contributors.py
@@ -9,11 +9,11 @@ from tournesol.models.comparisons import Comparison
 from tournesol.tests.factories.comparison import ComparisonFactory
 from tournesol.tests.factories.entity import EntityFactory
 from tournesol.tests.factories.ratings import ContributorRatingFactory
-from tournesol.utils.top_contributors import get_last_month_top_public_contributors
+from tournesol.utils.contributors import get_last_month_top_public_contributors
 
 
-class TestBestContributor(TestCase):
-    """TestCase for the top contributors of last month."""
+class UtilsContributorsTestCase(TestCase):
+    """TestCase of the utils.contributors module."""
 
     def setUp(self):
 
@@ -62,20 +62,26 @@ class TestBestContributor(TestCase):
         for comparison in self.comparisons:
             Comparison.objects.filter(pk=comparison.pk).update(datetime_add=last_month)
 
-        # Comparisons with other dates to be sure it only get those from the previous month
-        ComparisonFactory(user=self.users[4], entity_1=self.entities[1], entity_2=self.entities[2])
+        # Comparisons with other dates to be sure it only gets those from the previous month
+        ComparisonFactory(
+            user=self.users[4], entity_1=self.entities[1], entity_2=self.entities[2]
+        )
         older_comparison = ComparisonFactory(
             user=self.users[4], entity_1=self.entities[1], entity_2=self.entities[3]
         )
-        Comparison.objects.filter(pk=older_comparison.pk).update(datetime_add=time_ago(days=65))
+        Comparison.objects.filter(pk=older_comparison.pk).update(
+            datetime_add=time_ago(days=65)
+        )
 
     def test_get_last_month_top_public_contributors(self):
         """Test top public contributor of the previous month."""
 
-        top_contributors = get_last_month_top_public_contributors(poll_name=self.poll.name)
+        top_contributors = get_last_month_top_public_contributors(
+            poll_name=self.poll.name
+        )
 
-        assert top_contributors[0] == (self.users[2].username, 5)
-        assert top_contributors[1] == (self.users[0].username, 4)
-        assert top_contributors[2] == (self.users[3].username, 3)
-        assert top_contributors[3] == (self.users[1].username, 2)
-        assert top_contributors[4] == (self.users[4].username, 1)
+        self.assertEqual(top_contributors[0], (self.users[2].username, 5))
+        self.assertEqual(top_contributors[1], (self.users[0].username, 4))
+        self.assertEqual(top_contributors[2], (self.users[3].username, 3))
+        self.assertEqual(top_contributors[3], (self.users[1].username, 2))
+        self.assertEqual(top_contributors[4], (self.users[4].username, 1))
