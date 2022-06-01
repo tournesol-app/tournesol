52c62313e94c87419c094f1fa4e0122273fe8289
Aidan <53896963+aidanjungo@users.noreply.github.com>
2022-06-01 15:28:35 +0200

    [back] feat: add `get_top_public_contributors` func (#956)
    Co-authored-by: Gresille & Siffle <39056254+GresilleSiffle@users.noreply.github.com>

---
+++ backend/tournesol/tests/test_utils_contributors.py
@@ -0,0 +1,102 @@
+from datetime import timedelta
+
+from django.test import TestCase
+from django.utils import timezone
+
+from core.tests.factories.user import UserFactory
+from core.utils.time import time_ago
+from tournesol.models import Poll
+from tournesol.models.comparisons import Comparison
+from tournesol.tests.factories.comparison import ComparisonFactory
+from tournesol.tests.factories.entity import EntityFactory
+from tournesol.tests.factories.ratings import ContributorRatingFactory
+from tournesol.utils.contributors import get_top_public_contributors_last_month
+
+
+class UtilsContributorsTestCase(TestCase):
+    """TestCase of the utils.contributors module."""
+
+    def setUp(self):
+
+        self.poll = Poll.default_poll()
+
+        self.users = UserFactory.create_batch(5)
+        self.entities = EntityFactory.create_batch(6)
+
+        for user in self.users:
+            for entity in self.entities:
+
+                is_public = True
+                # All comparisons involving the entity 2 will be private.
+                if entity == self.entities[2]:
+                    is_public = False
+
+                ContributorRatingFactory.create(
+                    user=user,
+                    entity=entity,
+                    is_public=is_public,
+                )
+
+        comparisons_parameters = [
+            # Made by the user 0.
+            dict(user=self.users[0], entity_1=self.entities[0], entity_2=self.entities[1]),
+            dict(user=self.users[0], entity_1=self.entities[0], entity_2=self.entities[2]),
+            dict(user=self.users[0], entity_1=self.entities[0], entity_2=self.entities[3]),
+            dict(user=self.users[0], entity_1=self.entities[1], entity_2=self.entities[5]),
+            # Made by the user 1.
+            dict(user=self.users[1], entity_1=self.entities[0], entity_2=self.entities[1]),
+            dict(user=self.users[1], entity_1=self.entities[0], entity_2=self.entities[2]),
+            # Made by the user 2.
+            dict(user=self.users[2], entity_1=self.entities[0], entity_2=self.entities[1]),
+            dict(user=self.users[2], entity_1=self.entities[0], entity_2=self.entities[2]),
+            dict(user=self.users[2], entity_1=self.entities[0], entity_2=self.entities[3]),
+            dict(user=self.users[2], entity_1=self.entities[0], entity_2=self.entities[4]),
+            dict(user=self.users[2], entity_1=self.entities[0], entity_2=self.entities[5]),
+            dict(user=self.users[2], entity_1=self.entities[1], entity_2=self.entities[5]),
+            # Made by the user 3.
+            dict(user=self.users[3], entity_1=self.entities[0], entity_2=self.entities[1]),
+            dict(user=self.users[3], entity_1=self.entities[0], entity_2=self.entities[2]),
+            dict(user=self.users[3], entity_1=self.entities[0], entity_2=self.entities[3]),
+            # Made by the user 4.
+            dict(user=self.users[4], entity_1=self.entities[0], entity_2=self.entities[1]),
+        ]
+
+        self.comparisons = [
+            ComparisonFactory(**comp) for comp in comparisons_parameters
+        ]
+
+        now = timezone.now()
+        last_month = timezone.datetime(
+            now.year, now.month, 1, tzinfo=timezone.get_current_timezone()
+        ) - timedelta(days=15)
+
+        for comparison in self.comparisons:
+            Comparison.objects.filter(pk=comparison.pk).update(datetime_add=last_month)
+
+        # Add public comparisons younger and older than the last month. They
+        # must not be counted in the top contributors of the last month.
+        ComparisonFactory(
+            user=self.users[4], entity_1=self.entities[1], entity_2=self.entities[3]
+        )
+        older_comparison = ComparisonFactory(
+            user=self.users[4], entity_1=self.entities[1], entity_2=self.entities[4]
+        )
+        Comparison.objects.filter(pk=older_comparison.pk).update(
+            datetime_add=time_ago(days=65)
+        )
+
+    def test_get_last_month_top_public_contributors(self):
+        """Test top public contributor of the previous month."""
+
+        top_contributors = [
+            (contrib.username, contrib.n_comparisons)
+            for contrib in get_top_public_contributors_last_month(
+                poll_name=self.poll.name
+            ).iterator()
+        ]
+
+        self.assertEqual(top_contributors[0], (self.users[2].username, 5))
+        self.assertEqual(top_contributors[1], (self.users[0].username, 3))
+        self.assertEqual(top_contributors[2], (self.users[3].username, 2))
+        self.assertEqual(top_contributors[3], (self.users[1].username, 1))
+        self.assertEqual(top_contributors[4], (self.users[4].username, 1))
---
+++ backend/tournesol/utils/contributors.py
@@ -0,0 +1,103 @@
+"""
+Shortcut functions related to the contributors.
+"""
+
+from django.db.models.query import RawQuerySet
+from django.utils import timezone
+
+from core.models.user import User
+
+
+def get_top_public_contributors_last_month(
+    poll_name: str, top: int = 10
+) -> RawQuerySet:
+    """
+    Return the `username` of the top contributors of the last month.
+
+    See: `get_top_public_contributors`.
+    """
+    now = timezone.now()
+
+    last_month = now.month - 1
+    year = now.year
+
+    if last_month == 12:
+        year -= 1
+
+    return get_top_public_contributors(poll_name, year, last_month, top)
+
+
+def get_top_public_contributors(
+    poll_name: str, year: int, month: int, top: int
+) -> RawQuerySet:
+    """
+    Return the `username` of the top contributors of a given month/year with
+    their number of comparisons, ordered by this number of comparisons.
+
+    Keyword arguments:
+
+    poll_name -- the name of the desired poll
+    year -- consider only comparisons added this year
+    month -- consider only comparison added this month (january is 1, december is 12)
+    top -- the maximum number of contributors to return
+
+    Ex:
+
+        get_top_public_contributors('videos', 2022, 2, 20)
+
+        Return the top 20 contributors of February 2022 for the poll 'videos'.
+    """
+
+    return User.objects.raw(
+        """
+        WITH public_comparisons AS (
+            SELECT
+                core_user.username,
+                core_user.id AS user_id,
+                tournesol_comparison.id AS comparison_id
+
+            FROM tournesol_comparison
+
+            -- this JOIN allows to filter on the desired poll
+            JOIN tournesol_poll
+              ON tournesol_poll.id = tournesol_comparison.poll_id
+
+            JOIN core_user
+              ON core_user.id = tournesol_comparison.user_id
+
+            -- this JOIN allows to filter by public ratings for the entity_1
+            -- the poll has been already filtered, no need to filter it again
+            JOIN tournesol_contributorrating AS rating_1
+              ON rating_1.entity_id = tournesol_comparison.entity_1_id
+             AND rating_1.user_id = tournesol_comparison.user_id
+
+            -- this JOIN allows to filter by public ratings for the entity_2
+            -- the poll has been already filtered, no need to filter it again
+            JOIN tournesol_contributorrating AS rating_2
+              ON rating_2.entity_id = tournesol_comparison.entity_2_id
+             AND rating_2.user_id = tournesol_comparison.user_id
+
+            WHERE tournesol_poll.name = %(poll_name)s
+
+              -- keep only comparisons from the last month
+              AND EXTRACT('month' from tournesol_comparison.datetime_add)
+                = %(month)s
+              AND EXTRACT('year' from tournesol_comparison.datetime_add)
+                = %(year)s
+
+              -- keep only public ratings
+              AND rating_1.is_public = true
+              AND rating_2.is_public = true
+        )
+        SELECT
+            user_id AS id,
+            username,
+            count(comparison_id) as n_comparisons
+
+        FROM public_comparisons
+        GROUP BY user_id, username
+        ORDER BY n_comparisons DESC
+        LIMIT %(top)s;
+    """,
+        {"poll_name": poll_name, "month": month, "year": year, "top": top},
+    )
