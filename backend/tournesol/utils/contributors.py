2e3eb39ce9bb8f7e4557add0dccae0064e9a4256
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-06-01 09:56:19 +0200

    use raw SQL for benchmark

--- backend/tournesol/utils/contributors.py
+++ backend/tournesol/utils/contributors.py
@@ -9,7 +9,6 @@ from typing import List, Tuple
 from django.utils import timezone
 
 from tournesol.models.comparisons import Comparison
-from tournesol.models.ratings import ContributorRating
 
 
 def get_last_month_top_public_contributors(poll_name: str) -> List[Tuple]:
@@ -23,34 +22,43 @@ def get_last_month_top_public_contributors(poll_name: str) -> List[Tuple]:
         now.year, now.month, 1, tzinfo=timezone.get_current_timezone()
     ) - timedelta(days=15)
 
-    public_ratings = (
-        ContributorRating.objects.filter(is_public=True, poll__name=poll_name)
-        .select_related("user", "entity")
-        .values_list("user__username", "entity__uid")
+    public_comparisons = Comparison.objects.raw(
+        f"""
+        SELECT
+            core_user.username,
+            tournesol_comparison.id
+        FROM
+            tournesol_comparison
+
+        -- this JOIN allows to filter on the desired poll
+        JOIN tournesol_poll
+          ON tournesol_poll.id = tournesol_comparison.poll_id
+
+        JOIN core_user
+          ON core_user.id = tournesol_comparison.user_id
+
+        -- this JOIN allows to filter by public entity_1 ratings
+        -- the poll has been already filtered, no need to filter it again here
+        JOIN tournesol_contributorrating AS rating_1
+          ON rating_1.entity_id = tournesol_comparison.entity_1_id
+         AND rating_1.user_id = tournesol_comparison.user_id
+
+        -- this JOIN allows to filter by public entity_2 ratings
+        -- the poll has been already filtered, no need to filter it again here
+        JOIN tournesol_contributorrating AS rating_2
+          ON rating_2.entity_id = tournesol_comparison.entity_2_id
+         AND rating_2.user_id = tournesol_comparison.user_id
+
+        WHERE tournesol_poll.name = '{poll_name}'
+
+          AND EXTRACT('month' from tournesol_comparison.datetime_add) = EXTRACT('month' from current_timestamp) - 1
+          AND EXTRACT('year' from tournesol_comparison.datetime_add) = EXTRACT('year' from current_timestamp)
+
+          AND rating_1.is_public = true
+          AND rating_2.is_public = true;
+    """
     )
 
-    comparisons = (
-        Comparison.objects.filter(
-            poll__name=poll_name,
-            datetime_add__month=last_month.month,
-            datetime_add__year=last_month.year,
-        )
-        .select_related("entity_1", "entity_2", "user")
-        .values("entity_1__uid", "entity_2__uid", "user__username")
-    )
-
-    public_comparisons = [
-        comparison
-        for comparison in comparisons
-        if (
-            (comparison["user__username"], comparison["entity_1__uid"])
-            in public_ratings
-            and (comparison["user__username"], comparison["entity_2__uid"])
-            in public_ratings
-        )
-    ]
-
-    usernames = [comparison["user__username"] for comparison in public_comparisons]
-
+    usernames = [comparison.username for comparison in public_comparisons]
     top_contributors_counter = collections.Counter(usernames)
     return top_contributors_counter.most_common(10)
