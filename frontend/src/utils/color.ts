4325c03ea6a76d404b7153c711f827cc3bd626d4
Michael Witrant <mike@lepton.fr>
2022-06-04 11:01:17 +0200

    Make lighten function safer and documented
    Co-authored-by: Gresille & Siffle

--- frontend/src/utils/color.ts
+++ frontend/src/utils/color.ts
@@ -1,16 +1,33 @@
-// Copied from https://stackoverflow.com/a/59387478
-export const lighten = (color: string, lighten: number) => {
+/**
+ * Returns a lightened version of a hexadecimal color.
+ *
+ * Adapted from https://stackoverflow.com/a/59387478
+ *
+ * @param color A 6 digits hexadecimal color to lighten, that may include a leading '#'.
+ * @param amount A real number representing the amount of lightness to add, for example `0.5`. A negative value will darken the color.
+ * @returns A hexadecimal lightened version of the original `color`, including a leading '#'.
+ */
+export const lighten = (color: string, amount: number) => {
   const c = color.replace('#', '');
   const rgb = [
-    parseInt(c.substr(0, 2), 16),
-    parseInt(c.substr(2, 2), 16),
-    parseInt(c.substr(4, 2), 16),
+    parseInt(c.substring(0, 2), 16),
+    parseInt(c.substring(2, 4), 16),
+    parseInt(c.substring(4, 6), 16),
   ];
-  let returnstatement = '#';
+
+  let lightenedColor = '#';
   rgb.forEach((color) => {
-    returnstatement += Math.round(
-      (255 - color) * (1 - Math.pow(Math.E, -lighten)) + color
-    ).toString(16);
+    let newColor = Math.round(
+      (255 - color) * (1 - Math.pow(Math.E, -amount)) + color
+    );
+
+    if (newColor < 0) newColor = 0;
+    if (newColor > 255) newColor = 255;
+
+    let hex = newColor.toString(16);
+    if (hex.length === 1) hex = `0${hex}`;
+
+    lightenedColor += hex;
   });
-  return returnstatement;
+  return lightenedColor;
 };
