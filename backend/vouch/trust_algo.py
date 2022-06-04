f57bd02e28f1a84d9c48b79971a6ebdcb6702f67
root <berangere.colbois@epfl.ch>
2022-06-04 21:01:09 +0200

    fixes following the advices of Adrien and Romain

--- backend/vouch/trust_algo.py
+++ backend/vouch/trust_algo.py
@@ -1,11 +1,11 @@
 import numpy as np
 from vouch.models import Voucher
 from core.models import user
-
+from django.db.models import Q
 # hyper parameter of algorithm - select them wisely
-a = 0.2  # define the minimum ratio of trust that is left to pre-trusted users
-eps = 0.00000001
-alpha = 0.1  # define how much voting weight goes to pre-trusted users for each user
+A = 0.2  # define the minimum ratio of trust that is left to pre-trusted users
+EPS = 1e-8
+ALPHA = 0.1  # define how much voting weight goes to pre-trusted users for each user
 
 
 def normalize_trust_values(C, trust_status):
@@ -17,10 +17,10 @@ def normalize_trust_values(C, trust_status):
     for i in range(len(C[0])):
         vouching_size = np.sum(np.array(C[i]) > 0, axis=0)
         for j in range(len(C[0])):
-            trust_indic = 1 if trust_status[j] > 0 else 0  # whether user j is pre-trusted
-            vouch_indic = 1 if C[i][j] > 0 else 0  # whether user j has been vouched by user i
-            C[i][j] = alpha / (alpha * trust_size + vouching_size) * trust_indic
-            C[i][j] += 1 / (alpha * trust_size + vouching_size) * vouch_indic
+            trust_indic = int(trust_status[j] > 0)
+            vouch_indic = int(C[i][j] > 0)  
+            C[i][j] = ALPHA / (ALPHA * trust_size + vouching_size) * trust_indic
+            C[i][j] += 1 / (ALPHA * trust_size + vouching_size) * vouch_indic
     return C
 
 
@@ -32,9 +32,9 @@ def get_trust_vector(C, p):
     t = p
     newt = t
     delta = 10
-    while (delta >= eps):
+    while (delta >= EPS):
         newt = C.T.dot(t)
-        newt = (1 - a) * newt + a * p
+        newt = (1 - A) * newt + A * p
         delta = np.linalg.norm(newt - t)
         t = newt
     return newt
@@ -58,17 +58,17 @@ def trust_algo():
     """
     # list of users, list of their trust status (regarding their email) in same order,
     # and number of users
-    users = list(User.objects.all().annotate(_is_trusted=Q(pk__in=User.trusted_users())))
+    users = list(user.User.objects.all().annotate(_is_trusted=Q(pk__in=user.User.trusted_users())))
     trust_status = [int(u._is_trusted) for u in users]
     nb_users = len(users)
 
     # create a matrix C where entry c_ij is the trust value user i gave to user j
-    C_ = np.empty([nb_users, nb_users], dtype=float)
-    for i, by in enumerate(users):
-        for j, to in enumerate(users):
-            voucher = Voucher.objects.filter(by=by).filter(to=to)
-            C_[i][j] = voucher[0].trust_value if voucher.exists() else 0
 
+    C_ = np.zeros([nb_users, nb_users], dtype=float)
+    for v in Voucher.objects.all():
+        i = users.index(v.by)
+        j = users.index(v.to)
+        C_[i][j] = v.trust_value 
     # improved eigen trust algorithm
     p = trust_status
     p = p/np.sum(p)
