e3cf489687bff5d42011762cf88f16535a52c595
aidanjungo <aidan@cfse.ch>
2022-06-09 22:16:57 +0200

    flake8

--- backend/twitterbot/tournesolbot.py
+++ backend/twitterbot/tournesolbot.py
@@ -51,10 +51,9 @@ def prepare_tweet(video):
 
     # Replace "@" by a smaller "@" to avoid false mentions in the tweet
     video_title = video.metadata["name"].replace("@", "﹫")
-    
+
     # Replace "." in between words to avoid in the tweet false detection of links
     video_title = re.sub(r"\b(?:\.)\b", "｡", video_title)
-
 
     # Generate the text of the tweet
     tweet_text = settings.tweet_text_template[language].format(
