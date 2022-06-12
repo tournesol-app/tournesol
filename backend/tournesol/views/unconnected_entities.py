475d9218541a133d552383fd10b3bba526a0bec4
Adrien Matissart <a@matissart.net>
2022-06-12 22:46:48 +0200

    corrections after review and remove recursion

--- backend/tournesol/views/unconnected_entities.py
+++ backend/tournesol/views/unconnected_entities.py
@@ -30,35 +30,36 @@ class UnconnectedEntitiesView(
     def get_queryset(self):
         # Get related entities from source entity
         source_node = get_object_or_404(Entity, uid=self.kwargs.get("uid"))
-        comparison_pairs = list(
+        comparisons = list(
             Comparison.objects.filter(
                 poll=self.poll_from_url, user=self.request.user
             ).values_list("entity_1_id", "entity_2_id")
         )
 
         all_connections = defaultdict(set)
-        already_visited_nodes = set()
 
-        for (entity_1_id, entity_2_id) in comparison_pairs:
+        for (entity_1_id, entity_2_id) in comparisons:
             all_connections[entity_1_id].add(entity_2_id)
             all_connections[entity_2_id].add(entity_1_id)
 
         def get_related_entities(entity_id):
-            already_visited_nodes.add(entity_id)
-            related_entities = set()
-            if entity_id not in all_connections:
-                return set()
+            already_visited_nodes = set()
+            related_entities = {entity_id}
+            to_visit = {entity_id}
 
-            for other_node in all_connections[entity_id]:
-                if other_node not in already_visited_nodes:
-                    related_entities.update(get_related_entities(other_node))
+            while to_visit:
+                node = to_visit.pop()
+                already_visited_nodes.add(node)
+                connections = all_connections.get(node, set())
+                to_visit.update(connections - already_visited_nodes)
+                related_entities.update(connections)
 
-            return all_connections[entity_id].union(related_entities)
+            return related_entities
 
         user_related_entities = get_related_entities(source_node.id)
         user_all_entities = set()
 
-        for (entity_1_id, entity_2_id) in comparison_pairs:
+        for (entity_1_id, entity_2_id) in comparisons:
             user_all_entities.add(entity_1_id)
             user_all_entities.add(entity_2_id)
 
