139578fbe5b1a6c5df2aa007d004e579279a425b
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-06-14 11:42:54 +0200

    * `UserFactory` accept the `email` parameter, so we can use it   intead of call `User.objects.create`
    * as there is only one email domain `@trusted.test`, we can
      use `EmailDomain.objects.get` instead of
      `EmailDomain.objects.filter().first()`. The former means "give
      me this object" and will fail if more than one object is found, the
      later means "take all objects that looks like this, and give me the
      first one". The `get` is more elegant in our case.
    
    * `TrustAlgoTestCase` is a more precise name than `UnitTest`. `UnitTest` is
      very generic and may also be an existing class name in an third-party
      module
    
    * I removed some delimiter comments as the class name itself is already
      acting as a delimiter
    
    * I move the top-level module documentation at the top level of the module,
      because it's the usual place for top-level docstring in Python

--- backend/vouch/tests/test_trust_algo.py
+++ backend/vouch/tests/test_trust_algo.py
@@ -1,21 +1,19 @@
+"""
+All test cases of the trust algorithm.
+"""
+
 import numpy as np
 import pytest
-from core.models.user import EmailDomain, User
-from core.models import user
-from vouch.models import Voucher
-from core.tests.factories.user import UserFactory
-
-from trust_algo import normalize_trust_values, get_trust_vector, rescale, trust_algo
-"""
-Test module for vouch and trust algo
-"""
 from django.test import TestCase
 
+from core.models import user
+from core.models.user import EmailDomain
+from core.tests.factories.user import UserFactory
+from vouch.models import Voucher
+from vouch.trust_algo import get_trust_vector, normalize_trust_values, rescale, trust_algo
 
-# ========== unit tests - trustalgo ===============
-# ensure normalization works
-class UnitTest(TestCase):
-    # create users and connections
+
+class TrustAlgoTestCse(TestCase):
     _user_0 = "username_0"
     _user_1 = "username_1"
     _user_2 = "username_2"
@@ -26,6 +24,50 @@ class UnitTest(TestCase):
     _user_7 = "username_7"
     _user_8 = "username_8"
     _user_9 = "username_9"
+
+    def setUp(self) -> None:
+        self.user_0 = UserFactory(username=self._user_0)
+        self.user_1 = UserFactory(username=self._user_1, email="user1@trusted.test")
+        self.user_2 = UserFactory(username=self._user_2)
+        self.user_3 = UserFactory(username=self._user_3, email="user3@trusted.test")
+        self.user_4 = UserFactory(username=self._user_4)
+        self.user_5 = UserFactory(username=self._user_5)
+        self.user_6 = UserFactory(username=self._user_6, email="user6@trusted.test")
+        self.user_7 = UserFactory(username=self._user_7)
+        self.user_8 = UserFactory(username=self._user_8)
+        self.user_9 = UserFactory(username=self._user_9)
+
+        email_domain = EmailDomain.objects.get(domain="@trusted.test")
+        email_domain.status = EmailDomain.STATUS_ACCEPTED
+        email_domain.save()
+
+        Voucher.objects.bulk_create(
+            [
+                # user_0 has given zero voucher
+                # user_1 has given three vouchers
+                Voucher(by=self.user_1, to=self.user_0, trust_value=10),
+                Voucher(by=self.user_1, to=self.user_3, trust_value=13),
+                Voucher(by=self.user_1, to=self.user_7, trust_value=17),
+                # user_2 has given one voucher
+                Voucher(by=self.user_2, to=self.user_5, trust_value=15),
+                # user_3 has given two vouchers
+                Voucher(by=self.user_3, to=self.user_1, trust_value=31),
+                Voucher(by=self.user_3, to=self.user_5, trust_value=35),
+                # user_4 has given one voucher
+                Voucher(by=self.user_4, to=self.user_7, trust_value=47),
+                # user_5 has given one voucher
+                Voucher(by=self.user_5, to=self.user_1, trust_value=51),
+                # user_6 has given zero voucher
+                # user_7 has given two vouchers
+                Voucher(by=self.user_7, to=self.user_1, trust_value=71),
+                Voucher(by=self.user_7, to=self.user_2, trust_value=72),
+                # user_8 has given one voucher
+                Voucher(by=self.user_8, to=self.user_3, trust_value=83),
+                # user_9 has given two vouchers
+                Voucher(by=self.user_9, to=self.user_4, trust_value=94),
+                Voucher(by=self.user_9, to=self.user_5, trust_value=95),
+            ]
+        )
 
     def test_normalization(self):
         C = np.random.rand(10, 10)
@@ -69,53 +111,6 @@ class UnitTest(TestCase):
 
             if trust_stat[i] > 0:
                 assert rescaled[i] >= 0.999999999
-
-    def setUp(self) -> None:
-        self.user_0 = UserFactory(username=self._user_0)
-        # UserFactory(username=self._user_1)
-        self.user_1 = User.objects.create_user(username=self._user_1, email="user1@trusted.test")
-        self.user_2 = UserFactory(username=self._user_2)
-        # UserFactory(username=self._user_3) #UserFactory(username=self._user_3)
-        self.user_3 = User.objects.create_user(username=self._user_3, email="user3@trusted.test")
-        self.user_4 = UserFactory(username=self._user_4)
-        self.user_5 = UserFactory(username=self._user_5)
-        # UserFactory(username=self._user_6)
-        self.user_6 = User.objects.create_user(username=self._user_6, email="user6@trusted.test")
-        self.user_7 = UserFactory(username=self._user_7)
-        self.user_8 = UserFactory(username=self._user_8)
-        self.user_9 = UserFactory(username=self._user_9)
-
-        email_domain = EmailDomain.objects.filter(domain="@trusted.test").first()
-        email_domain.status = EmailDomain.STATUS_ACCEPTED
-        email_domain.save()
-
-        Voucher.objects.bulk_create(
-            [
-                # user_0 has given zero voucher
-                # user_1 has given three vouchers
-                Voucher(by=self.user_1, to=self.user_0, trust_value=10),
-                Voucher(by=self.user_1, to=self.user_3, trust_value=13),
-                Voucher(by=self.user_1, to=self.user_7, trust_value=17),
-                # user_2 has given one voucher
-                Voucher(by=self.user_2, to=self.user_5, trust_value=15),
-                # user_3 has given two vouchers
-                Voucher(by=self.user_3, to=self.user_1, trust_value=31),
-                Voucher(by=self.user_3, to=self.user_5, trust_value=35),
-                # user_4 has given one voucher
-                Voucher(by=self.user_4, to=self.user_7, trust_value=47),
-                # user_5 has given one voucher
-                Voucher(by=self.user_5, to=self.user_1, trust_value=51),
-                # user_6 has given zero voucher
-                # user_7 has given two vouchers
-                Voucher(by=self.user_7, to=self.user_1, trust_value=71),
-                Voucher(by=self.user_7, to=self.user_2, trust_value=72),
-                # user_8 has given one voucher
-                Voucher(by=self.user_8, to=self.user_3, trust_value=83),
-                # user_9 has given two vouchers
-                Voucher(by=self.user_9, to=self.user_4, trust_value=94),
-                Voucher(by=self.user_9, to=self.user_5, trust_value=95),
-            ]
-        )
 
     def test_trust_algo(self):
         users = list(user.User.objects.all())
