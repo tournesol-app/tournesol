3bf73319adf78a75ce9e881f7adf5c3b8093d8eb
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-06-15 16:14:26 +0200

    factorize the raw SQL query

--- backend/tournesol/views/exports.py
+++ backend/tournesol/views/exports.py
@@ -12,6 +12,7 @@ from rest_framework.views import APIView
 
 from core.models import User
 from tournesol.entities.base import UID_DELIMITER
+from tournesol.lib.public_dataset import get_dataset
 from tournesol.models import Comparison
 from tournesol.serializers.comparison import ComparisonSerializer
 from tournesol.utils.cache import cache_page_no_i18n
@@ -59,55 +60,6 @@ def write_public_comparisons_file(write_target) -> None:
     ]
     writer = csv.DictWriter(write_target, fieldnames=fieldnames)
     writer.writeheader()
-
-    comparisons = Comparison.objects.raw(
-        """
-        SELECT
-            tournesol_comparison.id,
-
-            core_user.username,
-            entity_1.uid AS entity_a,
-            entity_2.uid AS entity_b,
-            comparisoncriteriascore.criteria,
-            comparisoncriteriascore.weight,
-            comparisoncriteriascore.score
-
-        FROM tournesol_comparison
-
-        -- this JOIN allows to filter on the desired poll
-        JOIN tournesol_poll
-          ON tournesol_poll.id = tournesol_comparison.poll_id
-
-        JOIN core_user
-          ON core_user.id = tournesol_comparison.user_id
-
-        JOIN tournesol_comparisoncriteriascore AS comparisoncriteriascore
-          ON comparisoncriteriascore.comparison_id = tournesol_comparison.id
-
-        JOIN tournesol_entity AS entity_1
-          ON entity_1.id = tournesol_comparison.entity_1_id
-
-        JOIN tournesol_entity AS entity_2
-          ON entity_2.id = tournesol_comparison.entity_2_id
-
-        -- this JOIN allows to filter by public ratings for the entity_1
-        -- the poll has been already filtered, no need to filter it again
-        JOIN tournesol_contributorrating AS rating_1
-          ON rating_1.entity_id = tournesol_comparison.entity_1_id
-         AND rating_1.user_id = tournesol_comparison.user_id
-
-        -- this JOIN allows to filter by public ratings for the entity_2
-        -- the poll has been already filtered, no need to filter it again
-        JOIN tournesol_contributorrating AS rating_2
-          ON rating_2.entity_id = tournesol_comparison.entity_2_id
-         AND rating_2.user_id = tournesol_comparison.user_id
-
-        WHERE tournesol_poll.name = 'videos'
-          -- keep only public ratings
-          AND rating_1.is_public = true
-          AND rating_2.is_public = true
-        """
-    )
     writer.writerows(
         {
             "public_username": comparison.username,
@@ -117,7 +69,7 @@ def write_public_comparisons_file(write_target) -> None:
             "weight": comparison.weight,
             "score": comparison.score,
         }
-        for comparison in comparisons
+        for comparison in get_dataset()
     )
 
 
