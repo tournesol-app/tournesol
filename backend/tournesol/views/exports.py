63274547057cb5e8fbf0ff10fb4471f15a0706fb
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-06-15 15:55:05 +0200

    [back] perf: speedup `ExportPublicComparisonsView` with raw SQL

--- backend/tournesol/views/exports.py
+++ backend/tournesol/views/exports.py
@@ -60,40 +60,65 @@ def write_public_comparisons_file(request, write_target):
     ]
     writer = csv.DictWriter(write_target, fieldnames=fieldnames)
     writer.writeheader()
-    public_data = (
-        ContributorRating.objects
-        .filter(is_public=True, poll__name=DEFAULT_POLL_NAME)
-        .select_related("user", "entity")
+
+    comparisons = Comparison.objects.raw(
+        """
+        SELECT
+            tournesol_comparison.id,
+
+            core_user.username,
+            entity_1.uid AS entity_a,
+            entity_2.uid AS entity_b,
+            comparisoncriteriascore.criteria,
+            comparisoncriteriascore.weight,
+            comparisoncriteriascore.score
+
+        FROM tournesol_comparison
+
+        -- this JOIN allows to filter on the desired poll
+        JOIN tournesol_poll
+          ON tournesol_poll.id = tournesol_comparison.poll_id
+
+        JOIN core_user
+          ON core_user.id = tournesol_comparison.user_id
+
+        JOIN tournesol_comparisoncriteriascore AS comparisoncriteriascore
+          ON comparisoncriteriascore.comparison_id = tournesol_comparison.id
+
+        JOIN tournesol_entity AS entity_1
+          ON entity_1.id = tournesol_comparison.entity_1_id
+
+        JOIN tournesol_entity AS entity_2
+          ON entity_2.id = tournesol_comparison.entity_2_id
+
+        -- this JOIN allows to filter by public ratings for the entity_1
+        -- the poll has been already filtered, no need to filter it again
+        JOIN tournesol_contributorrating AS rating_1
+          ON rating_1.entity_id = tournesol_comparison.entity_1_id
+         AND rating_1.user_id = tournesol_comparison.user_id
+
+        -- this JOIN allows to filter by public ratings for the entity_2
+        -- the poll has been already filtered, no need to filter it again
+        JOIN tournesol_contributorrating AS rating_2
+          ON rating_2.entity_id = tournesol_comparison.entity_2_id
+         AND rating_2.user_id = tournesol_comparison.user_id
+
+        WHERE tournesol_poll.name = 'videos'
+          -- keep only public ratings
+          AND rating_1.is_public = true
+          AND rating_2.is_public = true;
+        """
     )
-    serialized_comparisons = []
-    public_videos = set((rating.user, rating.entity) for rating in public_data)
-    comparisons = (
-        Comparison.objects
-        .filter(poll__name=DEFAULT_POLL_NAME)
-        .select_related("entity_1", "entity_2", "user")
-        .prefetch_related("criteria_scores")
-    )
-    public_comparisons = [
-        comparison
-        for comparison in comparisons
-        if (
-            (comparison.user, comparison.entity_1) in public_videos
-            and (comparison.user, comparison.entity_2) in public_videos
-        )
-    ]
-    public_usernames = [comparison.user.username for comparison in public_comparisons]
-    serialized_comparisons = ComparisonSerializer(public_comparisons, many=True).data
     writer.writerows(
         {
-            "public_username": public_username,
-            "video_a": comparison["entity_a"]["uid"].split(UID_DELIMITER)[1],
-            "video_b": comparison["entity_b"]["uid"].split(UID_DELIMITER)[1],
-            **criteria_score,
+            "public_username": comparison.username,
+            "video_a": comparison.uid_a.split(UID_DELIMITER)[1],
+            "video_b": comparison.uid_b.split(UID_DELIMITER)[1],
+            "criteria": comparison.criteria,
+            "weight": comparison.weight,
+            "score": comparison.score,
         }
-        for (public_username, comparison) in zip(
-            public_usernames, serialized_comparisons
-        )
-        for criteria_score in comparison["criteria_scores"]
+        for comparison in comparisons
     )
 
 
@@ -169,7 +194,9 @@ class ExportProofOfVoteView(PollScopedViewMixin, APIView):
     def get(self, request, *args, **kwargs):
         poll = self.poll_from_url
         response = HttpResponse(content_type="text/csv")
-        response["Content-Disposition"] = f'attachment; filename="proof_of_vote_{poll.name}.csv"'
+        response[
+            "Content-Disposition"
+        ] = f'attachment; filename="proof_of_vote_{poll.name}.csv"'
 
         fieldnames = [
             "user_id",
@@ -182,12 +209,8 @@ class ExportProofOfVoteView(PollScopedViewMixin, APIView):
         writer.writeheader()
         writer.writerows(
             {**d, "signature": poll.get_proof_of_vote(d["user_id"])}
-            for d in User.objects.filter(comparisons__poll=poll)
-            .values(
-                "username",
-                "email",
-                n_comparisons=Count("*"),
-                user_id=F("id")
+            for d in User.objects.filter(comparisons__poll=poll).values(
+                "username", "email", n_comparisons=Count("*"), user_id=F("id")
             )
         )
         return response
