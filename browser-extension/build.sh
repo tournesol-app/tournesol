9d4ed9f89a722ae818daded7d50252fbd0c58f46
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-06-20 15:05:26 +0200

    [ext] fix: the LICENSE file was in a sub-folder
    The `build.sh` script includes the license file as before.
    
    The `package.json` now clearly informs the users about the licence
    and the package name.

--- browser-extension/build.sh
+++ browser-extension/build.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
+
+#
+# Build a .zip target that can be uploaded to:
+# - Chrome Web Store
+# - Firefox Add-ons
+#
+
 set -eu
 
-cd src
-zip -r -FS ../tournesol_extension.zip *
+TARGET='tournesol_extension.zip'
+
+pushd src
+zip -r -FS ../${TARGET} *
+popd
+
+zip ${TARGET} LICENSE
+
