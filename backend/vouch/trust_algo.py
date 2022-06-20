6962c05dc756d5dad0a8037d52cfdd2cbbf840ec
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-06-20 11:09:09 +0200

    Reword some comments, add type hints to a function.

--- backend/vouch/trust_algo.py
+++ backend/vouch/trust_algo.py
@@ -1,19 +1,32 @@
 import numpy as np
+from numpy.typing import NDArray
 from vouch.models import Voucher
 from core.models import user
 from django.db.models import Q
-# hyper parameter of algorithm - select them wisely
-A = 0.2  # define the minimum ratio of trust that is left to pre-trusted users
+
+# Hyperparameters of the algorithm - select them wisely
+
+# The minimum ratio of trust that is left to pre-trusted users.
+A = 0.2
 EPS = 1e-8
-ALPHA = 0.1  # define how much voting weight goes to pre-trusted users for each user
+# How much voting weight goes to pre-trusted users for each user.
+ALPHA = 0.1
 
 
-def normalize_trust_values(C, trust_status):
+def normalize_trust_values(C: NDArray, trust_status: NDArray) -> NDArray:
     """
-    Normalize the trust values per user, and make users vouch for pre-trusted set.
-    If they vouch for other people, shift some of their ouching weight to others
+    Normalize the trust values per user, and make users vouch for the
+    pre-trusted set. If they vouch for other people, shift some of their
+    vouching weight to others.
+
+    Keyword arguments:
+    C -- A 2 dimensional array of trust values. The 1st dimension represents
+         the emitters, and the 2nd dimension represents the receivers.
+    trust_status -- TODO: describe this argument.
     """
-    trust_size = np.sum(np.array(trust_status) > 0, axis=0)  # size of the pre-trusted set
+    # Size of the pre-trusted set.
+    trust_size = np.sum(np.array(trust_status) > 0, axis=0)
+
     for i in range(len(C[0])):
         vouching_size = np.sum(np.array(C[i]) > 0, axis=0)
         for j in range(len(C[0])):
@@ -26,13 +39,14 @@ def normalize_trust_values(C, trust_status):
 
 def get_trust_vector(C, p):
     """
-    Return a vector of global trust values per user, given the vouchers in the network
-    and the set of pre-trusted users. This part comes directly from EigenTrust
+    Return a vector of global trust values per user, given the vouchers in the
+    network and the set of pre-trusted users. This part comes directly from
+    EigenTrust.
     """
     t = p
     newt = t
     delta = 10
-    while (delta >= EPS):
+    while delta >= EPS:
         newt = C.T.dot(t)
         newt = (1 - A) * newt + A * p
         delta = np.linalg.norm(newt - t)
@@ -42,43 +56,53 @@ def get_trust_vector(C, p):
 
 def rescale(trust_vector, trust_status):
     """
-    Go from ratio of trust to actual voting weight that should be assigned
-    to users given the trust the network puts in them
+    Go from ratio of trust to actual voting weight that should be assigned to
+    users given the trust the network puts in them.
     """
-    mint = np.amin([trust_vector[i] for i in range(len(trust_vector)) if trust_status[i] > 0])
+    mint = np.amin(
+        [trust_vector[i] for i in range(len(trust_vector)) if trust_status[i] > 0]
+    )
     return np.array(trust_vector) / mint
 
 
 def trust_algo():
     """
-    Improved version of the EigenTrust algorithm
-    Return a global trust score for each user, based on the set of
-    pre-trusted users (the ones with an email from a trusted domain)
-    and on vouchings made between users
+    Improved version of the EigenTrust algorithm.
+
+    Compute a global trust score for each user, based on the set of
+    pre-trusted users* and on vouching made between users.
+
+    (* the ones with an email from a trusted domain).
     """
-    # list of users, list of their trust status (regarding their email) in same order,
-    # and number of users
-    users = list(user.User.objects.all().annotate(_is_trusted=Q(pk__in=user.User.trusted_users())))
+    # List of users, list of their trust status (regarding their email) in the
+    # same order, and number of users.
+    users = list(
+        user.User.objects.all().annotate(
+            _is_trusted=Q(pk__in=user.User.trusted_users())
+        )
+    )
     trust_status = [int(u._is_trusted) for u in users]
     nb_users = len(users)
 
-    # create a matrix C where entry c_ij is the trust value user i gave to user j
-
+    # Create a matrix C where entry c_ij is the trust value a user i gave to
+    # a user j.
     C_ = np.zeros([nb_users, nb_users], dtype=float)
     for v in Voucher.objects.iterator():
         i = users.index(v.by)
         j = users.index(v.to)
         C_[i][j] = v.trust_value
-    # improved eigen trust algorithm
+
+    # Improved eigen trust algorithm.
     p = trust_status
-    p = p/np.sum(p)
-    # make the matrix to make it stochastic and give trust of users who didn't
-    # vouch much to pre-trusted set
+    p = p / np.sum(p)
+    # Make the matrix to make it stochastic and give trust of users who didn't
+    # vouch much to pre-trusted set.
     C = normalize_trust_values(C_, trust_status)
-    # get the a vector of trust given to each users by the network
+    # Get the a vector of trust given to each user by the network.
     trust_vector = get_trust_vector(C, p)
-    # normalize and rescale it
-    trust_vector = trust_vector/np.sum(trust_vector)
+
+    # Normalize and rescale it.
+    trust_vector = trust_vector / np.sum(trust_vector)
     voting_weight = rescale(trust_vector, trust_status)
     for k, u in enumerate(users):
         u.trust_score = float(voting_weight[k])
