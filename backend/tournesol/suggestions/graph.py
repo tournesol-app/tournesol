7de96193353b6e36cac39291abce160097ce62f6
Marc Gay-Balmaz <marc.gay-balmaz@epfl.ch>
2022-07-05 00:14:39 +0200

    [Back] Fixed sparsity score contribution

--- backend/tournesol/suggestions/graph.py
+++ backend/tournesol/suggestions/graph.py
@@ -322,7 +322,7 @@ class Graph(CompleteGraph):
                         if eigenvalues[-2] > self.LAMBDA_THRESHOLD:
                             u_index = sg.uid_to_index[vb.uid]
                             v_index = sg.uid_to_index[va.uid]
-                            va._graph_sparsity_score[vb] = sg.similarity_matrix[u_index, v_index]
+                            va._graph_sparsity_score[vb] = 1 - sg.similarity_matrix[u_index, v_index]
                         elif va.uid not in sg.uid_to_index:
                             va._graph_sparsity_score[vb] = 1
                         else:
