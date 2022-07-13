2553f953ca24c61dc1ac8e7e63661929519e20ad
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-07-13 10:57:24 +0200

    add "en" when "needed"

--- frontend/src/utils/polls/videos.ts
+++ frontend/src/utils/polls/videos.ts
@@ -1,4 +1,5 @@
 import { TFunction } from 'react-i18next';
+import { SUPPORTED_LANGUAGES } from 'src/i18n';
 import { PollsService, Recommendation } from 'src/services/openapi';
 import { OrderedDialogs } from 'src/utils/types';
 import { recommendationsLanguagesFromNavigator } from 'src/utils/recommendationsLanguages';
@@ -11,16 +12,27 @@ let VIDEOS: Promise<Recommendation[]> | null = null;
  * The recommendations returned:
  *   - are safe recommendations
  *   - are in the TOP 100 of all-time recommendations...
- *   - ...having a maximum duration of 5 minutes
- *   - are filtered according to the user's navigator languages
+ *     - ...having a maximum duration of 5 minutes
+ *   - are filtered according to the user's navigator languages...
+ *     - ...the "en" language is added if needed
  *
- * TODO:
- *   - if no reco. are available in the navigator languages, use `en`?
+ * It is possible that Tournesol doesn't have any recommendation matching one
+ * of the user's navigator languages.
+ *
+ * To prevent returning an empty list of recommendations, if the user's
+ * navigator languages don't contain at least one of the languages supported
+ * by the UI, the default language "en" will be added to the metadata filter
+ * of the recommendations request.
  */
 export function getTutorialVideos(): Promise<Recommendation[]> {
   if (VIDEOS != null) {
     return VIDEOS;
   }
+
+  const supportedLangCodes = SUPPORTED_LANGUAGES.map((l) => l.code);
+  const langIsSupported = (langCode: string) =>
+    supportedLangCodes.includes(langCode);
+
   const metadata: Record<string, string | string[]> = {};
 
   const minutesMax = 5;
@@ -28,6 +40,13 @@ export function getTutorialVideos(): Promise<Recommendation[]> {
 
   metadata['duration:lte:int'] = (60 * minutesMax).toString();
   metadata['language'] = recommendationsLanguagesFromNavigator().split(',');
+
+  // Add "en" to the recommendations request, so that users will always get
+  // recommendations, even if Tournesol doesn't have any recommendation
+  // matching their navigator languages.
+  if (!metadata['language'].some(langIsSupported)) {
+    metadata['language'].push('en');
+  }
 
   VIDEOS = PollsService.pollsRecommendationsList({
     name: 'videos',
