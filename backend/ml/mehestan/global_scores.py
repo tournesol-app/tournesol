f463b6e1322f119fc8e8fd3cd59a9516b3ae5e37
cthiounn <45511529+cthiounn@users.noreply.github.com>
2022-07-27 12:32:06 +0200

    fix ohmv2  : coordinate descent with subiterations

--- backend/ml/mehestan/global_scores.py
+++ backend/ml/mehestan/global_scores.py
@@ -49,7 +49,7 @@ def get_significantly_different_pairs(scores: pd.DataFrame):
     that are significantly different, according to the contributor scores.
     (Used for collaborative preference scaling)
     """
-    scores = scores[["uid", "raw_score", "raw_uncertainty"]]
+    scores = scores[["uid", "score", "uncertainty"]]
     left, right = np.triu_indices(len(scores), k=1)
     pairs = (
         scores.iloc[left]
@@ -62,8 +62,8 @@ def get_significantly_different_pairs(scores: pd.DataFrame):
     )
     pairs.set_index(["uid_a", "uid_b"], inplace=True)
     return pairs.loc[
-        np.abs(pairs.raw_score_a - pairs.raw_score_b)
-        >= 2 * (pairs.raw_uncertainty_a + pairs.raw_uncertainty_b)
+        np.abs(pairs.score_a - pairs.score_b)
+        >= 2 * (pairs.uncertainty_a + pairs.uncertainty_b)
     ]
 
 
@@ -115,20 +115,20 @@ def compute_scaling(
             ABnm = ABn_all.join(ABm, how="inner", lsuffix="_n", rsuffix="_m")
             if len(ABnm) == 0:
                 continue
-            s_nqmab = np.abs(ABnm.raw_score_a_m - ABnm.raw_score_b_m) / np.abs(
-                ABnm.raw_score_a_n - ABnm.raw_score_b_n
+            s_nqmab = np.abs(ABnm.score_a_m - ABnm.score_b_m) / np.abs(
+                ABnm.score_a_n - ABnm.score_b_n
             )
 
             delta_s_nqmab = (
                 (
-                    np.abs(ABnm.raw_score_a_m - ABnm.raw_score_b_m)
-                    + ABnm.raw_uncertainty_a_m
-                    + ABnm.raw_uncertainty_b_m
+                    np.abs(ABnm.score_a_m - ABnm.score_b_m)
+                    + ABnm.uncertainty_a_m
+                    + ABnm.uncertainty_b_m
                 )
                 / (
-                    np.abs(ABnm.raw_score_a_n - ABnm.raw_score_b_n)
-                    - ABnm.raw_uncertainty_a_n
-                    - ABnm.raw_uncertainty_b_n
+                    np.abs(ABnm.score_a_n - ABnm.score_b_n)
+                    - ABnm.uncertainty_a_n
+                    - ABnm.uncertainty_b_n
                 )
             ) - s_nqmab
 
@@ -138,7 +138,7 @@ def compute_scaling(
             s_weights.append(scaling_weights[user_m])
 
         s_weights = np.array(s_weights)
-        theta_inf = np.max(user_scores.raw_score.abs())
+        theta_inf = np.max(user_scores.score.abs())
         s_nqm = np.array(s_nqm)
         delta_s_nqm = np.array(delta_s_nqm)
         if compute_uncertainties:
@@ -176,12 +176,11 @@ def compute_scaling(
             n_scores = user_scores.set_index("uid").loc[common_uids]
 
             tau_nqmab = (
-                s_dict.get(user_m, 1) * m_scores.raw_score
-                - s_dict[user_n] * n_scores.raw_score
+                s_dict.get(user_m, 1) * m_scores.score - s_dict[user_n] * n_scores.score
             )
             delta_tau_nqmab = (
-                s_dict[user_n] * n_scores.raw_uncertainty
-                + s_dict.get(user_m, 1) * m_scores.raw_uncertainty
+                s_dict[user_n] * n_scores.uncertainty
+                + s_dict.get(user_m, 1) * m_scores.uncertainty
             )
 
             tau = QrMed(1, 1, tau_nqmab, delta_tau_nqmab)
--- backend/ml/mehestan/online_heuristics.py
+++ backend/ml/mehestan/online_heuristics.py
@@ -2,7 +2,7 @@ import logging
 import os
 from functools import partial
 from multiprocessing import Pool
-from typing import Tuple
+from typing import Set, Tuple
 
 import numpy as np
 import pandas as pd
@@ -10,11 +10,7 @@ from django import db
 
 from core.models import User
 from ml.inputs import MlInput, MlInputFromDb
-from ml.outputs import (  # insert_or_update_contributor_score,
-    save_contributor_scores,
-    save_entity_scores,
-    save_tournesol_scores,
-)
+from ml.outputs import save_contributor_scores, save_entity_scores, save_tournesol_scores
 from tournesol.models import Entity, Poll
 from tournesol.models.entity_score import ScoreMode
 from tournesol.utils.constants import COMPARISON_MAX
@@ -28,21 +24,20 @@ from .poll_scaling import (
 logger = logging.getLogger(__name__)
 
 R_MAX = COMPARISON_MAX
+TAU_SUBITERATION_NUMBER = 2
 ALPHA = 0.01  # Signal-to-noise hyperparameter
 
 
 def get_new_scores_from_online_update(
     all_comparison_user_for_criteria: pd.DataFrame,
-    id_entity_a: str,
-    id_entity_b: str,
+    set_of_entity_to_update: Set[str],
     previous_individual_raw_scores: pd.DataFrame,
-) -> Tuple[float]:
+) -> Tuple[pd.DataFrame]:
+    new_raw_scores = previous_individual_raw_scores
+    new_raw_uncertainties = pd.DataFrame()
+
     scores = all_comparison_user_for_criteria[["entity_a", "entity_b", "score"]]
     all_entities = set(scores["entity_a"]) | set(scores["entity_b"])
-    all_scores_values = set(scores["score"])
-    # Null Matrix case
-    if len(all_scores_values) == 1 and np.isnan(all_scores_values.pop()):
-        return (0, 0, 0, 0)
     scores_sym = pd.concat(
         [
             scores,
@@ -58,113 +53,50 @@ def get_new_scores_from_online_update(
 
     # "Comparison tensor": matrix with all comparisons, values in [-R_MAX, R_MAX]
     r = scores_sym.pivot(index="entity_a", columns="entity_b", values="score")
-    dont_compute_a, dont_compute_b = False, False
-    if (
-        r.loc[
-            id_entity_a,
-        ]
-        .dropna()
-        .empty
-    ):
-        dont_compute_a = True
-    if (
-        r.loc[
-            id_entity_b,
-        ]
-        .dropna()
-        .empty
-    ):
-        dont_compute_b = True
 
-    if dont_compute_a and dont_compute_b:
-        return (0, 0, 0, 0)
-    else:
-        r_tilde = r / (1.0 + R_MAX)
-        r_tilde2 = r_tilde**2
-        # r.loc[a:b] is negative when a is prefered to b.
-        l = -1.0 * r_tilde / np.sqrt(1.0 - r_tilde2)  # noqa: E741
-        k = (1.0 - r_tilde2) ** 3
+    r_set = r.loc[list(set_of_entity_to_update)]
+    r_tilde = r_set / (1.0 + R_MAX)
+    r_tilde2 = r_tilde**2
+    # r.loc[a:b] is negative when a is prefered to b.
+    l = -1.0 * r_tilde / np.sqrt(1.0 - r_tilde2)  # noqa: E741
+    k = (1.0 - r_tilde2) ** 3
 
-        L = k.mul(l).sum(axis=1)
-        Kaa_np = np.array(k.sum(axis=1) + ALPHA)
-        L_tilde = L / Kaa_np
+    L = k.mul(l).sum(axis=1)
+    Kaa_np = np.array(k.sum(axis=1) + ALPHA)
 
-        U_ab = -k / Kaa_np[:, None]
-        U_ab = U_ab.fillna(0)
+    # to compute dot_product, we need vector of previous_scores to be complete
+    for entity in all_entities:
+        if not new_raw_scores.index.isin([entity]).any():
+            new_raw_scores.loc[entity] = 0.0
+    new_raw_scores = new_raw_scores[new_raw_scores.index.isin(all_entities)].copy()
 
-        # to compute dot_product, we need vector of previous_scores to be complete
-        for entity in all_entities:
-            if not previous_individual_raw_scores.index.isin([entity]).any():
-                previous_individual_raw_scores.loc[entity] = 0.0
-        previous_individual_raw_scores = previous_individual_raw_scores[
-            previous_individual_raw_scores.index.isin(all_entities)
-        ].copy()
+    new_raw_scores = (
+        (L - (k.fillna(0).dot(new_raw_scores))["raw_score"]) / Kaa_np
+    ).to_frame(name="raw_score")
 
-        dot_product = U_ab.dot(previous_individual_raw_scores)
-
-        if dont_compute_a:
-            theta_star_a = 0.0
-        else:
-            theta_star_a = compute_new_individual_score_with_heuristics_update(
-                id_entity_a, L_tilde, dot_product
-            )
-
-        if dont_compute_b:
-            theta_star_b = 0.0
-        else:
-            theta_star_b = compute_new_individual_score_with_heuristics_update(
-                id_entity_b, L_tilde, dot_product
-            )
-
-        previous_individual_raw_scores.loc[
-            previous_individual_raw_scores.index == id_entity_a, "raw_score"
-        ] = theta_star_a
-        previous_individual_raw_scores.loc[
-            previous_individual_raw_scores.index == id_entity_b, "raw_score"
-        ] = theta_star_b
-
-        # Compute uncertainties
-        scores_series = previous_individual_raw_scores.squeeze()
-        scores_np = scores_series.to_numpy()
-        theta_star_ab = pd.DataFrame(
-            np.subtract.outer(scores_np, scores_np),
-            index=scores_series.index,
-            columns=scores_series.index,
-        )
-
-        K_diag = pd.DataFrame(
-            data=np.diag(k.sum(axis=1) + ALPHA),
-            index=k.index,
-            columns=k.index,
-        )
-
-        sigma2 = (1.0 + (np.nansum(k * (l - theta_star_ab) ** 2) / 2)) / len(scores)
-
-        delta_star = pd.Series(
-            np.sqrt(sigma2) / np.sqrt(np.diag(K_diag)), index=K_diag.index
-        )
-        if dont_compute_a:
-            delta_star_a = 0.0
-        else:
-            delta_star_a = delta_star[id_entity_a]
-        if dont_compute_b:
-            delta_star_b = 0.0
-        else:
-            delta_star_b = delta_star[id_entity_b]
-
-    return (theta_star_a, delta_star_a, theta_star_b, delta_star_b)
-
-
-def compute_new_individual_score_with_heuristics_update(
-    id_entity_a, L_tilde, dot_product
-):
-    L_tilde_a = L_tilde[id_entity_a]
-    theta_star_a = (
-        (L_tilde_a - dot_product[dot_product.index == id_entity_a].values)
-        .squeeze()[()]
-        .item()
+    # Compute uncertainties
+    scores_series = previous_individual_raw_scores.squeeze()
+    scores_np = scores_series.to_numpy()
+    theta_star_ab = pd.DataFrame(
+        np.subtract.outer(scores_np, scores_np),
+        index=scores_series.index,
+        columns=scores_series.index,
     )
-    return theta_star_a
+
+    K_diag = pd.DataFrame(
+        data=np.diag(k.sum(axis=1) + ALPHA),
+        index=k.index,
+        columns=k.index,
+    )
+
+    sigma2 = (1.0 + (np.nansum(k * (l - theta_star_ab) ** 2) / 2)) / len(scores)
+
+    delta_star = pd.Series(
+        np.sqrt(sigma2) / np.sqrt(np.diag(K_diag)), index=K_diag.index
+    )
+    new_raw_uncertainties = delta_star.to_frame(name="raw_uncertainty")
+
+    return new_raw_scores, new_raw_uncertainties
 
 
 def _run_online_heuristics_for_criterion(
@@ -202,6 +134,7 @@ def _run_online_heuristics_for_criterion(
     all_comparison_of_user_for_criteria = ml_input.get_comparisons(
         criteria=criteria, user_id=user_id
     )
+
     entity_id_a = Entity.objects.get(uid=uid_a).pk
     entity_id_b = Entity.objects.get(uid=uid_b).pk
 
@@ -223,26 +156,48 @@ def _run_online_heuristics_for_criterion(
     ):
         return
 
-    # Now with previous trick and valid condition check,
-    # all_comparison_of_user_for_criteria have a pair (entity_id_b,entity_id_a)
-    (
-        theta_star_a,
-        delta_star_a,
-        theta_star_b,
-        delta_star_b,
-    ) = compute_and_update_individual_scores_online_heuristics(
-        criteria,
-        ml_input,
-        user_id,
-        poll,
-        all_comparison_of_user_for_criteria,
-        entity_id_a,
-        entity_id_b,
+    set_of_entity_to_update = {entity_id_a, entity_id_b}
+    previous_individual_raw_scores = ml_input.get_indiv_score(
+        user_id=user_id, criteria=criteria
     )
-    # so far we have recompute new indiv score for a and b,
+    previous_individual_raw_scores = previous_individual_raw_scores[
+        ["entity_id", "raw_score"]
+    ]
+    previous_individual_raw_scores = previous_individual_raw_scores.set_index(
+        "entity_id"
+    )
+    new_raw_scores = previous_individual_raw_scores
+    for tau in range(0, TAU_SUBITERATION_NUMBER+1):
+        if tau > 0:
+            set_of_entity_to_update = compute_and_give_next_set_of_entity_to_update(
+                set_of_entity_to_update, all_comparison_of_user_for_criteria
+            )
+
+        # Now with previous trick and valid condition check,
+        # all_comparison_of_user_for_criteria have a pair (entity_id_b,entity_id_a)
+        new_raw_scores, new_raw_uncertainties = get_new_scores_from_online_update(
+            all_comparison_of_user_for_criteria,
+            set_of_entity_to_update,
+            new_raw_scores,
+        )
+        print(tau,new_raw_scores,sum(new_raw_scores["raw_score"]))
+
+    # so far we have recompute new indiv score for a and b and neighbours,
     # we need to recompute global score for a and b
     # in order to so, we need all individual scaled score concerning a and b
     # we will get those and inject the new raw score before scaling for {a|b}/criteria/user_id
+    theta_star_a = new_raw_scores.loc[
+        new_raw_scores.index == entity_id_a, "raw_score"
+    ].values.squeeze()[()]
+    delta_star_a = new_raw_uncertainties.loc[
+        new_raw_uncertainties.index == entity_id_a, "raw_uncertainty"
+    ].values.squeeze()[()]
+    theta_star_b = new_raw_scores.loc[
+        new_raw_scores.index == entity_id_b, "raw_score"
+    ].values.squeeze()[()]
+    delta_star_b = new_raw_uncertainties.loc[
+        new_raw_uncertainties.index == entity_id_b, "raw_uncertainty"
+    ].values.squeeze()[()]
     new_data_a = (entity_id_a, theta_star_a, delta_star_a)
     new_data_b = (entity_id_b, theta_star_b, delta_star_b)
 
@@ -277,6 +232,22 @@ def _run_online_heuristics_for_criterion(
             single_criteria=criteria,
             single_user_id=user_id,
         )
+
+
+def compute_and_give_next_set_of_entity_to_update(
+    set_of_entity_to_update: Set[str], all_comparison_of_user_for_criteria: pd.DataFrame
+):
+    set_of_neighbours_1 = set(
+        all_comparison_of_user_for_criteria.loc[
+            all_comparison_of_user_for_criteria.entity_a.isin(set_of_entity_to_update)
+        ]["entity_b"]
+    )
+    set_of_neighbours_2 = set(
+        all_comparison_of_user_for_criteria.loc[
+            all_comparison_of_user_for_criteria.entity_b.isin(set_of_entity_to_update)
+        ]["entity_a"]
+    )
+    return set_of_entity_to_update | set_of_neighbours_1 | set_of_neighbours_2
 
 
 def calculate_and_save_global_scores_in_all_score_mode(
@@ -422,49 +393,6 @@ def check_requirements_are_good_for_online_heuristics(
     return True
 
 
-def compute_and_update_individual_scores_online_heuristics(
-    criteria: str,
-    ml_input: MlInput,
-    user_id: int,
-    poll: Poll,
-    df_all_comparison_user_for_criteria: pd.DataFrame,
-    entity_id_a: int,
-    entity_id_b: int,
-):
-    """
-    this function apply the online heuristics to raw score for the user and the concerned entities
-    1. we get all the previous scores from the user and the criteria
-    2. we compute the new raw_score and raw_uncertainty (get_new_scores_from_online_update)
-    3. we save the new raw_score
-    """
-    previous_individual_raw_scores = ml_input.get_indiv_score(
-        user_id=user_id, criteria=criteria
-    )
-    previous_individual_raw_scores = previous_individual_raw_scores[
-        ["entity_id", "raw_score"]
-    ]
-    previous_individual_raw_scores = previous_individual_raw_scores.set_index(
-        "entity_id"
-    )
-    (
-        theta_star_a,
-        delta_star_a,
-        theta_star_b,
-        delta_star_b,
-    ) = get_new_scores_from_online_update(
-        df_all_comparison_user_for_criteria,
-        entity_id_a,
-        entity_id_b,
-        previous_individual_raw_scores,
-    )
-    return (
-        theta_star_a,
-        delta_star_a,
-        theta_star_b,
-        delta_star_b,
-    )
-
-
 def run_online_heuristics(
     ml_input: MlInput,
     uid_a: str,
@@ -502,6 +430,20 @@ def run_online_heuristics(
     poll_pk = poll.pk
     criteria = poll.criterias_list
 
+    _run_online_heuristics_for_criterion(
+        ml_input=ml_input,
+        poll_pk=poll_pk,
+        criteria=poll.main_criteria,
+        uid_a=uid_a,
+        uid_b=uid_b,
+        user_id=user_id,
+        delete_comparison_case=delete_comparison_case,
+    )
+    save_tournesol_scores(poll)
+    logger.info(
+        "Online Heuristic Mehestan for poll '%s': main_criteria Done", poll.name
+    )
+
     partial_online_heuristics = partial(
         _run_online_heuristics_for_criterion,
         ml_input=ml_input,
@@ -511,28 +453,28 @@ def run_online_heuristics(
         user_id=user_id,
         delete_comparison_case=delete_comparison_case,
     )
+    # compute each criterion in parallel
+    remaining_criteria = [c for c in criteria if c != poll.main_criteria]
     if parallel_computing:
         os.register_at_fork(before=db.connections.close_all)
 
-        # compute each criterion in parallel
-        with Pool(processes=max(1, os.cpu_count() - 1)) as pool:
+        cpu_count = os.cpu_count() or 1
+        with Pool(processes=max(1, cpu_count - 1)) as pool:
             for _ in pool.imap_unordered(
                 partial_online_heuristics,
-                criteria,
+                remaining_criteria,
             ):
                 pass
     else:
-        for criterion in criteria:
+        for criterion in remaining_criteria:
             logger.info(
                 "Sequential Online Heuristic Mehestan  \
                 for poll '%s  for criterion '%s': Start ",
                 poll.name,
                 criterion,
             )
-
             partial_online_heuristics(criterion)
 
-    save_tournesol_scores(poll)
     logger.info("Online Heuristic Mehestan for poll '%s': Done", poll.name)
 
 
