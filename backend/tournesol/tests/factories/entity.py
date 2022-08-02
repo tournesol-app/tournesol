ad707094fee32d6680daa29b91549aba0f6f3f5e
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-08-02 16:09:57 +0200

    [back] refactor: remove the default poll of the `RateLater` model

--- backend/tournesol/tests/factories/entity.py
+++ backend/tournesol/tests/factories/entity.py
@@ -5,28 +5,28 @@ import string
 import factory
 
 from core.tests.factories.user import UserFactory
-from tournesol.models import Entity, EntityCriteriaScore, RateLater
+from tournesol.models import Entity, EntityCriteriaScore, Poll, RateLater
 
 
 class EntityFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Entity
 
-    uid = factory.Sequence(lambda n: 'uid_%s' % n)
+    uid = factory.Sequence(lambda n: "uid_%s" % n)
 
 
 def generate_youtube_id():
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=11))
+    return "".join(random.choices(string.ascii_uppercase + string.digits, k=11))
 
 
 class VideoMetadataFactory(factory.DictFactory):
     source = "youtube"
     video_id = factory.LazyFunction(generate_youtube_id)
-    name = factory.Sequence(lambda n: 'Entity title %s' % n)
-    description = factory.Sequence(lambda n: 'Entity description %s' % n)
+    name = factory.Sequence(lambda n: "Entity title %s" % n)
+    description = factory.Sequence(lambda n: "Entity description %s" % n)
     duration = 60
-    language = 'aa'
-    uploader = factory.Sequence(lambda n: 'Uploader %s' % n)
+    language = "aa"
+    uploader = factory.Sequence(lambda n: "Uploader %s" % n)
     publication_date = factory.LazyFunction(lambda: datetime.date.today().isoformat())
 
 
@@ -37,7 +37,6 @@ class VideoFactory(EntityFactory):
 
 
 class VideoCriteriaScoreFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = EntityCriteriaScore
 
@@ -47,9 +46,9 @@ class VideoCriteriaScoreFactory(factory.django.DjangoModelFactory):
 
 
 class RateLaterFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = RateLater
 
     entity = factory.SubFactory(VideoFactory)
     user = factory.SubFactory(UserFactory)
+    poll = factory.LazyAttribute(lambda n: Poll.default_poll())
