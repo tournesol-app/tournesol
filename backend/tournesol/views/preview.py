42c10a7e00ca2f51701f8c9f56e8276dd7502aab
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-08-23 14:53:12 +0200

    return early and more tweaks
    - use the return early pattern
    
    - more explicit error message
    
    - run black

--- backend/tournesol/views/preview.py
+++ backend/tournesol/views/preview.py
@@ -8,6 +8,7 @@ from drf_spectacular.utils import OpenApiTypes, extend_schema
 from PIL import Image, ImageDraw, ImageFont
 from rest_framework.views import APIView
 
+from tournesol.entities.video import TYPE_VIDEO
 from tournesol.models.entity import Entity
 from tournesol.utils.cache import cache_page_no_i18n
 
@@ -46,50 +47,60 @@ class DynamicWebsitePreviewEntity(APIView):
 
         try:
             entity = Entity.objects.get(uid=uid)
-            response = HttpResponse(content_type="image/png")
-            tournesol_footer = Image.new("RGBA", (320, 60), (255, 200, 0, 255))
-            tournesol_footer_draw = ImageDraw.Draw(tournesol_footer)
+        except Entity.DoesNotExist as e:
+            logger.error(f"Preview impossible entity with UID {uid}.")
+            logger.error(f"Exception caught: {e}")
+            return DynamicWebsitePreviewDefault.default_preview()
 
-            truncated_title = entity.metadata.get("name", "")[:200]
-            # TODO: optimize this with a dichotomic search
-            while tournesol_footer_draw.textlength(truncated_title, font=fnt_title) > 300:
-                truncated_title = truncated_title[:-4] + "..."
+        if entity.type != TYPE_VIDEO:
+            logger.info(f"Preview not implemented for entity with UID {entity.uid}.")
+            return DynamicWebsitePreviewDefault.default_preview()
+
+        response = HttpResponse(content_type="image/png")
+        tournesol_footer = Image.new("RGBA", (320, 60), (255, 200, 0, 255))
+        tournesol_footer_draw = ImageDraw.Draw(tournesol_footer)
+
+        truncated_title = entity.metadata.get("name", "")[:200]
+        # TODO: optimize this with a dichotomic search
+        while tournesol_footer_draw.textlength(truncated_title, font=fnt_title) > 300:
+            truncated_title = truncated_title[:-4] + "..."
+        tournesol_footer_draw.text(
+            (10, 6), truncated_title, font=fnt_title, fill=(29, 26, 20, 255)
+        )
+
+        score = entity.tournesol_score
+        if score is not None:
             tournesol_footer_draw.text(
-                (10, 6), truncated_title, font=fnt_title, fill=(29, 26, 20, 255)
+                (10, 25),
+                "Tournesol Score: %.0f" % score,
+                font=fnt,
+                fill=(29, 26, 20, 255),
             )
 
-            score = entity.tournesol_score
-            if score is not None:
-                tournesol_footer_draw.text(
-                    (10, 25), "Tournesol Score: %.0f" % score, font=fnt, fill=(29, 26, 20, 255)
-                )
-
-            url = f"https://img.youtube.com/vi/{entity.video_id}/mqdefault.jpg"
+        url = f"https://img.youtube.com/vi/{entity.video_id}/mqdefault.jpg"
+        try:
             thumbnail_response = requests.get(url)
-            if thumbnail_response.status_code != 200:
-                logger.warning(
-                    f"Fetching youtube thumbnail has non-200 status: {thumbnail_response.status_code}"
-                )
-                # Choosing not to raise an error here because the reponse often has a non-200
-                # status while containing the right content (e.g. 304, 443)
-                # raise ConnectionError
-            youtube_thumbnail = Image.open(BytesIO(thumbnail_response.content)).convert("RGBA")
-
-            # Merges the two images into one
-            preview_image = Image.new("RGBA", (320, 240), (255, 255, 255, 0))
-            preview_image.paste(youtube_thumbnail)
-            preview_image.paste(tournesol_footer, box=(0, 180))
-            preview_image.save(response, 'png')
-            return response
-        except Entity.DoesNotExist as e:
-            # Probably: The entity does not exist on Tournesol
-            logger.error(f"Error Caught: {e}")
-            return DynamicWebsitePreviewDefault.default_preview()
-        except AttributeError as e:
-            # Probably: The entity is not a video and does not have a field `video_id`
-            logger.error(f"Error Caught: {e}")
-            return DynamicWebsitePreviewDefault.default_preview()
         except ConnectionError as e:
-            # Probably: The thumbnail could not be fetched from youtube
-            logger.error(f"Error Caught: {e}")
+            logger.error(f"Preview impossible entity with UID {uid}.")
+            logger.error(f"Exception caught: {e}")
             return DynamicWebsitePreviewDefault.default_preview()
+
+        if thumbnail_response.status_code != 200:
+            # We chose to not raise an error here because the responses often
+            # have a non-200 status while containing the right content (e.g.
+            # 304, 443).
+            # raise ConnectionError
+            logger.warning(
+                f"Fetching YouTube thumbnail has non-200 status: {thumbnail_response.status_code}"
+            )
+
+        youtube_thumbnail = Image.open(BytesIO(thumbnail_response.content)).convert(
+            "RGBA"
+        )
+
+        # Merge the two images into one.
+        preview_image = Image.new("RGBA", (320, 240), (255, 255, 255, 0))
+        preview_image.paste(youtube_thumbnail)
+        preview_image.paste(tournesol_footer, box=(0, 180))
+        preview_image.save(response, "png")
+        return response
