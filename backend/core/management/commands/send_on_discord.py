c57ecbbd157eb7ea4e62a241bb256bd3684b77f3
aidanjungo <aidan@cfse.ch>
2022-08-23 20:30:41 +0200

    fix lint

--- backend/core/management/commands/send_on_discord.py
+++ backend/core/management/commands/send_on_discord.py
@@ -3,12 +3,10 @@ Post a message in a channel of the Tournesol's Discord server
 """
 
 import logging
-import sys
 
 import requests
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-from django.utils import timezone
 
 
 def publish_on_discord(discord_channel, message):
@@ -19,7 +17,7 @@ def publish_on_discord(discord_channel, message):
     except KeyError:
         logging.warning(f"The webhook for {discord_channel} does not exist!")
         return
-  
+
     resp = requests.post(webhook_url, json={"content": message})
 
     resp.raise_for_status()
@@ -59,6 +57,6 @@ class Command(BaseCommand):
             self.stdout.write(f"Discord channel: {discord_channel}")
 
         publish_on_discord(discord_channel, options["message"])
-        
+
         self.stdout.write(self.style.SUCCESS(f"Message posted on {discord_channel}"))
         self.stdout.write("end")
