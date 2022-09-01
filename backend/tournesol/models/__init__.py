9ba121f5648fabfe9929533689c0039736afc25c
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-09-01 17:00:46 +0200

    expose the new model

--- backend/tournesol/models/__init__.py
+++ backend/tournesol/models/__init__.py
@@ -5,6 +5,7 @@ Models for Tournesol app
 from .comparisons import Comparison, ComparisonCriteriaScore
 from .criteria import Criteria, CriteriaLocale, CriteriaRank
 from .entity import Entity
+from .entity_poll_rating import EntityPollRating
 from .entity_score import EntityCriteriaScore
 from .poll import Poll
 from .rate_later import RateLater
