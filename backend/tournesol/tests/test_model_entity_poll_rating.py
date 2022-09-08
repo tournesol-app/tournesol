0509a5ed609fad6ab6b3d0bd90e6401a91322ed5
Mayeul-T <mayeul.touchard@polyconseil.fr>
2022-09-08 16:50:23 +0200

    [ISSUE-631] Fix method + add firsts tests

---
+++ backend/tournesol/tests/test_model_entity_poll_rating.py
@@ -0,0 +1,72 @@
+"""
+All test cases of the `EntityPollRating` model.
+"""
+
+from django.db import IntegrityError, transaction
+from django.test import TestCase
+
+from core.tests.factories.user import UserFactory
+from tournesol.models import Comparison, EntityPollRating
+from tournesol.models.rate_later import RateLater
+from tournesol.tests.factories.entity import VideoFactory
+from tournesol.tests.factories.poll import PollFactory
+
+
+class RateLaterTestCase(TestCase):
+    """
+    TestCase of the `RateLater` model.
+    """
+
+    _user = "username"
+
+    def setUp(self):
+        self.video_1 = VideoFactory()
+        self.video_2 = VideoFactory()
+        self.user = UserFactory(username=self._user)
+        self.poll = PollFactory()
+        self.comparaison = Comparison.objects.create(entity_1=self.video_1, entity_2=self.video_2,
+                                                     poll=self.poll, user=self.user)
+        self.entity_poll_rating_1 = EntityPollRating.objects.create(entity=self.video_1,
+                                                                    poll=self.poll)
+        self.entity_poll_rating_2 = EntityPollRating.objects.create(entity=self.video_2,
+                                                                    poll=self.poll)
+
+        self.entity_poll_rating_1.update_n_ratings()
+        self.entity_poll_rating_2.update_n_ratings()
+
+    def test_update_n_ratings(self):
+        # [GIVEN] Add new comparison between viode 1 and a third video
+        self.video_3 = VideoFactory()
+        Comparison.objects.create(entity_1=self.video_1, entity_2=self.video_3, poll=self.poll,
+                                  user=self.user)
+
+        # [WHEN] We update the entity poll ratings
+        self.entity_poll_rating_1.update_n_ratings()
+        self.entity_poll_rating_2.update_n_ratings()
+
+        # [THEN] Only video nb comparison has been increased
+        updated_rating_1 = self.video_1.all_poll_ratings.filter(poll=self.poll).first()
+        updated_rating_2 = self.video_2.all_poll_ratings.filter(poll=self.poll).first()
+        self.assertEqual(updated_rating_1.n_comparisons, 2)
+        self.assertEqual(updated_rating_1.n_contributors, 1)
+        self.assertEqual(updated_rating_2.n_comparisons, 1)
+        self.assertEqual(updated_rating_2.n_contributors, 1)
+
+    def test_update_n_ratings_n_contributors(self):
+        # [GIVEN] Add new comparison between viode 1 and video 2 with a second user
+        self.user_2 = UserFactory(username="username_2")
+        self.video_3 = VideoFactory()
+        Comparison.objects.create(entity_1=self.video_1, entity_2=self.video_3, poll=self.poll,
+                                  user=self.user_2)
+
+        # [WHEN] We update the entity poll ratings
+        self.entity_poll_rating_1.update_n_ratings()
+        self.entity_poll_rating_2.update_n_ratings()
+
+        # [THEN] Only video 1 nb comparison and nb contributors has been increased
+        updated_rating_1 = self.video_1.all_poll_ratings.filter(poll=self.poll).first()
+        updated_rating_2 = self.video_2.all_poll_ratings.filter(poll=self.poll).first()
+        self.assertEqual(updated_rating_1.n_comparisons, 2)
+        self.assertEqual(updated_rating_1.n_contributors, 2)
+        self.assertEqual(updated_rating_2.n_comparisons, 1)
+        self.assertEqual(updated_rating_2.n_contributors, 1)
