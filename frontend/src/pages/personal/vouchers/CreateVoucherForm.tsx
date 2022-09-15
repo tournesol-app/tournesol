tournesol-app/tournesol

commit cbd9f5cf6661d143fc1e99142c9fe57e1e771170

[front] style: add visual gap in the two columns layout of `PersonalVouchersPage`
This allow to have nice space between columns on device with a low resolution,
and also follows the material ui responsive layout recommendations.

See: https://material.io/design/layout/responsive-layout-grid.html#columns-gutters-and-margins

frontend/src/pages/personal/vouchers/VouchersPage.tsx
@@ -17,11 +17,11 @@ const PersonalVouchersPage = () => {
       <ContentHeader title={t('personalMenu.vouchers')} />
       <ContentBox maxWidth="lg">
         <CreateVoucherForm />
-        <Grid container>
-          <Grid item md={6}>
+        <Grid container gap={2}>
+          <Grid item md>
             <GivenVouchers />
           </Grid>
-          <Grid item md={6}>
+          <Grid item md>
             <ReceivedVouchers />
           </Grid>
         </Grid>

commit 815dab251ee3700c6ee9a281ca97c19fec3a90d8

[front] fix: bug introduced by cbd9f5cf6661d143fc1e99142c9fe57e1e771170
The horizontal size of given and received vouchers components was
dynamic.

frontend/src/pages/personal/vouchers/CreateVoucherForm.tsx
@@ -8,7 +8,8 @@ import { useNotifications } from 'src/hooks';
 import { usePersonalVouchers } from './context';
 
 const inputProps = {
-  // If not set some browsers may auto fill the input with a saved username because the input label in English is "Username"
+  // If not set some browsers may auto fill the input with a saved username
+  // because the input label in English is "Username".
   autoComplete: 'new-password',
 };
 

frontend/src/pages/personal/vouchers/VouchersPage.tsx
@@ -17,11 +17,11 @@ const PersonalVouchersPage = () => {
       <ContentHeader title={t('personalMenu.vouching')} />
       <ContentBox maxWidth="lg">
         <CreateVoucherForm />
-        <Grid container gap={2}>
-          <Grid item md>
+        <Grid container gap={2} justifyContent="space-between">
+          <Grid item md={5}>
             <GivenVouchers />
           </Grid>
-          <Grid item md>
+          <Grid item md={5}>
             <ReceivedVouchers />
           </Grid>
         </Grid>