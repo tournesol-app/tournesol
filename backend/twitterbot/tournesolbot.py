5b6276071cd9848335ea71127d60278ac35b79ea
aidanjungo <aidan@cfse.ch>
2022-09-16 17:44:17 +0200

    move the function to its correct place

--- backend/twitterbot/tournesolbot.py
+++ backend/twitterbot/tournesolbot.py
@@ -1,14 +1,18 @@
+import datetime
 import random
 import re
 from pathlib import Path
 
-from tournesol.utils.contributors import get_top_public_contributors_last_month
+import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnnotationBbox, OffsetImage
+
 from core.utils.time import time_ago
 from tournesol.models import Entity
 from tournesol.models.criteria import CriteriaLocale
 from tournesol.models.entity_score import ScoreMode
 from tournesol.models.poll import DEFAULT_POLL_NAME
+from tournesol.utils.contributors import get_top_public_contributors_last_month
 from twitterbot import settings
 from twitterbot.models.tweeted import TweetInfo
 from twitterbot.twitter_api import TwitterBot
@@ -189,4 +193,63 @@ def tweet_video_recommendation(bot_name, assumeyes=False):
 def generate_top_contributor_figure() -> Path:
     """Generate a figure with the top contributor of each video."""
 
-    pass
+    now = datetime.datetime.now()
+
+    last_month = now.month - 1
+    year = now.year
+
+    if last_month == 12:
+        year -= 1
+
+    # TODO: get month name from Django
+    MONTHS = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December",
+    }
+
+    top_contributors = get_top_public_contributors_last_month(
+        poll_name=DEFAULT_POLL_NAME, top=10
+    )
+
+    fig_path = Path("/tmp/top_contributor.png")
+
+    plt.xkcd()
+    fig, ax = plt.subplots(dpi=150)
+
+    short_usernames = [
+        name[:11] + "..." if len(name) > 13 else name
+        for name in list(top_contributors.keys())
+    ]
+
+    plt.bar(
+        short_usernames, list(top_contributors.values()), color="#567234", width=0.15
+    )
+    plt.title(
+        f"Tournesol top public contributors of {MONTHS[last_month]} {year}", fontsize=14
+    )
+    plt.xticks(rotation=70, fontsize=10)
+    plt.ylabel("Number of comparisons", fontsize=12)
+    plt.yticks(fontsize=10)
+    plt.subplots_adjust(bottom=0.22, left=0.15, right=0.95)
+
+    tournesol_logo = mpimg.imread("./Logo128.png")
+    imagebox = OffsetImage(tournesol_logo, zoom=0.18)
+
+    for top_pos, nb_rating in enumerate(top_contributors.values()):
+        ab = AnnotationBbox(imagebox, (top_pos, nb_rating), frameon=False)
+        ax.add_artist(ab)
+
+    plt.savefig("test_top_contributor.png", dpi=150)
+    plt.show()
+
+    return fig_path
