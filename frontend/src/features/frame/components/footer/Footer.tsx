1e801aa34913c1296831b9957d462226403aa730
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-09-22 10:16:29 +0200

    reuse existing link when possible

--- frontend/src/features/frame/components/footer/Footer.tsx
+++ frontend/src/features/frame/components/footer/Footer.tsx
@@ -2,72 +2,80 @@ import React from 'react';
 
 import { Box } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
-import FooterSection from './FooterSection';
 
-const footerSections = [
-  {
-    id: 'get-recommendations',
-    title: 'Get recommendations',
-    items: [
-      {
-        name: 'Chrome extension',
-        to: 'https://chrome.google.com/webstore/detail/tournesol-extension/nidimbejmadpggdgooppinedbggeacla',
-      },
-      {
-        name: 'Firefox extension',
-        to: 'https://addons.mozilla.org/fr/firefox/addon/tournesol-extension/',
-      },
-      { name: 'Twitter Bot EN', to: 'https://twitter.com/tournesolbotfr' },
-      { name: 'Twitter Bot FR', to: 'https://twitter.com/tournesolbot' },
-    ],
-  },
-  {
-    id: 'follow-us',
-    title: 'Follow Us',
-    items: [
-      { name: 'Twitter', to: 'https://twitter.com/TournesolApp' },
-      { name: 'Discord', to: 'https://discord.gg/TvsFB8RNBV' },
-      {
-        name: 'YouTube',
-        to: 'https://www.youtube.com/channel/UCH8TsmKEX_PR4jxsg2W3vOg',
-      },
-      {
-        name: 'Science4all',
-        to: 'https://www.youtube.com/c/Science4Allfran%C3%A7ais',
-      },
-    ],
-  },
-  {
-    id: 'support-us',
-    title: 'Support Us',
-    items: [
-      { name: 'Direct Transfer', to: '/about/donate' },
-      { name: 'uTip', to: 'https://utip.io/tournesol' },
-      { name: 'PayPal', to: 'https://www.paypal.com/paypalme/tournesolapp' },
-      { name: 'Compare videos 🌻', to: '/comparison' },
-    ],
-  },
-  {
-    id: 'research',
-    title: 'Research',
-    items: [
-      { name: 'White Paper', to: 'https://arxiv.org/abs/2107.07334' },
-      { name: 'Public Dataset', to: '' },
-    ],
-  },
-  {
-    id: 'more',
-    title: 'More',
-    items: [
-      { name: 'Privacy Policy', to: '/about/privacy_policy' },
-      // { name: 'FAQ', to: '' },
-      { name: 'Wiki', to: 'https://wiki.tournesol.app/' },
-      { name: 'Code Source', to: 'https://github.com/tournesol-app/tournesol' },
-    ],
-  },
-];
+import FooterSection from './FooterSection';
+import { getWebExtensionUrl } from 'src/utils/extension';
+import { getWikiBaseUrl } from 'src/utils/url';
 
 const Footer = () => {
+  const apiUrl = process.env.REACT_APP_API_URL;
+
+  const footerSections = [
+    {
+      id: 'get-recommendations',
+      title: 'Get recommendations',
+      items: [
+        {
+          name: 'Chrome extension',
+          to: getWebExtensionUrl('chrome') || '',
+        },
+        {
+          name: 'Firefox extension',
+          to: getWebExtensionUrl('firefox') || '',
+        },
+        { name: 'Twitter Bot EN', to: 'https://twitter.com/tournesolbotfr' },
+        { name: 'Twitter Bot FR', to: 'https://twitter.com/tournesolbot' },
+      ],
+    },
+    {
+      id: 'follow-us',
+      title: 'Follow Us',
+      items: [
+        { name: 'Twitter', to: 'https://twitter.com/TournesolApp' },
+        { name: 'Discord', to: 'https://discord.gg/TvsFB8RNBV' },
+        {
+          name: 'YouTube',
+          to: 'https://www.youtube.com/channel/UCH8TsmKEX_PR4jxsg2W3vOg',
+        },
+        {
+          name: 'Science4all',
+          to: 'https://www.youtube.com/c/Science4Allfran%C3%A7ais',
+        },
+      ],
+    },
+    {
+      id: 'support-us',
+      title: 'Support Us',
+      items: [
+        { name: 'Direct Transfer', to: '/about/donate' },
+        { name: 'uTip', to: 'https://utip.io/tournesol' },
+        { name: 'PayPal', to: 'https://www.paypal.com/paypalme/tournesolapp' },
+        { name: 'Compare videos 🌻', to: '/comparison' },
+      ],
+    },
+    {
+      id: 'research',
+      title: 'Research',
+      items: [
+        { name: 'White Paper', to: 'https://arxiv.org/abs/2107.07334' },
+        { name: 'Public Dataset', to: `${apiUrl}/exports/comparisons/` },
+      ],
+    },
+    {
+      id: 'more',
+      title: 'More',
+      items: [
+        { name: 'Privacy Policy', to: '/about/privacy_policy' },
+        // { name: 'FAQ', to: '' },
+        { name: 'Wiki', to: getWikiBaseUrl() },
+        {
+          name: 'Code Source',
+          to: 'https://github.com/tournesol-app/tournesol',
+        },
+      ],
+    },
+  ];
+
   return (
     <Box padding={2} color="#fff" bgcolor="#1282B2">
       <Grid
