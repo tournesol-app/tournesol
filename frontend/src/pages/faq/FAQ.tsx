953411fc80a94632a4c4de733b1141229caaf4c1
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-09-26 15:29:04 +0200

    use the API

--- frontend/src/pages/faq/FAQ.tsx
+++ frontend/src/pages/faq/FAQ.tsx
@@ -1,44 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Typography } from '@mui/material';
+import {
+  List,
+  ListItemButton,
+  ListItemText,
+  Paper,
+  Typography,
+} from '@mui/material';
 
 import { ContentBox, ContentHeader } from 'src/components';
+import { FAQEntry, FaqService } from 'src/services/openapi';
 
 const FAQ = () => {
+  const [entries, setEntries] = useState<Array<FAQEntry>>([]);
+
+  useEffect(() => {
+    async function getFaqEntries() {
+      try {
+        const faq = await FaqService.faqList({});
+        if (faq.results) {
+          setEntries(faq.results);
+        }
+      } catch (error) {
+        // do something
+      }
+    }
+
+    getFaqEntries();
+  }, []);
+
+  console.log(entries);
   return (
     <>
       <ContentHeader title="Frequently Asked Questions" />
       <ContentBox maxWidth="md">
-        <Typography variant="h4" gutterBottom>
-          Can you explain what is Tournesol again?
-        </Typography>
-        <Typography paragraph textAlign="justify">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Typography>
-
-        <Typography variant="h4" gutterBottom>
-          Why should I participate?
-        </Typography>
-        <Typography paragraph textAlign="justify">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </Typography>
-
-        <Typography variant="h4" gutterBottom>
-          I like both video Im comparing, what should I do?
-        </Typography>
-        <Typography paragraph textAlign="justify">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </Typography>
+        <Paper
+          square
+          sx={{ color: '#fff', p: 2, pb: 2, mb: 2, backgroundColor: '#1282B2' }}
+        >
+          <Typography variant="h6">Table of Content</Typography>
+          <List dense={true}>
+            {entries.map((entry) => (
+              <ListItemButton key={entry.name} component="a">
+                <ListItemText>{entry.question}</ListItemText>
+              </ListItemButton>
+            ))}
+          </List>
+        </Paper>
+        {entries.map((entry) => (
+          <React.Fragment key={entry.name}>
+            <Typography variant="h4" gutterBottom>
+              {entry.question}
+            </Typography>
+            <Typography paragraph textAlign="justify">
+              {entry.answer}
+            </Typography>
+          </React.Fragment>
+        ))}
       </ContentBox>
     </>
   );
