1adfc094effa1f4d598fb3561f2fd44b0df83488
Michaël Witrant <mike@lepton.fr>
2022-10-20 16:19:29 +0200

    [back] feat: include voting rights in the public dataset (#1154)
    Note that the voting rights are named trust score is the exported CSV file, because
    the `User`'s voting_right field we be renamed trust_score soon. 
    
    Co-authored-by: Adrien Matissart <a@matissart.net>
    Co-authored-by: Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>

--- backend/tournesol/tests/test_exports.py
+++ backend/tournesol/tests/test_exports.py
@@ -1,5 +1,7 @@
 import csv
 import io
+import re
+import zipfile
 
 from django.core.cache import cache
 from django.test import TestCase
@@ -30,9 +32,9 @@ class ExportTest(TestCase):
         ComparisonCriteriaScore.objects.create(
             comparison=self.comparison, score=5, criteria="largely_recommended"
         )
-        self.user_without_comparisons = UserFactory()
+        self.user_without_comparisons = UserFactory(username="user_without_comparisons")
 
-        self.public_comparisons = UserFactory()
+        self.public_comparisons = UserFactory(username="public_comparisons", voting_right=0.5844)
         self.video_public_1 = VideoFactory()
         self.video_public_2 = VideoFactory()
         self.video_private_3 = VideoFactory()
@@ -75,6 +77,28 @@ class ExportTest(TestCase):
         self.client = APIClient()
         cache.clear()
 
+    def add_comparison(self, user, is_public=True):
+        video1 = VideoFactory()
+        video2 = VideoFactory()
+        ContributorRating.objects.create(
+            poll=self.poll_videos,
+            user=user,
+            entity=video1,
+            is_public=is_public,
+        )
+        ContributorRating.objects.create(
+            poll=self.poll_videos,
+            user=user,
+            entity=video2,
+            is_public=is_public,
+        )
+        ComparisonFactory(
+            poll=self.poll_videos,
+            user=user,
+            entity_1=video1,
+            entity_2=video2,
+        )
+
     def test_not_authenticated_cannot_download_comparisons(self):
         resp = self.client.get("/users/me/exports/comparisons/")
         self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
@@ -85,7 +109,7 @@ class ExportTest(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         # Ensures that the csv contians a single row with the correct score
         csv_file = csv.DictReader(io.StringIO(resp.content.decode()))
-        comparison_list = [row for row in csv_file]
+        comparison_list = list(csv_file)
         self.assertEqual(len(comparison_list), 1)
         self.assertEqual(float(comparison_list[0]["score"]), 5.0)
 
@@ -95,7 +119,7 @@ class ExportTest(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         # Ensures the csv does not contain information about comparison criteria score
         csv_file = csv.DictReader(io.StringIO(resp.content.decode()))
-        comparison_list = [row for row in csv_file]
+        comparison_list = list(csv_file)
         self.assertEqual(len(comparison_list), 0)
 
     def test_not_authenticated_cannot_download_all(self):
@@ -106,18 +130,20 @@ class ExportTest(TestCase):
         self.client.force_authenticate(self.user_with_comparisons)
         resp = self.client.get("/users/me/exports/all/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(resp.headers["Content-Type"], "application/zip")
 
     def test_authenticated_without_comparisons_can_download_all(self):
         self.client.force_authenticate(self.user_without_comparisons)
         resp = self.client.get("/users/me/exports/all/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(resp.headers["Content-Type"], "application/zip")
 
     def test_not_authenticated_can_download_public_comparisons(self):
         resp = self.client.get("/exports/comparisons/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         # Ensures the csv does not contain information that are not public comparisons
         csv_file = csv.DictReader(io.StringIO(resp.content.decode()))
-        comparison_list = [row for row in csv_file]
+        comparison_list = list(csv_file)
         self.assertEqual(len(comparison_list), 1)
         self.assertEqual(comparison_list[0]["video_a"], self.video_public_1.video_id)
         self.assertEqual(comparison_list[0]["video_b"], self.video_public_2.video_id)
@@ -150,9 +176,85 @@ class ExportTest(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         # Ensures the csv does not contain information that are not public comparisons
         csv_file = csv.DictReader(io.StringIO(resp.content.decode()))
-        comparison_list = [row for row in csv_file]
+        comparison_list = list(csv_file)
         self.assertEqual(len(comparison_list), 2)
         self.assertEqual(comparison_list[0]["video_a"], self.video_public_1.video_id)
         self.assertEqual(comparison_list[0]["video_b"], self.video_public_2.video_id)
         self.assertEqual(comparison_list[1]["video_a"], self.video_public_3.video_id)
         self.assertEqual(comparison_list[1]["video_b"], self.video_public_4.video_id)
+
+    def test_not_authenticated_can_download_all_exports(self):
+        # Make sure this user has multiple public comparisons so
+        # that we can verify they are only added once in the CSV
+        self.add_comparison(user=self.public_comparisons, is_public=True)
+
+        response = self.client.get("/exports/all/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.headers["Content-Type"], "application/zip")
+
+        content_disposition = response.headers["Content-Disposition"]
+        match = re.search("attachment; filename=(tournesol_export_\\d{8}T\\d{6}Z).zip", content_disposition)
+        self.assertIsNotNone(match)
+        root = match.group(1)
+
+        zip_content = io.BytesIO(response.content)
+        with zipfile.ZipFile(zip_content, 'r') as zip_file:
+            expected_files = [
+                root + "/README.txt",
+                root + "/comparisons.csv",
+                root + "/users.csv",
+            ]
+            self.assertEqual(zip_file.namelist(), expected_files)
+
+            with zip_file.open(root + '/README.txt', 'r') as file:
+                content = file.read()
+                with open('tournesol/resources/export_readme.txt', 'rb') as readme_file:
+                    expected_content = readme_file.read()
+                self.assertEqual(content, expected_content)
+
+            with zip_file.open(root + '/comparisons.csv', 'r') as file:
+                content = file.read()
+                expected_content = self.client.get("/exports/comparisons/").content
+                self.assertEqual(content, expected_content)
+
+            with zip_file.open(root + '/users.csv', 'r') as file:
+                content = file.read().decode('utf-8')
+                csv_file = csv.DictReader(io.StringIO(content))
+                rows = list(csv_file)
+
+                usernames = [row["public_username"] for row in rows]
+                self.assertNotIn(self.user_with_comparisons.username, usernames)
+                self.assertNotIn(self.user_without_comparisons.username, usernames)
+                self.assertIn(self.public_comparisons.username, usernames)
+
+                username = self.public_comparisons.username
+                user_rows = [row for row in rows if row["public_username"] == username]
+                self.assertEqual(len(user_rows), 1)
+                user_row = user_rows[0]
+                self.assertEqual(user_row["trust_score"], '0.5844')
+
+    def test_all_export_sorts_by_username(self):
+        last_user = UserFactory(username="z")
+        first_user = UserFactory(username="a")
+
+        self.add_comparison(user=last_user, is_public=True)
+        self.add_comparison(user=first_user, is_public=True)
+
+        response = self.client.get("/exports/all/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        content_disposition = response.headers["Content-Disposition"]
+        match = re.search("filename=(.+).zip", content_disposition)
+        root = match.group(1)
+
+        zip_content = io.BytesIO(response.content)
+
+        with zipfile.ZipFile(zip_content, 'r') as zip_file:
+            with zip_file.open(root + '/users.csv', 'r') as file:
+                content = file.read().decode('utf-8')
+                csv_file = csv.DictReader(io.StringIO(content))
+                rows = list(csv_file)
+
+                usernames = [row["public_username"] for row in rows]
+                self.assertEqual("a", usernames[0])
+                self.assertEqual("z", usernames[-1])
