22637d632d6b36fc216d1f8c75817972c418cca5
Gresille & Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-12-07 14:29:28 +0100

    [back] fix: temporarily downgrade the previews' quality (#1231)
    The images returned by the preview API could be broken if one of
    the videos doesn't have a `maxres` thumbnail. 
    
    For now, only `mq` will be use.
    
    In the future, we could check if both videos have `maxres` thumbnails
    to determine the preview quality.

--- backend/tournesol/views/preview.py
+++ backend/tournesol/views/preview.py
@@ -322,8 +322,13 @@ class DynamicWebsitePreviewEntity(BasePreviewAPIView):
             entity, fnt_config, upscale_ratio=upscale_ratio
         )
 
+        # Not all YT videos have a `maxres` thumbnail available. It could be
+        # good to use `maxres` instead of the `mq` quality when we are sure
+        # that both videos have a `maxres` thumbnail available. In the
+        # meantime, to avoid creating broken preview images we will use `mq`.
+        thumbnail_quality = "mq"
         try:
-            youtube_thumbnail = self.get_yt_thumbnail(entity, quality="maxres")
+            youtube_thumbnail = self.get_yt_thumbnail(entity, quality=thumbnail_quality)
         except ConnectionError:
             return self.default_preview()
 
@@ -665,7 +670,11 @@ class DynamicWebsitePreviewComparison(BasePreviewAPIView, APIView):
         if not self.is_video(entity_a) or not self.is_video(entity_b):
             return self.default_preview()
 
-        thumbnail_quality = "maxres"
+        # Not all YT videos have a `maxres` thumbnail available. It could be
+        # good to use `maxres` instead of the `mq` quality when we are sure
+        # that both videos have a `maxres` thumbnail available. In the
+        # meantime, to avoid creating broken preview images we will use `mq`.
+        thumbnail_quality = "mq"
         try:
             thumbnail_a = self.get_yt_thumbnail(entity_a, quality=thumbnail_quality)
             thumbnail_b = self.get_yt_thumbnail(entity_b, quality=thumbnail_quality)
