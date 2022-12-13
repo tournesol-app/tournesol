24f70c43a12b960dcd7aeae72a0b212e3cc51296
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-12-13 15:56:44 +0100

    invite anonymous users to participate

--- frontend/src/pages/home/banners/TempStudyBanner.tsx
+++ frontend/src/pages/home/banners/TempStudyBanner.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link as RouterLink } from 'react-router-dom';
 
 import {
   Box,
@@ -12,7 +13,7 @@ import {
 } from '@mui/material';
 import { Biotech, Campaign } from '@mui/icons-material';
 
-import { useCurrentPoll } from 'src/hooks';
+import { useCurrentPoll, useLoginState } from 'src/hooks';
 import { UsersService } from 'src/services/openapi';
 
 // TODO: these values are placeholder values that will be updated.
@@ -22,6 +23,7 @@ const STUDY_DATE_END = new Date('2024-01-01T00:00:00Z');
 const TempStudyBanner = () => {
   const { t } = useTranslation();
   const { name: pollName } = useCurrentPoll();
+  const { isLoggedIn } = useLoginState();
 
   const [proofOfVote, setProofOfVote] = useState('');
 
@@ -48,24 +50,46 @@ const TempStudyBanner = () => {
       <Grid container width="100%" flexDirection="column" alignItems="center">
         <Grid item xl={9} width="100%">
           <Paper sx={{ p: 2 }}>
-            <Stack spacing={2} direction="row" alignItems="center">
-              <Campaign fontSize="large" sx={{ color: '#1282B2' }} />
-              <Typography paragraph>
-                {t('tempStudyBanner.isTheTournesolProjectReallyEffective')}
-              </Typography>
-              <Box display="flex" justifyContent="center">
-                <Button
-                  variant="contained"
-                  component={Link}
-                  target="_blank"
-                  rel="noopener"
-                  href={`https://tournesol.app?proof=${proofOfVote}`}
-                  endIcon={<Biotech />}
-                >
-                  {t('tempStudyBanner.join')}
-                </Button>
-              </Box>
-            </Stack>
+            <Grid
+              container
+              gap={2}
+              alignItems="center"
+              justifyContent="space-between"
+            >
+              <Grid item xs={12} md={9}>
+                <Stack direction="row" spacing={2} alignItems="center">
+                  <Campaign fontSize="large" sx={{ color: '#1282B2' }} />
+                  <Typography paragraph mb={0}>
+                    {t('tempStudyBanner.isTheTournesolProjectReallyEffective')}
+                  </Typography>
+                </Stack>
+              </Grid>
+              <Grid item xs={12} md={2}>
+                <Box display="flex" justifyContent="flex-end" width="100%">
+                  {isLoggedIn ? (
+                    <Button
+                      variant="contained"
+                      component={Link}
+                      target="_blank"
+                      rel="noopener"
+                      href={`https://tournesol.app?proof=${proofOfVote}`}
+                      endIcon={<Biotech />}
+                    >
+                      {t('tempStudyBanner.join')}
+                    </Button>
+                  ) : (
+                    <Button
+                      to="/login"
+                      color="secondary"
+                      variant="outlined"
+                      component={RouterLink}
+                    >
+                      {t('tempStudyBanner.loginToParticipate')}
+                    </Button>
+                  )}
+                </Box>
+              </Grid>
+            </Grid>
           </Paper>
         </Grid>
       </Grid>
