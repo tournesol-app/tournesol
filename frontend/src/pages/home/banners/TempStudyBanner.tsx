e072087e887153036fd61fe5d5ca51db77defe6a
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2022-12-13 12:06:19 +0100

    add a proof of vote

--- frontend/src/pages/home/banners/TempStudyBanner.tsx
+++ frontend/src/pages/home/banners/TempStudyBanner.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import {
@@ -12,14 +12,32 @@ import {
 } from '@mui/material';
 import { Biotech, Campaign } from '@mui/icons-material';
 
-// These are placeholder values that will be updated.
-const STUDY_DATE_START = new Date('2024-01-01T00:00:00Z');
-const STUDY_DATE_END = new Date('2024-06-01T00:00:00Z');
+import { useCurrentPoll } from 'src/hooks';
+import { UsersService } from 'src/services/openapi';
+
+// TODO: these values are placeholder values that will be updated.
+const STUDY_DATE_START = new Date('2022-01-01T00:00:00Z');
+const STUDY_DATE_END = new Date('2024-01-01T00:00:00Z');
 
 const TempStudyBanner = () => {
   const { t } = useTranslation();
+  const { name: pollName } = useCurrentPoll();
+
+  const [proofOfVote, setProofOfVote] = useState('');
 
   const now = new Date();
+
+  useEffect(() => {
+    if (now < STUDY_DATE_START || now > STUDY_DATE_END) {
+      return;
+    }
+
+    UsersService.usersMeProofOfVotesRetrieve({ pollName })
+      .then(({ signature }) => setProofOfVote(signature))
+      .catch(console.error);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pollName]);
 
   if (now < STUDY_DATE_START || now > STUDY_DATE_END) {
     return <></>;
@@ -41,7 +59,7 @@ const TempStudyBanner = () => {
                   component={Link}
                   target="_blank"
                   rel="noopener"
-                  href="https://tournesol.app"
+                  href={`https://tournesol.app?proof=${proofOfVote}`}
                   endIcon={<Biotech />}
                 >
                   {t('tempStudyBanner.join')}
