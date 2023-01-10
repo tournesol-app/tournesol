ad6fc071edadf0a2a82f8989ada5ac86ee6435cf
Louis Faucon <lpfaucon@gmail.com>
2023-01-10 20:00:17 +0100

    [back] feat: enable non-strict option in unconnected entities API route (#1270)

--- backend/tournesol/views/unconnected_entities.py
+++ backend/tournesol/views/unconnected_entities.py
@@ -1,12 +1,14 @@
 """
 API endpoints interacting with unconnected entities.
 """
-from collections import defaultdict
+import math
+from collections import defaultdict, deque
 
-from django.db.models import Func, OuterRef, Q, Subquery
 from django.shortcuts import get_object_or_404
-from drf_spectacular.utils import extend_schema, extend_schema_view
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema, extend_schema_view
 from rest_framework import generics
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
 from tournesol.models import Comparison, Entity
@@ -14,11 +16,39 @@ from tournesol.serializers.entity import EntityNoExtraFieldSerializer
 from tournesol.views.mixins.poll import PollScopedViewMixin
 
 
+# We override the pagination filtering for our queryset for performance reasons. We decided to
+# already filter and sort the queryset in python directly because the annotation and sorting
+# operations were inneficient when done on the database.
+class SortedEntityIdLimitOffsetPagination(LimitOffsetPagination):
+
+    def paginate_queryset(self, queryset, request, view=None):
+        # Here the parameter queryset is instead the list of ids returned by
+        # UnconnectedEntitiesView.get_queryset. This is done this way for performance.
+        paginated_entity_ids = super().paginate_queryset(queryset, request, view)
+        entity_by_id = {e.id: e for e in Entity.objects.filter(id__in=paginated_entity_ids)}
+        entities = [entity_by_id[e_id] for e_id in queryset if e_id in entity_by_id]
+        return entities
+
+
 @extend_schema_view(
     get=extend_schema(
         description="List unconnected entities of the current user from a target entity"
                     " and the user's graph of comparisons (entities are vertices and"
-                    " comparisons are edges)."
+                    " comparisons are edges).",
+        parameters=[
+            OpenApiParameter(
+                "strict",
+                type=OpenApiTypes.BOOL,
+                default=True,
+                location=OpenApiParameter.QUERY,
+                description=(
+                    "Set to 'false' to include entities connected to the target entity. In this "
+                    "case, the returned list contains first all unconnected entities, then "
+                    "connected entities sorted by decreasing distance to the target entity."
+                ),
+                required=False,
+            )
+        ]
     )
 )
 class UnconnectedEntitiesView(PollScopedViewMixin, generics.ListAPIView):
@@ -28,54 +58,41 @@ class UnconnectedEntitiesView(PollScopedViewMixin, generics.ListAPIView):
 
     serializer_class = EntityNoExtraFieldSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = SortedEntityIdLimitOffsetPagination
 
     def get_queryset(self):
         # Get related entities from source entity
         source_node = get_object_or_404(Entity, uid=self.kwargs.get("uid"))
-        comparisons = list(
-            Comparison.objects.filter(
-                poll=self.poll_from_url, user=self.request.user
-            ).values_list("entity_1_id", "entity_2_id")
+
+        neighbors = defaultdict(set)
+        for (entity_1_id, entity_2_id) in Comparison.objects.filter(
+            poll=self.poll_from_url, user=self.request.user
+        ).values_list("entity_1_id", "entity_2_id"):
+            neighbors[entity_1_id].add(entity_2_id)
+            neighbors[entity_2_id].add(entity_1_id)
+
+        distances = {source_node.id: 0}
+        bfs_queue = deque([(source_node.id, 0)])
+
+        while bfs_queue:
+            node, distance = bfs_queue.popleft()
+            for next_node in neighbors[node]:
+                if next_node not in distances:
+                    distances[next_node] = distance+1
+                    bfs_queue.append((next_node, distance+1))
+
+        # The entities filtered are the entities which don't have a distance
+        # because they are unreachable if the strict option has been left to
+        # True (the default). Otherwise all the entities except the source are
+        # selected.
+        strict = self.request.query_params.get("strict") != "false"
+        entity_ids = set(e_id for e_id in neighbors if (
+            (e_id not in distances) if strict else (distances.get(e_id, 2) > 1)
+        ))
+        sorted_entity_ids = sorted(
+            entity_ids,
+            # Sorting first by distance and secondly by number of comparisons
+            # and thirdly by entity_id such that we avoid non-determinism
+            key=lambda x: (-distances.get(x, math.inf), len(neighbors.get(x, [])), x)
         )
-
-        all_connections = defaultdict(set)
-
-        for (entity_1_id, entity_2_id) in comparisons:
-            all_connections[entity_1_id].add(entity_2_id)
-            all_connections[entity_2_id].add(entity_1_id)
-
-        def get_related_entities(entity_id):
-            already_visited_nodes = set()
-            related_entities = {entity_id}
-            to_visit = {entity_id}
-
-            while to_visit:
-                node = to_visit.pop()
-                already_visited_nodes.add(node)
-                connections = all_connections.get(node, set())
-                to_visit.update(connections - already_visited_nodes)
-                related_entities.update(connections)
-
-            return related_entities
-
-        user_related_entities = get_related_entities(source_node.id)
-        user_all_entities = set()
-
-        for (entity_1_id, entity_2_id) in comparisons:
-            user_all_entities.add(entity_1_id)
-            user_all_entities.add(entity_2_id)
-
-        # Order the entities by number of comparisons so that the logged user
-        # is invited to compare entities with the least comparisons first.
-        comparison_counts = (
-            Comparison.objects.filter(user=self.request.user)
-            .filter(Q(entity_1=OuterRef("pk")) | Q(entity_2=OuterRef("pk")))
-            .annotate(count=Func("id", function="Count"))
-            .values("count")
-        )
-
-        return (
-            Entity.objects.filter(id__in=user_all_entities - user_related_entities)
-            .annotate(n_comparisons=Subquery(comparison_counts))
-            .order_by("n_comparisons")
-        )
+        return sorted_entity_ids
