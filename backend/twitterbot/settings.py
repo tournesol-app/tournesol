79a61e2ab28069e1f360e026268474ffeb3d7e7e
Aidan <53896963+aidanjungo@users.noreply.github.com>
2023-02-04 13:42:08 +0100

    [back] feat: Generate and tweet montly Top contributor figure (#1311)
    * add function to generate the top contributor figure
    
    * add a tweet_top_contributor_graph function
    
    * add matplotib to requierements
    
    * add tests
    
    * add management command
    
    ---------
    
    Co-authored-by: Adrien Matissart <amatissart@users.noreply.github.com>

--- backend/twitterbot/settings.py
+++ backend/twitterbot/settings.py
@@ -27,6 +27,28 @@ tweet_text_template = {
     ),
 }
 
+# Monthly top contributor text template
+graph_title_text_template = {
+    "en": ("Who compared the most on Tournesol in {month_name} {year}?"),
+    "fr": ("Qui a le plus comparé sur Tournesol en {month_name} {year}?"),
+}
+
+graph_ylabel_text_template = {
+    "en": ("Number of public comparisons"),
+    "fr": ("Nombre de comparaisons publiques"),
+}
+
+top_contrib_tweet_text_template = {
+    "en": (
+        "Thank you to all our contributors. Here are the top 10 from last month. "
+        "And you, how many comparisons did you make last month? \U0001F33B"
+    ),
+    "fr": (
+        "Merci à tout·es nos contributeur·rice·s. Voici le top 10 du mois dernier. "
+        "Et vous, combien de comparaisons avez-vous faites le mois dernier? \U0001F33B"
+    ),
+}
+
 # Name of the Discord channel where the twitterbot will post its tweets.
 # An empty value won't trigger any post.
 TWITTERBOT_DISCORD_CHANNEL = "twitter"
