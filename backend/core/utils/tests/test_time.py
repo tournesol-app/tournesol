794f9f19e291bcc1341750e7edd42caf54c5082a
aidanjungo <aidan@cfse.ch>
2023-02-16 10:36:41 +0100

    Add time ahead function and test

--- backend/core/utils/tests/test_time.py
+++ backend/core/utils/tests/test_time.py
@@ -1,4 +1,4 @@
-from core.utils.time import time_ago
+from core.utils.time import time_ago, time_ahead
 
 
 def test_time_ago():
@@ -8,3 +8,12 @@ def test_time_ago():
     assert time_ago(hours=10) > time_ago(days=1)
     assert time_ago(days=1) > time_ago(days=1, hours= 3)
     assert time_ago(days=6, hours=23) > time_ago(weeks=1)
+
+
+def test_time_ahead():
+    """Test method time_ahead."""
+    
+    assert time_ahead(hours=1) < time_ahead(hours=10)
+    assert time_ahead(hours=10) < time_ahead(days=1)
+    assert time_ahead(days=1) < time_ahead(days=1, hours= 3)
+    assert time_ahead(days=6, hours=23) < time_ahead(weeks=1)
--- backend/core/utils/time.py
+++ backend/core/utils/time.py
@@ -9,3 +9,8 @@ def time_ago(**kwargs):
     """Return a time in the past as specified as keyword arguments."""
 
     return timezone.now() - timedelta(**kwargs)
+
+def time_ahead(**kwargs):
+    """Return a time in the future as specified as keyword arguments."""
+
+    return timezone.now() + timedelta(**kwargs)
