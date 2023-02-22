b7e1d5ac1a51a7f8613d1429fb4fcb7e4c4fcbe7
Iwazaru <entzminger.quentin@gmail.com>
2023-02-22 14:33:25 +0100

    wip [front] mobile navbar opening/closing logic added in the topbar

--- frontend/src/features/frame/components/topbar/TopBar.tsx
+++ frontend/src/features/frame/components/topbar/TopBar.tsx
@@ -57,6 +57,14 @@ const useStyles = makeStyles(() => ({
     cursor: 'pointer',
     borderRadius: '0px 4px 4px 0px',
   },
+  searchClose: {
+    cursor: 'pointer',
+    position: 'absolute',
+    left: 'calc(50% - 5px)',
+  },
+  searchOpen: {
+    cursor: 'pointer',
+  },
 }));
 
 const Logo = () => {
@@ -122,7 +130,13 @@ const TopBar = () => {
   const theme = useTheme();
   const { options } = useCurrentPoll();
 
+  const classes = useStyles();
+
   const [mobileSearchOpen, setMobileSearchOpen] = useState(false);
+
+  const searchOpeningHandler = (open: boolean) => {
+    setMobileSearchOpen(open);
+  };
 
   return (
     <AppBar
@@ -154,13 +168,19 @@ const TopBar = () => {
           <Hidden mdUp>
             {!mobileSearchOpen && <Logo />}
             {!mobileSearchOpen && (
-              <Grid item xs={2} padding={2}>
+              <Grid
+                item
+                xs={2}
+                padding={2}
+                onClick={() => searchOpeningHandler(true)}
+              >
                 <svg
                   width="27"
                   height="27"
                   viewBox="0 0 18 18"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
+                  className={classes.searchOpen}
                 >
                   <path
                     d="M11 0C9.27609 0 7.62279 0.684819 6.40381 1.90381C5.18482 3.12279 4.5 4.77609 4.5 6.5C4.5 8.11 5.09 9.59 6.06 10.73L5.79 11H5L0 16L1.5 17.5L6.5 12.5V11.71L6.77 11.44C7.94945 12.4468 9.44929 12.9999 11 13C12.7239 13 14.3772 12.3152 15.5962 11.0962C16.8152 9.87721 17.5 8.22391 17.5 6.5C17.5 4.77609 16.8152 3.12279 15.5962 1.90381C14.3772 0.684819 12.7239 0 11 0ZM11 2C13.5 2 15.5 4 15.5 6.5C15.5 9 13.5 11 11 11C8.5 11 6.5 9 6.5 6.5C6.5 4 8.5 2 11 2Z"
@@ -170,8 +190,15 @@ const TopBar = () => {
               </Grid>
             )}
             {mobileSearchOpen && (
-              <Grid item md={12} maxWidth="100%">
+              <Grid item md={12} width="100%">
                 {options?.withSearchBar && <Search />}
+                <span
+                  title="fermer la barre de recherche"
+                  onClick={() => searchOpeningHandler(false)}
+                  className={classes.searchClose}
+                >
+                  X
+                </span>
               </Grid>
             )}
             {!mobileSearchOpen && <AccountInfo />}
