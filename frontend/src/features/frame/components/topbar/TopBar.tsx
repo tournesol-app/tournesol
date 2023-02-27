f8b88be1269874623de808799ed30f7ecbf01eeb
Louis Faucon <lpfaucon@gmail.com>
2023-02-27 17:49:12 +0100

    refactor: use useMediaQuery instead of Hidden in TopBar component

--- frontend/src/features/frame/components/topbar/TopBar.tsx
+++ frontend/src/features/frame/components/topbar/TopBar.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import AppBar from '@mui/material/AppBar';
 import Grid from '@mui/material/Grid';
-import Hidden from '@mui/material/Hidden';
 import Toolbar from '@mui/material/Toolbar';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 import { useTheme } from '@mui/material';
 import { useCurrentPoll } from 'src/hooks/useCurrentPoll';
@@ -18,6 +18,7 @@ export const topBarHeight = 80;
 const TopBar = () => {
   const theme = useTheme();
   const { options } = useCurrentPoll();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
     <AppBar
@@ -39,12 +40,7 @@ const TopBar = () => {
         }}
       >
         <Grid container sx={{ width: '100%' }} alignItems="center">
-          <Hidden mdDown>
-            <TopBarDesktop />
-          </Hidden>
-          <Hidden mdUp>
-            <TopBarMobile />
-          </Hidden>
+          {isSmallScreen ? <TopBarMobile /> : <TopBarDesktop />}
         </Grid>
       </Toolbar>
     </AppBar>
