77b624f932607d60c53f5c65fa4407372a5be587
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-02-28 15:27:36 +0100

    attempt to fix extra page views

--- frontend/src/hooks/useWebAnalytics.ts
+++ frontend/src/hooks/useWebAnalytics.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import Plausible from 'plausible-tracker';
 
@@ -10,21 +10,9 @@ import Plausible from 'plausible-tracker';
  * See: https://github.com/plausible/plausible-tracker
  */
 export const useWebAnalytics = () => {
-  const [client, setClient] = useState(Plausible({ apiHost: '' }));
-
-  useEffect(() => {
-    if (process.env.REACT_APP_WEBSITE_ANALYTICS_URL) {
-      /**
-       * By default, Plausible will not make HTTP requests to track events
-       * when location.hostname` is "localhost".
-       */
-      setClient(
-        Plausible({
-          apiHost: process.env.REACT_APP_WEBSITE_ANALYTICS_URL,
-        })
-      );
-    }
-  }, []);
+  const [client, setClient] = useState(
+    Plausible({ apiHost: process.env.REACT_APP_WEBSITE_ANALYTICS_URL })
+  );
 
   return {
     client: client,
