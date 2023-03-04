1571ae0d52bf90cb06cad2a4b36165d3254f0cb1
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-03-04 18:00:52 +0100

    add module docstring and function docstring

--- data-visualization/pages/03_users_traffic_evolution.py
+++ data-visualization/pages/03_users_traffic_evolution.py
@@ -1,3 +1,16 @@
+"""
+A page dedicated to the evolution of the Tournesol community.
+
+List of concepts:
+
+    New contributor:
+        A new contributor is a user making his/her first comparison.
+
+    Contribution:
+        A contribution is comparison between two videos involving one or more
+        criteria.
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -19,22 +32,25 @@ st.title("Community evolution (public dataset)")
 
 # Load public dataset (the function is cached to not overload the API)
 st.session_state.df = set_df().sort_values("week_date")
-
-
 assert (
     st.session_state.df.week_date.is_monotonic_increasing
 ), "dataframe should be ordered by increasing date"
 
 
-# User evolution
-def add_contributor_evolution():
+def add_contributors_evolution():
+    """
+    Display the number of new contributors per week.
+    """
     df = st.session_state.df
     st.markdown("Number of new contributors per week.")
     fig = df.groupby("public_username").first().groupby("week_date").size().plot()
     st.plotly_chart(fig)
 
 
-def add_contributor_cumulative_evolution():
+def add_contributors_cumulative_evolution():
+    """
+    Display the cumulative number of new contributors per week.
+    """
     df = st.session_state.df
     st.markdown("Cumulated Number of new contributors per week.")
     fig = (
@@ -48,14 +64,20 @@ def add_contributor_cumulative_evolution():
     st.plotly_chart(fig)
 
 
-def add_vote_evolution():
+def add_contributions_evolution():
+    """
+    Display the number of contributions.
+    """
     df = st.session_state.df
     st.markdown("Number of contributions per week.")
     fig = df.groupby("week_date").size().plot()
     st.plotly_chart(fig)
 
 
-def add_vote_cumulative_evolution():
+def add_contributions_cumulative_evolution():
+    """
+    Display the cumulative number of contributions.
+    """
     df = st.session_state.df
     st.markdown("Cumulated number of contributions per week")
     fig = df.groupby("week_date").size().cumsum().plot()
@@ -63,8 +85,8 @@ def add_vote_cumulative_evolution():
 
 
 pd.options.plotting.backend = "plotly"
-# Cursor position
-add_contributor_evolution()
-add_contributor_cumulative_evolution()
-add_vote_evolution()
-add_vote_cumulative_evolution()
+
+add_contributors_evolution()
+add_contributors_cumulative_evolution()
+add_contributions_evolution()
+add_contributions_cumulative_evolution()
