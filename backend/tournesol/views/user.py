78dc11d126c464bb539b22243eb1b8585442fb63
Adrien Matissart <a@matissart.net>
2023-03-19 17:17:01 +0100

    Replace user email on delete, to allow immediate reuse

--- backend/tournesol/views/user.py
+++ backend/tournesol/views/user.py
@@ -13,21 +13,28 @@ logger = logging.getLogger(__name__)
 
 class CurrentUserView(APIView):
     """Manage the deletion of the authenticated user."""
+
     permission_classes = [IsAuthenticated]
 
     @extend_schema(responses={204: None})
     def delete(self, request):
         """
-        Delete and logout the authenticated user.
-        All related resources are also deleted: comparisons, rate-later list, access tokens, etc.
+        Deactivate and logout the authenticated user.
+        All related resources (comparisons, etc.) will be deleted separately by 'delete_inactive_users' after
+        a delay, in order to avoid conflicts with ml-train currently running.
         """
         user = request.user
         user.is_active = False
         user.last_login = timezone.now()
-        user.save(update_fields=["is_active", "last_login"])
+
+        # The email is replaced with a placeholder address, in order to allow the email to be reused immediatly.
+        # Deleting the email would not work as the db expects the email to be present and unique.
+        deleted_user_email = user.email
+        user.email = f"__deleted__{user.id}@deleted.invalid"
+        user.save(update_fields=["is_active", "last_login", "email"])
 
         logout(request)
         logger.info(
-            "User '%s' with email '%s' has been deleted.", user.username, user.email
+            "User '%s' with email '%s' has been deleted.", user.username, deleted_user_email
         )
         return Response(status=status.HTTP_204_NO_CONTENT)
