5605c4b81f12804bb9ed98359ecaa92b0ccf6476
Iwazaru <entzminger.quentin@gmail.com>
2023-04-13 10:01:37 +0200

    [extension] update banner urls

--- browser-extension/src/models/banner/BannerOptions.js
+++ browser-extension/src/models/banner/BannerOptions.js
@@ -17,8 +17,8 @@ export class BannerOptions {
 // TODO: these values are placeholder values that should be updated.
 export const defaultBannerOptions = new BannerOptions(
   '2020-01-01T00:00:00Z',
-  '2024-01-01T00:00:00Z',
-  'https://docs.google.com/forms/d/e/1FAIpQLScQzlEKBSA3MqxI0kaPazbyIUnZ4PjFcrR8EFiikG1quyAoiw/viewform?usp=pp_url&entry.939413650=',
-  'https://docs.google.com/forms/d/e/1FAIpQLSf9PXr-f8o9QqDR-Pi63xRZx4y4nOumNDdwi_jvUWc6LxZRAw/viewform?usp=pp_url&entry.1924714025=',
+  '2022-01-01T00:00:00Z',
+  'https://docs.google.com/forms/d/e/1FAIpQLSd4_8nF0kVnHj3GvTlEAFw-PHAGOAGc1j1NKZbXr8Ga_nIY9w/viewform?usp=pp_url&entry.939413650=',
+  'https://docs.google.com/forms/d/e/1FAIpQLSfEXZLlkLA6ngx8LV-VVpIxV9AZ9MgN-H_U0aTOnVhrXv1XLQ/viewform?usp=pp_url&entry.1924714025=',
   'browser_extension_study_2023'
 );
