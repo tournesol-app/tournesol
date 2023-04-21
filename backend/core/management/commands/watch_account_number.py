c6a82e330b5086eee941e50c9ee3b546f791812b
shinseinova <89401728+ShinseiNova@users.noreply.github.com>
2023-04-21 10:30:18 +0200

    [back] feat: the command watch_account_number takes a date as argument

--- backend/core/management/commands/watch_account_number.py
+++ backend/core/management/commands/watch_account_number.py
@@ -2,14 +2,37 @@
 Send an alert on Discord when the number of account using a trusted domain
 passes a threshold
 """
+from argparse import ArgumentTypeError
+
 from django.core.management.base import BaseCommand
+
+from datetime import datetime
+from django.utils import timezone
+
+
+def validate_date(date_str):
+    try:
+        return datetime.strptime(date_str, '%Y-%m-%d').date()
+    except ValueError as err:
+        raise ArgumentTypeError(err)
 
 
 class Command(BaseCommand):
-    help = "Send an alert on Discord when there are more users with" \
-           "an email addresses considered trusted than a threshold X."
+    help = "Send an alert on Discord if a threshold of users using" \
+           "a trusted domain has been reached between the given date and the day before"
+
+    def add_arguments(self, parser):
+
+        parser.add_argument(
+            "-d",
+            "--date",
+            type=validate_date,
+            default=timezone.now().date(),
+            help="count the accounts created on this date (format yyyy-mm-dd)",
+        )
 
     def handle(self, *args, **options):
         self.stdout.write(f"start command: {__name__}")
+        self.stdout.write(f"date: {options['date']}")
         self.stdout.write(self.style.SUCCESS("success"))
         self.stdout.write("end")
