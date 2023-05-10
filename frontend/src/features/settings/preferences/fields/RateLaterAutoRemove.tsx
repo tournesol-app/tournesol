7589b1db5047ee1b8dcbe7b7a7fe584f7a974f54
ShinseiNova <89401728+ShinseiNova@users.noreply.github.com>
2023-05-10 16:50:48 +0200

    [front] refactor: split the settings form in several fields (#1549)
    Co-authored-by: Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>

---
+++ frontend/src/features/settings/preferences/fields/RateLaterAutoRemove.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+
+import { TextField, Typography } from '@mui/material';
+
+import { ApiError } from 'src/services/openapi';
+
+interface RateLaterAutoRemoveProps {
+  apiErrors: ApiError | null;
+  value: number;
+  onChange: (number: number) => void;
+  pollName: string;
+}
+
+const RateLaterAutoRemoveField = ({
+  apiErrors,
+  value,
+  onChange,
+  pollName,
+}: RateLaterAutoRemoveProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <TextField
+      required
+      fullWidth
+      label={t('pollUserSettingsForm.autoRemove')}
+      helperText={
+        <>
+          <Trans
+            t={t}
+            i18nKey="pollUserSettingsForm.autoRemoveHelpText"
+            count={value}
+          >
+            Entities will be removed from your rate-later list after
+            {{ value }} comparisons.
+          </Trans>
+          {apiErrors &&
+            apiErrors.body[pollName]?.rate_later__auto_remove &&
+            apiErrors.body[pollName].rate_later__auto_remove.map(
+              (error: string, idx: number) => (
+                <Typography
+                  key={`rate_later__auto_remove_error_${idx}`}
+                  color="red"
+                  display="block"
+                  variant="caption"
+                >
+                  {error}
+                </Typography>
+              )
+            )}
+        </>
+      }
+      name="rate_later__auto_remove"
+      color="secondary"
+      size="small"
+      type="number"
+      variant="outlined"
+      value={value}
+      onChange={(event) => onChange(Number(event.target.value))}
+      inputProps={{
+        min: 1,
+        'data-testid': `${pollName}_rate_later__auto_remove`,
+      }}
+    />
+  );
+};
+
+export default RateLaterAutoRemoveField;
---
+++ frontend/src/features/settings/preferences/fields/WeeklyCollectiveGoalDisplay.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+
+import {
+  BlankEnum,
+  ComparisonUi_weeklyCollectiveGoalDisplayEnum,
+} from 'src/services/openapi';
+
+interface WeeklyCollectiveGoalDisplayProps {
+  value: ComparisonUi_weeklyCollectiveGoalDisplayEnum | BlankEnum;
+  onChange: (
+    target: ComparisonUi_weeklyCollectiveGoalDisplayEnum | BlankEnum
+  ) => void;
+  pollName: string;
+}
+
+const WeeklyCollectiveGoalDisplayField = ({
+  value,
+  onChange,
+  pollName,
+}: WeeklyCollectiveGoalDisplayProps) => {
+  const { t } = useTranslation();
+
+  const settingChoices = [
+    {
+      label: t('pollUserSettingsForm.always'),
+      value: ComparisonUi_weeklyCollectiveGoalDisplayEnum.ALWAYS,
+    },
+    {
+      label: t('pollUserSettingsForm.websiteOnly'),
+      value: ComparisonUi_weeklyCollectiveGoalDisplayEnum.WEBSITE_ONLY,
+    },
+    {
+      label: t('pollUserSettingsForm.embeddedOnly'),
+      value: ComparisonUi_weeklyCollectiveGoalDisplayEnum.EMBEDDED_ONLY,
+    },
+    {
+      label: t('pollUserSettingsForm.never'),
+      value: ComparisonUi_weeklyCollectiveGoalDisplayEnum.NEVER,
+    },
+  ];
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel
+        id="label__comparison_ui__weekly_collective_goal_display"
+        color="secondary"
+      >
+        {t('pollUserSettingsForm.displayTheTheWeeklyCollectiveGoal')}
+      </InputLabel>
+      <Select
+        size="small"
+        color="secondary"
+        id="comparison_ui__weekly_collective_goal_display"
+        labelId="label__comparison_ui__weekly_collective_goal_display"
+        value={value}
+        label={t('pollUserSettingsForm.displayTheTheWeeklyCollectiveGoal')}
+        onChange={(event) =>
+          onChange(
+            event.target.value as
+              | ComparisonUi_weeklyCollectiveGoalDisplayEnum
+              | BlankEnum
+          )
+        }
+        inputProps={{
+          'data-testid': `${pollName}_weekly_collective_goal_display`,
+        }}
+      >
+        {settingChoices.map((item) => (
+          <MenuItem key={item.value} value={item.value}>
+            {item.label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default WeeklyCollectiveGoalDisplayField;
