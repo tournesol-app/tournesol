4222f6226dbb1992f539de02a2e2e2ad2338fc25
ShinseiNova <89401728+ShinseiNova@users.noreply.github.com>
2023-05-16 13:01:05 +0200

    [front] fix: retrieve user's settings from API when login on preferences page (#1556)
    The preferences form now consider the API as more trustful than the Redux store
    and perform a settings refresh each time it is opened. The settings are then
    dispatched in the store to keep it up-to-date.
    
    This new behaviour fixes some edge cases with the preferences form and should
    make the maintenance / debug of the user settings more easy:  
    
    * (fix) When redirected to the settings / preferences page after a login, the users
       now see their own preferences instead of the default value of the form.
    
    *  (feat) If the settings are updated on a device, the users will be able to see their
       modification in the preferences form on other devices without having to reload
       the app with a page refresh.
    
    Co-authored-by: Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>

--- frontend/src/features/settings/preferences/VideosPollUserSettingsForm.tsx
+++ frontend/src/features/settings/preferences/VideosPollUserSettingsForm.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { Button, Grid, Typography } from '@mui/material';
 
@@ -8,10 +8,7 @@ import {
   DEFAULT_RATE_LATER_AUTO_REMOVAL,
   YOUTUBE_POLL_NAME,
 } from 'src/utils/constants';
-import {
-  replaceSettings,
-  selectSettings,
-} from 'src/features/settings/userSettingsSlice';
+import { replaceSettings } from 'src/features/settings/userSettingsSlice';
 import { useNotifications } from 'src/hooks';
 import {
   ApiError,
@@ -36,22 +33,34 @@ const VideosPollUserSettingsForm = () => {
   const [disabled, setDisabled] = useState(false);
   const [apiErrors, setApiErrors] = useState<ApiError | null>(null);
 
-  // List of user's settings.
-  const userSettings = useSelector(selectSettings).settings;
-
   // Comparison (page)
   const [compUiWeeklyColGoalDisplay, setCompUiWeeklyColGoalDisplay] = useState<
     ComparisonUi_weeklyCollectiveGoalDisplayEnum | BlankEnum
-  >(
-    userSettings?.[pollName]?.comparison_ui__weekly_collective_goal_display ??
-      ComparisonUi_weeklyCollectiveGoalDisplayEnum.ALWAYS
-  );
+  >(ComparisonUi_weeklyCollectiveGoalDisplayEnum.ALWAYS);
 
   // Rate-later settings
   const [rateLaterAutoRemoval, setRateLaterAutoRemoval] = useState(
-    userSettings?.[pollName]?.rate_later__auto_remove ??
-      DEFAULT_RATE_LATER_AUTO_REMOVAL
+    DEFAULT_RATE_LATER_AUTO_REMOVAL
   );
+
+  useEffect(() => {
+    UsersService.usersMeSettingsRetrieve().then((settings) => {
+      const pollSettings = settings?.[pollName];
+      if (
+        pollSettings?.comparison_ui__weekly_collective_goal_display != undefined
+      ) {
+        setCompUiWeeklyColGoalDisplay(
+          pollSettings.comparison_ui__weekly_collective_goal_display
+        );
+      }
+      if (pollSettings?.rate_later__auto_remove != undefined) {
+        setRateLaterAutoRemoval(pollSettings.rate_later__auto_remove);
+      }
+
+      dispatch(replaceSettings(settings));
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
