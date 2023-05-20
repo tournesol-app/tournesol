e34e4e1b50429a563be1ade0c53b8a4e860bb17d
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-05-20 11:36:37 +0200

    Give more time to the React tree to be rendered before scrolling

--- frontend/src/hooks/useScrollToLocation.ts
+++ frontend/src/hooks/useScrollToLocation.ts
@@ -9,9 +9,15 @@ export const useScrollToLocation = () => {
     if (hash && !scrolledRef.current) {
       const id = hash.replace('#', '');
       const element = document.getElementById(id);
+
       if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-        scrolledRef.current = true;
+        // Give more time to the React tree to be rendered, to increase the
+        // chance of scrolling all the way down to the target.
+        // Insecure Bayesian Web Development.
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: 'smooth' });
+          scrolledRef.current = true;
+        }, 600);
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
