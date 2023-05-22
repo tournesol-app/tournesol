3975b2fc61030eb0fc168faa4628f10621a3057e
Gresille & Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-05-22 14:13:48 +0200

    [front] refactor: increase collective goal to 2500 from 2000 (#1567)

--- frontend/src/features/goals/collective.tsx
+++ frontend/src/features/goals/collective.tsx
@@ -1,11 +1,11 @@
-export const WEEKLY_COMPARISON_GOAL = 2000;
+export const WEEKLY_COMPARISON_GOAL = 2500;
 
 /**
  * Return an emoji to reward the users for their progression in the weekly
  * collective goal.
  */
 export const getWeeklyProgressionEmoji = (progression: number) => {
-  if (progression > 140) return '❤️‍🔥';
+  if (progression > 125) return '❤️‍🔥';
   if (progression > 100) return '🥳 🎉';
   if (progression > 75) return '🌻';
   if (progression > 50) return '🌷';
