726e849646da111f8e39412af91c8a74595f126b
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-05-25 15:23:13 +0200

    add the section B. Account Terms

--- frontend/src/pages/about/TermsOfService/TermsOfService.tsx
+++ frontend/src/pages/about/TermsOfService/TermsOfService.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 import { ContentBox, ContentHeader } from 'src/components';
+
+import AccountTerms from './sections/AccountTerms';
 import Definitions from './sections/Definitions';
 
 const TermsOfServicePage = () => {
@@ -17,63 +19,70 @@ const TermsOfServicePage = () => {
         <Typography variant="h3" gutterBottom>
           {t('termsOfService.termsOfService')}
         </Typography>
-        <Typography
-          variant="h4"
-          fontStyle="italic"
-          gutterBottom
-          id="definitions"
-        >
-          {t('termsOfService.definitions')}
-        </Typography>
-        <Definitions />
-        <Typography
-          variant="h4"
-          fontStyle="italic"
-          gutterBottom
-          id="account-terms"
-        >
-          {t('termsOfService.accountTerms')}
-        </Typography>
-        <Typography
-          variant="h4"
-          fontStyle="italic"
-          gutterBottom
-          id="acceptable-use"
-        >
-          {t('termsOfService.acceptableUse')}
-        </Typography>
-        <Typography
-          variant="h4"
-          fontStyle="italic"
-          gutterBottom
-          id="moderation"
-        >
-          {t('termsOfService.moderation')}
-        </Typography>
-        <Typography
-          variant="h4"
-          fontStyle="italic"
-          gutterBottom
-          id="cancellation"
-        >
-          {t('termsOfService.cancellationAndTermination')}
-        </Typography>
-        <Typography
-          variant="h4"
-          fontStyle="italic"
-          gutterBottom
-          id="communications"
-        >
-          {t('termsOfService.communicationsWithTheAssociation')}
-        </Typography>
-        <Typography
-          variant="h4"
-          fontStyle="italic"
-          gutterBottom
-          id="changes-to-these-terms"
-        >
-          {t('termsOfService.changesToTheseTerms')}
-        </Typography>
+        <Box display="flex" flexDirection="column" gap={4}>
+          <Box>
+            <Typography
+              variant="h4"
+              fontStyle="italic"
+              gutterBottom
+              id="definitions"
+            >
+              {t('termsOfService.definitions')}
+            </Typography>
+            <Definitions />
+          </Box>
+          <Box>
+            <Typography
+              variant="h4"
+              fontStyle="italic"
+              gutterBottom
+              id="account-terms"
+            >
+              {t('termsOfService.accountTerms')}
+            </Typography>
+            <AccountTerms />
+          </Box>
+          <Typography
+            variant="h4"
+            fontStyle="italic"
+            gutterBottom
+            id="acceptable-use"
+          >
+            {t('termsOfService.acceptableUse')}
+          </Typography>
+          <Typography
+            variant="h4"
+            fontStyle="italic"
+            gutterBottom
+            id="moderation"
+          >
+            {t('termsOfService.moderation')}
+          </Typography>
+          <Typography
+            variant="h4"
+            fontStyle="italic"
+            gutterBottom
+            id="cancellation"
+          >
+            {t('termsOfService.cancellationAndTermination')}
+          </Typography>
+          <Typography
+            variant="h4"
+            fontStyle="italic"
+            gutterBottom
+            id="communications"
+          >
+            {t('termsOfService.communicationsWithTheAssociation')}
+          </Typography>
+          <Typography
+            variant="h4"
+            fontStyle="italic"
+            gutterBottom
+            id="changes-to-these-terms"
+          >
+            {t('termsOfService.changesToTheseTerms')}
+          </Typography>
+        </Box>
       </ContentBox>
     </>
   );
