ead0c624ea6c547a3846b966d7294a4f35d8a374
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-05-25 09:35:19 +0200

    add the ToS skeleton

--- frontend/src/pages/about/TermsOfService.tsx
+++ frontend/src/pages/about/TermsOfService.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { Typography } from '@mui/material';
 
 import { ContentBox, ContentHeader } from 'src/components';
-import { useTranslation } from 'react-i18next';
 
 const TermsOfServicePage = () => {
   const { t } = useTranslation();
@@ -10,6 +12,67 @@ const TermsOfServicePage = () => {
       <ContentHeader
         title={`${t('menu.about')} > ${t('termsOfService.termsOfService')}`}
       />
+      <ContentBox maxWidth="md">
+        <Typography variant="h3" gutterBottom>
+          {t('termsOfService.termsOfService')}
+        </Typography>
+        <Typography
+          variant="h4"
+          fontStyle="italic"
+          gutterBottom
+          id="definitions"
+        >
+          {t('termsOfService.definitions')}
+        </Typography>
+        <Typography
+          variant="h4"
+          fontStyle="italic"
+          gutterBottom
+          id="account-terms"
+        >
+          {t('termsOfService.accountTerms')}
+        </Typography>
+        <Typography
+          variant="h4"
+          fontStyle="italic"
+          gutterBottom
+          id="acceptable-use"
+        >
+          {t('termsOfService.acceptableUse')}
+        </Typography>
+        <Typography
+          variant="h4"
+          fontStyle="italic"
+          gutterBottom
+          id="moderation"
+        >
+          {t('termsOfService.moderation')}
+        </Typography>
+        <Typography
+          variant="h4"
+          fontStyle="italic"
+          gutterBottom
+          id="cancellation"
+        >
+          {t('termsOfService.cancellationAndTermination')}
+        </Typography>
+        <Typography
+          variant="h4"
+          fontStyle="italic"
+          gutterBottom
+          id="communications"
+        >
+          {t('termsOfService.communicationsWithTheAssociation')}
+        </Typography>
+        <Typography
+          variant="h4"
+          fontStyle="italic"
+          gutterBottom
+          id="changes-to-these-terms"
+        >
+          {t('termsOfService.changesToTheseTerms')}
+        </Typography>
+      </ContentBox>
     </>
   );
 };
