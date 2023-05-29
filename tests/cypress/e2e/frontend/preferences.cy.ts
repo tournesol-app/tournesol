752465fec37f148f77bbccd779ebcc927469ca59
shinseinova <89401728+ShinseiNova@users.noreply.github.com>
2023-05-29 16:33:23 +0200

    add test on rate later setting

--- tests/cypress/e2e/frontend/preferences.cy.ts
+++ tests/cypress/e2e/frontend/preferences.cy.ts
@@ -3,7 +3,7 @@ describe("My user's preferences", () => {
     cy.recreateUser("blankuser", "blankuser@preferences.test", "tournesol");
   });
 
-  it('Show weekly collective goal', () => {
+  it('Show weekly collective goal', () => { 
     cy.visit('/settings/preferences');
     cy.focused().type("blankuser");
     cy.get('input[name="password"]').click().type("tournesol").type('{enter}');
@@ -39,5 +39,22 @@ describe("My user's preferences", () => {
     cy.contains('Weekly collective goal').should('not.exist');
     cy.visit('/comparison?embed=1');
     cy.contains('Weekly collective goal').should('not.exist');
+  });
+
+  it('Rate later setting', () => {
+    cy.visit('/rate_later');
+    cy.focused().type("blankuser");
+    cy.get('input[name="password"]').click().type("tournesol").type('{enter}');
+    cy.contains('removed after 4 comparison(s)').should('be.visible');
+
+    cy.visit('/settings/preferences');
+    cy.contains('Automatic removal').click().type('{selectAll}1');
+    cy.contains('Update preferences').click();
+    cy.visit('/rate_later');
+    cy.get('input[placeholder="Video ID or URL"]').type('dQw4w9WgXcQ').type('{enter}');
+    cy.visit('/comparison?uidA=yt%3AdQw4w9WgXcQ&uidB=yt%3APayvWj2piKg');
+    cy.get('button').contains('submit').click();
+    cy.visit('/rate_later');
+    cy.contains('Your rate-later list contains 0 videos').should('be.visible');
   });
 });
