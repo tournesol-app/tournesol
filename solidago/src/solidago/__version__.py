b5654c0e06bea66a39a415848395f5cfd18845e3
Adrien Matissart <a@matissart.net>
2023-05-29 11:23:56 +0200

    [solidago] fix: wrong working dir in solidago-publish workflow

--- solidago/src/solidago/__version__.py
+++ solidago/src/solidago/__version__.py
@@ -1,4 +1,4 @@
-# Changing the version will automatically publish a new version on pypi
+# Changing the version will automatically publish a new version on PyPI.
 # (see /.github/workflows/solidago-publish.yml)
 
 __version__ = "0.0.2"
