480569af9a1f7e35dd99d7d3e9193b4ef039bb35
shinseinova <89401728+ShinseiNova@users.noreply.github.com>
2023-05-30 10:49:12 +0200

    lint fix

--- frontend/src/components/buttons/PreferencesIconButtonLink.tsx
+++ frontend/src/components/buttons/PreferencesIconButtonLink.tsx
@@ -6,7 +6,10 @@ import { Settings } from '@mui/icons-material';
 
 const PreferencesIconButtonLink = ({ hash = '' }: { hash?: string }) => {
   return (
-    <Link aria-label='Link to the preferences page' to={`settings/preferences${hash}`}>
+    <Link
+      aria-label="Link to the preferences page"
+      to={`settings/preferences${hash}`}
+    >
       <IconButton>
         <Settings />
       </IconButton>
