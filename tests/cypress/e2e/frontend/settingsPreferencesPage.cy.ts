93691bb05181027a2705489e20081764283ad7d2
shinseinova <89401728+ShinseiNova@users.noreply.github.com>
2023-05-30 10:56:00 +0200

    uncomment code

--- tests/cypress/e2e/frontend/settingsPreferencesPage.cy.ts
+++ tests/cypress/e2e/frontend/settingsPreferencesPage.cy.ts
@@ -8,79 +8,79 @@ describe('Settings - preferences page', () => {
     cy.get('input[name="password"]').click().type("tournesol").type('{enter}');
   }
 
-  // describe('Navigation', () => {
-  //   it('is accessible from the personal menu', () => {
-  //     cy.visit('/login');
-  //     login();
+  describe('Navigation', () => {
+    it('is accessible from the personal menu', () => {
+      cy.visit('/login');
+      login();
 
-  //     cy.get('button#personal-menu-button').click();
-  //     cy.get('#personal-menu').contains('Preferences').click();
+      cy.get('button#personal-menu-button').click();
+      cy.get('#personal-menu').contains('Preferences').click();
 
-  //     cy.location('pathname').should('equal', '/settings/preferences');
-  //   });
-  // });
+      cy.location('pathname').should('equal', '/settings/preferences');
+    });
+  });
 
-  // describe('Setting - display weekly collective goal', () => {
-  //   const fieldSelector = '#videos_comparison_ui__weekly_collective_goal_display';
+  describe('Setting - display weekly collective goal', () => {
+    const fieldSelector = '#videos_comparison_ui__weekly_collective_goal_display';
 
-  //   it('handles the value ALWAYS', () => {
-  //     cy.visit('/settings/preferences');
-  //     login();
+    it('handles the value ALWAYS', () => {
+      cy.visit('/settings/preferences');
+      login();
 
-  //     // Ensure the default value is ALWAYS
-  //     cy.get(
-  //       '[data-testid=videos_weekly_collective_goal_display]'
-  //     ).should('have.value', 'ALWAYS');
+      // Ensure the default value is ALWAYS
+      cy.get(
+        '[data-testid=videos_weekly_collective_goal_display]'
+      ).should('have.value', 'ALWAYS');
 
-  //     cy.visit('/comparison');
-  //     cy.contains('Weekly collective goal').should('be.visible');
-  //     cy.visit('/comparison?embed=1');
-  //     cy.contains('Weekly collective goal').should('be.visible');
-  //   });
+      cy.visit('/comparison');
+      cy.contains('Weekly collective goal').should('be.visible');
+      cy.visit('/comparison?embed=1');
+      cy.contains('Weekly collective goal').should('be.visible');
+    });
 
-  //   it('handles the value WEBSITE_ONLY', () => {
-  //     cy.visit('/settings/preferences');
-  //     login();
+    it('handles the value WEBSITE_ONLY', () => {
+      cy.visit('/settings/preferences');
+      login();
 
-  //     cy.get(fieldSelector).click();
-  //     cy.contains('Website only').click();
-  //     cy.contains('Update preferences').click();
+      cy.get(fieldSelector).click();
+      cy.contains('Website only').click();
+      cy.contains('Update preferences').click();
 
-  //     cy.visit('/comparison');
-  //     cy.contains('Weekly collective goal').should('be.visible');
-  //     cy.visit('/comparison?embed=1');
-  //     cy.contains('Weekly collective goal').should('not.exist');
+      cy.visit('/comparison');
+      cy.contains('Weekly collective goal').should('be.visible');
+      cy.visit('/comparison?embed=1');
+      cy.contains('Weekly collective goal').should('not.exist');
 
-  //   });
+    });
 
-  //   it('handles the value EMBEDDED_ONLY', () => {
-  //     cy.visit('/settings/preferences');
-  //     login();
+    it('handles the value EMBEDDED_ONLY', () => {
+      cy.visit('/settings/preferences');
+      login();
 
-  //     cy.get(fieldSelector).click();
-  //     cy.contains('Extension only').click();
-  //     cy.contains('Update preferences').click();
+      cy.get(fieldSelector).click();
+      cy.contains('Extension only').click();
+      cy.contains('Update preferences').click();
 
-  //     cy.visit('/comparison');
-  //     cy.contains('Weekly collective goal').should('not.exist');
-  //     cy.visit('/comparison?embed=1');
-  //     cy.contains('Weekly collective goal').should('be.visible');
-  //   });
+      cy.visit('/comparison');
+      cy.contains('Weekly collective goal').should('not.exist');
+      cy.visit('/comparison?embed=1');
+      cy.contains('Weekly collective goal').should('be.visible');
+    });
 
-  //   it('handles the value NEVER', () => {
-  //     cy.visit('/settings/preferences');
-  //     login();
+    it('handles the value NEVER', () => {
+      cy.visit('/settings/preferences');
+      login();
 
-  //     cy.get(fieldSelector).click();
-  //     cy.contains('Never').click();
-  //     cy.contains('Update preferences').click();
+      cy.get(fieldSelector).click();
+      cy.contains('Never').click();
+      cy.contains('Update preferences').click();
 
-  //     cy.visit('/comparison');
-  //     cy.contains('Weekly collective goal').should('not.exist');
-  //     cy.visit('/comparison?embed=1');
-  //     cy.contains('Weekly collective goal').should('not.exist');
-  //   });
-  // });
+      cy.visit('/comparison');
+      cy.contains('Weekly collective goal').should('not.exist');
+      cy.visit('/comparison?embed=1');
+      cy.contains('Weekly collective goal').should('not.exist');
+    });
+  });
 
   describe('Setting - rate-later auto removal', () => {
     it('handles changing the value', () => {
