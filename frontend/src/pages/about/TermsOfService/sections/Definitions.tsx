f2b29e067177f5f2fcdb0b604083d95c16f96ece
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-06-01 09:16:49 +0200

    add the Website in the Definitions section

--- frontend/src/pages/about/TermsOfService/sections/Definitions.tsx
+++ frontend/src/pages/about/TermsOfService/sections/Definitions.tsx
@@ -3,26 +3,35 @@ import { Trans, useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 import { Alert, AlertTitle, Box, Typography } from '@mui/material';
-import {
-  LooksOne,
-  LooksTwo,
-  Looks3,
-  Looks4,
-  Looks5,
-  Looks6,
-} from '@mui/icons-material';
 
 const Section = ({
-  icon,
   text,
+  num,
 }: {
-  icon: React.ReactElement;
+  num: number;
   text: string | React.ReactElement;
 }) => {
   return (
     <Box display="flex" gap={1}>
-      {icon}
-      <Typography paragraph>{text}</Typography>
+      <Typography
+        px={2}
+        color="secondary.main"
+        fontSize="1.2em"
+        fontWeight="bold"
+      >
+        {num}
+      </Typography>
+      <Typography
+        paragraph
+        sx={{
+          '& pre': {
+            margin: 0,
+            display: 'inline',
+          },
+        }}
+      >
+        {text}
+      </Typography>
     </Box>
   );
 };
@@ -42,39 +51,40 @@ const Definitions = () => {
           {t('termsOfService.definitions.shortVersion')}
         </Alert>
       </Box>
+      <Section num={1} text={t('termsOfService.definitions.account')} />
       <Section
-        icon={<LooksOne />}
-        text={t('termsOfService.definitions.account')}
-      />
-      <Section
-        icon={<LooksTwo />}
+        num={2}
         text={
           <Trans t={t} i18nKey="termsOfService.definitions.agreement">
             The &quot;Agreement&quot; refers, collectively, to all the terms,
             conditions, notices contained or referenced in this document (the
             &quot;Terms of Service&quot; or the Terms) and all other operating
-            rules, policies (including the Tournesol Privacy Policy, available
-            at{' '}
-            <Link to="/about/privacy_policy">
-              https://tournesol.app/about/privacy_policy
-            </Link>
-            ).
+            rules, policies (including our{' '}
+            <Link to="/about/privacy_policy">Privacy Policy</Link>) and
+            procedures that we may publish from time to time on the website.
           </Trans>
         }
       />
+      <Section num={3} text={t('termsOfService.definitions.association')} />
+      <Section num={4} text={t('termsOfService.definitions.content')} />
+      <Section num={5} text={t('termsOfService.definitions.service')} />
+      <Section num={6} text={t('termsOfService.definitions.user')} />
       <Section
-        icon={<Looks3 />}
-        text={t('termsOfService.definitions.association')}
+        num={7}
+        text={
+          <Trans t={t} i18nKey="termsOfService.definitions.website">
+            The &quot;Website&quot; refers to the Tournesol project&apos;s
+            website located at <Link to="/">tournesol.app</Link>, and all
+            content and services provided by the Association at or through the
+            Website. It also refers to the Association-owned subdomains of
+            tournesol.app, such as <pre>api.tournesol.app</pre>. Occasionally,
+            websites owned by the Association may provide different or
+            additional terms of service. If those additional terms conflict with
+            this Agreement, the more specific terms apply to the relevant page
+            or service.
+          </Trans>
+        }
       />
-      <Section
-        icon={<Looks4 />}
-        text={t('termsOfService.definitions.content')}
-      />
-      <Section
-        icon={<Looks5 />}
-        text={t('termsOfService.definitions.service')}
-      />
-      <Section icon={<Looks6 />} text={t('termsOfService.definitions.user')} />
     </Box>
   );
 };
