876eb08aec7200180f4c4fea62a3138926508cdc
shinseinova <89401728+ShinseiNova@users.noreply.github.com>
2023-06-05 09:23:49 +0200

    resolve conflicts

--- frontend/src/features/settings/preferences/VideosPollUserSettingsForm.tsx
+++ frontend/src/features/settings/preferences/VideosPollUserSettingsForm.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Alert, Button, Grid, Typography } from '@mui/material';
 
-import { LoaderWrapper } from 'src/components';
-import { replaceSettings } from 'src/features/settings/userSettingsSlice';
+import {
+  replaceSettings,
+  selectSettings,
+} from 'src/features/settings/userSettingsSlice';
 import { useNotifications, useScrollToLocation } from 'src/hooks';
 import {
   ApiError,
@@ -37,64 +39,62 @@ const VideosPollUserSettingsForm = () => {
   const dispatch = useDispatch();
   const { showSuccessAlert, showErrorAlert } = useNotifications();
 
-  const [loading, setLoading] = useState(true);
   const [disabled, setDisabled] = useState(false);
   const [apiErrors, setApiErrors] = useState<ApiError | null>(null);
 
   useScrollToLocation();
 
+  const userSettings = useSelector(selectSettings).settings;
+  const pollSettings = userSettings?.videos;
+
   // Comparison (page)
   const [compUiWeeklyColGoalDisplay, setCompUiWeeklyColGoalDisplay] = useState<
     ComparisonUi_weeklyCollectiveGoalDisplayEnum | BlankEnum
-  >(ComparisonUi_weeklyCollectiveGoalDisplayEnum.ALWAYS);
+  >(
+    pollSettings?.comparison_ui__weekly_collective_goal_display ??
+      ComparisonUi_weeklyCollectiveGoalDisplayEnum.ALWAYS
+  );
 
   // Rate-later settings
   const [rateLaterAutoRemoval, setRateLaterAutoRemoval] = useState(
-    DEFAULT_RATE_LATER_AUTO_REMOVAL
+    pollSettings?.rate_later__auto_remove ?? DEFAULT_RATE_LATER_AUTO_REMOVAL
   );
 
   // Recommendations (page)
-  const [recoDefaultUnsafe, setRecoDefaultUnsafe] = useState(false);
+  const [recoDefaultUnsafe, setRecoDefaultUnsafe] = useState(
+    pollSettings?.recommendations__default_unsafe ?? false
+  );
   const [recoDefaultUploadDate, setRecoDefaultUploadDate] = useState<
     Recommendations_defaultDateEnum | BlankEnum
-  >(Recommendations_defaultDateEnum.MONTH);
+  >(
+    pollSettings?.recommendations__default_date ??
+      Recommendations_defaultDateEnum.MONTH
+  );
 
-  /**
-   * Initialize the form with up-to-date settings from the API, and dispatch
-   * them in the Redux store.
-   */
   useEffect(() => {
-    UsersService.usersMeSettingsRetrieve()
-      .then((settings) => {
-        const pollSettings = settings?.[pollName];
-        if (
-          pollSettings?.comparison_ui__weekly_collective_goal_display !=
-          undefined
-        ) {
-          setCompUiWeeklyColGoalDisplay(
-            pollSettings.comparison_ui__weekly_collective_goal_display
-          );
-        }
-        // if (pollSettings?.comparison__criteria_order != undefined) {
-        //   console.log(stringToCriteriasPos(pollSettings.comparison__criteria_order));
-        // }
-        if (pollSettings?.rate_later__auto_remove != undefined) {
-          setRateLaterAutoRemoval(pollSettings.rate_later__auto_remove);
-        }
-        if (pollSettings?.recommendations__default_unsafe != undefined) {
-          setRecoDefaultUnsafe(pollSettings.recommendations__default_unsafe);
-        }
-        if (pollSettings?.recommendations__default_date != undefined) {
-          setRecoDefaultUploadDate(pollSettings.recommendations__default_date);
-        }
-
-        dispatch(replaceSettings(settings));
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    if (!pollSettings) {
+      return;
+    }
+    if (
+      pollSettings.comparison_ui__weekly_collective_goal_display != undefined
+    ) {
+      setCompUiWeeklyColGoalDisplay(
+        pollSettings.comparison_ui__weekly_collective_goal_display
+      );
+    }
+    // if (pollSettings?.comparison__criteria_order != undefined) {
+    //   console.log(stringToCriteriasPos(pollSettings.comparison__criteria_order));
+    // }
+    if (pollSettings.rate_later__auto_remove != undefined) {
+      setRateLaterAutoRemoval(pollSettings.rate_later__auto_remove);
+    }
+    if (pollSettings.recommendations__default_unsafe != undefined) {
+      setRecoDefaultUnsafe(pollSettings.recommendations__default_unsafe);
+    }
+    if (pollSettings.recommendations__default_date != undefined) {
+      setRecoDefaultUploadDate(pollSettings.recommendations__default_date);
+    }
+  }, [pollSettings]);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -138,86 +138,84 @@ const VideosPollUserSettingsForm = () => {
   ]);
 
   return (
-    <LoaderWrapper isLoading={loading}>
-      <form onSubmit={handleSubmit}>
-        <Grid container spacing={4} direction="column" alignItems="stretch">
-          <Grid item>
-            <Typography id="comparison_page" variant="h6">
-              {t('pollUserSettingsForm.comparisonPage')}
-            </Typography>
-          </Grid>
-          <Grid item>
-            <WeeklyCollectiveGoalDisplayField
-              value={compUiWeeklyColGoalDisplay}
-              onChange={setCompUiWeeklyColGoalDisplay}
-              pollName={pollName}
-            />
-          </Grid>
-          <Grid item>
-            <ComparisonCriteriaOrderField
-              checkedCriterias={checkedCriterias}
-              setCheckedCriterias={setCheckedCriterias}
-            />
-          </Grid>
-          <Grid item>
-            <Typography id="rate_later" variant="h6">
-              {t('pollUserSettingsForm.rateLater')}
-            </Typography>
-          </Grid>
-          <Grid item>
-            <RateLaterAutoRemoveField
-              apiErrors={apiErrors}
-              value={rateLaterAutoRemoval}
-              onChange={setRateLaterAutoRemoval}
-              pollName={pollName}
-            />
-          </Grid>
-          <Grid item>
-            <Typography id="recommendations_page" variant="h6">
-              {t('pollUserSettingsForm.recommendationsPage')}
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Alert severity="info">
-              <Trans
-                t={t}
-                i18nKey="pollUserSettingsForm.customizeYourDefaultSearchFilter"
-              >
-                Customize <strong>the default search filters</strong> according
-                to your own preferences. Those filters are applied{' '}
-                <strong>only</strong> when you access the recommendations from
-                the <strong>main menu</strong>.
-              </Trans>
-            </Alert>
-          </Grid>
-          <Grid item>
-            <RecommendationsDefaultDate
-              value={recoDefaultUploadDate}
-              onChange={setRecoDefaultUploadDate}
-              pollName={pollName}
-            />
-          </Grid>
-          <Grid item>
-            <RecommendationsDefaultUnsafe
-              value={recoDefaultUnsafe}
-              onChange={setRecoDefaultUnsafe}
-              pollName={pollName}
-            />
-          </Grid>
-          <Grid item>
-            <Button
-              fullWidth
-              type="submit"
-              color="secondary"
-              variant="contained"
-              disabled={disabled}
-            >
-              {t('pollUserSettingsForm.updatePreferences')}
-            </Button>
-          </Grid>
+    <form onSubmit={handleSubmit}>
+      <Grid container spacing={4} direction="column" alignItems="stretch">
+        <Grid item>
+          <Typography id="comparison_page" variant="h6">
+            {t('pollUserSettingsForm.comparisonPage')}
+          </Typography>
         </Grid>
-      </form>
-    </LoaderWrapper>
+        <Grid item>
+          <WeeklyCollectiveGoalDisplayField
+            value={compUiWeeklyColGoalDisplay}
+            onChange={setCompUiWeeklyColGoalDisplay}
+            pollName={pollName}
+          />
+        </Grid>
+        <Grid item>
+          <ComparisonCriteriaOrderField
+            checkedCriterias={checkedCriterias}
+            setCheckedCriterias={setCheckedCriterias}
+          />
+        </Grid>
+        <Grid item>
+          <Typography id="rate_later" variant="h6">
+            {t('pollUserSettingsForm.rateLater')}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <RateLaterAutoRemoveField
+            apiErrors={apiErrors}
+            value={rateLaterAutoRemoval}
+            onChange={setRateLaterAutoRemoval}
+            pollName={pollName}
+          />
+        </Grid>
+        <Grid item>
+          <Typography id="recommendations_page" variant="h6">
+            {t('pollUserSettingsForm.recommendationsPage')}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Alert severity="info">
+            <Trans
+              t={t}
+              i18nKey="pollUserSettingsForm.customizeYourDefaultSearchFilter"
+            >
+              Customize <strong>the default search filters</strong> according to
+              your own preferences. Those filters are applied{' '}
+              <strong>only</strong> when you access the recommendations from the{' '}
+              <strong>main menu</strong>.
+            </Trans>
+          </Alert>
+        </Grid>
+        <Grid item>
+          <RecommendationsDefaultDate
+            value={recoDefaultUploadDate}
+            onChange={setRecoDefaultUploadDate}
+            pollName={pollName}
+          />
+        </Grid>
+        <Grid item>
+          <RecommendationsDefaultUnsafe
+            value={recoDefaultUnsafe}
+            onChange={setRecoDefaultUnsafe}
+            pollName={pollName}
+          />
+        </Grid>
+        <Grid item>
+          <Button
+            fullWidth
+            type="submit"
+            color="secondary"
+            variant="contained"
+            disabled={disabled}
+          >
+            {t('pollUserSettingsForm.updatePreferences')}
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
   );
 };
 
