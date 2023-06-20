2319ae3fcac5ddbac0d9be8fe3aadd38ffddb2e5
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-06-20 14:26:38 +0200

    harmonize previews' names

--- backend/tournesol/views/previews/__init__.py
+++ backend/tournesol/views/previews/__init__.py
@@ -1,5 +1,5 @@
 from .comparisons import DynamicWebsitePreviewComparison
 from .default import DynamicWebsitePreviewDefault
-from .entity import DynamicWebsitePreviewEntity
+from .entities import DynamicWebsitePreviewEntity
 from .faq import DynamicWebsitePreviewFAQ
 from .recommendations import DynamicWebsitePreviewRecommendations
