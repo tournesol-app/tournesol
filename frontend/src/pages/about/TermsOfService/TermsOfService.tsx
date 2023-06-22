91037fc175660d9fa4bdcc0f3d860503156ee187
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-06-22 10:27:19 +0200

    add the public domain disclaimer

--- frontend/src/pages/about/TermsOfService/TermsOfService.tsx
+++ frontend/src/pages/about/TermsOfService/TermsOfService.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
-import { Box, Typography } from '@mui/material';
+import { Box, Link, Paper, Typography } from '@mui/material';
 
 import { ContentBox, ContentHeader } from 'src/components';
 
@@ -32,6 +32,32 @@ const TermsOfServicePage = () => {
           <CancellationAndTermination />
           <CommunicationsWithAssociation />
           <ChangesToTheseTerms />
+          <Paper sx={{ p: 2 }}>
+            <Typography paragraph>
+              {t('terms.copying.thisDocumentIsInspiredByTheGitHubTos')}
+            </Typography>
+            <Typography paragraph mb={0}>
+              <Trans
+                t={t}
+                i18nKey="terms.copying.thePresentTextsAreDedicatedToThePublicDomain"
+              >
+                The present texts are dedicated to the Public Domain, as stated
+                by the license{' '}
+                <Link
+                  href="https://creativecommons.org/publicdomain/zero/1.0/"
+                  target="_blank"
+                  rel="noopener"
+                  sx={{
+                    color: 'revert',
+                    textDecoration: 'revert',
+                  }}
+                >
+                  Creative Commons Zero v1.0 Universal (CC0 1.0)
+                </Link>
+                .
+              </Trans>
+            </Typography>
+          </Paper>
         </Box>
       </ContentBox>
     </>
