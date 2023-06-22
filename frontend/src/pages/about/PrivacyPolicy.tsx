8a12b2cfbae4820aae9b6f44b477588ed2f28c2a
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-06-22 17:40:13 +0200

    rework the style of the first section

--- frontend/src/pages/about/PrivacyPolicy.tsx
+++ frontend/src/pages/about/PrivacyPolicy.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Paper, Typography } from '@mui/material';
 
 import { ContentBox, ContentHeader } from 'src/components';
 import { useTranslation } from 'react-i18next';
@@ -15,50 +15,52 @@ const PrivacyPolicyPage = () => {
         <Typography variant="h3" gutterBottom>
           {t('privacyPolicy.privacyPolicy')}
         </Typography>
-        <Typography
-          id="personalData"
-          variant="h4"
-          gutterBottom
-          sx={{ fontStyle: 'italic' }}
-        >
-          {t('privacyPolicy.whatPersonalDataTournesolCollectAndWhy')}
-        </Typography>
-        <Typography id="ratings" variant="h5">
-          {t('privacyPolicy.ratings')}
-        </Typography>
-        <Typography paragraph>
-          {t('privacyPolicy.whyTournesolCollectsRatings')}
-        </Typography>
-        <Typography id="search" variant="h5">
-          {t('privacyPolicy.search')}
-        </Typography>
-        <Typography paragraph>
-          {t('privacyPolicy.whyTournesolCollectsSearchQueries')}
-        </Typography>
-        <Typography id="contributorProfile" variant="h5">
-          {t('privacyPolicy.contributorProfile')}
-        </Typography>
-        <Typography paragraph>
-          {t('privacyPolicy.whyTournesolCollectsContributorProfile')}
-        </Typography>
-        <Typography id="contactForm" variant="h5">
-          {t('privacyPolicy.contactForm')}
-        </Typography>
-        <Typography paragraph>
-          {t('privacyPolicy.thereIsNoContactForm')}
-        </Typography>
-        <Typography id="loginInformation" variant="h5">
-          {t('privacyPolicy.loginInformation')}
-        </Typography>
-        <Typography paragraph>
-          {t('privacyPolicy.loginInformationDetails')}
-        </Typography>
-        <Typography id="embeddedContent" variant="h5">
-          {t('privacyPolicy.embeddedContentFromOtherWebsites')}
-        </Typography>
-        <Typography paragraph>
-          {t('privacyPolicy.whatDataCollectEmbeddedWebsites')}
-        </Typography>
+        <Paper sx={{ p: 2 }}>
+          <Typography
+            id="what-personal-data-tournesol-collects-and-why"
+            variant="h4"
+            fontStyle="italic"
+            gutterBottom
+          >
+            {t('privacyPolicy.whatPersonalDataTournesolCollectAndWhy')}
+          </Typography>
+          <Typography id="ratings" variant="h5" gutterBottom>
+            {t('privacyPolicy.ratings')}
+          </Typography>
+          <Typography paragraph>
+            {t('privacyPolicy.whyTournesolCollectsRatings')}
+          </Typography>
+          <Typography id="search" variant="h5" gutterBottom>
+            {t('privacyPolicy.search')}
+          </Typography>
+          <Typography paragraph>
+            {t('privacyPolicy.whyTournesolCollectsSearchQueries')}
+          </Typography>
+          <Typography id="contributorProfile" variant="h5" gutterBottom>
+            {t('privacyPolicy.contributorProfile')}
+          </Typography>
+          <Typography paragraph>
+            {t('privacyPolicy.whyTournesolCollectsContributorProfile')}
+          </Typography>
+          <Typography id="contactForm" variant="h5" gutterBottom>
+            {t('privacyPolicy.contactForm')}
+          </Typography>
+          <Typography paragraph>
+            {t('privacyPolicy.thereIsNoContactForm')}
+          </Typography>
+          <Typography id="loginInformation" variant="h5" gutterBottom>
+            {t('privacyPolicy.loginInformation')}
+          </Typography>
+          <Typography paragraph>
+            {t('privacyPolicy.loginInformationDetails')}
+          </Typography>
+          <Typography id="embeddedContent" variant="h5" gutterBottom>
+            {t('privacyPolicy.embeddedContentFromOtherWebsites')}
+          </Typography>
+          <Typography paragraph mb={0}>
+            {t('privacyPolicy.whatDataCollectEmbeddedWebsites')}
+          </Typography>
+        </Paper>
         <Typography
           id="shareData"
           variant="h4"
