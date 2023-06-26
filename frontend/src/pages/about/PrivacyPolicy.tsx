c9be4d87001cc20d029f349d7dd336d030323b29
shinseinova <89401728+ShinseiNova@users.noreply.github.com>
2023-06-26 10:50:04 +0200

    add papers for each section

--- frontend/src/pages/about/PrivacyPolicy.tsx
+++ frontend/src/pages/about/PrivacyPolicy.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Paper, Typography } from '@mui/material';
+import { Box, Paper, Typography } from '@mui/material';
 
 import { ContentBox, ContentHeader } from 'src/components';
 import { useTranslation } from 'react-i18next';
@@ -15,105 +15,117 @@ const PrivacyPolicyPage = () => {
         <Typography variant="h3" gutterBottom>
           {t('privacyPolicy.privacyPolicy')}
         </Typography>
-        <Paper sx={{ p: 2 }}>
-          <Typography
-            id="what-personal-data-tournesol-collects-and-why"
-            variant="h4"
-            fontStyle="italic"
-            gutterBottom
-          >
-            {t('privacyPolicy.whatPersonalDataTournesolCollectAndWhy')}
-          </Typography>
-          <Typography id="ratings" variant="h5" gutterBottom>
-            {t('privacyPolicy.ratings')}
-          </Typography>
-          <Typography paragraph>
-            {t('privacyPolicy.whyTournesolCollectsRatings')}
-          </Typography>
-          <Typography id="search" variant="h5" gutterBottom>
-            {t('privacyPolicy.search')}
-          </Typography>
-          <Typography paragraph>
-            {t('privacyPolicy.whyTournesolCollectsSearchQueries')}
-          </Typography>
-          <Typography id="contributorProfile" variant="h5" gutterBottom>
-            {t('privacyPolicy.contributorProfile')}
-          </Typography>
-          <Typography paragraph>
-            {t('privacyPolicy.whyTournesolCollectsContributorProfile')}
-          </Typography>
-          <Typography id="contactForm" variant="h5" gutterBottom>
-            {t('privacyPolicy.contactForm')}
-          </Typography>
-          <Typography paragraph>
-            {t('privacyPolicy.thereIsNoContactForm')}
-          </Typography>
-          <Typography id="loginInformation" variant="h5" gutterBottom>
-            {t('privacyPolicy.loginInformation')}
-          </Typography>
-          <Typography paragraph>
-            {t('privacyPolicy.loginInformationDetails')}
-          </Typography>
-          <Typography id="embeddedContent" variant="h5" gutterBottom>
-            {t('privacyPolicy.embeddedContentFromOtherWebsites')}
-          </Typography>
-          <Typography paragraph mb={0}>
-            {t('privacyPolicy.whatDataCollectEmbeddedWebsites')}
-          </Typography>
-        </Paper>
-        <Typography
-          id="shareData"
-          variant="h4"
-          gutterBottom
-          sx={{ fontStyle: 'italic' }}
-        >
-          {t('privacyPolicy.whoTournesolSharesUsersNContributorsDataWith')}
-        </Typography>
-        <Typography paragraph>
-          {t(
-            'privacyPolicy.whoTournesolSharesUsersNContributorsDataWithParagraph'
-          )}
-        </Typography>
-        <Typography id="publicData" variant="h5">
-          {t('privacyPolicy.publicData')}
-        </Typography>
-        <Typography paragraph>
-          {t('privacyPolicy.whereGoPublicData')}
-        </Typography>
-        <Typography id="aggregateData" variant="h5">
-          {t('privacyPolicy.aggregateData')}
-        </Typography>
-        <Typography paragraph>
-          {t('privacyPolicy.whereGoAggregateData')}
-        </Typography>
-        <Typography id="researchPurposes" variant="h5">
-          {t('privacyPolicy.researchPurposes')}
-        </Typography>
-        <Typography paragraph>
-          {t('privacyPolicy.researchPurposesParagraph')}
-        </Typography>
-        <Typography
-          id="retainsData"
-          variant="h4"
-          gutterBottom
-          sx={{ fontStyle: 'italic' }}
-        >
-          {t('privacyPolicy.howLongTournesolRetainsContributorsData')}
-        </Typography>
-        <Typography paragraph>
-          {t('privacyPolicy.howLongTournesolRetainsContributorsDataParagraph')}
-        </Typography>
-        <Typography
-          id="rightsData"
-          variant="h4"
-          gutterBottom
-          sx={{ fontStyle: 'italic' }}
-        >
-          {t('privacyPolicy.whatRightsContributorsHaveOverTheirData')}
-        </Typography>
-        <Typography paragraph>
-          {t('privacyPolicy.whatRightsContributorsHaveOverTheirDataParagraph')}
-        </Typography>
+        <Box display="flex" flexDirection="column" gap={4}>
+          <Paper sx={{ p: 2 }}>
+            <Typography
+              id="what-personal-data-tournesol-collects-and-why"
+              variant="h4"
+              fontStyle="italic"
+              gutterBottom
+            >
+              {t('privacyPolicy.whatPersonalDataTournesolCollectAndWhy')}
+            </Typography>
+            <Typography id="ratings" variant="h5" gutterBottom>
+              {t('privacyPolicy.ratings')}
+            </Typography>
+            <Typography paragraph>
+              {t('privacyPolicy.whyTournesolCollectsRatings')}
+            </Typography>
+            <Typography id="search" variant="h5" gutterBottom>
+              {t('privacyPolicy.search')}
+            </Typography>
+            <Typography paragraph>
+              {t('privacyPolicy.whyTournesolCollectsSearchQueries')}
+            </Typography>
+            <Typography id="contributorProfile" variant="h5" gutterBottom>
+              {t('privacyPolicy.contributorProfile')}
+            </Typography>
+            <Typography paragraph>
+              {t('privacyPolicy.whyTournesolCollectsContributorProfile')}
+            </Typography>
+            <Typography id="contactForm" variant="h5" gutterBottom>
+              {t('privacyPolicy.contactForm')}
+            </Typography>
+            <Typography paragraph>
+              {t('privacyPolicy.thereIsNoContactForm')}
+            </Typography>
+            <Typography id="loginInformation" variant="h5" gutterBottom>
+              {t('privacyPolicy.loginInformation')}
+            </Typography>
+            <Typography paragraph>
+              {t('privacyPolicy.loginInformationDetails')}
+            </Typography>
+            <Typography id="embeddedContent" variant="h5" gutterBottom>
+              {t('privacyPolicy.embeddedContentFromOtherWebsites')}
+            </Typography>
+            <Typography paragraph mb={0}>
+              {t('privacyPolicy.whatDataCollectEmbeddedWebsites')}
+            </Typography>
+          </Paper>
+          <Paper sx={{ p: 2 }}>
+            <Typography
+              id="shareData"
+              variant="h4"
+              gutterBottom
+              sx={{ fontStyle: 'italic' }}
+            >
+              {t('privacyPolicy.whoTournesolSharesUsersNContributorsDataWith')}
+            </Typography>
+            <Typography paragraph>
+              {t(
+                'privacyPolicy.whoTournesolSharesUsersNContributorsDataWithParagraph'
+              )}
+            </Typography>
+            <Typography id="publicData" variant="h5">
+              {t('privacyPolicy.publicData')}
+            </Typography>
+            <Typography paragraph>
+              {t('privacyPolicy.whereGoPublicData')}
+            </Typography>
+            <Typography id="aggregateData" variant="h5">
+              {t('privacyPolicy.aggregateData')}
+            </Typography>
+            <Typography paragraph>
+              {t('privacyPolicy.whereGoAggregateData')}
+            </Typography>
+            <Typography id="researchPurposes" variant="h5">
+              {t('privacyPolicy.researchPurposes')}
+            </Typography>
+            <Typography paragraph>
+              {t('privacyPolicy.researchPurposesParagraph')}
+            </Typography>
+          </Paper>
+          <Paper sx={{ p: 2 }}>
+            <Typography
+              id="retainsData"
+              variant="h4"
+              gutterBottom
+              sx={{ fontStyle: 'italic' }}
+            >
+              {t('privacyPolicy.howLongTournesolRetainsContributorsData')}
+            </Typography>
+            <Typography paragraph>
+              {t(
+                'privacyPolicy.howLongTournesolRetainsContributorsDataParagraph'
+              )}
+            </Typography>
+          </Paper>
+          <Paper sx={{ p: 2 }}>
+            <Typography
+              id="rightsData"
+              variant="h4"
+              gutterBottom
+              sx={{ fontStyle: 'italic' }}
+            >
+              {t('privacyPolicy.whatRightsContributorsHaveOverTheirData')}
+            </Typography>
+            <Typography paragraph>
+              {t(
+                'privacyPolicy.whatRightsContributorsHaveOverTheirDataParagraph'
+              )}
+            </Typography>
+          </Paper>
+        </Box>
       </ContentBox>
     </>
   );
