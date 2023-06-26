a2c3fec1467d6655efde5dd507e9db709258a803
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-06-26 09:46:25 +0200

    make the title required and update the field description

--- backend/backoffice/admin.py
+++ backend/backoffice/admin.py
@@ -7,7 +7,7 @@ from backoffice.models import TalkEntry
 class TalkEntryAdmin(admin.ModelAdmin):
     search_fields = ("title", "speakers")
     ordering = ("-date",)
-    list_display = ("date", "title", "get_youtube_link",  "display")
+    list_display = ("date", "title", "get_youtube_link", "display")
     list_display_links = ("date",)
     list_filter = ("display",)
 
--- backend/backoffice/models.py
+++ backend/backoffice/models.py
@@ -1,3 +1,5 @@
+import datetime
+from typing import Optional
 from zoneinfo import ZoneInfo
 
 from django.db import models
@@ -5,44 +7,41 @@ from django.utils.html import format_html
 
 
 class TalkEntry(models.Model):
+    title = models.CharField(
+        help_text="The title of the Talk.",
+        max_length=255,
+    )
     name = models.SlugField(
         unique=True,
-        help_text="Unique reference of the talk",
+        help_text="Unique identifier of the Talk (usable as an URL slug).",
     )
     date = models.DateTimeField(
-        help_text="Date and time when the talk (please mind the server time zone).",
+        help_text="Date and time of the Talk (please mind the server time zone).",
         null=True,
-        default=None,
         blank=True,
     )
     invitation_link = models.URLField(
-        help_text="the invitation link of the talk",
+        help_text="Allow the users to join the Talk.",
         null=True,
         blank=True,
     )
     youtube_link = models.URLField(
-        help_text="the youtube link of the talk",
+        help_text="The Talk's YouTube link.",
         null=True,
         blank=True,
     )
     speakers = models.TextField(
-        help_text="Speakers of the talk and short description",
+        help_text="A list of speakers and their titles (researcher, PhD student, etc.).",
         null=True,
         blank=True,
-    )
-    title = models.CharField(
-        help_text="the title of the talk",
-        null=True,
-        blank=True,
-        max_length=255,
     )
     abstract = models.TextField(
-        help_text="Description of the content of the talk",
+        help_text="Description of the Talk.",
         null=True,
         blank=True,
     )
     display = models.BooleanField(
-        help_text='Determine if talk is display on frontend',
+        help_text="If False, the Talk should not be returned by the API.",
         default=False
     )
 
@@ -54,10 +53,9 @@ class TalkEntry(models.Model):
     def __str__(self) -> str:
         return self.name
 
-    def get_date_gmt(self):
-        """Return the date in time zone Europe/Paris."""
+    def date_as_tz_europe_paris(self) -> Optional[datetime.datetime]:
         if self.date:
-            return self.date.astimezone(ZoneInfo('Europe/Paris')).strftime('%Y-%m-%dT%H:%M:%S%z')
+            return self.date.astimezone(ZoneInfo('Europe/Paris'))
         return None
 
     def html_youtube_link(self):
