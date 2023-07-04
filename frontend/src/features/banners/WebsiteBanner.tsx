efda24c422ce16a720329f787017c6d459b620bf
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-07-04 16:09:30 +0200

    simplify website banner

--- frontend/src/features/banners/WebsiteBanner.tsx
+++ frontend/src/features/banners/WebsiteBanner.tsx
@@ -24,61 +24,46 @@ const WebsiteBanner = ({ banner }: WebsiteBannerSingleProps) => {
   const mediaBelowXl = useMediaQuery(theme.breakpoints.down('xl'));
 
   return (
-    <>
-      {banner !== undefined && (
-        <Box py={3} bgcolor="#1282B2">
-          <Grid
-            container
-            width="100%"
-            flexDirection="column"
-            alignItems="center"
-          >
-            <Grid item xl={9} width="100%" spacing={2} direction="column">
-              <Paper sx={{ p: 2 }} square={mediaBelowXl}>
-                <Stack
-                  // Using != direction per breakpoint requires to define != spacing
-                  // per breakpoint.
-                  spacing={{ xs: 2, sm: 2 }}
-                  direction={{ sm: 'column', md: 'row' }}
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <Stack direction="column" spacing={2}>
-                    <Stack direction="row" spacing={2} alignItems="center">
-                      <Campaign fontSize="large" sx={{ color: '#1282B2' }} />
-                      <Typography paragraph>
-                        <strong>{banner?.title}</strong>
-                      </Typography>
-                    </Stack>
-                    <Stack
-                      direction={{ sm: 'column', md: 'row' }}
-                      spacing={2}
-                      alignItems="center"
-                    >
-                      <Typography paragraph mb={0}>
-                        {banner.text}
-                      </Typography>
-                      <Box display="flex" justifyContent="flex-end">
-                        {banner?.action_link && (
-                          <Button
-                            variant="contained"
-                            color="primary"
-                            component={Link}
-                            href={banner.action_link}
-                          >
-                            {banner.action_label}
-                          </Button>
-                        )}
-                      </Box>
-                    </Stack>
-                  </Stack>
-                </Stack>
-              </Paper>
-            </Grid>
-          </Grid>
-        </Box>
-      )}
-    </>
+    <Box py={3} bgcolor={theme.palette.background.emphatic}>
+      <Grid container width="100%" flexDirection="column" alignItems="center">
+        <Grid item width="100%" xl={9}>
+          <Paper sx={{ p: 2 }} square={mediaBelowXl}>
+            <Stack direction="column" spacing={1}>
+              <Stack direction="row" spacing={2} alignItems="center">
+                <Campaign
+                  fontSize="large"
+                  sx={{ color: theme.palette.secondary.main }}
+                />
+                <Typography paragraph>
+                  <strong>{banner.title}</strong>
+                </Typography>
+              </Stack>
+              <Stack
+                direction={{ sm: 'column', md: 'row' }}
+                spacing={{ xs: 2, sm: 2 }}
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Typography paragraph mb={0}>
+                  {banner.text}
+                </Typography>
+
+                {banner.action_link && (
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    component={Link}
+                    href={banner.action_link}
+                  >
+                    {banner.action_label}
+                  </Button>
+                )}
+              </Stack>
+            </Stack>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
