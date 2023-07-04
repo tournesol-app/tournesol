447d255accb9bbc8b9ff2b61c87c4981e4dca31d
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-07-04 15:55:42 +0200

    simplify WebsiteBanners

--- frontend/src/features/banners/WebsiteBanners.tsx
+++ frontend/src/features/banners/WebsiteBanners.tsx
@@ -1,46 +1,45 @@
 import React, { useEffect, useState } from 'react';
 
-import { useNotifications } from 'src/hooks';
-
 import { BackofficeService, Banner } from 'src/services/openapi';
 
 import WebsiteBanner from './WebsiteBanner';
+import { useTranslation } from 'react-i18next';
+
+const sortBannersByPriority = (a: Banner, b: Banner) => {
+  if (a.priority !== undefined && b.priority !== undefined) {
+    return b.priority - a.priority;
+  }
+
+  return 0;
+};
 
 const WebsiteBanners = () => {
-  const { contactAdministrator } = useNotifications();
+  const { i18n } = useTranslation();
 
   const [banners, setBanners] = useState<Array<Banner>>([]);
 
-  const sortBanners = (a: Banner, b: Banner) => {
-    if (a.priority !== undefined && b.priority !== undefined) {
-      return b.priority - a.priority;
-    }
-
-    return 0;
-  };
-
   useEffect(() => {
     async function getBanners() {
-      const bannersList = await BackofficeService.backofficeBannersList({
-        limit: 100,
-      }).catch(() => {
-        contactAdministrator('error');
+      const bannersList = await BackofficeService.backofficeBannersList(
+        {}
+      ).catch(() => {
+        console.warn('Failed to retrieve the banners from the API.');
       });
 
-      if (bannersList?.results !== undefined) {
-        bannersList.results.sort(sortBanners);
+      if (bannersList?.results && bannersList.results.length > 0) {
+        bannersList.results.sort(sortBannersByPriority);
         setBanners(bannersList.results);
       }
     }
 
     getBanners();
-  }, [contactAdministrator]);
+  }, [i18n.resolvedLanguage]);
 
-  return (
-    <>
-      <WebsiteBanner banner={banners[0]} />
-    </>
-  );
+  if (banners.length <= 0) {
+    return <></>;
+  }
+
+  return <WebsiteBanner banner={banners[0]} />;
 };
 
 export default WebsiteBanners;
