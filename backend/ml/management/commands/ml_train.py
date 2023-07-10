29dac818435536f9101985a4e965af1e3372dba2
Louis Faucon <lpfaucon@gmail.com>
2023-07-10 17:35:24 +0200

    logs: log when starting and finishing the sum trust score updates

--- backend/ml/management/commands/ml_train.py
+++ backend/ml/management/commands/ml_train.py
@@ -32,4 +32,6 @@ class Command(BaseCommand):
                 raise NotImplementedError("Licchavi is no longer supported")
             else:
                 raise ValueError(f"unknown algorithm {repr(poll.algorithm)}'")
+            self.stdout.write(f"Starting bulk update of sum_trust_score for poll {poll.name}")
             EntityPollRating.bulk_update_sum_trust_scores(poll)
+            self.stdout.write(f"Finished bulk update of sum_trust_score for poll {poll.name}")
