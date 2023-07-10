5a395208b43d3612f0e0a24c0792be9896edf035
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-07-10 14:33:01 +0200

    wait 2sec

--- tests/cypress/e2e/frontend/signup.cy.ts
+++ tests/cypress/e2e/frontend/signup.cy.ts
@@ -49,7 +49,7 @@ describe('Signup', () => {
     cy.get('input[name="password"]').click().type('tourne50l').type('{enter}');
     cy.visit('/settings/preferences');
 
-    cy.wait(400);
+    cy.wait(2000);
     // [THEN] only the selected notifications are checked
     cy.get('[data-testid=notifications_email__research]').should('be.checked');
     cy.get('input[name=notifications_email__new_features]').should('not.be.checked');
