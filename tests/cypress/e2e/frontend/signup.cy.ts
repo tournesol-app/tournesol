feefad826dbdaf786449cb5b9b67e374b1290fbf
Gresille & Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-07-10 15:53:22 +0200

    [tests] fix: attempt to fix the failing sign up test (#1674)

--- tests/cypress/e2e/frontend/signup.cy.ts
+++ tests/cypress/e2e/frontend/signup.cy.ts
@@ -1,8 +1,20 @@
 describe('Signup', () => {
 
   beforeEach(() => {
-    cy.sql("DELETE FROM oauth2_provider_refreshtoken");
-    cy.sql("DELETE FROM oauth2_provider_accesstoken");
+    cy.sql(`
+      DELETE FROM oauth2_provider_refreshtoken
+      WHERE user_id IN (
+        SELECT user_id FROM core_user WHERE username = 'test-register'
+      );
+    `);
+
+    cy.sql(`
+      DELETE FROM oauth2_provider_accesstoken
+      WHERE user_id IN (
+        SELECT user_id FROM core_user WHERE username = 'test-register'
+      );
+    `);
+
     cy.sql("DELETE FROM core_user where username = 'test-register'");
   });
 
@@ -35,23 +47,25 @@ describe('Signup', () => {
     cy.get('input[name=username]').type('test-register');
     cy.get('input[name=password]').type('tourne50l');
     cy.get('input[name=password_confirm]').type('tourne50l');
-    cy.get('form input[name=accept_terms]').check();
+    cy.get('input[name=accept_terms]').check();
 
     // [GIVEN] the notification "research" is selected
-    cy.get('input[name=notifications_email__research]').check();
+    cy.get('[data-testid=notifications_email__research]').check();
 
     cy.contains('Sign up').click();
+    cy.contains('verification link').should('be.visible');
     cy.getEmailLink().then(verificationLink => cy.visit(verificationLink));
 
     // [WHEN] the user goes to its preference page
     cy.contains('Log in').click();
     cy.focused().type('test-register');
     cy.get('input[name="password"]').click().type('tourne50l').type('{enter}');
+
     cy.wait(500);
     cy.visit('/settings/preferences');
 
     // [THEN] only the selected notifications are checked
-    cy.get('input[name=notifications_email__research]').should('be.checked');
-    cy.get('input[name=notifications_email__new_features]').should('not.be.checked');
+    cy.get('[data-testid=notifications_email__research]').should('be.checked');
+    cy.get('[data-testid=notifications_email__new_features]').should('not.be.checked');
   });
 });
