4d97d996099a41b42c9d744a68fafdc2e52a82b7
shinseinova <89401728+ShinseiNova@users.noreply.github.com>
2023-07-11 16:42:49 +0200

    move each backoffice serializer in their own file

---
+++ backend/backoffice/serializers/talk.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from backoffice.models import TalkEntry
+
+
+class TalkEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TalkEntry
+        fields = [
+            "name",
+            "title",
+            "date",
+            "date_as_tz_europe_paris",
+            "speakers",
+            "abstract",
+            "invitation_link",
+            "youtube_link",
+        ]
