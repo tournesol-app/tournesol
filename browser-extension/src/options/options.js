f1936e8e161ca45d74d80ae438cc4b9d9713d71c
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-07-12 15:25:06 +0200

    save the settings in the extension storage

---
+++ browser-extension/src/options/options.js
@@ -0,0 +1,25 @@
+const DEFAULT_RECO_LANG = 'en';
+
+const loadPreferences = () => {
+  chrome.storage.local.get(
+    { recommendations__default_languages: DEFAULT_RECO_LANG },
+    (settings) => {
+      document.getElementById('recommendations__default_languages').value =
+        settings.recommendations__default_languages;
+    }
+  );
+};
+
+const savePreferences = () => {
+  const recoDefaultLanguages = document.getElementById('recommendations__default_languages').value;
+
+  browser.storage.local.set(
+    { recommendations__default_languages: recoDefaultLanguages },
+    () => {
+      // todo: add a visual feedback here
+    }
+  );
+};
+
+document.addEventListener('DOMContentLoaded', loadPreferences);
+document.getElementById('save-preferences').addEventListener('click', savePreferences);
