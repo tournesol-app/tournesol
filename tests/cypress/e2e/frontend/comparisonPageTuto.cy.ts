9677a743a468f085c85411bccd7c8201c0f8d70d
shinseinova <89401728+ShinseiNova@users.noreply.github.com>
2023-07-19 15:46:22 +0200

    test the course of the tutorial

--- tests/cypress/e2e/frontend/comparisonPageTuto.cy.ts
+++ tests/cypress/e2e/frontend/comparisonPageTuto.cy.ts
@@ -13,5 +13,40 @@ describe('Comparison page tutorial', () => {
       cy.contains('Weekly collective goal').should('not.exist');
     })
   });
+
+  describe('tutorial behavior', () => {
+    it('follows the course of the tutorial', () => {
+      cy.visit('/comparison');
+      cy.focused().type(username);
+      cy.get('input[name="password"]').click().type('tournesol').type('{enter}');
+
+      cy.get('[data-testid=tip_prev]').should('have.attr', 'tabindex', '-1');
+      cy.get('[data-testid=tip_next]').should('have.attr', 'tabindex', '-1');
+      cy.get('button#expert_submit_btn').click();
+
+      cy.get('[data-testid=tip_prev]').should('have.attr', 'tabindex', '0');
+      cy.get('[data-testid=tip_next]').should('have.attr', 'tabindex', '-1');
+      cy.get('button#expert_submit_btn').click();
+
+      cy.get('[data-testid=tip_prev]').should('have.attr', 'tabindex', '0');
+      cy.get('[data-testid=tip_next]').should('have.attr', 'tabindex', '-1');
+      cy.get('button#expert_submit_btn').click();
+
+      cy.contains("Install the extension");
+      cy.contains("button", "continue").click();
+      for (var tutorialLength=0; tutorialLength<3; tutorialLength++){
+        cy.get('[data-testid=tip_prev]').should('have.attr', 'tabindex', '0').click();
+      }
+      cy.get('[data-testid=tip_prev]').should('have.attr', 'tabindex', '-1');
+      for (var tutorialLength=0; tutorialLength<3; tutorialLength++){
+        cy.get('[data-testid=tip_next]').should('have.attr', 'tabindex', '0').click();
+      }
+      cy.get('[data-testid=tip_next]').should('have.attr', 'tabindex', '-1');
+      cy.get('button#expert_submit_btn').click();
+
+      cy.contains('Weekly collective goal').should('be.visible');
+    })
+  });
+
 });
   
