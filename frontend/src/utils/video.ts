2c3498a81da85e9af47978f063f67d860bbfb6d2
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-08-18 16:08:40 +0200

    [front] feat: add support of youtube.com/live/ URL

--- frontend/src/utils/video.ts
+++ frontend/src/utils/video.ts
@@ -8,7 +8,8 @@ export function extractVideoId(idOrUrl: string) {
 
   const matchUrl = idOrUrl.match(
     new RegExp(
-      '(?:https?:\\/\\/)?(?:www\\.)?(?:youtube\\.com\\/watch\\?v=|youtu\\.be\\/|' +
+      '(?:https?:\\/\\/)?(?:www\\.)?' +
+        '(?:youtube\\.com\\/watch\\?v=|youtube\\.com\\/live\\/|youtu\\.be\\/|' +
         escapedCurrentHost +
         '\\/entities\\/yt:|yt:)([A-Za-z0-9-_]+)'
     )
