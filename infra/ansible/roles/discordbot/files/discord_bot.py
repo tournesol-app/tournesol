849e6a3f84827b50a1d191cd485492cdd69ef51f
jst <jst@technocracy.dev>
2023-08-27 21:16:55 +0200

    do not reply to videos IDs that do not exist in Tournesol

--- infra/ansible/roles/discordbot/files/discord_bot.py
+++ infra/ansible/roles/discordbot/files/discord_bot.py
@@ -3,6 +3,7 @@
 import discord
 import re
 import os
+import requests
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -10,6 +11,20 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 yt_url_re = re.compile(r"\b(?:https?:)?(?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|live)(?:\.php)?(?:\?\S*v=|\/))([a-zA-Z0-9\_-]{11})(?:[\?&][a-zA-Z0-9\_-]+=[a-zA-Z0-9\_-]+)*(?:[&\/\#].*)?\b")
+
+
+token = os.environ["DISCORD_BOT_TOKEN"]
+tournesol_api_url = os.environ["TOURNESOL_API_URL"]
+
+
+def video_exists(video_id: str) -> bool:
+    resp = requests.head(f"{tournesol_api_url}/video/{video_id}/")
+    if resp.status_code != requests.codes.ok:
+        print(f"video {video_id} not found in Tournesol")
+        return False
+    print(f"video {video_id} found in Tournesol")
+    return True
+
 
 @client.event
 async def on_ready():
@@ -22,10 +37,11 @@ async def on_message(message):
     url_matches = set(yt_url_re.findall(message.content))
     if len(url_matches) > 0:
         print(f'Found a message containing video ids: {", ".join(url_matches)}')
-        links = [f"https://tournesol.app/entities/yt:{video_id}" for video_id in url_matches]
-        msg = f"Please use Tournesol links when posting on social media to help us promote the project:\n" + "\n".join(links)
-        if message.content:
+        links = [f"https://tournesol.app/entities/yt:{video_id}" for video_id in url_matches if video_exists(video_id)]
+        if len(links) > 0:
+            msg = f"Please use Tournesol links when posting on social media to help us promote the project:\n" + "\n".join(links)
+        # if message.content:
             await message.channel.send(msg)
 
-token = os.environ["DISCORD_BOT_TOKEN"]
+
 client.run(token)
