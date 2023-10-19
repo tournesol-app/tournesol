ac569e9ec63a341fd4a219d019e11592d1534214
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-10-19 12:04:14 +0200

    [back] feat: add a consolidation step in the ClassicEntitySuggestionStrategy strat

--- backend/tournesol/views/suggestions/strategies/classic.py
+++ backend/tournesol/views/suggestions/strategies/classic.py
@@ -1,4 +1,5 @@
 import random
+from dataclasses import dataclass
 
 from django.conf import settings
 
@@ -8,6 +9,12 @@ from tournesol.models.rate_later import RATE_LATER_AUTO_REMOVE_DEFAULT
 from tournesol.serializers.suggestion import ResultFromPollRating
 
 from .base import ContributionSuggestionStrategy
+
+
+@dataclass
+class UIDsPool:
+    uids: list[int]
+    sample_size: int
 
 
 class ClassicEntitySuggestionStrategy(ContributionSuggestionStrategy):
@@ -23,9 +30,12 @@ class ClassicEntitySuggestionStrategy(ContributionSuggestionStrategy):
         - use the user's preferred language(s)
     """
 
-    n_entity_rate_later = 14
-    n_entity_reco_last_month = 3
-    n_entity_reco_all_time = 3
+    # The maximum number of results returned by the strategy.
+    max_suggestions = 20
+
+    sample_size_compared = 9
+    sample_size_rate_later = 7
+    sample_size_reco_last_month = 4
 
     top_recommendations_limit = 400
     recent_recommendations_days = 30
@@ -62,23 +72,38 @@ class ClassicEntitySuggestionStrategy(ContributionSuggestionStrategy):
             .values_list("entity_id", flat=True)
         )
 
-    def _uids_from_pool_rate_later(self) -> list[int]:
+    def _uids_from_pool_compared(self) -> list[int]:
+        poll = self.poll
+        user = self.request.user
+
+        max_threshold = user.settings.get(poll.name, {}).get(
+            "rate_later__auto_remove", RATE_LATER_AUTO_REMOVE_DEFAULT
+        )
+
+        compared = (
+            ContributorRating.objects.filter(poll=poll, user=user)
+            .select_related("entity")
+            .annotate_n_comparisons()
+            .filter(n_comparisons__lt=max_threshold)
+            .values_list("entity_id", flat=True)
+        )
+
+        return random.sample(list(compared), min(len(compared), self.max_suggestions))
+
+    def _uids_from_pool_rate_later(self, exclude_uids: list[int]) -> list[int]:
         """
         Return random UIDs from the user's rate-later list.
         """
         poll = self.poll
         user = self.request.user
 
-        rate_later_size = RateLater.objects.filter(poll=poll, user=user).count()
-
-        if not rate_later_size:
-            return []
-
-        results = RateLater.objects.filter(poll=poll, user=user).values_list(
-            "entity_id", flat=True
+        results = (
+            RateLater.objects.filter(poll=poll, user=user)
+            .exclude(entity_id__in=exclude_uids)
+            .values_list("entity_id", flat=True)
         )
 
-        return random.sample(list(results), min(rate_later_size, self.n_entity_rate_later))
+        return random.sample(list(results), min(len(results), self.max_suggestions))
 
     def _uids_from_pool_reco_last_month(self, exclude_uids: list[int]) -> list[int]:
         """
@@ -103,7 +128,7 @@ class ClassicEntitySuggestionStrategy(ContributionSuggestionStrategy):
         if not size:
             return []
 
-        return random.sample(results, min(size, self.n_entity_reco_last_month))
+        return random.sample(results, min(size, self.max_suggestions))
 
     def _uids_from_pool_reco_all_time(self, exclude_uids: list[int]) -> list[int]:
         """
@@ -130,7 +155,58 @@ class ClassicEntitySuggestionStrategy(ContributionSuggestionStrategy):
         if not size:
             return []
 
-        return random.sample(results, min(size, self.n_entity_reco_all_time))
+        return random.sample(results, min(size, self.max_suggestions))
+
+    def _consolidate_results(self, pool1: UIDsPool, pool2: UIDsPool, pool3: UIDsPool):
+        """
+        Return a consolidated list of elements from all provided pools.
+
+        A list is considered consolidated when its population size is equals,
+        or as close as possible, to the sum of all pool's sample sizes.
+        """
+        sample1_size = len(pool1.uids[: pool1.sample_size])
+        sample2_size = len(pool2.uids[: pool2.sample_size])
+        sample3_size = len(pool3.uids[: pool3.sample_size])
+
+        extra_sample2 = 0
+        extra_sample3 = 0
+
+        free_slots_in_pool1 = pool1.sample_size - sample1_size
+        free_slots_in_pool2 = pool2.sample_size - sample2_size
+        free_slots_in_pool3 = pool3.sample_size - sample3_size
+
+        # If the pool 1 contains less UIDs than expected, try to pick more
+        # UIDs from the pools 2 and 3.
+        if free_slots_in_pool1 > 0:
+            extra_sample2 = free_slots_in_pool1 // 2
+            extra_sample3 = free_slots_in_pool1 // 2
+
+            if free_slots_in_pool1 % 2 == 1:
+                extra_sample2 += 1
+
+        sample1 = pool1.uids[: pool1.sample_size]
+
+        # If the pool 3 contains less UIDs than expected, try to pick more
+        # UIDs from the pool 2.
+        if free_slots_in_pool3 > 0:
+            extra_sample2 += extra_sample3 + free_slots_in_pool3
+
+        sample2 = pool2.uids[: pool2.sample_size + extra_sample2]
+
+        # If the pool 2 contains less UIDs than expected, try to pick more
+        # UIDs from the pool 3.
+        if free_slots_in_pool2 > 0:
+            extra_sample3 += extra_sample2 + free_slots_in_pool2
+
+        sample3 = pool3.uids[: pool3.sample_size + extra_sample3]
+
+        free_slots = self.max_suggestions - len(sample1) - len(sample2) - len(sample3)
+
+        # In case of need, add more UIDs from the pool 1.
+        if free_slots > 0:
+            sample1 = pool1.uids[: pool1.sample_size + free_slots]
+
+        return sample1 + sample2 + sample3
 
     def get_serializer_class(self):
         return ResultFromPollRating
@@ -143,19 +219,43 @@ class ClassicEntitySuggestionStrategy(ContributionSuggestionStrategy):
 
     def get_result_for_user_intermediate(self):
         poll = self.poll
-        rate_later = self._uids_from_pool_rate_later()
-        last_month = self._uids_from_pool_reco_last_month(rate_later)
-        all_time = self._uids_from_pool_reco_all_time(rate_later + last_month)
 
-        results = (
+        pool1 = self._uids_from_pool_compared()
+        pool2 = self._uids_from_pool_rate_later(pool1)
+        pool3 = self._uids_from_pool_reco_last_month(pool1 + pool2)
+
+        sample1_size = len(pool1[: self.sample_size_compared])
+        sample2_size = len(pool2[: self.sample_size_rate_later])
+        sample3_size = len(pool3[: self.sample_size_reco_last_month])
+
+        if sample1_size + sample2_size + sample3_size >= self.max_suggestions:
+            return (
+                EntityPollRating.objects.filter(
+                    poll=poll,
+                )
+                .select_related("entity")
+                .filter(entity_id__in=pool1 + pool2 + pool3)
+            )
+
+        results = self._consolidate_results(
+            UIDsPool(pool1, self.sample_size_compared),
+            UIDsPool(pool2, self.sample_size_rate_later),
+            UIDsPool(pool3, self.sample_size_reco_last_month),
+        )
+
+        free_slots = self.max_suggestions - len(results)
+
+        if free_slots > 0:
+            extra_pool = self._uids_from_pool_reco_all_time(results)
+            results += extra_pool[:free_slots]
+
+        return (
             EntityPollRating.objects.filter(
                 poll=poll,
             )
             .select_related("entity")
-            .filter(entity_id__in=rate_later + last_month + all_time)
+            .filter(entity_id__in=results)
         )
-
-        return results
 
     def get_result_for_user_advanced(self):
         raise NotImplementedError
