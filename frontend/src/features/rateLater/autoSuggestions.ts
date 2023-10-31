c657fe67158844b1ca667b048c9f5baa051e2786
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-10-31 10:24:55 +0100

    [front] refactor: connect Auto btn to new API

---
+++ frontend/src/features/rateLater/autoSuggestions.ts
@@ -0,0 +1,37 @@
+export const AUTO_SUGGESTIONS: { [key: string]: string[] } = {};
+
+export const clearAutoSuggestions = () => {
+  Object.keys(AUTO_SUGGESTIONS).forEach((key) => (AUTO_SUGGESTIONS[key] = []));
+};
+
+export const isAutoSuggestionsEmpty = (poll: string) =>
+  AUTO_SUGGESTIONS[poll] == null || AUTO_SUGGESTIONS[poll].length === 0;
+
+export const fillAutoSuggestions = (
+  poll: string,
+  uids: string[],
+  exclude?: string[]
+) => {
+  if (exclude == null) {
+    AUTO_SUGGESTIONS[poll] = [...uids];
+  }
+
+  AUTO_SUGGESTIONS[poll] = uids.filter((uid) => !exclude?.includes(uid));
+};
+
+export const autoSuggestionsRandom = (
+  poll: string,
+  exclude?: string[]
+): string | null => {
+  if (isAutoSuggestionsEmpty(poll)) {
+    return null;
+  }
+
+  const uids = [
+    ...AUTO_SUGGESTIONS[poll].filter((uid) => !exclude?.includes(uid)),
+  ];
+  const selected = uids[Math.floor(Math.random() * uids.length)];
+
+  AUTO_SUGGESTIONS[poll].splice(AUTO_SUGGESTIONS[poll].indexOf(selected), 1);
+  return selected;
+};
