ba48b8a71e42bd5edf5d3c74954e33e494aced99
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2023-12-18 11:12:44 +0100

    [front] style: "better" look of compact EntityCard in comparisons page

--- frontend/src/components/entity/style.ts
+++ frontend/src/components/entity/style.ts
@@ -7,7 +7,7 @@ export const entityCardMainSx: SxProps = {
   border: '1px solid #DCD8CB',
   boxShadow: '0px 0px 8px rgba(0, 0, 0, 0.02), 0px 2px 4px rgba(0, 0, 0, 0.05)',
   borderRadius: '4px',
-  alignContent: 'flex-start',
+  alignContent: 'space-between',
   overflow: 'hidden',
   fontSize: {
     xs: '14px',
