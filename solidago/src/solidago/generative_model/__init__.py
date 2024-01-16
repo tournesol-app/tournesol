ed143e93a6b0fde50ba474654bf9d70fbb6738b2
Lê Nguyên Hoang <len@science4all.org>
2024-01-16 09:00:26 +0100

    Adding generative model solution

--- solidago/src/solidago/generative_model/__init__.py
+++ solidago/src/solidago/generative_model/__init__.py
@@ -14,37 +14,30 @@ from .true_score_model import TrueScoreModel, SvdTrueScoreModel
 from .engagement_model import EngagementModel, SimpleEngagementModel
 from .comparison_model import ComparisonModel, KnaryGBT
 
-class GenerativeModel(TournesolInput):
+class SyntheticData(TournesolInput):
     def __init__(
-        self,
-        n_users: int,
-        n_entities: int,
-        user_model: UserModel = SvdUserModel(),
-        vouch_model: VouchModel = ErdosRenyiVouchModel(),
-        entity_model: EntityModel = SvdEntityModel(),
-        true_score_model: TrueScoreModel = SvdTrueScoreModel(),
-        engagement_model: EngagementModel = SimpleEngagementModel(),
-        comparison_model: ComparisonModel = KnaryGBT(21, 10),
-        random_seed: Optional[int] = None,
+        self, 
+        users: pd.DataFrame = None,
+        vouches: pd.DataFrame = None,
+        entities: pd.DataFrame = None,
+        true_scores: pd.DataFrame = None,
+        comparisons: pd.DataFrame = None
     ):
-        """ Generate a random dataset
-        Inputs:
-        - user_model is a callable generator of users
-        - vouch_model is a callable generator of vouches
-        - entity_model is a callable generator of entities
-        - true_score_model is a callable generator of true scores
-        - comparison_model is a callable generator of comparisons
-        - random_seed (if int) allows reproducibility
-        """
-        self.random_seed = random_seed
-        if random_seed is not None:
-            np.random.seed(random_seed)
-        self.users = user_model(n_users)
-        self.vouches = vouch_model(self.users)
-        self.entities = entity_model(n_entities)
-        self.true_scores = true_score_model(self.users, self.entities)
-        self.comparisons = engagement_model(self.users, self.true_scores)
-        self.comparisons = comparison_model(self.true_scores, self.comparisons)
+        def df(**kwargs):
+            dtypes = [(key, kwargs[key]) for key in kwargs]
+            return pd.DataFrame(np.empty(0, np.dtype(list(dtypes))))
+            
+        self.users = users if users is not None else df(
+            user_id=int, public_username=str, trust_score= float
+        ).set_index("user_id")
+        self.vouches = vouches if vouches is not None else df(
+            voucher=int, vouchee=int, vouch=float
+        )
+        self.entities = entities
+        self.true_scores = true_scores
+        self.comparisons = comparisons if comparisons is not None else df(
+            user_id=int, score=float, week_date=str, entity_a=int, entity_b=int
+        )
  
     def get_comparisons(
         self,
@@ -85,5 +78,52 @@ class GenerativeModel(TournesolInput):
     ) -> Optional[pd.DataFrame]:
         raise NotImplementedError
 
+
+class GenerativeModel:
+    def __init__(
+        self,
+        user_model: UserModel = SvdUserModel(),
+        vouch_model: VouchModel = ErdosRenyiVouchModel(),
+        entity_model: EntityModel = SvdEntityModel(),
+        true_score_model: TrueScoreModel = SvdTrueScoreModel(),
+        engagement_model: EngagementModel = SimpleEngagementModel(),
+        comparison_model: ComparisonModel = KnaryGBT(21, 10)
+    ):
+        """ Generate a random dataset
+        Inputs:
+        - user_model is a callable generator of users
+        - vouch_model is a callable generator of vouches
+        - entity_model is a callable generator of entities
+        - true_score_model is a callable generator of true scores
+        - comparison_model is a callable generator of comparisons
+        - random_seed (if int) allows reproducibility
+        """
+        self.user_model = user_model
+        self.vouch_model = vouch_model
+        self.entity_model = entity_model
+        self.true_score_model = true_score_model
+        self.engagement_model = engagement_model
+        self.comparison_model = comparison_model
+ 
+    def __call__(
+        self, n_users: int, n_entities: int, 
+        random_seed: Optional[int] = None
+    ) -> SyntheticData:
+        """ Generates a random dataset
+        Inputs:
+        - n_users
+        - n_entities
+        - random_seed (if int) allows reproducibility
+        """
+        if random_seed is not None:
+            np.random.seed(random_seed)
+        
+        users = self.user_model(n_users)
+        vouches = self.vouch_model(users)
+        entities = self.entity_model(n_entities)
+        true_scores = self.true_score_model(users, entities)
+        comparisons = self.engagement_model(users, true_scores)
+        comparisons = self.comparison_model(true_scores, comparisons)
+        return SyntheticData(users, vouches, entities, true_scores, comparisons)
 
 
--- solidago/tests/test_lipschitrust.py
+++ solidago/tests/test_lipschitrust.py
@@ -26,34 +26,6 @@ def test_lipschitrust_simple():
     assert trusts[4] > 0
     assert trusts[2] == pytest.approx(0.8 * 0.8 / 7)
 
-def test_lipschitrust_tournesol():
-    data = TournesolInputFromPublicDataset("tests/tournesol_dataset.zip")
-    
-    is_pretrusted = [trust_score >= 0.8 for trust_score in data.users["trust_score"]]
-    data.users = data.users.assign(is_pretrusted=is_pretrusted)
-    le_science4all = data.get_user_index("le_science4all")
-    amatissart = data.get_user_index("amatissart")
-    lpfaucon = data.get_user_index("lpfaucon")
-    aidjango = data.get_user_index("aidjango")
-    biscuissec = data.get_user_index("biscuissec")
-    
-    vouches = pd.DataFrame({
-        "voucher": [le_science4all, le_science4all, lpfaucon, biscuissec],
-        "vouchee": [amatissart, lpfaucon, biscuissec, aidjango],
-        "vouch": [1, 1, 1, 1]
-    })
-    
-    trust_propagator = LipschiTrust(
-        pretrust_value=0.8,
-        decay=0.8,
-        sink_vouch=5.0,
-        error=1e-8
-    )
-    
-    trusts = trust_propagator(data.users, vouches)
-    assert trusts[le_science4all] == 0.8
-    assert trusts[aidjango] > 0.8
-
 def test_lipschitrust_generative():
     users = SvdUserModel(p_trustworthy=0.8, p_pretrusted=0.2, svd_dimension=5)(50)
     vouches = ErdosRenyiVouchModel()(users)
