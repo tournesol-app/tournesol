748af7ee063a88f05ef4a971620b73474b9c2c0e
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-01-23 16:02:14 +0100

    fix too many locals

--- backend/core/management/commands/registration_reminder.py
+++ backend/core/management/commands/registration_reminder.py
@@ -20,14 +20,26 @@ class Command(BaseCommand):
     help = "Notify by email active users who haven't contributed since their"\
            " account was created."
 
+    def get_subject(self, n_comparisons):
+        if n_comparisons > 0:
+            return _("Thank you for your contributions")
+        return _("Thank you for your registration")
+
+    def get_message(self, n_comparisons):
+        if n_comparisons > 0:
+            return render_to_string("core/no_contrib_email/body_signup_contrib.txt")
+        return render_to_string("core/no_contrib_email/body_no_contrib.txt")
+
+    def get_html_message(self, n_comparisons):
+        if n_comparisons > 0:
+            return render_to_string("core/no_contrib_email/body_signup_contrib.html")
+        return render_to_string("core/no_contrib_email/body_no_contrib.html")
+
     def notify_users(self, users):
         from_email = settings.REST_REGISTRATION["VERIFICATION_FROM_EMAIL"]
 
         fails = []
         successes = 0
-
-        subject_no_comp = _("Thank you for your registration")
-        subject_signup_comp = _("Thank you for your contributions")
 
         for user in users:
             if user.n_comp_after_signup > 0:
@@ -40,27 +52,11 @@ class Command(BaseCommand):
 
             translation.activate(lang_code)
 
-            msg_no_comp = render_to_string("core/no_contrib_email/body_no_contrib.txt")
-            msg_signup_comp = render_to_string("core/no_contrib_email/body_signup_contrib.txt")
-            html_msg_no_comp = render_to_string("core/no_contrib_email/body_no_contrib.html")
-            html_msg_signup_comp = render_to_string(
-                "core/no_contrib_email/body_signup_contrib.html"
-            )
-
-            if user.n_comp_signup > 0:
-                subject = subject_signup_comp
-                message = msg_signup_comp
-                html_message = html_msg_signup_comp
-            else:
-                subject = subject_no_comp
-                message = msg_no_comp
-                html_message = html_msg_no_comp
-
             try:
                 send_mail(
-                    subject=subject,
-                    message=message,
-                    html_message=html_message,
+                    subject=self.get_subject(user.n_comp_signup),
+                    message=self.get_message(user.n_comp_signup),
+                    html_message=self.get_html_message(user.n_comp_signup),
                     from_email=from_email,
                     recipient_list=[user.email],
                     fail_silently=False,
