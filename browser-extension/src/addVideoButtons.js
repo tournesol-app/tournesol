cc47d9631452ae9c869e30cf5b223ed1be58b65b
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-01-25 16:07:17 +0100

    run yarn lint:fix

--- browser-extension/src/addVideoButtons.js
+++ browser-extension/src/addVideoButtons.js
@@ -165,7 +165,10 @@ function addVideoButtons() {
       label: 'Watch on Tournesol',
       iconSrc: chrome.runtime.getURL('images/watch.svg'),
       onClick: () => {
-        window.open(`${frontendUrl}/entities/yt:${videoId}?utm_source=extension&utm_medium=video_button`, '_blank');
+        window.open(
+          `${frontendUrl}/entities/yt:${videoId}?utm_source=extension&utm_medium=video_button`,
+          '_blank'
+        );
       },
     });
   }
