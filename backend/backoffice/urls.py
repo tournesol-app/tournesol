85f28f1c19660d371cec4ad84ffc95f578cafc3f
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-02-26 16:40:17 +0100

    back: add the route /backoffice/events/

--- backend/backoffice/urls.py
+++ backend/backoffice/urls.py
@@ -1,21 +1,32 @@
 from django.urls import path
 
-from backoffice.views import BannerListView, FAQEntryLocalizedListView, TalkEntryListView
+from backoffice.views import (
+    BannerListView,
+    FAQEntryLocalizedListView,
+    OnlineEventListView,
+    TalkEntryListView,
+)
 
 urlpatterns = [
+    path(
+        "banners/",
+        BannerListView.as_view(),
+        name="banner_list",
+    ),
+    path(
+        "events/",
+        OnlineEventListView.as_view(),
+        name="online_event_list",
+    ),
     path(
         "faq/",
         FAQEntryLocalizedListView.as_view(),
         name="faq_list_localized",
     ),
+    # Kept for retro-compatibility, use OnlineEventListView instead.
     path(
         "talks/",
         TalkEntryListView.as_view(),
         name="talk_list",
-    ),
-    path(
-        "banners/",
-        BannerListView.as_view(),
-        name="banner_list",
     ),
 ]
--- backend/backoffice/views/__init__.py
+++ backend/backoffice/views/__init__.py
@@ -1,3 +1,4 @@
 from .banners import BannerListView
 from .faqs import FAQEntryLocalizedListView
+from .online_events import OnlineEventListView
 from .talks import TalkEntryListView
