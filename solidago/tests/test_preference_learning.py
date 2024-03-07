54ed46767f04019c31bc23db1e314dacb3c32d58
Adrien Matissart <a@matissart.net>
2024-03-07 11:04:36 +0100

    make torch an optional dependency

--- solidago/tests/test_preference_learning.py
+++ solidago/tests/test_preference_learning.py
@@ -12,7 +12,7 @@ def test_uniform_gbt(test):
             target = td.learned_models[user](entity, td.entities.loc[entity])
             assert output == pytest.approx(target, abs=1e-1), (user, entity)
 
-@pytest.mark.parametrize("test", range(3))
+@pytest.mark.parametrize("test", range(4))
 def test_lbfgs_uniform_gbt(test):
     pytest.importorskip("torch")
     from solidago.preference_learning import LBFGSUniformGBT
