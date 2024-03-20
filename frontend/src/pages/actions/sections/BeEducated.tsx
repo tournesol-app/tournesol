5a27a0cb3916955bf5c26194c0beca512cca0ea9
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-03-20 11:44:02 +0100

    [front] feat: finish the section BeEducated.tsx

--- frontend/src/pages/actions/sections/BeEducated.tsx
+++ frontend/src/pages/actions/sections/BeEducated.tsx
@@ -1,48 +1,203 @@
 import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 
-import { Alert, AlertTitle, Box, Link, Typography } from '@mui/material';
+import { Alert, Box, Link, Typography } from '@mui/material';
 
-const BeEducatedSection = () => {
+const booksToReadAndOfferEn = [
+  {
+    text: 'Manufacturing Consensus',
+    href: 'https://yalebooks.yale.edu/book/9780300251234/manufacturing-consensus/',
+  },
+  {
+    text: 'Twitter and Tear Gas',
+    href: 'https://www.twitterandteargas.org/downloads/twitter-and-tear-gas-by-zeynep-tufekci.pdf',
+  },
+];
+
+const booksToReadAndOfferFr = [
+  {
+    text: 'Algocratie',
+    href: 'https://www.actes-sud.fr/algocratie',
+  },
+  {
+    text: 'Le Fabuleux Chantier',
+    href: 'https://laboutique.edpsciences.fr/produit/1107/9782759824304/Le%20fabuleux%20chantier',
+  },
+  {
+    text: 'Toxic Data',
+    href: 'https://editions.flammarion.com/toxic-data/9782080274946',
+  },
+];
+
+const videosToWatchAndShareEn = [
+  {
+    text: 'Science4All (english)',
+    href: 'https://tournesol.app/recommendations?language=&uploader=Science4All+%28english%29',
+  },
+];
+
+const videosToWatchAndShareFr = [
+  {
+    text: 'Science4All',
+    href: 'https://tournesol.app/recommendations?language=&uploader=Science4All',
+  },
+  {
+    text: 'ApresLaBiere',
+    href: 'https://tournesol.app/recommendations?language=&uploader=ApresLaBiere',
+  },
+  {
+    text: 'La Fabrique Sociale',
+    href: 'https://tournesol.app/recommendations?language=&uploader=La%20Fabrique%20Sociale',
+  },
+];
+
+const ExternalLink = ({ text, href }: { text: string; href: string }) => {
+  return (
+    <Link
+      href={href}
+      target="_blank"
+      rel="noopener"
+      sx={{
+        color: 'revert',
+        textDecoration: 'revert',
+      }}
+    >
+      {text}
+    </Link>
+  );
+};
+
+const BooksToReadAndOffer = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Typography>
+        {t('actionsPage.beEducated.readAndOfferMoreBooksLike')}
+      </Typography>
+      <ul>
+        <li>
+          <Typography>{t('actionsPage.beEducated.inEnglish')}</Typography>
+          <ul>
+            {booksToReadAndOfferEn.map((book, idx) => (
+              <li key={`book_en_${idx}`}>
+                <ExternalLink {...book} />
+              </li>
+            ))}
+          </ul>
+        </li>
+        <li>
+          <Typography>{t('actionsPage.beEducated.inFrench')}</Typography>
+          <ul>
+            {booksToReadAndOfferFr.map((book, idx) => (
+              <li key={`book_fr_${idx}`}>
+                <ExternalLink {...book} />
+              </li>
+            ))}
+          </ul>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+const VideosToWatchAndShare = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Typography>
+        {t('actionsPage.beEducated.watchAndShareVideosFrom')}{' '}
+      </Typography>
+      <ul>
+        <li>
+          <Typography>{t('actionsPage.beEducated.inEnglish')}</Typography>
+          <ul>
+            {videosToWatchAndShareEn.map((videos, idx) => (
+              <li key={`videos_en_${idx}`}>
+                <ExternalLink {...videos} />
+              </li>
+            ))}
+            <li>
+              <Typography>{t('actionsPage.beEducated.andMore')}</Typography>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Typography>{t('actionsPage.beEducated.inFrench')}</Typography>
+          <ul>
+            {videosToWatchAndShareFr.map((videos, idx) => (
+              <li key={`videos_fr_${idx}`}>
+                <ExternalLink {...videos} />
+              </li>
+            ))}
+            <li>
+              <Typography>{t('actionsPage.beEducated.andMore')}</Typography>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+const BeEducated = () => {
   const { t } = useTranslation();
   return (
     <Box sx={{ '& li': { mt: 1 } }}>
-      <Typography
-        variant="h4"
-        fontStyle="italic"
-        gutterBottom
-        id="actions-be-educated"
-      >
-        Être formé et former les autres aux fondamentaux de la démocratie
-        numérique.
+      <Typography variant="h4" fontStyle="italic" gutterBottom id="be-educated">
+        {t('actionsPage.beEducated.beTrainedAndTrainOthers')}
       </Typography>
       <Box my={2}>
         <Alert severity="info" icon={false}>
-          Si tu es éduqué tu as un meilleur impact
+          {t('actionsPage.beEducated.why')}
         </Alert>
       </Box>
       <ul>
         <li>
           <Typography>
-            Lire et offrir le livre La Dictature des Algorithmes, issu des
-            travaux de l’Association Tournesol.
+            {t(
+              'actionsPage.beEducated.readAndOfferBooksResultingFromAssociationWorks'
+            )}{' '}
+            <ExternalLink
+              text="La Dictature des Algorithmes (fr)"
+              href="https://www.tallandier.com/livre/la-dictature-des-algorithmes/"
+            />
+          </Typography>
+        </li>
+        <li>
+          <BooksToReadAndOffer />
+        </li>
+        <li>
+          <VideosToWatchAndShare />
+        </li>
+        <li>
+          <Typography>
+            {t('actionsPage.beEducated.listenAndSharePodcastsLike')}{' '}
+            <ExternalLink
+              text="Your Undivided Attention (en)"
+              href="https://www.humanetech.com/podcast"
+            />
           </Typography>
         </li>
         <li>
           <Typography>
-            Visionner et partager les contenus de Science4All, Après la Bière et
-            La Fabrique Sociale, parmi d’autres.
-          </Typography>
-        </li>
-        <li>
-          <Typography>
-            Écouter et recommander des podcasts comme Your Undivided Attention.
-          </Typography>
-        </li>
-        <li>
-          <Typography>
-            Découvrir, consommer et diffuser les jeux de la développeuse Nicky
-            Case.
+            <Trans
+              t={t}
+              i18nKey="actionsPage.beEducated.discoverPlayAndShareEducationalGamesLike"
+            >
+              Discover, play and share educational games like{' '}
+              <Link
+                href="https://ncase.me/"
+                target="_blank"
+                rel="noopener"
+                sx={{
+                  color: 'revert',
+                  textDecoration: 'revert',
+                }}
+              >
+                Nicky Case&apos;s
+              </Link>
+              .
+            </Trans>
           </Typography>
         </li>
       </ul>
@@ -50,4 +205,4 @@ const BeEducatedSection = () => {
   );
 };
 
-export default BeEducatedSection;
+export default BeEducated;
