6d74fcc99c13ab3d1132ff0c95ef6c8f9c7eda72
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-03-20 16:59:42 +0100

    [front] feat: complete the section JoinMovements.tsx (why? is still missing)

--- frontend/src/pages/actions/sections/JoinMovements.tsx
+++ frontend/src/pages/actions/sections/JoinMovements.tsx
@@ -1,7 +1,59 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
+import { Link as RouterLink } from 'react-router-dom';
 
-import { Alert, Box, Typography } from '@mui/material';
+import { Alert, Box, Link, Typography } from '@mui/material';
+
+import { githubTournesolUrl } from 'src/utils/url';
+
+const sourceCodeToContributeTo = [
+  {
+    text: 'Tournesol',
+    href: githubTournesolUrl,
+  },
+  {
+    text: 'Captain Fact',
+    href: 'https://github.com/captainfact',
+  },
+  {
+    text: 'Polis',
+    href: 'https://pol.is/homehttps://github.com/compdemocracy',
+  },
+];
+
+const ExternalLink = ({ text, href }: { text: string; href: string }) => {
+  return (
+    <Link
+      href={href}
+      target="_blank"
+      rel="noopener"
+      sx={{
+        color: 'revert',
+        textDecoration: 'revert',
+      }}
+    >
+      {text}
+    </Link>
+  );
+};
+
+const SourceCodeToContributeTo = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <Typography>
+        {t('actionsPage.joinMovements.contributeToTheOpenSourceCodeOf')}
+      </Typography>
+      <ul>
+        {sourceCodeToContributeTo.map((sourceCode, idx) => (
+          <li key={`source_code_${idx}`}>
+            <ExternalLink {...sourceCode} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 const JoinMovements = () => {
   const { t } = useTranslation();
@@ -18,6 +70,32 @@ const JoinMovements = () => {
       <Alert severity="info" icon={false}>
         {t('actionsPage.joinMovements.why')}
       </Alert>
+      <ul>
+        <li>
+          <Typography>
+            <Trans
+              t={t}
+              i18nKey="actionsPage.joinMovements.makeComparisonsOnTournesol"
+            >
+              Make{' '}
+              <Link
+                component={RouterLink}
+                to="/comparison"
+                sx={{
+                  color: 'revert',
+                  textDecoration: 'revert',
+                }}
+              >
+                comparisons
+              </Link>{' '}
+              on Tournesol.
+            </Trans>
+          </Typography>
+        </li>
+        <li>
+          <SourceCodeToContributeTo />
+        </li>
+      </ul>
     </Box>
   );
 };
