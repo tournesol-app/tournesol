3e87950078b04ccfcc354aca6516b4d8919f0fee
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-03-21 10:12:16 +0100

    [front] feat: add description to organizations in Donate.tsx

--- frontend/src/pages/actions/sections/Donate.tsx
+++ frontend/src/pages/actions/sections/Donate.tsx
@@ -1,51 +1,73 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Box, Typography } from '@mui/material';
+import { Alert, Box, Typography } from '@mui/material';
 
 import { ExternalLink } from 'src/components';
 
-const organisationToDonateTo = [
-  {
-    text: 'AlgorithmWatch',
-    href: 'https://algorithmwatch.org/en/',
-  },
-  {
-    text: 'Association Tournesol',
-    href: 'https://tournesol.app/about/donate',
-  },
-  {
-    text: 'Mozilla Foundation',
-    href: 'https://foundation.mozilla.org/fr/?form=donate-header',
-  },
-  {
-    text: 'Wikimedia France',
-    href: 'https://dons.wikimedia.fr/souteneznous/~mon-don',
-  },
-  {
-    text: 'Wikimedia Foundation',
-    href: 'https://donate.wikimedia.org/wiki/Ways_to_Give',
-  },
-];
+interface Organization {
+  name: string;
+  href: string;
+  desc: string;
+}
 
-const OrganisationToDonateTo = () => {
-  const { t } = useTranslation();
+const OrganisationToDonateTo = ({
+  organizations,
+}: {
+  organizations: Organization[];
+}) => {
   return (
     <>
-      <Typography>{t('actionsPage.donate.donateTo')}</Typography>
-      <ul>
-        {organisationToDonateTo.map((organisation, idx) => (
-          <li key={`organisation_${idx}`}>
-            <ExternalLink {...organisation} />
-          </li>
-        ))}
-      </ul>
+      {organizations.map((organisation, idx) => (
+        <Box key={`organisation_${idx}`}>
+          <Box display="flex" gap={1}>
+            <Typography variant="h6" gutterBottom>
+              {organisation.name}
+            </Typography>
+            <ExternalLink
+              key={`organisation_${idx}`}
+              text={organisation.href}
+              href={organisation.href}
+            />
+          </Box>
+          <Typography paragraph>{organisation.desc}</Typography>
+        </Box>
+      ))}
     </>
   );
 };
 
 const Donate = () => {
   const { t } = useTranslation();
+
+  const organizations = [
+    {
+      name: 'AlgorithmWatch',
+      href: 'https://algorithmwatch.org/en/donate/',
+      desc: t('actionsPage.donate.algorithmWatchDesc'),
+    },
+    {
+      name: 'Association Tournesol',
+      href: 'https://tournesol.app/about/donate',
+      desc: t('actionsPage.donate.associationTournesolDesc'),
+    },
+    {
+      name: 'Mozilla Foundation',
+      href: 'https://foundation.mozilla.org/fr/?form=donate-header',
+      desc: t('actionsPage.donate.mozillaFoundationDesc'),
+    },
+    {
+      name: 'Wikimedia Foundation',
+      href: 'https://donate.wikimedia.org/wiki/Ways_to_Give',
+      desc: t('actionsPage.donate.wikimediaFoundationDesc'),
+    },
+    {
+      name: t('actionsPage.donate.yourLocalWikimediaChapter'),
+      href: 'https://meta.wikimedia.org/wiki/Wikimedia_chapters',
+      desc: t('actionsPage.donate.localWikimediaChaptersDesc'),
+    },
+  ];
+
   return (
     <Box sx={{ '& li': { mt: 1 } }}>
       <Typography variant="h4" fontStyle="italic" gutterBottom id="donate">
@@ -53,7 +75,12 @@ const Donate = () => {
           'actionsPage.donate.donateToOrganisationsFightingForQualityOfInformation'
         )}
       </Typography>
-      <OrganisationToDonateTo />
+      <Box my={2}>
+        <Alert severity="info" icon={false}>
+          {t('actionsPage.donate.hereIsAlist')}
+        </Alert>
+      </Box>
+      <OrganisationToDonateTo organizations={organizations} />
     </Box>
   );
 };
