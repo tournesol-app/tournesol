6b55ba1af1197e1272a60af262646c44c83836e1
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-03-21 11:37:48 +0100

    [front] feat: add donate and website links in Donate.tsx

--- frontend/src/pages/actions/sections/Donate.tsx
+++ frontend/src/pages/actions/sections/Donate.tsx
@@ -7,8 +7,9 @@ import { ExternalLink } from 'src/components';
 
 interface Organization {
   name: string;
-  href: string;
   desc: string;
+  href: string;
+  hrefDonate?: string;
 }
 
 const OrganisationToDonateTo = ({
@@ -16,24 +17,34 @@ const OrganisationToDonateTo = ({
 }: {
   organizations: Organization[];
 }) => {
+  const { t } = useTranslation();
   return (
-    <>
+    <Box display="flex" flexDirection="column" gap={4}>
       {organizations.map((organisation, idx) => (
-        <Box key={`organisation_${idx}`}>
+        <Box key={`organization_${idx}`}>
           <Box display="flex" gap={1}>
             <Typography variant="h6" gutterBottom>
               {organisation.name}
             </Typography>
+            {'·'}
             <ExternalLink
-              key={`organisation_${idx}`}
-              text={organisation.href}
+              text={t('actionsPage.donate.website')}
               href={organisation.href}
             />
+            {organisation.hrefDonate && (
+              <>
+                {'·'}
+                <ExternalLink
+                  text={t('actionsPage.donate.donate')}
+                  href={organisation.hrefDonate}
+                />
+              </>
+            )}
           </Box>
-          <Typography paragraph>{organisation.desc}</Typography>
+          <Typography>{organisation.desc}</Typography>
         </Box>
       ))}
-    </>
+    </Box>
   );
 };
 
@@ -43,28 +54,32 @@ const Donate = () => {
   const organizations = [
     {
       name: 'AlgorithmWatch',
-      href: 'https://algorithmwatch.org/en/donate/',
       desc: t('actionsPage.donate.algorithmWatchDesc'),
+      href: 'https://algorithmwatch.org/en/',
+      hrefDonate: 'https://algorithmwatch.org/en/donate/',
     },
     {
       name: 'Association Tournesol',
-      href: 'https://tournesol.app/about/donate',
       desc: t('actionsPage.donate.associationTournesolDesc'),
+      href: 'https://tournesol.app',
+      hrefDonate: 'https://tournesol.app/about/donate',
     },
     {
       name: 'Mozilla Foundation',
-      href: 'https://foundation.mozilla.org/fr/?form=donate-header',
       desc: t('actionsPage.donate.mozillaFoundationDesc'),
+      href: 'https://foundation.mozilla.org',
+      hrefDonate: 'https://foundation.mozilla.org?form=donate-header',
     },
     {
       name: 'Wikimedia Foundation',
-      href: 'https://donate.wikimedia.org/wiki/Ways_to_Give',
       desc: t('actionsPage.donate.wikimediaFoundationDesc'),
+      href: 'https://www.wikimedia.org',
+      hrefDonate: 'https://donate.wikimedia.org/wiki/Ways_to_Give',
     },
     {
       name: t('actionsPage.donate.yourLocalWikimediaChapter'),
-      href: 'https://meta.wikimedia.org/wiki/Wikimedia_chapters',
       desc: t('actionsPage.donate.localWikimediaChaptersDesc'),
+      href: 'https://meta.wikimedia.org/wiki/Wikimedia_chapters',
     },
   ];
 
