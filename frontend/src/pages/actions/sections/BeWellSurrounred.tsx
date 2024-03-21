1374cc221a2d2f3b1bf6c6616d2e966828b4bc1f
Adrien Matissart <a@matissart.net>
2024-03-21 10:52:26 +0100

    add content in section BeWellSurrounded

--- frontend/src/pages/actions/sections/BeWellSurrounred.tsx
+++ frontend/src/pages/actions/sections/BeWellSurrounred.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
-
-import { Box, Typography } from '@mui/material';
+import { Trans, useTranslation } from 'react-i18next';
+import { Box, Link, Typography } from '@mui/material';
 
 const BeWellSurrounred = () => {
   const { t } = useTranslation();
   return (
-    <Box sx={{ '& li': { mt: 1 } }}>
+    <Box
+      sx={{
+        '& li': { mt: 1 },
+        '& a': { color: 'revert', textDecoration: 'revert' },
+      }}
+    >
       <Typography
         variant="h4"
         fontStyle="italic"
         gutterBottom
         id="be-well-surrounred"
       >
-        {t('actionsPage.beWellSurrounred.beWellSurrounred')}
+        {t('actionsPage.beWellSurrounded.beWellSurrounred')}
+      </Typography>
+      <ul>
+        <li>
+          <Typography>
+            <Trans t={t} i18nKey="actionsPage.beWellSurrounded.joinDiscord">
+              Join{' '}
+              <Link
+                href="https://discord.com/invite/TvsFB8RNBV"
+                target="_blank"
+                rel="noopener"
+              >
+                Tournesol Discord
+              </Link>{' '}
+              (say hi and present yourself).
+            </Trans>
+          </Typography>
+        </li>
+        <li>
+          <Typography>
+            {t('actionsPage.beWellSurrounded.joinGroups')}
+          </Typography>
+        </li>
+      </ul>
+      <Typography paragraph>
+        {t('actionsPage.beWellSurrounded.takeCareMentalHealth')}
       </Typography>
     </Box>
   );
