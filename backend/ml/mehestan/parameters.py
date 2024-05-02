a89e00be52e8b006e1ce898d2c2dc36edc505111
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-05-02 16:17:26 +0200

    delete obsolete COMPARISON_MAX

--- backend/ml/mehestan/parameters.py
+++ backend/ml/mehestan/parameters.py
@@ -1,8 +1,7 @@
 from solidago.pipeline.legacy2023.parameters import PipelineParameters
 
-from tournesol.utils.constants import COMPARISON_MAX, MEHESTAN_MAX_SCALED_SCORE
+from tournesol.utils.constants import MEHESTAN_MAX_SCALED_SCORE
 
 
 class MehestanParameters(PipelineParameters):
-    r_max = COMPARISON_MAX
     max_squashed_score = MEHESTAN_MAX_SCALED_SCORE
