a2a3c54ff7cbb09d1a3fce9cf6431af5c430c2d9
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-05-14 15:55:56 +0200

    front: order criteria in CriteriaButtonsScoreReview

--- frontend/src/features/comparisons/CriteriaButtonsScoreReview.tsx
+++ frontend/src/features/comparisons/CriteriaButtonsScoreReview.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import { Box } from '@mui/material';
-import { ComparisonRequest } from 'src/services/openapi';
 import { CriteriaIcon } from 'src/components';
+import { useCurrentPoll } from 'src/hooks';
+import { ComparisonRequest } from 'src/services/openapi';
 import { scoreButtons } from './ComparisonCriterionButtons';
 
 interface CriteriaButtonsScoreReviewProps {
@@ -12,6 +13,19 @@ interface CriteriaButtonsScoreReviewProps {
 const CriteriaButtonsScoreReview = ({
   initialComparison,
 }: CriteriaButtonsScoreReviewProps) => {
+  const { criterias } = useCurrentPoll();
+
+  const orderedCriteriaScores = criterias
+    .map((criterion) => {
+      const ratedCrit = initialComparison?.criteria_scores.find(
+        (crit) => crit.criteria === criterion.name
+      );
+      if (ratedCrit) {
+        return ratedCrit;
+      }
+    })
+    .filter((crit) => crit != undefined);
+
   if (!initialComparison?.criteria_scores) return <></>;
 
   return (
@@ -19,7 +33,6 @@ const CriteriaButtonsScoreReview = ({
       {scoreButtons.map((scoreBtn) => (
         <Box
           key={scoreBtn.score}
-          minWidth="38px"
           display="flex"
           flexDirection="column"
           alignItems="center"
@@ -29,7 +42,8 @@ const CriteriaButtonsScoreReview = ({
             {scoreBtn.icons}
           </Box>
 
-          {initialComparison?.criteria_scores.map((crit) => {
+          {orderedCriteriaScores.map((crit) => {
+            if (crit == undefined) return;
             if (crit.score !== scoreBtn.score) return;
 
             return (
