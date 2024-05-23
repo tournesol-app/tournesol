138f94d414075d4c3d44cf16b9bcd81970a828ad
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-05-23 12:02:44 +0200

    front: use a media query instead of checking the user agent

--- frontend/src/features/comparisons/ComparisonInput.tsx
+++ frontend/src/features/comparisons/ComparisonInput.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { Alert, Box, Paper } from '@mui/material';
+import { Alert, Box, Paper, useMediaQuery } from '@mui/material';
 
 import { useCurrentPoll } from 'src/hooks';
 import {
@@ -9,7 +9,6 @@ import {
   ComparisonRequest,
 } from 'src/services/openapi';
 import ComparisonSliders from 'src/features/comparisons/ComparisonSliders';
-import { isMobileDevice } from 'src/utils/extension';
 
 import CriteriaButtons from './inputs/CriteriaButtons';
 import { BUTTON_SCORE_MAX } from './inputs/CriterionButtons';
@@ -54,23 +53,22 @@ const ComparisonInput = ({
 }: ComparisonInputProps) => {
   const { t } = useTranslation();
   const { options } = useCurrentPoll();
+  const pointerCoarse = useMediaQuery('(pointer:coarse)');
 
   const mainScoreMax = getCriterionScoreMax(
     initialComparison?.criteria_scores,
     options?.mainCriterionName
   );
 
-  // TODO: decide if isMobileDevice() is the correct method to use.
-  const mobileDevice = isMobileDevice();
   const buttonsUsed = mainScoreMax == BUTTON_SCORE_MAX;
   const slidersUsed = mainScoreMax == SLIDER_SCORE_MAX;
-  const fallBackToButtons = !buttonsUsed && !slidersUsed && mobileDevice;
+  const fallBackToButtons = !buttonsUsed && !slidersUsed && pointerCoarse;
 
   return (
     <>
       {buttonsUsed || fallBackToButtons ? (
         <>
-          {!mobileDevice && (
+          {!pointerCoarse && (
             <Alert icon={false} severity="info">
               {t('comparisonInput.thisComparisonWasMadeOnAMobileDevice')}
             </Alert>
@@ -87,7 +85,7 @@ const ComparisonInput = ({
         </>
       ) : (
         <>
-          {mobileDevice && (
+          {pointerCoarse && (
             <Alert icon={false} severity="info">
               {t('comparisonInput.thisComparisonWasMadeOnAComputer')}
             </Alert>
@@ -99,7 +97,7 @@ const ComparisonInput = ({
               uidA={uidA || ''}
               uidB={uidB || ''}
               isComparisonPublic={isComparisonPublic}
-              readOnly={mobileDevice}
+              readOnly={pointerCoarse}
             />
           </Paper>
         </>
