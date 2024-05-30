4732ca08e0e4a15d5664e91b39aaf20a1fedb74a
Adrien Matissart <a@matissart.net>
2024-05-30 15:51:19 +0200

    fix inconsistencies in uncertainties values, use dichtomy instead of scipy

--- solidago/src/solidago/preference_learning/lbfgs_generalized_bradley_terry.py
+++ solidago/src/solidago/preference_learning/lbfgs_generalized_bradley_terry.py
@@ -3,7 +3,6 @@ from typing import Optional
 
 import pandas as pd
 import numpy as np
-from scipy.optimize import brentq
 
 try:
     import torch
@@ -14,15 +13,15 @@ except ImportError as exc:
     ) from exc
 
 from solidago.scoring_model import ScoringModel, DirectScoringModel
+from solidago.solvers import dichotomy
 from .comparison_learning import ComparisonBasedPreferenceLearning
-
 
 class LBFGSGeneralizedBradleyTerry(ComparisonBasedPreferenceLearning):
     def __init__(
         self,
         prior_std_dev: float = 7,
         convergence_error: float = 1e-5,
-        n_steps: int = 1,
+        max_iter: int = 100,
     ):
         """
 
@@ -35,7 +34,7 @@ class LBFGSGeneralizedBradleyTerry(ComparisonBasedPreferenceLearning):
         """
         self.prior_std_dev = prior_std_dev
         self.convergence_error = convergence_error
-        self.n_steps = n_steps
+        self.max_iter = max_iter
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     @abstractmethod
@@ -43,22 +42,6 @@ class LBFGSGeneralizedBradleyTerry(ComparisonBasedPreferenceLearning):
         """The beauty of the generalized Bradley-Terry model is that it suffices
         to specify its cumulant generating function derivative to fully define it,
         and to allow a fast computation of its corresponding maximum a posterior.
-
-        Parameters
-        ----------
-        score_diff: float
-            Score difference
-
-        Returns
-        -------
-        out: float
-        """
-        pass
-
-    @abstractmethod
-    def cumulant_generating_function_second_derivative(self, score_diff: float) -> float:
-        """We estimate uncertainty by the flatness of the negative log likelihood,
-        which is directly given by the second derivative of the cumulant generating function.
 
         Parameters
         ----------
@@ -95,88 +78,99 @@ class LBFGSGeneralizedBradleyTerry(ComparisonBasedPreferenceLearning):
 
         entities = list(set(comparisons["entity_a"]) | set(comparisons["entity_b"]))
         entity_coordinates = {entity: c for c, entity in enumerate(entities)}
-        comparisons = (
-            comparisons
-            .assign(
-                entity_a=comparisons["entity_a"].map(entity_coordinates),
-                entity_b=comparisons["entity_b"].map(entity_coordinates),
-                comparison=comparisons["comparison"] / comparisons["comparison_max"],
-            )
+        comparisons_np = (
+            comparisons["entity_a"].map(entity_coordinates).to_numpy(),
+            comparisons["entity_b"].map(entity_coordinates).to_numpy(),
+            (comparisons["comparison"] / comparisons["comparison_max"]).to_numpy()
         )
 
         solution = torch.normal(
-            0, 1, (len(entities),), requires_grad=True, dtype=float, device=self.device
+            0, 1, (len(entities),), requires_grad=True, dtype=torch.float32, device=self.device
         )
-        for (entity, coord) in entity_coordinates.items():
-            if initialization is not None and entity in initialization:
-                solution[coord] = initialization[entity]
 
-        lbfgs = torch.optim.LBFGS((solution,))
+        if initialization is not None:
+            for (entity_id, values) in initialization.iter_entities():
+                entity_coord = entity_coordinates.get(entity_id)
+                if entity_coord is not None:
+                    score, _left, _right = values
+                    solution[entity_coord] = score
+
+        lbfgs = torch.optim.LBFGS(
+            (solution,),
+            max_iter=100,
+            tolerance_change=self.convergence_error
+        )
 
         def closure():
             lbfgs.zero_grad()
-            loss = self.loss(solution, comparisons)
+            loss = self.loss(solution, comparisons_np)
             loss.backward()
             return loss
 
-        for _step in range(self.n_steps):
-            lbfgs.step(closure)
+        lbfgs.step(closure)  # type: ignore
+        solution = solution.detach()
 
-        ll_actual = self.loss(solution, comparisons, with_regularization=False).detach()
-        def loss_increase_to_solve(delta, coord):
-            solution_with_delta = solution.detach().clone()
+        def loss_with_delta(delta, comparisons, coord):
+            solution_with_delta = solution.clone()
             solution_with_delta[coord] += delta
-            return self.loss(
-                solution_with_delta,
-                comparisons,
-                with_regularization=False
-            ) - ll_actual - 1.0
+            return self.loss(solution_with_delta, comparisons, with_regularization=False).item()
 
         model = DirectScoringModel()
         for coordinate in range(len(solution)):
-            uncertainty_left: float = -1 * brentq(
-                loss_increase_to_solve,
-                args=(coordinate,),
-                a=-1e6,
-                b=0.0,
-                xtol=self.convergence_error,
-            )  # type: ignore
-            uncertainty_right: float = brentq(
-                loss_increase_to_solve,
-                args=(coordinate,),
-                a=0.0,
-                b=1e6,
-                xtol=self.convergence_error,
-            )  # type: ignore
+            mask = ((comparisons_np[0] == coordinate) | (comparisons_np[1] == coordinate))
+            comp = tuple(a[mask] for a in comparisons_np)
+            ll_actual = self.loss(solution, comp, with_regularization=False).item()
+
+            try:
+                uncertainty_left = -1 * dichotomy.solve(
+                    loss_with_delta,
+                    value=ll_actual + 1,
+                    args=(comp, coordinate),
+                    xmin=-1e3,
+                    xmax=0.0,
+                    error=1e-1,
+                )
+            except ValueError:
+                uncertainty_left = 700.65
+
+            try:
+                uncertainty_right = dichotomy.solve(
+                    loss_with_delta,
+                    value=ll_actual + 1,
+                    args=(comp, coordinate),
+                    xmin=0.0,
+                    xmax=1e3,
+                    error=1e-1,
+                )
+            except ValueError:
+                uncertainty_right = 700.65
+
             model[entities[coordinate]] = (
                 solution[coordinate].item(),
                 uncertainty_left,
                 uncertainty_right,
             )
         return model
-        
-    def loss(self, solution, comparisons, with_regularization=True) -> torch.Tensor:
-        score_diff = (
-            solution[comparisons["entity_b"].to_numpy()] 
-            - solution[comparisons["entity_a"].to_numpy()]
-        )
+
+    def loss(self, solution, comparisons, with_regularization=True):
+        comp_a, comp_b, comp_value = comparisons
+        score_diff = solution[comp_b] - solution[comp_a]
         loss = (
             self.cumulant_generating_function(score_diff).sum()
-            - (score_diff * torch.from_numpy(comparisons["comparison"].to_numpy())).sum()
+            - (score_diff * torch.from_numpy(comp_value)).sum()
         )
         if with_regularization:
             loss += torch.sum(solution**2) / (2 * self.prior_std_dev**2)
         return loss
-    
+
 
 class LBFGSUniformGBT(LBFGSGeneralizedBradleyTerry):
     def __init__(
         self,
         prior_std_dev: float = 7,
-        comparison_max: float = 10,
         convergence_error: float = 1e-5,
         cumulant_generating_function_error: float = 1e-5,
-        n_steps: int = 1,
+        max_iter: int = 100,
     ):
         """
         Parameters
@@ -186,8 +180,7 @@ class LBFGSUniformGBT(LBFGSGeneralizedBradleyTerry):
         error: float
             tolerated error
         """
-        super().__init__(prior_std_dev, convergence_error, n_steps)
-        self.comparison_max = comparison_max
+        super().__init__(prior_std_dev, convergence_error, max_iter=max_iter)
         self.cumulant_generating_function_error = cumulant_generating_function_error
 
     def cumulant_generating_function(self, score_diff: torch.Tensor) -> torch.Tensor:
@@ -202,49 +195,20 @@ class LBFGSUniformGBT(LBFGSGeneralizedBradleyTerry):
         -------
         out: float
         """
+        score_diff_abs = score_diff.abs()
         return torch.where(
-            score_diff != 0,
-            torch.log(torch.sinh(score_diff) / score_diff),
-            0.0
+            score_diff_abs > 0,
+            torch.where(
+                score_diff_abs < 20.0,
+                (torch.sinh(score_diff) / score_diff).log(),
+                score_diff_abs - np.log(2) - score_diff_abs.log(),
+            ),
+            0.0,
         )
-
-    def cumulant_generating_function_derivative(self, score_diff: float) -> float:
-        """ For.
-        
-        Parameters
-        ----------
-        score_diff: float
-            Score difference
-
-        Returns
-        -------
-        out: float
-        """
-        if np.abs(score_diff) < self.cumulant_generating_function_error:
-            return score_diff / 3
-        return 1 / np.tanh(score_diff) - 1 / score_diff
-
-    def cumulant_generating_function_second_derivative(self, score_diff: float) -> float:
-        """We estimate uncertainty by the flatness of the negative log likelihood,
-        which is directly given by the second derivative of the cumulant generating function.
-
-        Parameters
-        ----------
-        score_diff: float
-            Score difference
-
-        Returns
-        -------
-        out: float
-        """
-        if np.abs(score_diff) < self.cumulant_generating_function_error:
-            return (1 / 3) - (score_diff**2 / 15)
-        return 1 - (1 / np.tanh(score_diff) ** 2) + (1 / score_diff**2)
 
     def to_json(self):
         return type(self).__name__, dict(
             prior_std_dev=self.prior_std_dev,
-            comparison_max=self.comparison_max,
             convergence_error=self.convergence_error,
             cumulant_generating_function_error=self.cumulant_generating_function_error,
         )
@@ -253,9 +217,8 @@ class LBFGSUniformGBT(LBFGSGeneralizedBradleyTerry):
         prop_names = [
             "prior_std_dev",
             "convergence_error",
-            "comparison_max",
             "cumulant_generating_function_error",
-            "n_steps",
+            "max_iter",
         ]
         prop = ", ".join([f"{p}={getattr(self, p)}" for p in prop_names])
         return f"{type(self).__name__}({prop})"
