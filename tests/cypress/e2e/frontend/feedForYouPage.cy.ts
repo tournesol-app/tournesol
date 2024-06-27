f291fcedd72d034faec4c5e7acde527fd8de7b58
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-06-27 13:20:05 +0200

    e2e: add tests for the page For you

---
+++ tests/cypress/e2e/frontend/feedForYouPage.cy.ts
@@ -0,0 +1,47 @@
+describe('Page - For you', () => {
+  const login = () => {
+    cy.focused().type('user');
+    cy.get('input[name="password"]').click().type("tournesol").type('{enter}');
+  }
+
+  describe('Poll - videos', () => {
+
+    describe('General', () => {
+      it('requires authentication', () => {
+        cy.visit('/feed/foryou');
+        cy.location('pathname').should('equal', '/login');
+        login();
+        cy.location('pathname').should('equal', '/feed/foryou');
+      });
+
+      it('is accessible from the side bar', () => {
+        cy.visit('/login');
+        login();
+
+        cy.contains('For you').click();
+        cy.location('pathname').should('equal', '/feed/foryou');
+      });
+    });
+
+    /*
+    describe('Pagination', () => {
+      it('displays the pagination', () => {
+        cy.visit('/feed/foryou');
+        cy.contains('button', '< -10', {matchCase: false}).should('exist');
+        cy.contains('button', '< -1', {matchCase: false}).should('exist');
+        cy.contains('button', '+1 >', {matchCase: false}).should('exist');
+        cy.contains('button', '+10 >', {matchCase: false}).should('exist');
+      });
+    });
+    */
+
+    describe('Search filters', () => {
+      it("doesn't display seach filters", () => {
+        cy.visit('/feed/foryou');
+        login();
+        cy.contains('Filters', {matchCase: true}).should('not.exist');
+        cy.location('search').should('equal', '');
+      });
+    });
+  });
+});
