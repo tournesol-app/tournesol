fix numerical issues in gbt implementations

solidago/src/solidago/preference_learning/generalized_bradley_terry.py
@@ -265,12 +265,17 @@ class UniformGBT(GeneralizedBradleyTerry):
             score_diff_abs = np.abs(score_diff)
             return (
                 np.where(
-                    score_diff_abs < 20.0,
-                    np.log(np.sinh(score_diff) / score_diff),
-                    score_diff_abs - np.log(2) - np.log(score_diff_abs)
+                    score_diff_abs > 0,
+                    np.where(
+                        score_diff_abs < 20.0,
+                        np.log(np.sinh(score_diff) / score_diff),
+                        score_diff_abs - np.log(2) - np.log(score_diff_abs),
+                    ),
+                    0.0,
                 )
                 + r * score_diff
             ).sum()
+
         return f
 
     @cached_property

solidago/src/solidago/preference_learning/lbfgs_generalized_bradley_terry.py
@@ -99,8 +99,9 @@ class LBFGSGeneralizedBradleyTerry(ComparisonBasedPreferenceLearning):
 
         lbfgs = torch.optim.LBFGS(
             (solution,),
-            max_iter=100,
-            tolerance_change=self.convergence_error
+            max_iter=self.max_iter,
+            tolerance_change=self.convergence_error,
+            line_search_fn="strong_wolfe",
         )
 
         def closure():
@@ -110,7 +111,13 @@ class LBFGSGeneralizedBradleyTerry(ComparisonBasedPreferenceLearning):
             return loss
 
         lbfgs.step(closure)  # type: ignore
+
+        # TODO: check lbfgs state to check if max_iter was reached
+
         solution = solution.detach()
+
+        if solution.isnan().any():
+            raise Exception(f"Nan in solution, state: {lbfgs.state_dict()}")
 
         def loss_with_delta(delta, comparisons, coord):
             solution_with_delta = solution.clone()