deb65362b14b1ff109943be123f8122347f22efe
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-07-25 11:00:36 +0200

    [back] tests: finalize the test in test_api_exports_proof.py...
    ... also rename test_exports.py into test_api_exports.py

--- backend/tournesol/tests/test_api_exports_proof.py
+++ backend/tournesol/tests/test_api_exports_proof.py
@@ -1,7 +1,7 @@
 import csv
 import io
 
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from rest_framework import status
 from rest_framework.test import APIClient
 
@@ -10,7 +10,12 @@ from tournesol.models import Poll
 from tournesol.tests.factories.comparison import ComparisonFactory
 
 
+@override_settings(SECRET_KEY="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 class ExportProofOfVote(TestCase):
+    """
+    Test case of the API /exports/polls/{poll_name}/proof_of_vote/
+    """
+
     def setUp(self):
         self.client = APIClient()
         self.default_poll = Poll.default_poll()
@@ -22,7 +27,7 @@ class ExportProofOfVote(TestCase):
         response = self.client.get(f"/exports/polls/{self.default_poll.name}/proof_of_vote/")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_non_staff_cant_get_proof_of_vote(self):
+    def test_auth_non_staff_cant_get_proof_of_vote(self):
         """
         An authenticated non-administrator user should not be able to download the proof of vote.
         """
@@ -37,12 +42,12 @@ class ExportProofOfVote(TestCase):
         response = self.client.get(f"/exports/polls/{self.default_poll.name}/proof_of_vote/")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_staff_can_get_proof_of_vote_empty(self):
+    def test_auth_staff_can_get_proof_of_vote_empty(self):
         """
         An authenticated administrator user should be able to download an empty proof of vote.
         """
         user = User.objects.create_user(
-            username="non_saff",
+            username="non_staff",
             email="non_staff@example.org",
             is_active=True,
             is_staff=True,
@@ -56,10 +61,17 @@ class ExportProofOfVote(TestCase):
         csv_lines = [line for line in csv.reader(io.StringIO(response.content.decode()))]
         self.assertEqual(len(csv_lines), 1)
         self.assertEqual(
-            csv_lines[0], ["user_id", "username", "email", "n_comparisons", "signature"]
+            csv_lines[0],
+            [
+                "user_id",
+                "username",
+                "email",
+                "n_comparisons",
+                "signature",
+            ]
         )
 
-    def test_staff_can_get_proof_of_vote_non_empty(self):
+    def test_auth_staff_can_get_proof_of_vote_non_empty(self):
         """
         An authenticated administrator user should be able to download the proof of vote.
         """
@@ -79,7 +91,22 @@ class ExportProofOfVote(TestCase):
         csv_lines = [line for line in csv.reader(io.StringIO(response.content.decode()))]
         self.assertEqual(len(csv_lines), 2)
         self.assertEqual(
-            csv_lines[0], ["user_id", "username", "email", "n_comparisons", "signature"]
+            csv_lines[0],
+            [
+                "user_id",
+                "username",
+                "email",
+                "n_comparisons",
+                "signature",
+            ]
         )
-
-        self.assertEqual(csv_lines[1][:4], ["3", "non_staff", "non_staff@example.org", "1"])
+        self.assertEqual(
+            csv_lines[1],
+            [
+                "3",
+                "non_staff",
+                "non_staff@example.org",
+                "1",
+                "00003:Lrxt5_dGEG_rfOuGgxabsqJoF7rZL0ViIY0vygX36zo",
+            ]
+        )
