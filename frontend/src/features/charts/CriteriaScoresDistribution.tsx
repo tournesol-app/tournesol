cfb045e2a7fe4f3218fa841144a94aa69e47f98a
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2024-08-06 14:53:46 +0200

    [front] feat: normalize the size of Y axis of scores distribution

--- frontend/src/features/charts/CriteriaScoresDistribution.tsx
+++ frontend/src/features/charts/CriteriaScoresDistribution.tsx
@@ -45,9 +45,11 @@ const binLabel = (index: number, bins: number[], t: TFunction) => {
 const CriteriaScoresDistributionChart = ({
   criterion,
   criteriaDistributionScore,
+  yMax = 'dataMax',
 }: {
   criterion: string;
   criteriaDistributionScore: CriteriaDistributionScore;
+  yMax?: string | number;
 }) => {
   const { t } = useTranslation();
   const { bins, distribution } = criteriaDistributionScore;
@@ -115,6 +117,7 @@ const CriteriaScoresDistributionChart = ({
           }}
         />
         <YAxis
+          domain={[0, yMax]}
           label={{
             value: t('criteriaScoresDistribution.numberOfRatings'),
             angle: -90,
@@ -144,6 +147,7 @@ const CriteriaScoresDistribution = ({
   const { selectedCriterion, setSelectedCriterion } = useSelectedCriterion();
   const entity = reco.entity;
 
+  const [yAxisMax, setYAxisMax] = useState(0);
   const [criteriaScoresDistribution, setCriteriaScoresDistribution] = useState<
     CriteriaDistributionScore[]
   >([]);
@@ -171,6 +175,16 @@ const CriteriaScoresDistribution = ({
           uid: entity.uid,
         });
       setCriteriaScoresDistribution(result.criteria_scores_distributions);
+
+      let yMax = 0;
+      result.criteria_scores_distributions.forEach((criterion) => {
+        const distribMax = Math.max(...criterion.distribution);
+        if (distribMax > yMax) {
+          yMax = distribMax;
+        }
+      });
+
+      setYAxisMax(yMax);
     };
     getDistribution();
   }, [entity.uid, pollName]);
@@ -196,6 +210,7 @@ const CriteriaScoresDistribution = ({
           <CriteriaScoresDistributionChart
             criterion={selectedCriterion}
             criteriaDistributionScore={criteriaDistributionScore}
+            yMax={yAxisMax}
           />
         </Box>
       )}
