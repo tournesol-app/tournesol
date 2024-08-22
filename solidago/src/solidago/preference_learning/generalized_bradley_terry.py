solidago: fix numerical instability in gbt

solidago/src/solidago/preference_learning/generalized_bradley_terry.py
@@ -265,13 +265,13 @@ class UniformGBT(GeneralizedBradleyTerry):
             score_diff_abs = np.abs(score_diff)
             return (
                 np.where(
-                    score_diff_abs > 0,
+                    score_diff_abs > 1e-1,
                     np.where(
                         score_diff_abs < 20.0,
                         np.log(np.sinh(score_diff) / score_diff),
                         score_diff_abs - np.log(2) - np.log(score_diff_abs),
                     ),
-                    0.0,
+                    score_diff_abs ** 2 / 6 - score_diff_abs ** 4 / 180,
                 )
                 + r * score_diff
             ).sum()

solidago/src/solidago/preference_learning/lbfgs_generalized_bradley_terry.py
@@ -202,13 +202,13 @@ class LBFGSUniformGBT(LBFGSGeneralizedBradleyTerry):
         """
         score_diff_abs = score_diff.abs()
         return torch.where(
-            score_diff_abs > 0,
+            score_diff_abs > 1e-1,
             torch.where(
                 score_diff_abs < 20.0,
                 (torch.sinh(score_diff) / score_diff).log(),
                 score_diff_abs - np.log(2) - score_diff_abs.log(),
             ),
-            0.0,
+            score_diff_abs ** 2 / 6 - score_diff_abs ** 4 / 180,
         )
 
     def to_json(self):