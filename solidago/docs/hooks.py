2033498f4cfa24f77ba73bcd71da6b1a6e9e515f
Adrien Matissart <a@matissart.net>
2025-01-02 12:36:51 +0100

    update mkdocs config, add more docstrings, add sections about primitives and data structures

--- solidago/docs/hooks.py
+++ solidago/docs/hooks.py
@@ -10,6 +10,9 @@ from solidago import __version__
 def on_page_markdown(markdown: str, page: Page, config: Config, files: Files) -> str:
     # Add Solidago version in homepage
     if page.abs_url == '/':
-        version_str = f'> Documentation for version: **{__version__}**'
+        version_str = f'''
+!!! info "Version"
+    This documentation has been generated from version **{__version__}**.
+'''
         markdown = re.sub(r'{{ *version *}}', version_str, markdown)
     return markdown
--- solidago/src/solidago/trust_propagation/trust_all.py
+++ solidago/src/solidago/trust_propagation/trust_all.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 class TrustAll(TrustPropagation):
-    """`TrustAll` is a naive solution that assignes an equal amount of trust to all users"""
+    """`TrustAll` is a naive solution that assignes an equal amount of trust to all users."""
 
     def __call__(self, users: pd.DataFrame, vouches: pd.DataFrame):
         return users.assign(trust_score=1.0)
