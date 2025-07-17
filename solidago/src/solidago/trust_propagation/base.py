ca59e82ad0b24b70070e398e5551aff7e9960033
Adrien Matissart <a@matissart.net>
2025-07-17 18:51:21 +0200

    trust_propagation: use LipschiTrust implementation from state function

--- solidago/src/solidago/trust_propagation/base.py
+++ solidago/src/solidago/trust_propagation/base.py
@@ -2,10 +2,30 @@ from abc import ABC, abstractmethod
 
 import pandas as pd
 
+from solidago.state import State
+from solidago.state.users import Users
+from solidago.state.vouches import Vouches
+
 class TrustPropagation(ABC):
     """
     Base class for Trust Propagation algorithms
     """
+
+    @staticmethod
+    def init_state(users: pd.DataFrame, vouches: pd.DataFrame):
+        """
+        Temporary helper to use implementations from solidago.state_functions
+        until the pipeline is fully migrated to State + StateFunction.
+        """
+        state_users = Users.from_dict({
+            (user.Index,): ("", user.is_pretrusted, 0.0)
+            for user in users.itertuples()
+        })
+        state_vouches = Vouches.from_dict({
+            (vouch.voucher, vouch.vouchee, "Personhood"): (vouch.vouch, 0.0)
+            for vouch in vouches.itertuples()
+        })
+        return State.empty().assign(users=state_users, vouches=state_vouches)
 
     @abstractmethod
     def __call__(self,
