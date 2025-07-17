7dbc624f2fdc5c1d91421b1deaa6d4661f4fec31
Adrien Matissart <a@matissart.net>
2025-07-17 18:47:05 +0200

    init state and state functions for trust_propagation

--- solidago/src/solidago/state/base.py
+++ solidago/src/solidago/state/base.py
@@ -9,5 +9,12 @@ class State:
     users: Users
     vouches: Vouches
 
+    @classmethod
+    def empty(cls):
+        return cls(users=Users.from_dict({}), vouches=Vouches.from_dict({}))
+
     def copy(self):
         return replace(self)
+
+    def assign(self, **kwargs):
+        return replace(self, **kwargs)
