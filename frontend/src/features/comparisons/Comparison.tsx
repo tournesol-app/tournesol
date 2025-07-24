a8a357a6ebb1ed7716c06a89ebb6bc5bae2d6a9a
Adrien Matissart <a@matissart.net>
2025-07-24 20:57:22 +0200

    fix typing after upgrading react-i18next

--- frontend/src/features/comparisons/Comparison.tsx
+++ frontend/src/features/comparisons/Comparison.tsx
@@ -8,7 +8,8 @@ import React, {
 } from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation, useNavigate, Location } from 'react-router-dom';
-import { TFunction, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
+import { TFunction } from 'i18next';
 
 import {
   Box,
--- frontend/src/utils/polls/videos.tsx
+++ frontend/src/utils/polls/videos.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TFunction } from 'react-i18next';
+import { TFunction } from 'i18next';
 
 import { Button, Link } from '@mui/material';
 
