85b9dc71e648750807bd9c1afc1cac8d631b9fd6
Gresille&Siffle <39056254+GresilleSiffle@users.noreply.github.com>
2026-02-28 16:52:36 +0100

    [back] feat: add new entity suggestion strategy WatchedEntitySuggestionStrategy
    Based on `ClassicEntitySuggestionStrategy` this new strategy suggests in
    priority entities that have been watched/consumed/understood.

---
+++ backend/tournesol/lib/suggestions/strategies/tocompare/watched.py
@@ -0,0 +1,84 @@
+import random
+from dataclasses import dataclass
+
+from tournesol.lib.suggestions.strategies.tocompare.classic import ClassicEntitySuggestionStrategy
+from tournesol.models import ContributorRating, RateLater
+from tournesol.models.rate_later import RATE_LATER_AUTO_REMOVE_DEFAULT
+
+
+@dataclass
+class IdPool:
+    ids: list[int]
+    sample_size: int
+
+
+class WatchedEntitySuggestionStrategy(ClassicEntitySuggestionStrategy):
+    """
+    A variation of the strategy `ClassicEntitySuggestionStrategy` suggests
+    in priority entities that have been watched/consumed/understood.
+    """
+
+    # The expected number of entities retrieved from each pool. The sum should
+    # match the `max_suggestions`.
+    sample_size_compared = 13
+    sample_size_rate_later = 7
+    sample_size_reco_last_month = 0
+
+    def _ids_from_pool_compared(self) -> list[int]:
+        poll = self.poll
+        user = self.user
+
+        max_threshold = user.settings.get(poll.name, {}).get(
+            "rate_later__auto_remove", RATE_LATER_AUTO_REMOVE_DEFAULT
+        )
+
+        compared = (
+            ContributorRating.objects.filter(poll=poll, user=user, entity_seen=True)
+            .select_related("entity")
+            .annotate_n_comparisons()
+            .filter(n_comparisons__lt=max_threshold)
+            .filter(n_comparisons__gt=0)
+            .values_list("entity_id", flat=True)
+        )
+
+        return random.sample(
+            # not a cryptographic use # nosec B311
+            list(compared),
+            min(len(compared), self.max_suggestions),
+        )
+
+    def _ids_from_pool_rate_later(self, exclude_ids: list[int]) -> list[int]:
+        poll = self.poll
+        user = self.user
+
+        results = RateLater.objects.raw(
+            """
+          SELECT
+            rate_later.id,
+            rate_later.entity_id
+
+          FROM tournesol_ratelater rate_later
+
+          JOIN tournesol_contributorrating
+            ON tournesol_contributorrating.user_id = rate_later.user_id
+           AND tournesol_contributorrating.entity_id = rate_later.entity_id
+
+          WHERE rate_later.poll_id = %(poll_id)s
+            AND rate_later.user_id = %(user_id)s
+            AND tournesol_contributorrating.entity_seen = true
+
+            AND rate_later.entity_id NOT IN %(exclude_ids)s
+          """,
+            {"poll_id": poll.id, "user_id": user.id, "exclude_ids": tuple(exclude_ids)},
+        )
+
+        samples = random.sample(
+            # not a cryptographic use # nosec B311
+            list(results),
+            min(len(results), self.max_suggestions),
+        )
+
+        return [sample.id for sample in samples]
+
+    def get_results(self):
+        return self.get_results_for_user_intermediate()
